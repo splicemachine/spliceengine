4426d7cf60dff8a5007643c25e00545b0c34ae40
John Leach <jleach@splicemachine.com>
2013-01-23 18:43:11 +0000

    Moved docs

---
+++ docs/backup.sh
@@ -0,0 +1,14 @@
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_hive/target/structured_hive-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hbase-0.92.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_constants/target/structured_constants-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hbase-0.92.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_trx/target/structured_trx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hbase-0.92.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_idx/target/structured_idx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hbase-0.92.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_hive/target/structured_hive-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/share/hadoop/mapreduce/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_constants/target/structured_constants-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/share/hadoop/mapreduce/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_trx/target/structured_trx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/share/hadoop/mapreduce/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_idx/target/structured_idx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/share/hadoop/mapreduce/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_hive/target/structured_hive-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hive-0.8.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_constants/target/structured_constants-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hive-0.8.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_trx/target/structured_trx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hive-0.8.1-cdh4.0.0/lib/
+scp /Users/jleach/Documents/workspace/structured_hbase/structured_idx/target/structured_idx-1.0.0-SNAPSHOT.jar /Users/jleach/hadoop/hive-0.8.1-cdh4.0.0/lib/
+/Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/sbin/stop-yarn.sh
+/Users/jleach/hadoop/hadoop-2.0.0-cdh4.0.0/sbin/start-yarn.sh
