34038223ee19f82e22d0c7dcd4057ceb3c757585
Scott Fines <sfines@splicemachine.com>
2013-02-06 19:14:29 +0000

    Corrects BUG#219. Makes the default Locale be returned whenever something else isn't available from SQLChar

--- structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/ProjectRestrictOperationTest.java
+++ structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/ProjectRestrictOperationTest.java
@@ -24,7 +24,7 @@ public class ProjectRestrictOperationTest  {
 	private static Logger LOG = Logger.getLogger(ProjectRestrictOperationTest.class);
 	private static final Map<String,String> tableSchemas = Maps.newHashMap();
 	static{
-		tableSchemas.put("a","si varchar(40),sa varchar(40)");
+		tableSchemas.put("a","si varchar(40),sa varchar(40),sc int");
 	}
 	@Rule public static DerbyTestRule rule = new DerbyTestRule(tableSchemas,false,LOG);
 
@@ -33,10 +33,11 @@ public class ProjectRestrictOperationTest  {
 		DerbyTestRule.start();
 		rule.createTables();
 
-		PreparedStatement ps = rule.prepareStatement("insert into a values (?,?)");
+		PreparedStatement ps = rule.prepareStatement("insert into a values (?,?,?)");
 		for(int i=0;i<10;i++){
 			ps.setString(1,Integer.toString(i));
-			ps.setString(2,Integer.toString(i));
+			ps.setString(2,Integer.toString(i) + " Times ten");
+			ps.setInt(3,i);
 			ps.executeUpdate();
 		}
 	}
@@ -120,5 +121,112 @@ public class ProjectRestrictOperationTest  {
 			LOG.info(result);
 		}
 		Assert.assertEquals("Incorrect num rows returned!",corrects.size(),results.size());
+	}
+
+	@Test
+	public void testSumOperator() throws Exception{
+		int increment = 2;
+		ResultSet rs = rule.executeQuery("select sc, sc+"+increment+" from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			int sc = rs.getInt(1);
+			int scIncr = rs.getInt(2);
+			Assert.assertEquals("Sum doesn't work!",sc+increment,scIncr);
+			results.add(String.format("sc:%d,scIncr:%d",sc,scIncr));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
+	}
+
+	@Test
+	public void testTimesOperator() throws Exception{
+		int multiplier = 2;
+		ResultSet rs = rule.executeQuery("select sc, sc*"+multiplier+" from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			int sc = rs.getInt(1);
+			int scIncr = rs.getInt(2);
+			Assert.assertEquals("Sum doesn't work!",sc*multiplier,scIncr);
+			results.add(String.format("sc:%d,scIncr:%d",sc,scIncr));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
+	}
+
+	@Test
+	public void testModulusOperator() throws Exception{
+		int modulus = 2;
+		ResultSet rs = rule.executeQuery("select sc, mod(sc,"+modulus+") from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			int sc = rs.getInt(1);
+			int scIncr = rs.getInt(2);
+			Assert.assertEquals("Modulus doesn't work!",sc%modulus,scIncr);
+			results.add(String.format("sc:%d,scIncr:%d",sc,scIncr));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
+	}
+
+	@Test
+	public void testSubStringOperator() throws Exception{
+		ResultSet rs = rule.executeQuery("select sa, substr(sa,1,1) from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			String sa = rs.getString(1);
+			String sub = rs.getString(2);
+			String subSa = sa.substring(0,1);
+			Assert.assertEquals("Incorrect sub!",subSa,sub);
+			results.add(String.format("sa=%s,sub=%s",sa,sub));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
+	}
+
+	@Test
+	public void testConcatOperator() throws Exception{
+		String concat = "b";
+		ResultSet rs = rule.executeQuery("select si,si||'"+concat+"' from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			String sa = rs.getString(1);
+			String concatAttempt = rs.getString(2);
+			String correct = sa+concat;
+			Assert.assertEquals("Incorrect concat!",correct,concatAttempt);
+			results.add(String.format("sa=%s,concatAttempt=%s",sa,concatAttempt));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
+	}
+
+	@Test
+	public void testUpperLowerOperator() throws Exception{
+		ResultSet rs = rule.executeQuery("select sa,UPPER(sa),LOWER(sa) from a ");
+		List<String>results = Lists.newArrayList();
+		while(rs.next()){
+			String sa = rs.getString(1);
+			String upper = rs.getString(2);
+			String lower = rs.getString(3);
+			String correctUpper = sa.toUpperCase();
+			String correctLower = sa.toLowerCase();
+			Assert.assertEquals("upper incorrect",correctUpper,upper);
+			Assert.assertEquals("lower incorrect",correctLower,lower);
+
+			results.add(String.format("sa:%s,upper:%s,lower:%s",sa,upper,lower));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect num rows returned!",10,results.size());
 	}
 }
