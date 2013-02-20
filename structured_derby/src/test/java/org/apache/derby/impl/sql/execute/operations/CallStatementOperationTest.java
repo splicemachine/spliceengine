afa263b9c734053d57a34f4ef043787be9e787c3
Jessie Zhang <jzhang@splicemachine.com>
2013-02-20 21:26:10 +0000

    added the trouble getTable query

--- structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/CallStatementOperationTest.java
+++ structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/CallStatementOperationTest.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.splicemachine.derby.test.SpliceNetDerbyTest;
@@ -28,23 +29,28 @@ public class CallStatementOperationTest extends SpliceNetDerbyTest {
         ResultSet resultSet = null;
         conn.setAutoCommit(false);
         try{
+        	conn.setAutoCommit(false);
             resultSet = conn.getMetaData().getProcedures(null, null, null);
             while(resultSet.next()){
                 LOG.info("c1="+resultSet.getString(1));
             }
+            conn.commit();
         }finally{
             if(resultSet!=null)resultSet.close();
         }
     }
 
-    @Test
+    @Ignore
 	public void testCallSysSchemas() throws SQLException {
+    	conn.setAutoCommit(true);
 		LOG.info("start testCallStatement");
 		Statement s = null;
 		ResultSet result = null;
 		try {
-			CallableStatement cs = conn.prepareCall("CALL SYSIBM.SQLTABLES('', '', '', '', 'GETSCHEMAS=1')");
-			//CallableStatement cs = conn.prepareCall("CALL SYSIBM.METADATA()");
+			//CallableStatement cs = conn.prepareCall("CALL SYSIBM.SQLTABLES('', '', '', '', 'GETSCHEMAS=1')");
+			CallableStatement cs = conn.prepareCall("CALL SYSIBM.METADATA()");
+			
+			//CallableStatement cs = conn.prepareCall("CALL SYSIBM.SQLGETTYPEINFO(0,null)");
 			result = cs.executeQuery();
 			
 			//s = conn.createStatement();
@@ -69,7 +75,7 @@ public class CallStatementOperationTest extends SpliceNetDerbyTest {
 		}		
 	}
 
-    @Test
+    @Ignore
     public void testCallGetTypeInfo() throws Exception{
         Statement s = null;
         ResultSet rs = null;
@@ -85,6 +91,50 @@ public class CallStatementOperationTest extends SpliceNetDerbyTest {
         }
     }
 
+    @Test
+    public void testGetTables() {
+    	Statement s = null;
+        ResultSet rs = null;
+        try{
+            s = conn.createStatement();
+            
+            /* Orginal statement
+             * 
+             * SELECT CAST ('' AS VARCHAR(128)) AS TABLE_CAT, SCHEMANAME AS TABLE_SCHEM,  TABLENAME AS TABLE_NAME, 
+             * (CAST (RTRIM(TABLE_TYPE) AS VARCHAR(12))) AS TABLE_TYPE, CAST ('' AS VARCHAR(128)) AS REMARKS, CAST (NULL AS VARCHAR(128)) AS TYPE_CAT, 
+             * CAST (NULL AS VARCHAR(128)) AS TYPE_SCHEM, CAST (NULL AS VARCHAR(128)) AS TYPE_NAME, CAST (NULL AS VARCHAR(128)) AS SELF_REFERENCING_COL_NAME, 
+             * CAST (NULL AS VARCHAR(128)) AS REF_GENERATION FROM SYS.SYSTABLES, SYS.SYSSCHEMAS, (VALUES ('T','TABLE'), ('S','SYSTEM TABLE'), ('V', 'VIEW'), 
+             * ('A', 'SYNONYM')) T(TTABBREV,TABLE_TYPE) WHERE (TTABBREV=TABLETYPE 	AND (SYS.SYSTABLES.SCHEMAID = SYS.SYSSCHEMAS.SCHEMAID) 
+             * AND ((1=1) OR ? IS NOT NULL) AND (SYS.SYSSCHEMAS.SCHEMANAME LIKE ?) AND (TABLENAME LIKE ?) AND TABLETYPE IN (?, ?, ?, ?)) 
+             * ORDER BY TABLE_TYPE, TABLE_SCHEM, TABLE_NAME
+             * 
+             */
+            rs = s.executeQuery("SELECT CAST ('' AS VARCHAR(128)) AS TABLE_CAT, SCHEMANAME AS TABLE_SCHEM,  TABLENAME AS TABLE_NAME, " +
+             
+            		"(CAST (RTRIM(TABLE_TYPE) AS VARCHAR(12))) AS TABLE_TYPE, CAST ('' AS VARCHAR(128)) AS REMARKS, " +
+            		"CAST (NULL AS VARCHAR(128)) AS TYPE_CAT, CAST (NULL AS VARCHAR(128)) AS TYPE_SCHEM, CAST (NULL AS VARCHAR(128)) AS TYPE_NAME, " +
+            		"CAST (NULL AS VARCHAR(128)) AS SELF_REFERENCING_COL_NAME, CAST (NULL AS VARCHAR(128)) AS REF_GENERATION FROM SYS.SYSTABLES, " +
+            		"SYS.SYSSCHEMAS, (VALUES ('T','TABLE'), ('S','SYSTEM TABLE'), ('V', 'VIEW'), ('A', 'SYNONYM')) T(TTABBREV,TABLE_TYPE) " +
+            		"WHERE (TTABBREV=TABLETYPE 	AND (SYS.SYSTABLES.SCHEMAID = SYS.SYSSCHEMAS.SCHEMAID) AND ((1=1) OR ? IS NOT NULL) " +
+            		"AND (SYS.SYSSCHEMAS.SCHEMANAME LIKE '%APP%') " +
+            		"AND (TABLENAME LIKE '%') AND TABLETYPE IN (?, ?, ?, ?)) ORDER BY TABLE_TYPE, TABLE_SCHEM, TABLE_NAME");
+            while(rs.next()){
+                LOG.info(String.format("1=%s",rs.getString(1)));
+            }
+        }catch (Exception e) {
+        	e.printStackTrace();
+        } finally{
+        	try {
+            if(rs!=null)rs.close();
+            if(s!=null)s.close();
+        	} catch (Exception e) {
+        		
+        	}
+        }
+        
+    	
+    }
+    
 	@AfterClass 
 	public static void shutdown() throws SQLException {
 		stopConnection();		
