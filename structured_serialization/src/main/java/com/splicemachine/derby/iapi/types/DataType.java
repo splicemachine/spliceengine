b131eb99433895b400571d188fd6ccd140ac96a1
John Leach <jleach@inciteretail.com>
2013-03-19 14:05:47 -0500

    Adding baseline serialization

--- structured_serialization/src/main/java/com/splicemachine/derby/iapi/types/DataType.java
+++ structured_serialization/src/main/java/com/splicemachine/derby/iapi/types/DataType.java
@@ -1,4 +1,26 @@
+/*
+
+   Derby - Class org.apache.derby.iapi.types.DataType
+
+   Licensed to the Apache Software Foundation (ASF) under one or more
+   contributor license agreements.  See the NOTICE file distributed with
+   this work for additional information regarding copyright ownership.
+   The ASF licenses this file to you under the Apache License, Version 2.0
+   (the "License"); you may not use this file except in compliance with
+   the License.  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+ */
+
 package com.splicemachine.derby.iapi.types;
+
 import org.apache.derby.iapi.reference.SQLState;
 import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.services.i18n.MessageService;
@@ -6,8 +28,6 @@ import org.apache.derby.iapi.services.sanity.SanityManager;
 import org.apache.derby.iapi.types.BooleanDataValue;
 import org.apache.derby.iapi.types.DataTypeDescriptor;
 import org.apache.derby.iapi.types.DataValueDescriptor;
-import org.apache.derby.iapi.types.Orderable;
-import org.apache.derby.iapi.types.SQLBoolean;
 
 import java.io.InputStream;
 import java.sql.Blob;
@@ -18,6 +38,7 @@ import java.sql.Timestamp;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+
 import java.util.Calendar;
 
 /**
@@ -39,11 +60,9 @@ import java.util.Calendar;
  * values with.
  *
  */
-public abstract class SpliceDataType implements DataValueDescriptor, Comparable {
-	protected byte[] sortedStoredBytes; // moving
-
-	
-	
+public abstract class DataType
+    implements DataValueDescriptor, Comparable
+{
 	/*
 	 * DataValueDescriptor Interface
 	 */
@@ -668,7 +687,7 @@ public abstract class SpliceDataType implements DataValueDescriptor, Comparable 
 	 *
 	 * @exception StandardException				Thrown normalization error.
 	 */
-
+	
 	public void normalize(
 				DataTypeDescriptor desiredType,
 				DataValueDescriptor source)
