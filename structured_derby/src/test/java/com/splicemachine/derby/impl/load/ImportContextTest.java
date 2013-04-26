d7cf64285493735d60bfa7bd671d1c8d04cb5a34
Ryan Senior <rsenior@splicemachine.com>
2013-04-26 13:59:28 -0500

    Move serialization stuff into SerializationUtils

--- structured_derby/src/test/java/com/splicemachine/derby/impl/load/ImportContextTest.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/load/ImportContextTest.java
@@ -1,69 +1,10 @@
 package com.splicemachine.derby.impl.load;
 
+import com.splicemachine.homeless.SerializationUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
 public class ImportContextTest {
-
-    private byte[] serializeToBytes(Externalizable e) throws IOException{
-
-        ByteArrayOutputStream bos = null;
-        ObjectOutputStream oos = null;
-        byte[] object;
-
-        try{
-            bos = new ByteArrayOutputStream();
-            oos = new ObjectOutputStream(bos);
-            oos.writeObject(e);
-            oos.flush();
-
-            object = bos.toByteArray();
-        } finally {
-            if(oos != null){
-                oos.close();
-            }
-
-            if(bos != null){
-                bos.close();
-            }
-        }
-
-        return object;
-    }
-
-    private Object deserializeObject(byte[] b) throws IOException, ClassNotFoundException {
-        ByteArrayInputStream bis = null;
-        ObjectInputStream ois = null;
-        try{
-
-            bis = new ByteArrayInputStream(b);
-            ois = new ObjectInputStream(bis);
-
-            return ois.readObject();
-
-        } finally {
-            if(bis != null){
-                bis.close();
-            }
-
-            if(ois != null){
-                ois.close();
-            }
-        }
-
-    }
-
-
-    private <T extends Externalizable> T roundTripObject(T obj) throws IOException, ClassNotFoundException {
-        return (T) deserializeObject( serializeToBytes(obj));
-    }
 
     @Test
     public void testSerializeImportContext() throws Exception{
@@ -76,7 +17,7 @@ public class ImportContextTest {
                                 .bytesToRead(10)
                                 .build();
 
-        ImportContext newIC = roundTripObject(ic);
+        ImportContext newIC = SerializationUtils.roundTripObject(ic);
 
         Assert.assertEquals("/foo", newIC.getFilePath().toString());
         Assert.assertEquals(",", newIC.getColumnDelimiter());
