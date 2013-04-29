95211ed9854a30f7df0598b77b7962865694bf9f
John Leach <jleach@inciteretail.com>
2013-04-29 15:53:21 -0500

    Shrinking the size of the family to save byte[] transfer

--- structured_constants/src/main/java/com/splicemachine/constants/HBaseConstants.java
+++ structured_constants/src/main/java/com/splicemachine/constants/HBaseConstants.java
@@ -9,8 +9,8 @@ import org.apache.hadoop.hbase.util.Bytes;
  * @version %I%, %G%
  */
 public class HBaseConstants {
-	public static final String DEFAULT_FAMILY = "attributes";
-	public static final byte[] DEFAULT_FAMILY_BYTES = Bytes.toBytes("attributes");
+	public static final String DEFAULT_FAMILY = "A";
+	public static final byte[] DEFAULT_FAMILY_BYTES = Bytes.toBytes(DEFAULT_FAMILY);
     public static final String TEMP_TABLE = "SYS_TEMP";
     public static byte[] TEMP_TABLE_BYTES = Bytes.toBytes(TEMP_TABLE);
 	public static final int DEFAULT_VERSIONS = HColumnDescriptor.DEFAULT_VERSIONS;
@@ -18,7 +18,6 @@ public class HBaseConstants {
 	public static final Boolean DEFAULT_IN_MEMORY = HColumnDescriptor.DEFAULT_IN_MEMORY;
 	public static final Boolean DEFAULT_BLOCKCACHE=HColumnDescriptor.DEFAULT_BLOCKCACHE;
 	public static final int DEFAULT_TTL = HColumnDescriptor.DEFAULT_TTL;
-
 	public static final String DEFAULT_BLOOMFILTER = HColumnDescriptor.DEFAULT_BLOOMFILTER;
 	public static final String TABLE_COMPRESSION = "com.splicemachine.table.compression";
 	public static final String HBASE_ZOOKEEPER_CLIENT_PORT = "hbase.zookeeper.property.clientPort";
