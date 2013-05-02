1d6745fa011d0fb2daa566cd48cb0ec8606e6fdc
David McNeil <dmcneil@splicemachine.com>
2013-05-02 12:48:59 -0500

    si - commented RollForwardQueue

--- structured_si/src/main/java/com/splicemachine/si/impl/RollForwardAction.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/RollForwardAction.java
@@ -3,6 +3,10 @@ package com.splicemachine.si.impl;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Abstraction over the details of what it means to roll-forward a transaction. This allows the RollForwardQueue to be
+ * decoupled.
+ */
 public interface RollForwardAction {
     void rollForward(long transactionId, List rowList) throws IOException;
 }
