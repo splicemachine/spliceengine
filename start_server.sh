0866e6dff21172fce729939e5481a034df4337cf
Ryan Senior <rsenior@splicemachine.com>
2013-05-09 19:45:10 -0700

    Added start/stop the exec:exec splice server (for Jenkins)

---
+++ start_server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+currentDateTime=$(date +'%m-%d-%Y:%H:%M:%S')
+mvn clean install -DskipTests
+cd structured_derby
+mvn exec:exec -DspliceCI > server.log &
+
+if ps ax | grep -v grep | grep 'exec:exec' > /dev/null 
+then
+ sleep 300
+else
+ echo "splice is not running!!"
+fi
---
+++ stop_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ps ax | grep -v grep | grep 'exec:exec' > /dev/null 
+then
+ pid=$(ps ax | grep -v grep | grep 'exec:exec' | gawk '{print $1}')
+ kill -9 $pid
+ sleep 60
+else
+ echo "splice is not running!!"
+fi
+
+cp structured_derby/server.log structured_derby/logs/server.log.$currentDateTime
