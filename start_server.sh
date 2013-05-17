070df76af20f163d02528a09c5925c4e7d50b353
John Leach <jleach@inciteretail.com>
2013-05-16 23:30:24 -0500

    Adding zookeeper start

--- start_server.sh
+++ start_server.sh
@@ -2,6 +2,17 @@
 
 currentDateTime=$(date +'%m-%d-%Y:%H:%M:%S')
 cd structured_derby
+
+mvn exec:java -Dzoo > zoo.log &
+
+if ps ax | grep -v grep | grep 'exec:java' > /dev/null 
+then
+ sleep 300
+else
+ echo "zoo is not running!!"
+fi
+
+
 mvn exec:exec -DspliceCI > server.log &
 
 if ps ax | grep -v grep | grep 'exec:exec' > /dev/null 
