95188038ca122fa899bbb2f86f3548ea56354b19
John Leach <jleach@inciteretail.com>
2013-05-16 23:30:36 -0500

    Adding zookeeper stop

--- stop_server.sh
+++ stop_server.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if ps ax | grep -v grep | grep 'exec:java' > /dev/null 
+then
+ pid=$(ps ax | grep -v grep | grep 'exec:java' | gawk '{print $1}')
+ kill -9 $pid
+ sleep 60
+else
+ echo "zoo is not running!!"
+fi
+
 if ps ax | grep -v grep | grep 'exec:exec' > /dev/null 
 then
  pid=$(ps ax | grep -v grep | grep 'exec:exec' | gawk '{print $1}')
@@ -8,5 +17,7 @@ then
 else
  echo "splice is not running!!"
 fi
+
+cp structured_derby/zoo.log structured_derby/logs/zoo.log.$currentDateTime
 
 cp structured_derby/server.log structured_derby/logs/server.log.$currentDateTime
