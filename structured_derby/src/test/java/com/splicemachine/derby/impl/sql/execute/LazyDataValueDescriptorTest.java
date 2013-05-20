31d25e0c4ec8c8b1fd9a76754da4acfcf921ee61
Ryan Senior <rsenior@splicemachine.com>
2013-05-20 13:42:01 -0500

    Add test covering reusing DVD for deserialization and serializing the whole DVD object

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/LazyDataValueDescriptorTest.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/LazyDataValueDescriptorTest.java
@@ -1,7 +1,9 @@
 package com.splicemachine.derby.impl.sql.execute;
 
 import com.splicemachine.derby.impl.sql.execute.serial.StringDVDSerializer;
+import com.splicemachine.homeless.SerializationUtils;
 import org.apache.derby.iapi.types.SQLVarchar;
+import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -61,6 +63,45 @@ public class LazyDataValueDescriptorTest {
 
         Assert.assertEquals("foo", ldvd2.getString());
 
+    }
+
+    @Test
+    public void testReusingDVDForDeserialization() throws Exception{
+        byte[] fooBytes = justBytes("foo");
+        byte[] barBytes = justBytes("bar");
+
+        LazyDataValueDescriptor ldvd = new LazyDataValueDescriptor(new SQLVarchar(), new StringDVDSerializer());
+        ldvd.initForDeserialization(fooBytes);
+
+        Assert.assertTrue(ldvd.getDvd().isNull());
+        Assert.assertEquals("foo", ldvd.getString());
+        Assert.assertEquals("Bytes still present after forcing deserialization", fooBytes, ldvd.getBytes());
+
+        ldvd.initForDeserialization(barBytes);
+        Assert.assertEquals("LDVD should now have bytes representing \"bar\"", barBytes, ldvd.getBytes());
+        Assert.assertEquals("bar", ldvd.getString());
+
+    }
+
+    @Test
+    public void testSerializingWholeDescriptor() throws Exception{
+
+        byte[] fooBytes = justBytes("foo");
+
+        LazyDataValueDescriptor ldvd = new LazyDataValueDescriptor(new SQLVarchar("foo"), new StringDVDSerializer());
+
+        Assert.assertFalse(ldvd.isSerialized());
+        Assert.assertEquals("foo", ldvd.getString());
+        Assert.assertTrue("Foo bytes match", Bytes.equals(fooBytes, ldvd.getBytes()));
+        Assert.assertTrue(ldvd.isSerialized());
+
+        byte[]  wholeDvdBytes = SerializationUtils.serializeToBytes(ldvd);
+
+        LazyDataValueDescriptor ldvdFromBytes = (LazyDataValueDescriptor) SerializationUtils.deserializeObject(wholeDvdBytes);
+
+        Assert.assertTrue(ldvdFromBytes.isSerialized());
+        Assert.assertEquals("foo", ldvdFromBytes.getString());
+        Assert.assertTrue("Foo bytes match after deserialization of whole DVD", Bytes.equals(fooBytes, ldvd.getBytes()));
     }
 
 }
