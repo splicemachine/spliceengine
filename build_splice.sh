cced5f982eba9483210d8595508ec259f1eb4be6
Ryan Senior <rsenior@splicemachine.com>
2013-05-29 12:56:49 -0500

    chained together the build commands with && so builds don't fail silently

--- build_splice.sh
+++ build_splice.sh
@@ -1,8 +1,3 @@
 # call with your gpg passphrase as the first param
 
-ant clean
-mkdir bin/templates
-ant buildsource testing buildjars
-cd maven2
-./SpliceBuild.sh $1
-cd ..
+ant clean && mkdir -p bin/templates && ant buildsource testing buildjars && cd maven2 && ./SpliceBuild.sh $1 && cd ..
