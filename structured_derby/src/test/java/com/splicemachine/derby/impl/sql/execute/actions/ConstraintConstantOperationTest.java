f842eec1cc847e6ef1998122c83837522cee20c9
John Leach <jleach@inciteretail.com>
2013-06-05 13:37:11 -0500

    Merged

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/ConstraintConstantOperationTest.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/ConstraintConstantOperationTest.java
@@ -10,12 +10,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
-<<<<<<< HEAD
 
 @Ignore("Bug 535")
-=======
-@Ignore
->>>>>>> Commented out class until ready
 public class ConstraintConstantOperationTest extends SpliceUnitTest {
 
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
