0d220bd858de24f6d687e86445b68fb2ebedaffc
John Leach <jleach@inciteretail.com>
2013-06-07 14:40:30 -0500

    Perf Test

---
+++ structured_derby/src/test/java/com/splicemachine/perf/report/CorePerformanceReport.java
@@ -0,0 +1,95 @@
+package com.splicemachine.perf.report;
+
+import java.sql.ResultSet;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.derby.test.framework.SpliceUnitTest;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.derby.test.framework.tables.SpliceCustomerTable;
+import com.splicemachine.derby.test.framework.tables.SpliceOrderLineTable;
+
+@Ignore
+public class CorePerformanceReport extends SpliceUnitTest {
+    protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
+	public static final String CLASS_NAME = CorePerformanceReport.class.getSimpleName().toUpperCase();
+	public static final String TABLE_NAME_1 = "CUSTOMER";
+	public static final String TABLE_NAME_2 = "ORDER_LINE";	
+	public static final String TABLE_NAME_3 = "CUSTOMER_LOAD";
+	public static final String TABLE_NAME_4 = "ORDER_LINE_LOAD";	
+	
+	protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);	
+	protected static SpliceCustomerTable spliceTableWatcher1 = new SpliceCustomerTable(TABLE_NAME_1,CLASS_NAME) {
+		@Override
+		protected void starting(Description description) {
+			super.starting(description);
+			importData(getResourceDirectory()+"customer_iso_10k.csv");
+		}	
+	};
+	protected static SpliceOrderLineTable spliceTableWatcher2 = new SpliceOrderLineTable(TABLE_NAME_2,CLASS_NAME) {
+		@Override
+		protected void starting(Description description) {
+			super.starting(description);
+			importData(getResourceDirectory()+"order_line_500k.csv");
+		}
+	};
+	protected static SpliceCustomerTable spliceTableWatcher3 = new SpliceCustomerTable(TABLE_NAME_3,CLASS_NAME); 
+	protected static SpliceOrderLineTable spliceTableWatcher4 = new SpliceOrderLineTable(TABLE_NAME_4,CLASS_NAME);
+
+	
+	@ClassRule 
+	public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
+		.around(spliceSchemaWatcher)
+		.around(spliceTableWatcher1)
+		.around(spliceTableWatcher2)
+		.around(spliceTableWatcher3)
+		.around(spliceTableWatcher4);
+	
+	@Rule public SpliceWatcher methodWatcher = new SpliceWatcher();
+	
+	@Test 
+	public void load10KCustomerRecords() throws Exception {
+		spliceTableWatcher3.importData(getResourceDirectory()+"customer_iso_10k.csv");
+	}
+
+	@Test 
+	public void load500KOrderLineRecords() throws Exception {
+		spliceTableWatcher4.importData(getResourceDirectory()+"order_line_500k.csv");
+	}
+
+	@Test
+	public void count500KOrderLines() throws Exception {
+		ResultSet rs = methodWatcher.executeQuery(format("select count(*) from %s",this.getTableReference(TABLE_NAME_2)));
+		while (rs.next()) {
+			Assert.assertEquals(500000,rs.getInt(1));
+		}
+	}
+
+	@Test
+	public void aggregate10KCustomersByZipCode() throws Exception {
+		ResultSet rs = methodWatcher.executeQuery(format("select cst_zipcode, count(*) from %s group by cst_zipcode",this.getTableReference(TABLE_NAME_1)));
+		int count = 0;
+		while (rs.next()) {
+			count++;
+		}
+		Assert.assertEquals(566, count);
+	}	
+	/*
+	@Test
+	public void joinAndAggregateByZipcode500KOrderLinesWith10KCustomers() throws Exception {
+		int count = 0;
+		ResultSet rs = methodWatcher.executeQuery(format("select cst_zipcode, sum(orl_qty_sold*orl_unit_price) from %s " +
+				"left outer join %s on orl_customer_id=cst_id group by cst_zipcode",this.getTableReference(TABLE_NAME_1),this.getTableReference(TABLE_NAME_2)));
+		while (rs.next()) {
+			count++;
+		}	
+		Assert.assertEquals(217, count);
+	}
+	*/
+}
