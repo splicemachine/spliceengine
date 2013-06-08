780a2f5624aa52b9acf460e7d30c1fe527a18925
John Leach <jleach@inciteretail.com>
2013-06-08 11:13:27 -0500

    Performance Check

--- structured_derby/src/test/java/com/splicemachine/perf/report/CorePerformanceReport.java
+++ structured_derby/src/test/java/com/splicemachine/perf/report/CorePerformanceReport.java
@@ -15,6 +15,7 @@ import com.splicemachine.derby.test.framework.SpliceWatcher;
 import com.splicemachine.derby.test.framework.tables.SpliceCustomerTable;
 import com.splicemachine.derby.test.framework.tables.SpliceOrderLineTable;
 
+@Ignore
 public class CorePerformanceReport extends SpliceUnitTest {
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
 	public static final String CLASS_NAME = CorePerformanceReport.class.getSimpleName().toUpperCase();
