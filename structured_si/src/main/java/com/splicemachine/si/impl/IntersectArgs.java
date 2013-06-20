d28364fca9f4023ff4aef0f2b33625816125f755
David McNeil <dmcneil@splicemachine.com>
2013-06-20 11:37:38 -0500

    si - added missing class

---
+++ structured_si/src/main/java/com/splicemachine/si/impl/IntersectArgs.java
@@ -0,0 +1,16 @@
+package com.splicemachine.si.impl;
+
+/**
+ * The arguments passed into the intersect method. This object is used as a key in the cache that memoizes the method.
+ */
+public class IntersectArgs {
+    final boolean collapse;
+    final ImmutableTransaction transaction1;
+    final ImmutableTransaction transaction2;
+
+    public IntersectArgs(boolean collapse, ImmutableTransaction transaction1, ImmutableTransaction transaction2) {
+        this.collapse = collapse;
+        this.transaction1 = transaction1;
+        this.transaction2 = transaction2;
+    }
+}
