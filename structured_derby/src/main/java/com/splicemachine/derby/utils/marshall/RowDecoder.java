6c344d73312e0a96af81d5fc0d22284b19365f58
Scott Fines <sfines@splicemachine.com>
2013-06-26 11:26:51 -0500

    Resets rows to all null in between decodings

--- structured_derby/src/main/java/com/splicemachine/derby/utils/marshall/RowDecoder.java
+++ structured_derby/src/main/java/com/splicemachine/derby/utils/marshall/RowDecoder.java
@@ -87,6 +87,9 @@ public class RowDecoder {
     public ExecRow decode(KeyValue[] keyValues) throws StandardException{
         boolean rowSet = keyType == null; //if keyType!=null, we want to set on the keyDecoder, otherwise don't bother
         DataValueDescriptor []fields = template.getRowArray();
+        for(DataValueDescriptor field:fields){
+            field.setToNull(); //unset the value, so that previous runs don't break
+        }
         for(KeyValue value: keyValues){
             if(!rowSet){
                 if(keyDecoder==null)
