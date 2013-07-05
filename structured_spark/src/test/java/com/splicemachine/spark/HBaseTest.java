c1260a7311003ebc8148c8c5c0537ed99784cb72
John Leach <jleach@inciteretail.com>
2013-07-05 12:11:09 -0500

    putting in

--- structured_spark/src/test/java/com/splicemachine/spark/HBaseTest.java
+++ structured_spark/src/test/java/com/splicemachine/spark/HBaseTest.java
@@ -13,24 +13,21 @@ import spark.api.java.JavaPairRDD;
 import spark.api.java.JavaSparkContext;
 
 public class HBaseTest {
-		public static String test = "test1";
-	
+	  private static String tableName;
+	  public HBaseTest(String tableName) {
+		  this.tableName = tableName;
+	  }
 	
 	  public static void main(String[] args) throws Exception {
-	    JavaSparkContext ctx = new JavaSparkContext("local", "JavaWordCount");
+		String[] jars = {"/Users/johnleach/.m2/repository/org/apache/hbase/hbase/0.94.6-cdh4.3.0/hbase-0.94.6-cdh4.3.0.jar","/Users/johnleach/splicemachine/workspace/structured_hbase/structured_spark/target/structured_spark-1.0.1-SNAPSHOT.jar"};
+	    JavaSparkContext ctx = new JavaSparkContext("spark://Johns-MacBook-Pro-2.local:7077", "JavaWordCount", "/Users/johnleach/SpliceMachine/spark-0.7.2",jars);
+//	    JavaSparkContext ctx = new JavaSparkContext("local", "JavaWordCount");
 	    Configuration conf = HBaseConfiguration.create();
-	    conf.set(TableInputFormat.INPUT_TABLE, "32");
+	    conf.set(TableInputFormat.INPUT_TABLE, tableName);
 	    conf.setInt(TableInputFormat.SCAN_CACHEDROWS, 16000);
 	    conf.set(TableInputFormat.SCAN_COLUMN_FAMILY, "S");	    
 	    JavaPairRDD<ImmutableBytesWritable, Result> rows = ctx.newAPIHadoopRDD(conf, TableInputFormat.class, ImmutableBytesWritable.class, Result.class);	    
-	    if (test.equals("test")) {
-	    	System.out.println("Count : " + rows.count());
-	    } else {
-	    	 List<Tuple2<ImmutableBytesWritable, Result>> output = rows.collect();
-	    	    for (Tuple2 tuple : output) {
-	    	      System.out.println(tuple._1 + ": " + tuple._2);
-	    	    }
-	    }
+	    System.out.println("Count : " + rows.count());
 	    System.exit(0);
 	  }	  	  
 	}
