10f589148ba8aeb5828f94a7b14aec744736a308
John Leach <jleach@inciteretail.com>
2013-08-02 10:49:52 -0500

    Adding Gene's TPC Issue

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/WriteWriteRollbackTest.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/WriteWriteRollbackTest.java
@@ -67,7 +67,6 @@ public class WriteWriteRollbackTest extends SpliceUnitTest {
 
 
 
-    @Ignore("Currently failing")
     @Test
     public void testUpdateWriteWriteRollbackConcurrent() throws Exception {
 
