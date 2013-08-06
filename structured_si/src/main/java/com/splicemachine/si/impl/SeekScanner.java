cbe3a6ff0be8096a7a5c5819ce4c30c849e6ba47
Perry Trolard <trolard@gmail.com>
2013-08-06 14:56:45 -0500

    (From Gene) SI: modify SeekScanner.seekRow to loop rather than recurse
    This change allowed the import of a large (1.5M row) CSV that we were
    previously unable to load (I believe due to a StackOverflow).

--- structured_si/src/main/java/com/splicemachine/si/impl/SeekScanner.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/SeekScanner.java
@@ -33,31 +33,33 @@ public class SeekScanner<IHTable, Data, Result, KeyValue, OperationWithAttribute
      * @throws IOException
      */
     public List<KeyValue> seekRow(Data rowKey) throws IOException {
-        if (next == null) {
-            next = reader.nextResultsOnRegionScanner(scanner);
-        }
-        if (next == null || next.isEmpty()) {
-            next = null;
-            return Collections.emptyList();
-        }
-        final Data resultKey = dataLib.getKeyValueRow(next.get(0));
-        if (dataLib.valuesEqual(resultKey, rowKey)) {
-            List<KeyValue> result = next;
-            next = null;
-            return result;
-        } else {
-            final int comparison = hasher.toHashable(resultKey).compareTo(hasher.toHashable(rowKey));
-            if (comparison > 0) {
-                // we are past the target key
-                // save the 'next' value for the next read
-                return Collections.emptyList();
-            } else if (comparison < 0) {
-                // we need to seek to the target key
-                reader.seekOnRegionScanner(scanner, rowKey);
+        while (true) {
+            if (next == null) {
                 next = reader.nextResultsOnRegionScanner(scanner);
-                return seekRow(rowKey);
+            }
+            if (next == null || next.isEmpty()) {
+                next = null;
+                return Collections.emptyList();
+            }
+            final Data resultKey = dataLib.getKeyValueRow(next.get(0));
+            if (dataLib.valuesEqual(resultKey, rowKey)) {
+                List<KeyValue> result = next;
+                next = null;
+                return result;
             } else {
-                throw new RuntimeException("Keys should not match");
+                final int comparison = hasher.toHashable(resultKey).compareTo(hasher.toHashable(rowKey));
+                if (comparison > 0) {
+                    // we are past the target key
+                    // save the 'next' value for the next read
+                    return Collections.emptyList();
+                } else if (comparison < 0) {
+                    // we need to seek to the target key
+                    reader.seekOnRegionScanner(scanner, rowKey);
+                    next = reader.nextResultsOnRegionScanner(scanner);
+                    continue;
+                } else {
+                    throw new RuntimeException("Keys should not match");
+                }
             }
         }
     }
