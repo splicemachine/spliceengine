dd83d5dbec06b6c74d909723f9683d270d44934f
John Leach <jleach@inciteretail.com>
2013-08-30 09:56:56 -0500

    Changing to bulk

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/constraint/ConstraintHandler.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/constraint/ConstraintHandler.java
@@ -1,16 +1,19 @@
 package com.splicemachine.derby.impl.sql.execute.constraint;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
 import com.google.common.collect.Lists;
 import com.splicemachine.hbase.batch.WriteContext;
 import com.splicemachine.hbase.batch.WriteHandler;
 import com.splicemachine.hbase.writer.KVPair;
 import com.splicemachine.hbase.writer.WriteResult;
-import org.apache.hadoop.hbase.NotServingRegionException;
-import org.apache.hadoop.hbase.client.Mutation;
+import com.splicemachine.hbase.writer.WriteResult.Code;
 import org.apache.hadoop.hbase.regionserver.HRegion;
-
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 /**
  * @author Scott Fines
@@ -18,7 +21,7 @@ import java.util.List;
  */
 public class ConstraintHandler implements WriteHandler {
     private final Constraint localConstraint;
-    private List<Mutation> mutations = Lists.newArrayList();
+    private List<KVPair> mutations = Lists.newArrayList();
 
     private boolean failed=false;
 
@@ -28,28 +31,37 @@ public class ConstraintHandler implements WriteHandler {
 
     @Override
     public void next(KVPair mutation, WriteContext ctx) {
-        if(failed)
-            ctx.notRun(mutation);
-        try {
-            if(!HRegion.rowIsInRange(ctx.getRegion().getRegionInfo(),mutation.getRow())){
-                //we can't check the mutation, it'll explode
-                ctx.failed(mutation,WriteResult.wrongRegion());
-            }else if(!localConstraint.validate(mutation,ctx.getTransactionId(),ctx.getCoprocessorEnvironment())){
-                failed = true;
-                ctx.result(mutation,
-                        new WriteResult(WriteResult.convertType(localConstraint.getType()), localConstraint.getConstraintContext()));
-            }else
-                ctx.sendUpstream(mutation);
-        }catch(NotServingRegionException nsre){
-            ctx.failed(mutation,WriteResult.notServingRegion());
-        }catch (Exception e) {
-            failed=true;
-            ctx.failed(mutation,WriteResult.failed(e.getClass().getSimpleName()+":"+e.getMessage()));
+        if(!HRegion.rowIsInRange(ctx.getRegion().getRegionInfo(),mutation.getRow())){
+            ctx.failed(mutation,new WriteResult(Code.FAILED,"WrongRegion"));
+        }else{
+            mutations.add(mutation);
+            ctx.sendUpstream(mutation);
         }
     }
 
     @Override
     public void finishWrites(final WriteContext ctx) throws IOException {
         //no-op
+        boolean failed=false;
+        Predicate<KVPair> runPredicate = new Predicate<KVPair>() {
+            @Override
+            public boolean apply(@Nullable KVPair input) {
+                return ctx.canRun(input);
+            }
+        };
+        for(KVPair mutation: Collections2.filter(mutations,runPredicate)) {
+            if(failed)
+                ctx.notRun(mutation);
+            if(ctx.getRegion().isClosing()||ctx.getRegion().isClosed()){
+                ctx.failed(mutation,new WriteResult(Code.FAILED,"NotServingRegion"));
+            }
+        }
+        Collection<KVPair> failedWrites = localConstraint.validate(mutations,ctx.getTransactionId(),ctx.getCoprocessorEnvironment());
+        if(failedWrites.size()>0){
+            WriteResult result = new WriteResult(WriteResult.convertType(localConstraint.getType()), localConstraint.getConstraintContext());
+            for(KVPair mutation:failedWrites){
+                ctx.failed(mutation,result);
+            }
+        }
     }
 }
