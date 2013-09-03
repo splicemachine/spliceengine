756612a2b5de9556eb6f64e3cb0ea61e74f7ca8c
Perry Trolard <trolard@gmail.com>
2013-09-03 11:05:05 -0500

    Add autoShutoff test to RFQ test

--- structured_si/src/test/java/com/splicemachine/si/impl/RollForwardQueueTest.java
+++ structured_si/src/test/java/com/splicemachine/si/impl/RollForwardQueueTest.java
@@ -10,6 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RollForwardQueueTest {
     private final int SHORT_DELAY = 50;
@@ -164,4 +165,38 @@ public class RollForwardQueueTest {
         Thread.sleep(SHORT_WAIT);
         Assert.assertEquals(20, out[0]);
     }
+
+    @Test
+    public void autoShutoff() throws InterruptedException {
+        final Set<Byte> out = new HashSet<Byte>();
+        final AtomicReference<Boolean> actionSucceeds = new AtomicReference<Boolean>(false);
+        RollForwardAction action = new RollForwardAction() {
+            @Override
+            public Boolean rollForward(long transactionId, List rowList) {
+                for(Object row : rowList) {
+                    out.add(((byte[]) row)[0]);
+                }
+                return actionSucceeds.get();
+            }
+        };
+        final SynchronousRollForwardQueue queue = new SynchronousRollForwardQueue(new HHasher(), action, 5, SHORT_DELAY, VERY_LONG_DELAY, "test");
+
+        queue.recordRow(1, new byte[]{10}, false);
+        Thread.sleep(SHORT_WAIT);
+        Assert.assertEquals(1, out.size());
+        queue.recordRow(1, new byte[]{20}, false);
+        queue.recordRow(1, new byte[]{30}, false);
+        queue.recordRow(1, new byte[]{40}, false);
+        Assert.assertEquals(0, queue.getCount());
+        Thread.sleep(SHORT_WAIT);
+        Assert.assertEquals(1, out.size());
+
+        actionSucceeds.set(true);
+        queue.recordRow(1, new byte[]{50}, true);
+        queue.recordRow(1, new byte[]{60}, true);
+        Assert.assertEquals(2, queue.getCount());
+        Thread.sleep(SHORT_WAIT);
+        Assert.assertEquals(3, out.size());
+    }
+
 }
