42cfad1069fc242563ba0a39ac48633e6499aab1
John Leach <jleach@inciteretail.com>
2013-09-24 13:36:32 -0700

    Fixed self eviction issue

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/DelegateHashBuffer.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/DelegateHashBuffer.java
@@ -7,8 +7,10 @@ import org.apache.derby.iapi.sql.execute.ExecRow;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class DelegateHashBuffer<K,T extends ExecRow> implements HashBuffer<K,T> {
 	protected int maxCapacity;
@@ -21,14 +23,15 @@ public class DelegateHashBuffer<K,T extends ExecRow> implements HashBuffer<K,T> 
     }
 
 	@Override
-    @SuppressWarnings("LoopStatementThatDoesntLoop")
     public Entry<K,T> add(K key, T element){
 		buffer.put(key, element);
         if(buffer.size()>maxCapacity){
            //evict the first entry returned by the buffer's iterator
             Iterator<Entry<K,T>> entryIterator = buffer.entrySet().iterator();
-            if(entryIterator.hasNext()){
+            while(entryIterator.hasNext()){
                 Entry<K,T> toEvict = entryIterator.next();
+                if (toEvict.getKey().equals(key)) // Do Not Allow Self Eviction
+                	continue;
                 entryIterator.remove();;
                 return toEvict;
             }
