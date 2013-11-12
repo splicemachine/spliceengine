8bb2ffbd79ed25a06609b1ce647ce09ca710e477
John Leach <jleach@inciteretail.com>
2013-11-12 10:44:03 -0600

    Backed Out clone changes

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/SinkGroupedAggregator.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/SinkGroupedAggregator.java
@@ -105,7 +105,7 @@ public class SinkGroupedAggregator implements StandardIterator<GroupedRow> {
     private GroupedRow buffer(ExecRow nextRow) throws StandardException {
         GroupedRow firstEvicted = null;
         if(!isRollup){
-            return buffer.buffer(nextRow);
+            return buffer.buffer(nextRow.getClone());
         }else{
             rollupRows(nextRow);
             for(ExecRow rollup:rollupRows){
