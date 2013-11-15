54fef58a50053df37188287809e613354eb02361
Perry Trolard <trolard@gmail.com>
2013-11-15 14:06:08 -0600

    Adapter from StandardIterator to Iterator

--- structured_derby/src/main/java/com/splicemachine/derby/utils/StandardIterators.java
+++ structured_derby/src/main/java/com/splicemachine/derby/utils/StandardIterators.java
@@ -3,7 +3,6 @@ package com.splicemachine.derby.utils;
 import org.apache.derby.iapi.error.StandardException;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.Iterator;
 
 /**
@@ -16,6 +15,10 @@ public class StandardIterators {
 
     public static <T> StandardIterator<T> wrap(Iterable<T> data){
         return new IteratorIterator<T>(data.iterator());
+    }
+
+    public static <T> StandardIteratorIterator<T> asIter(StandardIterator<T> stdIt){
+        return new StandardIteratorIterator<T>(stdIt);
     }
 
     private static class IteratorIterator<T> implements StandardIterator<T>{
@@ -33,6 +36,60 @@ public class StandardIterators {
             if(!delegate.hasNext())
                 return null;
             return delegate.next();
+        }
+    }
+
+    public static class StandardIteratorIterator<T> implements Iterator<T>{
+        private final StandardIterator<T> delegate;
+        private boolean needToCheckDelegate = true;
+        private StandardException se;
+        private IOException ioe;
+        private T value;
+
+        private StandardIteratorIterator(StandardIterator<T> delegate){
+            this.delegate = delegate;
+        }
+
+        public boolean hasException(){
+            return se != null || ioe != null;
+        }
+
+        public void throwExceptions() throws StandardException, IOException {
+            if (se != null){
+                throw se;
+            }
+            if (ioe != null){
+                throw ioe;
+            }
+        }
+
+        @Override
+        public boolean hasNext(){
+            if (needToCheckDelegate && !hasException()){
+                try {
+                    value = delegate.next();
+                    needToCheckDelegate = false;
+                } catch (StandardException e){
+                    se = e;
+                } catch (IOException e){
+                    ioe = e;
+                }
+            }
+            return !hasException() && value != null;
+        }
+
+        @Override
+        public T next() {
+            if (hasException()){
+                return null;
+            }
+            needToCheckDelegate = true;
+            return value;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
         }
     }
 }
