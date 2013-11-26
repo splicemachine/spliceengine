151051f92d4d870ead94ea649ff234d8116d14a4
Scott Fines <scottfines@gmail.com>
2013-11-25 18:30:02 -0600

    Minor changes to how Index creation tasks execute

---
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/utilities/TableProcedures.java
@@ -0,0 +1,11 @@
+package com.splicemachine.derby.impl.sql.execute.utilities;
+
+/**
+ * Stored procedures for getting information about specific Tables.
+ *
+ * @author Scott Fines
+ * Date: 11/25/13
+ */
+public class TableProcedures {
+
+}
