662c2ff6a2b891ad9a2c50b602ebdc68c3056d16
Jun Yuan <jyuan@splicemachine.com>
2013-12-12 17:09:07 -0800

    add integration tests for standard deviation

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/StddevIT.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/StddevIT.java
@@ -12,8 +12,8 @@ import org.junit.rules.TestRule;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
-public class UserDefinedAggregateIT extends SpliceUnitTest {
-    public static final String CLASS_NAME = UserDefinedAggregateIT.class.getSimpleName().toUpperCase();
+public class StddevIT extends SpliceUnitTest {
+    public static final String CLASS_NAME = StddevIT.class.getSimpleName().toUpperCase();
 
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
     public static final String TABLE_NAME = "TAB";
@@ -35,28 +35,45 @@ public class UserDefinedAggregateIT extends SpliceUnitTest {
      */
     @Before
     public void setUp() throws Exception {
+        System.out.println("StddevIT:");
         Connection conn = methodWatcher.createConnection();
-        for (int i=0; i<10; i++) {
-            conn.createStatement().execute(
+        for (int j = 0; j < 100; ++j) {
+            for (int i=0; i<10; i++) {
+                conn.createStatement().execute(
                     String.format("insert into %s (i, d) values (%d, %f)",
                     		this.getTableReference(TABLE_NAME), i, i * 1.0));
+            }
         }
         ResultSet resultSet = conn.createStatement().executeQuery(
                 String.format("select * from %s", this.getTableReference(TABLE_NAME)));
-        Assert.assertEquals(10, resultSetSize(resultSet));
+        Assert.assertEquals(1000, resultSetSize(resultSet));
+        resultSet.close();
+        conn.createStatement().execute(
+                String.format("create derby aggregate StddevIT.stddevpop for double external name \'com.splicemachine.derby.impl.sql.execute.operations.SpliceStddevPop\'"));
 
         conn.createStatement().execute(
-                String.format("create derby aggregate stddevpop for double external name \'com.splicemachine.derby.impl.sql.execute.operations.SpliceStddevPop\'"));
+                String.format("create derby aggregate StddevIT.stddevsamp for double external name \'com.splicemachine.derby.impl.sql.execute.operations.SpliceStddevSamp\'"));
     }
     
     @Test
     public void test() throws Exception {
     	Connection conn = methodWatcher.createConnection();
     	ResultSet rs = conn.createStatement().executeQuery(
-                String.format("select stddevpop(i) from %s", this.getTableReference(TABLE_NAME)));
-        Assert.assertEquals(1, resultSetSize(rs));
+                String.format("select StddevIT.stddevpop(i) from %s", this.getTableReference(TABLE_NAME)));
+
+        System.out.println("StddevIT:");
         while(rs.next()){
         	Assert.assertEquals((int)rs.getDouble(1), 2);
         }
+        rs.close();
+
+        rs = conn.createStatement().executeQuery(
+                String.format("select StddevIT.stddevsamp(i) from %s", this.getTableReference(TABLE_NAME)));
+
+        while(rs.next()){
+            System.out.println("stddevIT -- samp = " + rs.getDouble(1));
+            Assert.assertEquals((int)rs.getDouble(1), 2);
+        }
+
     }
 }
