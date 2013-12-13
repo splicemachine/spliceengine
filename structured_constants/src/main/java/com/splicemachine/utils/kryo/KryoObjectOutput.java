6bce11f436a82d5efd725b7beeaa19d82437a794
John Leach <jleach@inciteretail.com>
2013-12-12 18:32:53 -0800

    Code Cleanup

--- structured_constants/src/main/java/com/splicemachine/utils/kryo/KryoObjectOutput.java
+++ structured_constants/src/main/java/com/splicemachine/utils/kryo/KryoObjectOutput.java
@@ -21,97 +21,81 @@ public class KryoObjectOutput implements ObjectOutput {
 
     @Override
     public void writeObject(Object obj) throws IOException {
-//        System.out.println("writeObject "+ obj);
         kryo.writeClassAndObject(output,obj);
     }
 
     @Override
     public void write(int b) throws IOException {
-//        System.out.println("write(int) "+ b);
         output.writeInt(b);
     }
 
     @Override
     public void write(byte[] b) throws IOException {
-//        System.out.println("write(byte[]) "+ b);
         output.write(b);
     }
 
     @Override
     public void write(byte[] b, int off, int len) throws IOException {
-//        System.out.println("write(byte[],off,len) "+ b+","+off+","+len);
         output.write(b,off,len);
     }
 
     @Override
     public void writeBoolean(boolean v) throws IOException {
-//        System.out.println("writeBoolean "+ v);
         output.writeBoolean(v);
     }
 
     @Override
     public void writeByte(int v) throws IOException {
-//        System.out.println("writeByte "+ v);
         output.writeByte(v);
     }
 
     @Override
     public void writeShort(int v) throws IOException {
-//        System.out.println("writeShort "+ v);
         output.writeShort(v);
     }
 
     @Override
     public void writeChar(int v) throws IOException {
-//        System.out.println("writeChar "+ v);
         output.writeChar((char)v);
     }
 
     @Override
     public void writeInt(int v) throws IOException {
-//        System.out.println("writeInt "+ v);
         output.writeInt(v);
     }
 
     @Override
     public void writeLong(long v) throws IOException {
-//        System.out.println("writeLong "+ v);
         output.writeLong(v);
     }
 
     @Override
     public void writeFloat(float v) throws IOException {
-//        System.out.println("writeFloat "+ v);
         output.writeFloat(v);
     }
 
     @Override
     public void writeDouble(double v) throws IOException {
-//        System.out.println("writeDouble "+ v);
         output.writeDouble(v);
     }
 
     @Override
     public void writeBytes(String s) throws IOException {
-//        System.out.println("writeBytes(String) "+ s);
         output.writeString(s);
     }
 
     @Override
     public void writeChars(String s) throws IOException {
-//        System.out.println("writeChars(String) "+ s);
         output.writeString(s);
     }
 
     @Override
     public void writeUTF(String s) throws IOException {
-//        System.out.println("writeUTF(String) "+ s);
         output.writeString(s);
     }
 
     @Override
     public void flush() throws IOException {
-//        System.out.println("flush");
         output.flush();
     }
 
