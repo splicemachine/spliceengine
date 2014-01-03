059cf0ab9cdb90ad7972bb2be4748a18795cf97c
jeffcunningham <jcunningham@splicemachine.com>
2014-01-03 08:12:02 -0600

    Added help and usage doc.

--- start-splice.sh
+++ start-splice.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+
+##################################################################################
+# Start Zookeeper and the Splice HBase servers
+# See usage() below.
+##################################################################################
+
+usage() {
+    # $1 is an error, if any
+    if [[ -n "${1}" ]]; then
+        echo "Error: ${1}"
+    fi
+    echo "Usage: $0 [true|false] [<hbase_profile>] -h[elp]"
+    echo "Where: "
+    echo "  true|false is an optional flag determining random task failures. Default is false."
+    echo "  <hbase_profile> is the optional splice hbase platform to run.  One of:"
+    echo "    cloudera-cdh4.3.0, hdp1.3, apache-hbase-0.94.5, mapr-0.94.5. Default is cloudera-cdh4.3.0."
+    echo "  -h => print this message"
+}
+
+if [[ ${1} == -h* ]]; then
+    usage
+    exit 0 # This is not an error, User asked help. Don't do "exit 1"
+fi
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/" && pwd )"
 pushd "${SCRIPT_DIR}/structured_derby" &>/dev/null
 ROOT_DIR="$( pwd )"
 
-CHAOS=$1
+CHAOS=${1}
 shopt -s nocasematch
 if [[ -z "${CHAOS}" || "${CHAOS}" != "true" ]]; then
     # default is NOT to run chaos monkey
@@ -13,7 +37,7 @@ else
 fi
 shopt -u nocasematch
 
-PROFILE=$2
+PROFILE=${2}
 if [[ -z "${PROFILE}" ]]; then
     # default profile
     PROFILE="cloudera-cdh4.3.0"
@@ -22,15 +46,13 @@ fi
 TARBALL="${ROOT_DIR}"/target/splice_machine-0.5rc6-SNAPSHOT-${PROFILE}_simple.tar.gz
 # fail if wrong profile was provided
 if [[ ! -e ${TARBALL} ]]; then
-    echo "Cannot find ${TARBALL}"
-    echo "An unexpected profile was provided \\"${PROFILE}\\""
-    echo "Cannot continue"
+    usage "Cannot find ${TARBALL}. An unexpected profile was provided \\"${PROFILE}\\""
     exit 1
 fi
 
 # Make package scrips executable
 chmod 0755 "${ROOT_DIR}"/target/classes/bin/*sh
-# Extract libs for classpath
+# Extract package libs for classpath
 tar xvf ${TARBALL} -C "${ROOT_DIR}"/target splicemachine/lib &>/dev/null
 # Config
 SPLICELOG="${ROOT_DIR}"/splice.log
@@ -44,9 +66,9 @@ currentDateTime=$(date +'%m-%d-%Y:%H:%M:%S')
 echo "=== Running profile ${PROFILE} at $currentDateTime === " > ${SPLICELOG}
 
 export SPLICE_SYS_ARGS="-Xdebug -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=10102"
-# Start server with retry logic
 ZOO_WAIT_TIME=45
 SPLICE_MAIN_CLASS="com.splicemachine.test.SpliceTestPlatform"
+# Start server with retry logic
 #echo "${ROOT_DIR}/target/classes" "${SPLICELOG}" "${ZOOLOG}" "${LOG4J_PATH}" "${ZOO_DIR}" "${ZOO_WAIT_TIME}" "${HBASE_ROOT_DIR_URI}" "${CLASSPATH}" "${SPLICE_MAIN_CLASS}" "${CHAOS}"
 "${ROOT_DIR}"/target/classes/bin/_retrySplice.sh "${ROOT_DIR}/target/classes" "${SPLICELOG}" "${ZOOLOG}" "${LOG4J_PATH}" "${ZOO_DIR}" "${ZOO_WAIT_TIME}" "${HBASE_ROOT_DIR_URI}" "${CLASSPATH}" "${SPLICE_MAIN_CLASS}" "${CHAOS}"
 
