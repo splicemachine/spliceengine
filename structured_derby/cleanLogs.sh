4520ade028b2be9adf1bdbe04c074a597cdade9b
jeffcunningham <jcunningham@splicemachine.com>
2014-01-15 13:59:53 -0600

    Missed the structured_derby path that must be appended to Jenkins WORKSPACE env var.

--- structured_derby/cleanLogs.sh
+++ structured_derby/cleanLogs.sh
@@ -3,9 +3,11 @@
 #
 # Remove old server, derby and zoo log files from the logs directory that are older than 3 weeks
 #
-ROOT_DIR=${WORKSPACE}
+ROOT_DIR="${WORKSPACE}"
 if [[ -z ${ROOT_DIR} ]]; then
   ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+else
+  ROOT_DIR="${ROOT_DIR}"/structured_derby
 fi
 # Test
 #find "${ROOT_DIR}"/logs -type f -mtime +7 -exec ls -lt {} \;
