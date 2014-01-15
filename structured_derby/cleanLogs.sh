f25c8552ef1dd500bc3d8a4e9039ed6c68428d40
jeffcunningham <jcunningham@splicemachine.com>
2014-01-15 13:47:59 -0600

    Adjusted for Jenkins exec env -- scripts get copied to tomcat tmp dir (and renamed).

--- structured_derby/cleanLogs.sh
+++ structured_derby/cleanLogs.sh
@@ -3,7 +3,10 @@
 #
 # Remove old server, derby and zoo log files from the logs directory that are older than 3 weeks
 #
-ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=${WORKSPACE}
+if [[ -z ${ROOT_DIR} ]]; then
+  ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+fi
 # Test
 #find "${ROOT_DIR}"/logs -type f -mtime +7 -exec ls -lt {} \;
 find "${ROOT_DIR}"/logs -type f -mtime +14 -exec rm -f {} \;
