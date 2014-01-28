041bed41e23055a7f8ed4ebc238f3f56025b545d
Scott Fines <scottfines@gmail.com>
2014-01-28 16:54:38 -0600

    Making DistinctScalarAggregateOperation report correct statistics

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/distinctscalar/DistinctScalarAggregateIterator.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/distinctscalar/DistinctScalarAggregateIterator.java
@@ -1,14 +1,18 @@
 package com.splicemachine.derby.impl.sql.execute.operations.distinctscalar;
 
+import com.splicemachine.constants.SpliceConstants;
 import com.splicemachine.derby.hbase.SpliceDriver;
 import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
+import com.splicemachine.derby.impl.sql.execute.operations.SpliceBaseOperation;
 import com.splicemachine.derby.impl.sql.execute.operations.framework.AbstractStandardIterator;
 import com.splicemachine.derby.impl.sql.execute.operations.framework.GroupedRow;
+import com.splicemachine.derby.utils.SpliceUtils;
 import com.splicemachine.derby.utils.StandardIterator;
 import com.splicemachine.derby.utils.marshall.KeyMarshall;
 import com.splicemachine.derby.utils.marshall.KeyType;
 import com.splicemachine.encoding.MultiFieldEncoder;
 
+import com.splicemachine.utils.SpliceUtilities;
 import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.sql.execute.ExecRow;
 
@@ -24,8 +28,9 @@ public class DistinctScalarAggregateIterator extends AbstractStandardIterator {
     private KeyMarshall keyHasher;
     private boolean completed;
     private int[] keyColumns;
+		private int rowsRead;
 
-    public DistinctScalarAggregateIterator(DistinctAggregateBuffer buffer,StandardIterator<ExecRow> source, int[] keyColumns) {
+		public DistinctScalarAggregateIterator(DistinctAggregateBuffer buffer,StandardIterator<ExecRow> source, int[] keyColumns) {
     	super(source);
         this.buffer = buffer;
         this.keyHasher = KeyType.BARE;
@@ -37,12 +42,14 @@ public class DistinctScalarAggregateIterator extends AbstractStandardIterator {
 	        boolean shouldContinue;
 	        GroupedRow toReturn = null;
 	        do{
+							SpliceBaseOperation.checkInterrupt(rowsRead, SpliceConstants.interruptLoopCheck);
 	            ExecRow nextRow = source.next(spliceRuntimeContext);
 	            shouldContinue = nextRow!=null;
 	            if(!shouldContinue)
 	                continue; //iterator exhausted, break from the loop
 	            toReturn = buffer.add(getGroupingKey(nextRow),nextRow);;
 	            shouldContinue = toReturn==null;
+							rowsRead++;
 	        }while(shouldContinue);
 	
 	        if(toReturn!=null)
@@ -53,6 +60,10 @@ public class DistinctScalarAggregateIterator extends AbstractStandardIterator {
                 return buffer.getFinalizedRow();
             return null;
     }
+
+		public int getRowsRead(){
+			return rowsRead;
+		}
 
     private byte[] getGroupingKey(ExecRow row) throws StandardException {
         if(keyEncoder==null){
--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/distinctscalar/SingleDistinctScalarAggregateIterator.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/distinctscalar/SingleDistinctScalarAggregateIterator.java
@@ -1,6 +1,8 @@
 package com.splicemachine.derby.impl.sql.execute.operations.distinctscalar;
 
+import com.splicemachine.constants.SpliceConstants;
 import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
+import com.splicemachine.derby.impl.sql.execute.operations.SpliceBaseOperation;
 import com.splicemachine.derby.impl.sql.execute.operations.WarningCollector;
 import com.splicemachine.derby.impl.sql.execute.operations.framework.AbstractStandardIterator;
 import com.splicemachine.derby.impl.sql.execute.operations.framework.EmptyRowSupplier;
@@ -19,64 +21,70 @@ import java.io.IOException;
  * Created on: 11/1/13
  */
 public class SingleDistinctScalarAggregateIterator extends AbstractStandardIterator {
-    private ExecRow currentRow;
-    private EmptyRowSupplier emptyRowSupplier;
-    private SpliceGenericAggregator[] aggregates;
-    private WarningCollector warningCollector;
+		private ExecRow currentRow;
+		private EmptyRowSupplier emptyRowSupplier;
+		private SpliceGenericAggregator[] aggregates;
+		private WarningCollector warningCollector;
+		private long rowsRead;
 
-    
-    public SingleDistinctScalarAggregateIterator(StandardIterator<ExecRow> source, EmptyRowSupplier emptyRowSupplier, WarningCollector warningCollector, SpliceGenericAggregator[] aggregates) {
-    	super(source);
-        this.emptyRowSupplier = emptyRowSupplier;
-        this.aggregates = aggregates;
-        this.warningCollector = warningCollector;
-    }
-    
-    public void merge(ExecRow newRow) throws StandardException{
-    	for(SpliceGenericAggregator aggregator:aggregates) {
-    		initialize(newRow);
-    		aggregator.merge(newRow, currentRow);
-        }
-    }
 
-    public void initialize(ExecRow newRow) throws StandardException{
-    	for(SpliceGenericAggregator aggregator:aggregates) {
-    		aggregator.initializeAndAccumulateIfNeeded(newRow,newRow);
-        }
-    }
+		public SingleDistinctScalarAggregateIterator(StandardIterator<ExecRow> source, EmptyRowSupplier emptyRowSupplier, WarningCollector warningCollector, SpliceGenericAggregator[] aggregates) {
+				super(source);
+				this.emptyRowSupplier = emptyRowSupplier;
+				this.aggregates = aggregates;
+				this.warningCollector = warningCollector;
+		}
 
-    @Override
-    public GroupedRow next(SpliceRuntimeContext spliceRuntimeContext) throws StandardException, IOException {
-    	ExecRow nextRow = source.next(spliceRuntimeContext);
-    	if (nextRow == null) {// Return Default Values...
-        	currentRow = emptyRowSupplier.get();
-        	finish();
-        	return new GroupedRow(currentRow,new byte[0]);        			
-        }
-        currentRow = nextRow.getClone();
-        initialize(currentRow);
-        boolean shouldContinue = true;
-    	do{
-	            nextRow = source.next(spliceRuntimeContext);
-	            shouldContinue = nextRow!=null;
-	            if(!shouldContinue)
-	                continue; //iterator exhausted, break from the loop
-	            merge(nextRow);
-    	}while(shouldContinue);	
-    	finish();
-    	
-    	return new GroupedRow(currentRow,new byte[0]);
-    }
-    
-    public void finish() throws StandardException{
-        if(currentRow==null)
-            currentRow = emptyRowSupplier.get();
-        boolean eliminatedNulls = false;
-        for(SpliceGenericAggregator aggregate:aggregates){
-            if(aggregate.finish(currentRow))
-                eliminatedNulls=true;
-        }
-        if(eliminatedNulls)
-            warningCollector.addWarning(SQLState.LANG_NULL_ELIMINATED_IN_SET_FUNCTION);
-    }
+		public void merge(ExecRow newRow) throws StandardException{
+				for(SpliceGenericAggregator aggregator:aggregates) {
+						initialize(newRow);
+						aggregator.merge(newRow, currentRow);
+				}
+		}
+
+		public void initialize(ExecRow newRow) throws StandardException{
+				for(SpliceGenericAggregator aggregator:aggregates) {
+						aggregator.initializeAndAccumulateIfNeeded(newRow,newRow);
+				}
+		}
+
+		@Override
+		public GroupedRow next(SpliceRuntimeContext spliceRuntimeContext) throws StandardException, IOException {
+				ExecRow nextRow = source.next(spliceRuntimeContext);
+				if (nextRow == null) {// Return Default Values...
+						currentRow = emptyRowSupplier.get();
+						finish();
+						return new GroupedRow(currentRow,new byte[0]);
+				}
+				currentRow = nextRow.getClone();
+				initialize(currentRow);
+				boolean shouldContinue = true;
+				do{
+						SpliceBaseOperation.checkInterrupt(rowsRead, SpliceConstants.interruptLoopCheck);
+						nextRow = source.next(spliceRuntimeContext);
+						shouldContinue = nextRow!=null;
+						if(!shouldContinue)
+								continue; //iterator exhausted, break from the loop
+						merge(nextRow);
+				}while(shouldContinue);
+				finish();
+
+				return new GroupedRow(currentRow,new byte[0]);
+		}
+
+		public long getRowsRead(){
+				return rowsRead;
+		}
+
+		public void finish() throws StandardException{
+				if(currentRow==null)
+						currentRow = emptyRowSupplier.get();
+				boolean eliminatedNulls = false;
+				for(SpliceGenericAggregator aggregate:aggregates){
+						if(aggregate.finish(currentRow))
+								eliminatedNulls=true;
+				}
+				if(eliminatedNulls)
+						warningCollector.addWarning(SQLState.LANG_NULL_ELIMINATED_IN_SET_FUNCTION);
+		}
 }
