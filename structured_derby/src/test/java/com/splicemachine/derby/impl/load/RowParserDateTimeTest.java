b0ce056a69da4e2e38357c7ae8c6a02e20f9abd4
Scott Fines <scottfines@gmail.com>
2014-02-05 08:26:48 -0600

    Modifying Unit Test to not have explicitly set dates, but instead to rely on parsing and unparsing of correct times to handle weird time zones. Should fix build

--- structured_derby/src/test/java/com/splicemachine/derby/impl/load/RowParserDateTimeTest.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/load/RowParserDateTimeTest.java
@@ -6,17 +6,23 @@ import org.apache.derby.iapi.sql.execute.ExecRow;
 import org.apache.derby.iapi.types.DataValueDescriptor;
 import org.apache.derby.iapi.types.SQLTimestamp;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.sql.Timestamp;
 import java.sql.Types;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Tests around RowParser's Date Time format.
+ *
+ * We want to make sure that we test parsing for all manner of different time zones. Otherwise,
+ * we may do something that works for common Tzs, but is all kinds of screwed up when trying to run in,
+ * say, Nepal, or India.
  *
  * @author Scott Fines
  * Created on: 9/30/13
@@ -25,20 +31,56 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class RowParserDateTimeTest {
 
+		private static final String[] formats = new String[]{
+						"yyyy/MM/dd HH:mm:ss",
+						"MM/dd/yyyy HH:mm:ss",
+						"dd/MM/yyyy HH:mm:ss",
+						"yyyy-MM-dd HH:mm:ss",
+		};
+		private static final String[] tzFormats = new String[]{
+						"yyyy-MM-dd HH:mm:ssZ",
+						"yyyy-MM-dd HH:mm:ssZ",
+						"yyyy-MM-dd HH:mm:ssZ",
+						"yyyy-MM-dd HH:mm:ss.SSSZ",
+						"yyyy-MM-dd HH:mm:ss.SSSZ",
+						"yyyy-MM-dd HH:mm:ss.SSSZ"
+		};
+		private static final TimeZone[] goofyTimeZones = new TimeZone[]{
+						TimeZone.getTimeZone("America/Caracas"), // off by 30 minutes
+						TimeZone.getTimeZone("Asia/Kathmandu"), //off by 15 or 45 minutes
+						TimeZone.getDefault(), //default time zone
+		};
 		@Parameterized.Parameters
 		public static Collection<Object[]> data() {
 				Collection<Object[]> data = Lists.newArrayList();
 
-				data.add(new Object[]{"yyyy/MM/dd HH:mm:ss", "2010/01/01 00:00:00", new Timestamp(2010-1900,0,1,0,0,0,0)});
-				data.add(new Object[]{"MM/dd/yyyy HH:mm:ss", "01/01/2010 00:00:00", new Timestamp(2010-1900,0,1,0,0,0,0)});
-				data.add(new Object[]{"dd/MM/yyyy HH:mm:ss", "01/01/2010 00:00:00", new Timestamp(2010-1900,0,1,0,0,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ss", "2010-01-01 00:00:00", new Timestamp(2010-1900,0,1,0,0,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ssZ", "2010-01-01 00:00:00-05", new Timestamp(2009-1900,11,31,23,0,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ssZ", "2010-01-01 00:00:00-0500", new Timestamp(2009-1900,11,31,23,0,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ssZ", "2010-01-01 00:00:00-0430", new Timestamp(2009-1900,11,31,22,30,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ss.SSSZ", "2010-01-01 00:00:00.000-0430", new Timestamp(2009-1900,11,31,22,30,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ss.SSSZ", "2010-01-01 00:00:00.000-0415", new Timestamp(2009-1900,11,31,22,15,0,0)});
-				data.add(new Object[]{"yyyy-MM-dd HH:mm:ss.SSSZ", "2010-01-01 00:00:00.000-0405", new Timestamp(2009-1900,11,31,22,05,0,0)});
+				long time = System.currentTimeMillis();
+				long timeToUse = 1000*(time/1000);
+				Date date = new Date(time);
+				Timestamp ts = new Timestamp(timeToUse);
+				for(String format:formats){
+						SimpleDateFormat sdf = new SimpleDateFormat(format);
+						String text = sdf.format(date);
+						data.add(new Object[]{format,text,ts});
+				}
+
+				//add specific timezone fields
+
+				//pick Caracas because it has a crazy-ass offset of -430 for most of the year
+				Date value = new Date(System.currentTimeMillis());
+				for(TimeZone timeZone:goofyTimeZones){
+						for(String tzFormat:tzFormats){
+								long t = value.getTime();
+								if(!tzFormat.matches(".*\\.S.*"))
+										t = 1000*(t/1000);
+
+								ts = new Timestamp(t);
+								SimpleDateFormat sdf = new SimpleDateFormat(tzFormat);
+								sdf.setTimeZone(timeZone);
+								String text = sdf.format(value);
+								data.add(new Object[]{tzFormat,text,ts});
+						}
+				}
 
 				return data;
 		}
@@ -69,48 +111,4 @@ public class RowParserDateTimeTest {
 
 				Assert.assertEquals("Incorrect parsed timestamp!", correctDate,processed.getColumn(1).getTimestamp(null));
 		}
-
-		@Ignore
-		@Test
-    public void testCanParseTwoDateFieldsCorrectlyWithSpecifiedFormat() throws Exception {
-        ExecRow testRow = new ValueRow(1);
-        DataValueDescriptor testDvd = new SQLTimestamp();
-        testRow.setColumn(1,testDvd);
-
-        RowParser parser = new RowParser(testRow,null,null,"MM/dd/yyyy HH:mm:ss");
-
-        ColumnContext ctx = new ColumnContext.Builder()
-                                    .columnType(Types.TIMESTAMP)
-                .nullable(true)
-                .build();
-        String[] testLine1 = new String[]{"01/01/2013 00:00:00"};
-        ExecRow processed = parser.process(testLine1,new ColumnContext[]{ctx});
-
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,0,1,0,0,0,0),processed.getColumn(1).getTimestamp(null));
-
-        testLine1 = new String[]{"02/01/2013 00:00:00"};
-        processed = parser.process(testLine1,new ColumnContext[]{ctx});
-
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,1,1,0,0,0,0),processed.getColumn(1).getTimestamp(null));
-        
-        
-        testLine1 = new String[]{"2013-02-01 00:00:00-00"};
-        parser = new RowParser(testRow,null,null,"yyyy-MM-dd HH:mm:ssZ");
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,1,1,0,0,0,0),processed.getColumn(1).getTimestamp(null));
-        testLine1 = new String[]{"2013-02-01 00:00:00.00-00"};
-        parser = new RowParser(testRow,null,null,"yyyy-MM-dd HH:mm:ss.SSZ");
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,1,1,0,0,0,0),processed.getColumn(1).getTimestamp(null));
-        testLine1 = new String[]{"2013-02-01 00:00:00.000-00"};
-        parser = new RowParser(testRow,null,null,"yyyy-MM-dd HH:mm:ss.SSSZ");
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,1,1,0,0,0,0),processed.getColumn(1).getTimestamp(null));
-
-        RowParser parser1 = new RowParser(testRow,null,null,"MM/dd/yyyy HH:mm:ss@1");      
-        ColumnContext ctx1 = new ColumnContext.Builder()
-                                    .columnType(Types.TIMESTAMP)
-                .nullable(true)
-                .build();
-        testLine1 = new String[]{"01/01/2013 00:00:00"};
-        ExecRow processed1 = parser1.process(testLine1,new ColumnContext[]{ctx1});
-        Assert.assertEquals("Incorrect parsed timestamp!",new Timestamp(2013-1900,0,1,0,0,0,0),processed1.getColumn(1).getTimestamp(null)); 
-    }
 }
