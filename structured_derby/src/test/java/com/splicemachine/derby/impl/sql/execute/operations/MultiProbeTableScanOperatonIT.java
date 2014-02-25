bd5180ac25593cdfd38bbca9f92413352586c688
Scott Fines <scottfines@gmail.com>
2014-02-25 11:25:57 -0600

    Resolves DB-1040 by making MultiProbe scan information properly serialize between servers. That way subqueries will work correctly

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MultiProbeTableScanOperatonIT.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MultiProbeTableScanOperatonIT.java
@@ -18,40 +18,80 @@ public class MultiProbeTableScanOperatonIT {
     protected static SpliceWatcher spliceClassWatcher = new DefaultedSpliceWatcher(CLASS_NAME);
     protected static SpliceSchemaWatcher schemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);
     protected static SpliceTableWatcher t1Watcher = new SpliceTableWatcher("user_groups",schemaWatcher.schemaName,"(user_id BIGINT NOT NULL,segment_id INT NOT NULL,unixtime BIGINT, primary key(segment_id, user_id))");
+		protected static SpliceTableWatcher t2Watcher = new SpliceTableWatcher("docs",schemaWatcher.schemaName,"(id varchar(128) not null)");
+		protected static SpliceTableWatcher t3Watcher = new SpliceTableWatcher("colls",schemaWatcher.schemaName,"(id varchar(128) not null,collid smallint not null)");
     private static int size = 10;
     
     @ClassRule
     public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
             .around(schemaWatcher)
             .around(t1Watcher)
+						.around(t2Watcher)
+						.around(t3Watcher)
             .around(new SpliceDataWatcher() {
-                @Override
-                protected void starting(Description description) {
-                    try {
-                        PreparedStatement ps = spliceClassWatcher.prepareStatement("insert into "+t1Watcher.toString()+" values (?,?,?)");
-                        for(int i=0;i<size;i++){
-                            ps.setInt(1,i);
-                            ps.setInt(2,i);
-                            ps.setLong(3, 1l);
-                            ps.execute();
-                        }
+								@Override
+								protected void starting(Description description) {
+										try {
+												PreparedStatement ps = spliceClassWatcher.prepareStatement("insert into " + t1Watcher.toString() + " values (?,?,?)");
+												for (int i = 0; i < size; i++) {
+														ps.setInt(1, i);
+														ps.setInt(2, i);
+														ps.setLong(3, 1l);
+														ps.execute();
+												}
 
-                        for(int i=0;i<size;i++){
-                        	if ((i == 4) || (i==6)) {
-	                            ps.setInt(1,size+i);
-	                            ps.setInt(2,i);
-	                            ps.setLong(3, 1l);
-	                            ps.execute();
-	                        }
-                        }
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }finally{
-                        spliceClassWatcher.closeAll();
-                    }
-                }
+												for (int i = 0; i < size; i++) {
+														if ((i == 4) || (i == 6)) {
+																ps.setInt(1, size + i);
+																ps.setInt(2, i);
+																ps.setLong(3, 1l);
+																ps.execute();
+														}
+												}
 
-            });
+												ps = spliceClassWatcher.prepareStatement("insert into " + t2Watcher.toString() + " values (?)");
+												ps.setString(1, "24");
+												ps.addBatch();
+												ps.setString(1, "25");
+												ps.addBatch();
+												ps.setString(1, "36");
+												ps.addBatch();
+												ps.setString(1, "27");
+												ps.addBatch();
+												ps.setString(1, "124");
+												ps.addBatch();
+												ps.setString(1, "567");
+												ps.addBatch();
+												ps.executeBatch();
+
+												ps = spliceClassWatcher.prepareStatement("insert into " + t3Watcher.toString() + " values (?,?)");
+												ps.setString(1, "123");
+												ps.setShort(2, (short) 2);
+												ps.addBatch();
+												ps.setString(1, "124");
+												ps.setShort(2, (short) -5);
+												ps.addBatch();
+												ps.setString(1, "24");
+												ps.setShort(2, (short) 1);
+												ps.addBatch();
+												ps.setString(1, "26");
+												ps.setShort(2, (short) -2);
+												ps.addBatch();
+												ps.setString(1, "36");
+												ps.setShort(2, (short) 1);
+												ps.addBatch();
+												ps.setString(1, "37");
+												ps.setShort(2, (short) 8);
+												ps.addBatch();
+												ps.executeBatch();
+										} catch (Exception e) {
+												throw new RuntimeException(e);
+										} finally {
+												spliceClassWatcher.closeAll();
+										}
+								}
+
+						});
 
     @Rule public SpliceWatcher methodWatcher = new DefaultedSpliceWatcher(CLASS_NAME);
 
@@ -84,7 +124,19 @@ public class MultiProbeTableScanOperatonIT {
         Assert.assertEquals("Incorrect records returned!",1,i);
     }
 
-
-
-
+		@Test
+		public void testMultiProbeInSubQueryWithIndex() throws Exception {
+				/* Regression test for DB-1040 */
+				SpliceIndexWatcher indexWatcher = new SpliceIndexWatcher(t3Watcher.tableName, t3Watcher.getSchema(),"new_index_3",t3Watcher.getSchema(),"(collid)");
+				indexWatcher.starting(null);
+				try{
+						ResultSet rs = methodWatcher.executeQuery("select count(id) from docs where id > any (select id from colls where collid in (-2,1))");
+						Assert.assertTrue("No results returned!",rs.next());
+						int count = rs.getInt(1);
+						Assert.assertEquals("Incorrect count returned!",4,count);
+						Assert.assertFalse("Too many rows returned!",rs.next());
+				}finally{
+						indexWatcher.drop();
+				}
+		}
 }
