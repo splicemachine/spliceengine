9b030b898c70fd62ff4d4fe57f2ff208d5a069f5
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2014-02-28 16:35:38 +0100

    Ensure COMMITTING transaction state is never seen
    (partial) fix for DB-1030

--- structured_si/src/main/java/com/splicemachine/si/api/TransactionStatus.java
+++ structured_si/src/main/java/com/splicemachine/si/api/TransactionStatus.java
@@ -6,20 +6,21 @@ package com.splicemachine.si.api;
  * Alternate paths are ACTIVE -> ERROR and ACTIVE -> ROLLED_BACK.
  */
 public enum TransactionStatus {
-    ACTIVE((byte)0x00),
-    ERROR((byte)0x01),
-    COMMITTING((byte)0x02),
-    COMMITTED((byte)0x03),
-    ROLLED_BACK((byte)0x04);
+    ACTIVE((byte) 0x00),
+    ERROR((byte) 0x01),
+    COMMITTING((byte) 0x02),
+    COMMITTED((byte) 0x03),
+    ROLLED_BACK((byte) 0x04);
 
-		private final byte id;
+    private final byte id;
 
-		TransactionStatus(byte id) {
-				this.id = id;
-		}
+    TransactionStatus(byte id) {
+        this.id = id;
+    }
 
-		public boolean isActive() {
-        return this.equals(ACTIVE);
+    public boolean isActive() {
+        // COMITTING is considered ACTIVE, since we still don't know what will happen with the transaction
+        return this.equals(ACTIVE) || this.equals(COMMITTING);
     }
 
     public boolean isFinished() {
@@ -34,35 +35,45 @@ public enum TransactionStatus {
         return this.equals(COMMITTING);
     }
 
-		public byte getId() {
-				return id;
-		}
+    public byte getId() {
+        return id;
+    }
 
-		public static TransactionStatus forByte(byte b){
-				switch (b){
-						case 0: return ACTIVE;
-						case 1: return ERROR;
-						case 2: return COMMITTING;
-						case 3: return COMMITTED;
-						case 4: return ROLLED_BACK;
-						default:
-								throw new IllegalArgumentException("Unknown transaction id!");
-				}
-		}
+    public static TransactionStatus forByte(byte b) {
+        switch (b) {
+            case 0:
+                return ACTIVE;
+            case 1:
+                return ERROR;
+            case 2:
+                return COMMITTING;
+            case 3:
+                return COMMITTED;
+            case 4:
+                return ROLLED_BACK;
+            default:
+                throw new IllegalArgumentException("Unknown transaction id!");
+        }
+    }
 
-		/*
-		 * Kept for backwards compatibility (for transaction tables
-		 * which are encoded using an int).
-		 */
-		public static TransactionStatus forInt(int value){
-				switch (value){
-						case 0: return ACTIVE;
-						case 1: return ERROR;
-						case 2: return COMMITTING;
-						case 3: return COMMITTED;
-						case 4: return ROLLED_BACK;
-						default:
-								throw new IllegalArgumentException("Unknown transaction id!");
-				}
-		}
+    /*
+     * Kept for backwards compatibility (for transaction tables
+     * which are encoded using an int).
+     */
+    public static TransactionStatus forInt(int value) {
+        switch (value) {
+            case 0:
+                return ACTIVE;
+            case 1:
+                return ERROR;
+            case 2:
+                return COMMITTING;
+            case 3:
+                return COMMITTED;
+            case 4:
+                return ROLLED_BACK;
+            default:
+                throw new IllegalArgumentException("Unknown transaction id!");
+        }
+    }
 }
