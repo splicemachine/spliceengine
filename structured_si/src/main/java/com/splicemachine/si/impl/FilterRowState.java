9e1a374d91be18f581ccec838c91c22a7ebabdbf
John Leach <jleach@inciteretail.com>
2014-03-03 23:31:12 -0600

    Code cleanup, remove duplicate call to setDecoder

--- structured_si/src/main/java/com/splicemachine/si/impl/FilterRowState.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/FilterRowState.java
@@ -3,7 +3,6 @@ package com.splicemachine.si.impl;
 import com.carrotsearch.hppc.LongArrayList;
 import com.carrotsearch.hppc.LongObjectOpenHashMap;
 import com.carrotsearch.hppc.ObjectArrayList;
-import com.splicemachine.si.data.api.SDataLib;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.OperationWithAttributes;
 import org.apache.hadoop.hbase.filter.Filter;
@@ -12,12 +11,11 @@ import org.apache.hadoop.hbase.filter.Filter;
  * Helper class for FilterState. Captures the state associated with the current row being processed by the filter.
  */
 public class FilterRowState<Result, Put extends OperationWithAttributes, Delete, Get extends OperationWithAttributes, Scan, Lock, OperationStatus> {
-    private final SDataLib<Put, Delete, Get, Scan> dataLib;
 
     /**
      * The key of the row currently being processed.
      */
-    private DecodedKeyValue<Result, Put, Delete, Get, Scan> currentRowKey = null;
+    private DecodedKeyValue currentRowKey = null;
 
     /**
      * Used to emulate the INCLUDE_AND_NEXT_COLUMN ReturnCode that is in later HBase versions .
@@ -42,15 +40,14 @@ public class FilterRowState<Result, Put extends OperationWithAttributes, Delete,
     boolean inData = false;
     Filter.ReturnCode shortCircuit = null;
 
-    public FilterRowState(SDataLib dataLib) {
-        this.dataLib = dataLib;
+    public FilterRowState() {
     }
 
     /**
      * Called for every key-value encountered by the filter. It is expected that key-values are read in row order.
      * Detects when the filter has moved to a new row and updates the state appropriately.
      */
-    public void updateCurrentRow(DecodedKeyValue<Result, Put, Delete, Get, Scan> keyValue) {
+    public void updateCurrentRow(DecodedKeyValue keyValue) {
         if (currentRowKey == null) {
             currentRowKey = keyValue;
             lastValidQualifier = null;
