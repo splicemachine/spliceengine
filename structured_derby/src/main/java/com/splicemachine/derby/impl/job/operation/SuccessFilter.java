c53074d99a4fd9cd002cc3d6c7ddc05d2b175254
Scott Fines <scottfines@gmail.com>
2014-03-20 13:21:44 -0500

    Making SuccessFilter work with protobufs

--- structured_derby/src/main/java/com/splicemachine/derby/impl/job/operation/SuccessFilter.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/job/operation/SuccessFilter.java
@@ -5,14 +5,21 @@ import java.io.DataOutput;
 import java.io.IOException;
 import java.util.List;
 
+import com.google.common.base.Function;
 import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.splicemachine.coprocessor.SpliceMessage;
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.exceptions.DeserializationException;
 import org.apache.hadoop.hbase.filter.FilterBase;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.log4j.Logger;
 
 import com.splicemachine.hbase.CellUtils;
 import com.splicemachine.utils.SpliceLogUtils;
+
+import javax.annotation.Nullable;
 
 /**
  * @author Scott Fines
@@ -103,4 +110,39 @@ public class SuccessFilter extends FilterBase {
             failedTasks.add(next);
         }
     }
+
+		@Override
+		public byte[] toByteArray() throws IOException {
+				SpliceMessage.SuccessFilterMessage filterM = SpliceMessage.SuccessFilterMessage.newBuilder()
+								.addAllFailedTasks(Lists.transform(failedTasks,new Function<byte[], ByteString>() {
+										@Nullable
+										@Override
+										public ByteString apply(@Nullable byte[] bytes) {
+												return ByteString.copyFrom(bytes);
+										}
+								})).build();
+				return filterM.toByteArray();
+		}
+
+		/*
+		 * DO NOT DELETE! Used by the ProtobufUtils to deserialize this, and we will break if we don't
+		 * implement this.
+		 */
+		@SuppressWarnings("UnusedDeclaration")
+		public static SuccessFilter parseFrom(final byte[] pbBytes) throws DeserializationException{
+				SpliceMessage.SuccessFilterMessage proto;
+				try{
+						proto = SpliceMessage.SuccessFilterMessage.parseFrom(pbBytes);
+				}catch(InvalidProtocolBufferException e){
+						throw new DeserializationException(e);
+				}
+				List<byte[]> failedTasks = Lists.transform(proto.getFailedTasksList(),new Function<ByteString, byte[]>() {
+						@Nullable
+						@Override
+						public byte[] apply(@Nullable ByteString bytes) {
+								return bytes.toByteArray();
+						}
+				});
+				return new SuccessFilter(failedTasks);
+		}
 }
