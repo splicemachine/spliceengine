d764e38c335357e282b688d9f1feac89a2fbf9b3
jeffcunningham <jcunningham@splicemachine.com>
2014-03-24 17:38:55 -0500

    Modified to be more reliable for execution from different locations.

--- structured_derby/src/main/resources/bin/load-demo.sh
+++ structured_derby/src/main/resources/bin/load-demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Load demo data into Splice Machine
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 
-./bin/sqlshell.sh ./demodata/sql/loadall.sql
+"${ROOT_DIR}"/bin/sqlshell.sh ./demodata/sql/loadall.sql
 
