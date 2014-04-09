83ef584b40bf6f99ef708a1407c57e63a5eed436
Jun Yuan <jyuan@splicemachine.com>
2014-04-09 16:28:32 -0700

    DB-1208: store current row location for OnceOperation

--- structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/OnceOperationIT.java
+++ structured_derby/src/test/java/org/apache/derby/impl/sql/execute/operations/OnceOperationIT.java
@@ -20,28 +20,40 @@ public class OnceOperationIT extends SpliceUnitTest {
 	private static Logger LOG = Logger.getLogger(OnceOperationIT.class);
 	protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
 	public static final String CLASS_NAME = OnceOperationIT.class.getSimpleName().toUpperCase();
-	public static final String TABLE_NAME = "A";
+	public static final String TABLE1_NAME = "A";
+    public static final String TABLE2_NAME = "territories";
+    public static final String TABLE3_NAME = "employee_territories";
 	protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);	
-	protected static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher(TABLE_NAME,CLASS_NAME,"(k int, l int)");
-	
-	@ClassRule 
+	protected static SpliceTableWatcher spliceTableWatcher1 = new SpliceTableWatcher(TABLE1_NAME,CLASS_NAME,"(k int, l int)");
+    protected static SpliceTableWatcher spliceTableWatcher2 = new SpliceTableWatcher(TABLE2_NAME,CLASS_NAME," (territoryid INTEGER NOT NULL,territory_Description VARCHAR(40) NOT NULL,regionid int NOT NULL, primary key (territoryid))");
+    protected static SpliceTableWatcher spliceTableWatcher3 = new SpliceTableWatcher(TABLE3_NAME,CLASS_NAME,"(employeeid INTEGER NOT NULL,territoryid INTEGER NOT NULL,primary key (employeeid) )");
+
+    @ClassRule
 	public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
 		.around(spliceSchemaWatcher)
-		.around(spliceTableWatcher)
+		.around(spliceTableWatcher1)
+        .around(spliceTableWatcher2)
+        .around(spliceTableWatcher3)
 		.around(new SpliceDataWatcher(){
 			@Override
 			protected void starting(Description description) {
 				try {
-				PreparedStatement statement = spliceClassWatcher.prepareStatement(String.format("insert into %s.%s values (?, ?)",CLASS_NAME,TABLE_NAME));
-				statement.setInt(1, 1);
-				statement.setInt(2, 2);
-				statement.execute();
-				statement.setInt(1, 3);
-				statement.setInt(2, 4);
-				statement.execute();
-				statement.setInt(1, 3);
-				statement.setInt(2, 4);
-				statement.execute();
+                    PreparedStatement statement = spliceClassWatcher.prepareStatement(String.format("insert into %s.%s values (?, ?)",CLASS_NAME,TABLE1_NAME));
+                    statement.setInt(1, 1);
+                    statement.setInt(2, 2);
+                    statement.execute();
+                    statement.setInt(1, 3);
+                    statement.setInt(2, 4);
+                    statement.execute();
+                    statement.setInt(1, 3);
+                    statement.setInt(2, 4);
+                    statement.execute();
+
+                    statement = spliceClassWatcher.prepareStatement(String.format("insert into %s.%s values (95014,'Cupertino',105)",CLASS_NAME,TABLE2_NAME));
+                    statement.execute();
+
+                    statement = spliceClassWatcher.prepareStatement(String.format("insert into %s.%s values (7725070,95014)",CLASS_NAME,TABLE3_NAME));
+                    statement.execute();
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
@@ -56,7 +68,7 @@ public class OnceOperationIT extends SpliceUnitTest {
 		
 	@Test
 	public void testValuesStatement() throws Exception {
-		ResultSet rs = methodWatcher.executeQuery(format("values (select k from %s where k = 1)",this.getTableReference(TABLE_NAME)));
+		ResultSet rs = methodWatcher.executeQuery(format("values (select k from %s where k = 1)",this.getTableReference(TABLE1_NAME)));
 		rs.next();
 		Assert.assertNotNull(rs.getInt(1));
 	}
@@ -64,7 +76,7 @@ public class OnceOperationIT extends SpliceUnitTest {
 	@Test(expected=SQLException.class)
 	public void testValuesStatementNonScalarError() throws Exception{
 		try {
-			ResultSet rs = methodWatcher.executeQuery(format("values (select k from %s where k = 3)",this.getTableReference(TABLE_NAME)));
+			ResultSet rs = methodWatcher.executeQuery(format("values (select k from %s where k = 3)",this.getTableReference(TABLE1_NAME)));
 			rs.next();
 		} catch (SQLException t) {
             t.printStackTrace();
@@ -72,4 +84,13 @@ public class OnceOperationIT extends SpliceUnitTest {
             throw t;
 		}
 	}
+
+    @Test
+    /* test case for DB-1208
+     */
+    public void testTableWithPrimaryKey() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery(format("select employeeid from %s a where territoryid = (select territoryid from %s b where a.territoryid = b.territoryid)", this.getTableReference(TABLE3_NAME), this.getTableReference(TABLE2_NAME)));
+        rs.next();
+        Assert.assertEquals(rs.getLong(1), 7725070);
+    }
 }
