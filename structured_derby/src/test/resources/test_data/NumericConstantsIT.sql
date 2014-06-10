d66644fd5704474a30611e0e37ab08d1751354d2
Keith Winkler <kwinkler@splicemachine.com>
2014-06-09 23:35:09 -0500

    DB-1362: Add tests for decimal(5,0) and decimal(11,2) columns.

--- structured_derby/src/test/resources/test_data/NumericConstantsIT.sql
+++ structured_derby/src/test/resources/test_data/NumericConstantsIT.sql
@@ -27,10 +27,22 @@ create table table_double (
   primary key(a)
 );
 
+create table table_decimal_5_0 (
+  a decimal not null,
+  primary key(a)
+);
+
+CREATE TABLE table_decimal_11_2 (
+  a DECIMAL(11, 2) NOT NULL,
+  PRIMARY KEY (a)
+);
+
+
 insert into table_smallint values (-32768),(-1), (0), (1), (32767);
 insert into table_integer values (-2147483648),(-1), (0), (1), (2147483647);
 insert into table_bigint values (-9223372036854775808),(-1), (0), (1), (9223372036854775807);
 
 insert into table_real values (-3.402E+38),(-1.0), (0.0), (1.0), (3.402E+38);
 insert into table_double values (-1.79769E+308),(-1.0), (0.0), (1.0), (1.79769E+308);
-
+insert into table_decimal_5_0 values (-99999.00),(-1.0), (0.0), (1.0), (99999.00);
+insert into table_decimal_11_2 values (-999999999.99),(-1.0), (0.0), (1.0), (999999999.99);
