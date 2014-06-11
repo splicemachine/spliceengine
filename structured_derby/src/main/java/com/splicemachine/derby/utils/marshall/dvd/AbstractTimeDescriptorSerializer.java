12e2decf5c3d4ce7009642fce09ca8b909c3cfe3
Perry Trolard <ptrolard@splicemachine.com>
2014-06-11 16:55:45 -0500

    Timestamp serializer: ensure Calendar not shared across threads
    Using a ThreadLocal here fixed one of the rascally, cluster-only
    RocketFuel Q3 bugs.

--- structured_derby/src/main/java/com/splicemachine/derby/utils/marshall/dvd/AbstractTimeDescriptorSerializer.java
+++ structured_derby/src/main/java/com/splicemachine/derby/utils/marshall/dvd/AbstractTimeDescriptorSerializer.java
@@ -9,31 +9,50 @@ import java.util.GregorianCalendar;
  * @author Scott Fines
  * Date: 4/2/14
  */
-public abstract class AbstractTimeDescriptorSerializer implements TimeValuedSerializer{
-		private Calendar calendar;
+public abstract class AbstractTimeDescriptorSerializer implements TimeValuedSerializer {
+    // Must isolate Calendar objects to a single thread here b/c Serializer objects are
+    // evidently shared; this fixes obscure cluster-only bug
+    private ThreadLocal<Calendar> calendar = new ThreadLocal<Calendar>() {
+        @Override
+        protected Calendar initialValue() {
+            return new GregorianCalendar();
+        }
+    };
 
-		@Override
-		public void setCalendar(Calendar calendar){
-				this.calendar = calendar;
-		}
+    @Override
+    public void setCalendar(Calendar calendar) {
+        this.calendar.set(calendar);
+    }
 
-		protected Calendar getCalendar(){
-				if(calendar==null)
-						calendar = new GregorianCalendar();
-				return calendar;
-		}
+    protected Calendar getCalendar() {
+        return calendar.get();
+    }
 
-		@Override public boolean isScalarType() { return true; }
-		@Override public boolean isFloatType() { return false; }
-		@Override public boolean isDoubleType() { return false; }
+    @Override
+    public boolean isScalarType() { return true; }
 
-		@Override public void close() { calendar = null; }
+    @Override
+    public boolean isFloatType() { return false; }
 
-		protected static abstract class Factory implements DescriptorSerializer.Factory{
-				@Override public boolean applies(DataValueDescriptor dvd) { return dvd!= null && applies(dvd.getTypeFormatId()); }
+    @Override
+    public boolean isDoubleType() { return false; }
 
-				@Override public boolean isScalar() { return true; }
-				@Override public boolean isFloat() { return false; }
-				@Override public boolean isDouble() { return false; }
-		}
+    @Override
+    public void close() { calendar = null; }
+
+    protected static abstract class Factory implements DescriptorSerializer.Factory {
+        @Override
+        public boolean applies(DataValueDescriptor dvd) {
+            return dvd != null && applies(dvd.getTypeFormatId());
+        }
+
+        @Override
+        public boolean isScalar() { return true; }
+
+        @Override
+        public boolean isFloat() { return false; }
+
+        @Override
+        public boolean isDouble() { return false; }
+    }
 }
