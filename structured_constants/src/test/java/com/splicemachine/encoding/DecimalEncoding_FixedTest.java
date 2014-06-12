9bc197df5594f1db28849e6d0d9039115f90e2aa
Keith Winkler <kwinkler@splicemachine.com>
2014-06-12 11:05:05 -0500

    DB-1401: Fix or delete @Ignore'd tests.

--- structured_constants/src/test/java/com/splicemachine/encoding/DecimalEncoding_FixedTest.java
+++ structured_constants/src/test/java/com/splicemachine/encoding/DecimalEncoding_FixedTest.java
@@ -8,6 +8,8 @@ import java.math.BigDecimal;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertTrue;
+
 /**
  * @author Scott Fines
  *         Created on: 6/18/13
@@ -19,47 +21,25 @@ public class DecimalEncoding_FixedTest {
         BigDecimal value = new BigDecimal("37661026");
         byte[] encoding = DecimalEncoding.toBytes(value,false);
         BigDecimal decoded = DecimalEncoding.toBigDecimal(encoding,false);
-        Assert.assertTrue("Incorrect decoding. Expected <"+value+">, Actual <"+ value+">",value.compareTo(decoded)==0);
+        assertTrue("Incorrect decoding. Expected <" + value + ">, Actual <" + value + ">", value.compareTo(decoded) == 0);
 
     }
 
     @Test
     public void testCanEncodedOnlyDecimalDescending() {
-        /*
-         * Regression test to ensure this specific number decodes properly.
-         */
+        /* Regression test to ensure this specific number decodes properly.  */
         BigDecimal value = new BigDecimal("0.02308185311033106312805784909869544208049774169921875");
         byte[] encoding = DecimalEncoding.toBytes(value, true);
         BigDecimal decoded = DecimalEncoding.toBigDecimal(encoding,true);
-        Assert.assertTrue("Incorrect decoding. Expected <" + value + ">, Actual <" + value + ">", value.compareTo(decoded) == 0);
+        assertTrue("Incorrect decoding. Expected <" + value + ">, Actual <" + value + ">", value.compareTo(decoded) == 0);
     }
 
     @Test
-    @Ignore("not actually a test")
-    public void testEncodeAsDoubleDecodeAsFloat() throws Exception {
-        double val = 1.0;
-
-        byte[] encoded = DecimalEncoding.toBytes(val,false);
-
-        float decodedFloat = DecimalEncoding.toFloat(encoded,false);
-        double decodedDouble = DecimalEncoding.toDouble(encoded,false);
-        System.out.printf("%f,%f%n",decodedFloat,decodedDouble);
-
-        float v2= 1.0f;
-
-        byte[] encoded2 = DecimalEncoding.toBytes(v2, false);
-//        System.out.println(Arrays.toString(encoded2));
-//        System.out.println(Arrays.toString(encoded));
-    }
-
-    @Test
-    @Ignore("not actually a test")
     public void testEncodeBigDecimalZeros() throws Exception {
-        BigDecimal decimal = new BigDecimal("00000000000000.012340004");
-
-        byte[] data = Encoding.encode(decimal);
-//        System.out.println(Arrays.toString(data));
-
+        BigDecimal bigDecimalIn = new BigDecimal("00000000000000.012340004");
+        byte[] data = Encoding.encode(bigDecimalIn);
+        BigDecimal bigDecimalOut = Encoding.decodeBigDecimal(data);
+        assertTrue(bigDecimalIn.compareTo(bigDecimalOut) == 0);
     }
 
     @Test
