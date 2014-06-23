bccb3b07df1244fb50862f9e7126195aabab1cd6
Walt Koetke <wkoetke@splicemachine.com>
2014-06-23 18:58:52 -0500

    Timestamp generation cleanup, such as replacing hardcoded values with configuration props and removing dead code.

--- structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClient.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClient.java
@@ -1,7 +1,16 @@
 package com.splicemachine.si.impl.timestamp;
 
+/**
+ * Client interface for the generation of unique monotonically increasing timestamps,
+ * as needed by the transaction system.
+ */
 public abstract class TimestampClient extends TimestampBaseHandler {
 
+	/**
+	 * Produces the next timestamp.
+	 * 
+	 * @return the next timestamp as a long
+	 */
 	public abstract long getNextTimestamp();
 	
 }
--- structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClientFactory.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClientFactory.java
@@ -1,11 +1,9 @@
 package com.splicemachine.si.impl.timestamp;
 
-import com.splicemachine.constants.SpliceConstants;
-
 public class TimestampClientFactory {
 
 	public static TimestampClient createNewInstance() {
-		return new TimestampClientMapImpl(SpliceConstants.timestampServerBindPort);
+		return new TimestampClientMapImpl();
 	}
 	
 }
