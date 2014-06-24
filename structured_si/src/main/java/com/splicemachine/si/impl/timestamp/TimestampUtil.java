f2c21dbf1c2ec57d2af7a9bf75e32d062ac0033e
Walt Koetke <wkoetke@splicemachine.com>
2014-06-24 18:13:04 -0500

    Reduce timestamp client caller id from 4 bytes to 2 bytes to send just a little less data over the wire. Also changed default reserved block size to 1024 from 10000 and did further code cleanup.

--- structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampUtil.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampUtil.java
@@ -86,6 +86,19 @@ public class TimestampUtil {
 		SpliceLogUtils.error(logger, message + " : " + obj, t);		
 	}
 	
+	public static void doClientErrorThrow(Logger logger, String message, Throwable t, Object obj) {
+		if (message == null) message = "";
+		if (obj == null) obj = "";
+		StringBuffer sb = new StringBuffer();
+		String fullMsg = sb.append(message).append(" : ").append(obj).toString();
+		if (t != null) {
+			SpliceLogUtils.logAndThrowRuntime(logger, fullMsg, t);
+		} else {
+			SpliceLogUtils.error(logger, fullMsg);
+			throw new RuntimeException(fullMsg);
+		}
+	}
+	
 	public static void doServerError(Logger logger, String message) {
 		SpliceLogUtils.error(logger, message);		
 	}
