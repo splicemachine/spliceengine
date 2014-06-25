603bdfa512b3d5085b4b246d8332358410cfe3f1
Walt Koetke <wkoetke@splicemachine.com>
2014-06-25 10:39:24 -0500

    Consolidate two classes into one. There is no longer the need for a base abstraction of the client class.

--- structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClientFactory.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/timestamp/TimestampClientFactory.java
@@ -3,7 +3,7 @@ package com.splicemachine.si.impl.timestamp;
 public class TimestampClientFactory {
 
 	public static TimestampClient createNewInstance() {
-		return new TimestampClientMapImpl();
+		return new TimestampClient();
 	}
 	
 }
