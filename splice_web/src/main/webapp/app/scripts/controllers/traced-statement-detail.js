4560493558da76c89a19a6ddbb7b2a04394a74ad
David Winters <dwinters@splicemachine.com>
2014-06-29 22:47:50 -0700

    DB-933 A poor man's viz of explain trace.  Simple indented list of operations.

--- splice_web/src/main/webapp/app/scripts/controllers/traced-statement-detail.js
+++ splice_web/src/main/webapp/app/scripts/controllers/traced-statement-detail.js
@@ -1,13 +1,23 @@
 'use strict';
 
 angular.module('spliceWebApp')
-  .controller('TracedStatementDetailCtrl', ['$scope', '$routeParams', 'TracedStatementDetailService', function ($scope, $routeParams, TracedStatementDetailService) {
-	  var str = TracedStatementDetailService.get({statementId: $routeParams.statementId});
-	  $scope.tracedStatement = str;
-	  $scope.tracedStatementString = JSON.stringify(str, undefined, 2);
-	  $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  }]);
+.controller('TracedStatementDetailCtrl', ['$scope', '$routeParams', 'TracedStatementDetailService', function ($scope, $routeParams, TracedStatementDetailService) {
+	var str = TracedStatementDetailService.get({statementId: $routeParams.statementId});
+//	$scope.tracedStatement = str;
+	$scope.tree = [str];
+//	$scope.tracedStatementString = JSON.stringify($scope.tracedStatement, null, 2);
+	$scope.awesomeThings = [
+	                        'HTML5 Boilerplate',
+	                        'AngularJS',
+	                        'Karma'
+	                        ];
+//	$scope.delete = function(data) {
+//		data.children = [];
+//	};
+//	$scope.add = function(data) {
+//		var post = data.children.length + 1;
+//		var newName = data.name + '-' + post;
+//		data.children.push({name: newName,children: []});
+//	};
+//	$scope.tree = [{name: "Node", children: []}];
+}]);
