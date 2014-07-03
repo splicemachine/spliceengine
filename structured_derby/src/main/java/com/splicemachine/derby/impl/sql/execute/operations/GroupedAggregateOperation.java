b031cfa2315b8a490006b3225b617b136255bb89
John Leach <jleach@inciteretail.com>
2014-07-02 19:58:50 -0500

    DB-1514 Fix overhead on Grouped Aggregate statistics gathering

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/GroupedAggregateOperation.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/GroupedAggregateOperation.java
@@ -318,15 +318,14 @@ public class GroupedAggregateOperation extends GenericAggregateOperation {
 										groupingKeys,groupingKeyOrder,nonGroupedUniqueColumns,serializers);
 						aggregator.open();
 						timer = spliceRuntimeContext.newTimer();
+						timer.startTiming();
 				}
-
-				timer.startTiming();
 				GroupedRow row = aggregator.next(spliceRuntimeContext);
 				if(row==null){
 						currentKey=null;
 						clearCurrentRow();
 						aggregator.close();
-						timer.tick(0);
+						timer.tick(aggregator.getRowsRead());
 						stopExecutionTime = System.currentTimeMillis();
 						return null;
 				}
@@ -334,7 +333,6 @@ public class GroupedAggregateOperation extends GenericAggregateOperation {
 				isCurrentDistinct = row.isDistinct();
 				ExecRow execRow = row.getRow();
 				setCurrentRow(execRow);
-				timer.tick(1);
 				return execRow;
 		}
 
