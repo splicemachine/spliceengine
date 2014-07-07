50d58c7cf537908a95b5ad3e19a1aeb7cff2598c
David Winters <dwinters@splicemachine.com>
2014-07-07 15:17:54 -0700

    DB-933 Integrated D3 SVG library into Angular for drawing operation trees and generic bar charts.

--- splice_web/src/main/webapp/app/scripts/app.js
+++ splice_web/src/main/webapp/app/scripts/app.js
@@ -1,28 +1,44 @@
 'use strict';
 
 angular
-  .module('spliceWebApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'spliceWebServices'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/traced-statements', {
-        templateUrl: 'views/traced-statement-list.html',
-        controller: 'TracedStatementListCtrl'
-      })
-      .when('/traced-statements/:statementId', {
-        templateUrl: 'views/traced-statement-detail.html',
-        controller: 'TracedStatementDetailCtrl'
-      })
-      .when('/yeoman', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/traced-statements'
-      });
-  });
+	.module('spliceWebApp', [
+		'ngCookies',
+		'ngResource',
+		'ngSanitize',
+		'ngRoute',
+		'spliceWebServices',
+		'spliceWebControllers',
+		'spliceWebDirectives'
+	])
+	.config(function ($routeProvider) {  // Configure routes
+		$routeProvider
+			.when('/traced-statements', {
+				templateUrl: 'views/traced-statement-list.html',
+				controller: 'TracedStatementListCtrl'
+			})
+			.when('/traced-statements/:statementId', {
+				templateUrl: 'views/traced-statement-detail.html',
+				controller: 'TracedStatementDetailCtrl',
+				// Do not render until the traced statement has been returned from the Splice REST service.
+				// In the future, we can start rendering the tree and refresh it when the REST service returns.
+				resolve: {
+					tracedStatement: ['TracedStatementDetailService', '$route', function(TracedStatementDetailService, $route) {
+						return TracedStatementDetailService.get({statementId: $route.current.params.statementId}).$promise;
+					}]
+				}
+			})
+			.when('/yeoman', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.otherwise({
+				redirectTo: '/traced-statements'
+			});
+	});
+
+// Setup dependency injection of D3 to keep from loading the library into the global scope.
+// TODO: The dependency injection of d3.js is not working and the benefit is minimal at best.
+//angular.module('d3', []);
+angular.module('spliceWebControllers', []);
+//angular.module('spliceWebDirectives', ['d3']);
+angular.module('spliceWebDirectives', []);
--- splice_web/src/main/webapp/app/scripts/services.js
+++ splice_web/src/main/webapp/app/scripts/services.js
@@ -3,17 +3,17 @@
 var spliceWebServices = angular.module('spliceWebServices', ['ngResource']);
 
 spliceWebServices.factory('TracedStatementListService',
-		['$resource',
-		 function($resource){
-			return $resource('http://localhost:8080/splice_web/webresources/myresource/sql2js?query=select+*+from+XPLAIN.SYSXPLAIN_STATEMENTHISTORY', {}, {
+	['$resource',
+		function($resource){
+			return $resource('/splice_web/webresources/myresource/sql2js?query=select+*+from+XPLAIN.SYSXPLAIN_STATEMENTHISTORY', {}, {
 				query: {method:'GET', isArray:true}
 			});
 		}]);
 
 spliceWebServices.factory('TracedStatementDetailService',
-		['$resource',
-		 function($resource){
-			return $resource('http://localhost:8080/splice_web/webresources/myresource/tracedStatements/:statementId', {}, {
+	['$resource',
+		function($resource){
+			return $resource('/splice_web/webresources/myresource/tracedStatements/:statementId', {}, {
 				get: {method:'GET'}
 			});
 		}]);
