a259b5316dc9544002e14023051bcbadb0cafadc
Scott Fines <scottfines@gmail.com>
2014-07-21 09:35:45 -0500

    Adding latency measures to ConcurrentQueryRunner so that we can see individual method call latency
    Conflicts:
    	structured_derby/src/main/java/com/splicemachine/test/ConcurrentQueryRunner.java

---
+++ structured_constants/src/main/java/com/splicemachine/stats/LatencyView.java
@@ -0,0 +1,15 @@
+package com.splicemachine.stats;
+
+/**
+ * @author Scott Fines
+ *         Date: 7/21/14
+ */
+public interface LatencyView {
+
+    public double getOverallLatency();
+    public long getP50Latency();
+    public long getP75Latency();
+    public long getP90Latency();
+    public long getP95Latency();
+    public long getP99Latency();
+}
