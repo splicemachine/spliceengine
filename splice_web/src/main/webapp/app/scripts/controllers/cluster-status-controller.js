d194056a5d69d4892c6ff56e38e7f88470c214fc
David Winters <dwinters@splicemachine.com>
2014-07-30 10:02:51 -0700

    DB-1569 Added clusterStatusController to update status of the green/red icon in header.

--- splice_web/src/main/webapp/app/scripts/controllers/cluster-status-controller.js
+++ splice_web/src/main/webapp/app/scripts/controllers/cluster-status-controller.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('spliceAdminControllers')
-	.controller('mainController',
+	.controller('clusterStatusController',
 	['$scope', 'primaryServerStatusCheckService', 'serversStatusCheckService', function ($scope, primaryServerStatusCheckService, serversStatusCheckService) {
+		// TODO: Merge these two "check" services into a single service to streamline and simplify.
 		// Check if the primary Splice server is up.
 		// TODO: Remove the single point of failure where the admin is dependent on a single Splice server.
 		// The admin server (or client) could cache JDBC URLs to all Splice servers.
