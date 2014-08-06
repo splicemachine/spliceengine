7e3711c164504de7e7a2a8db118ab20c98a6521f
Yanan Jian <yanan@Yanans-MacBook-Pro-2.local>
2014-08-06 09:06:08 -0700

    merged repo SpliceMR into mapreduce branch, delete unused files

--- test.sql
+++
@@ -1,1 +0,0 @@
-select s.schemaname,t.tablename,c.conglomeratenumber from sys.sysschemas s, sys.systables t, sys.sysconglomerates c where s.schemaid = t.schemaid and t.tableid = c.tableid and s.schemaname = 'APP' and t.tablename = 'USERTEST'
