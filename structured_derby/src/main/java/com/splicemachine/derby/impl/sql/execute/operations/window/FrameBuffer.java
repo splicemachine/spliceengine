f2064938c3845c146fd2be2ea8ee8a7c8290b606
Jun Yuan <jyuan@splicemachine.com>
2014-08-05 17:18:52 -0700

    window funciton: remove unnecessary code

--- structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/FrameBuffer.java
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/FrameBuffer.java
@@ -73,17 +73,12 @@ public class FrameBuffer {
 
         // initializes frame buffer
         WindowFrame.FrameMode frameMode = windowFrame.getFrameMode();
+        loadFrame(source);
 
-        if (frameMode == WindowFrame.FrameMode.RANGE) {
-            loadRangeFrame(source);
-        }
-        else if (frameMode == WindowFrame.FrameMode.ROWS) {
-            loadRowFrame(source);
-        }
 
     }
 
-    private void loadRowFrame(PartitionAwarePushBackIterator<ExecRow> source) throws IOException, StandardException {
+    private void loadFrame(PartitionAwarePushBackIterator<ExecRow> source) throws IOException, StandardException {
         long frameStart = windowFrame.getFrameStart().getValue();
         long frameEnd = windowFrame.getFrameEnd().getValue();
         start = end = 0;
@@ -117,64 +112,6 @@ public class FrameBuffer {
         end = rows.size() -1;
     }
 
-    private void loadRangeFrame(PartitionAwarePushBackIterator<ExecRow> source) throws IOException, StandardException {
-
-        long frameStart = windowFrame.getFrameStart().getValue();
-        Frame frameStartType = windowFrame.getFrameStart().getFrame();
-        long frameEnd = windowFrame.getFrameEnd().getValue();
-        Frame frameEndType = windowFrame.getFrameEnd().getFrame();
-        int[] sortColumn = windowContext.getSortColumns();
-
-        //TODO: make sure there is only one column in the sort column
-        // load first row into buffer
-        ExecRow row = source.next(runtimeContext);
-        DataValueDescriptor dvd = row.getColumn(sortColumn[0]+1);
-        partition = source.getPartition();
-        long value = dvd.getLong();
-        // rows.add(row.getClone());
-        // load enough rows into the buffer
-        boolean done = false;
-        start = 0;
-        do {
-            ExecRow clonedRow = null;
-            if (row != null && Bytes.compareTo(partition, source.getPartition()) == 0) {
-                clonedRow = row.getClone();
-                dvd = row.getColumn(sortColumn[0]+1);
-                long v = dvd.getLong();
-
-                if (frameStart != Long.MIN_VALUE && value + frameStart > v) {
-                    // We haven't seen the first row in the window yet, keep scanning
-                    start++;
-                }
-                else if ((frameStart == Long.MIN_VALUE || frameStartType == Frame.CURRENT_ROW || value + frameStart >=v) &&
-                    (frameEnd == Long.MAX_VALUE || frameEndType == Frame.CURRENT_ROW || value + frameEnd >= v)) {
-                    add(clonedRow);
-                }
-                else if (frameEnd != Long.MAX_VALUE && value + frameEnd < v) {
-                    // Stop scanner once a row is out of the range
-                    done = true;
-                }
-            }
-            else {
-                // stop once a row from next partition is seen;
-                done = true;
-            }
-
-            if (!done) {
-                rows.add(clonedRow);
-                if (frameEndType == Frame.CURRENT_ROW) {
-                    break;
-                }
-                row = source.next(runtimeContext);
-            }
-            else {
-                source.pushBack(row);
-            }
-        } while (!done);
-        current = 0;
-        end = rows.size()-1;
-    }
-
     public ExecRow next() throws IOException, StandardException{
         if (current >= rows.size()) {
             return null;
@@ -206,120 +143,6 @@ public class FrameBuffer {
     }
 
     public void move() throws StandardException, IOException{
-        FrameMode frameMode = windowFrame.getFrameMode();
-        if (frameMode == FrameMode.ROWS) {
-            moveRowFrame();
-        }
-        else if (frameMode == frameMode.RANGE) {
-            moveRangeFrame();
-        }
-    }
-
-    public void moveRangeFrame() throws StandardException, IOException {
-        long frameStart = windowFrame.getFrameStart().getValue();
-        long frameEnd = windowFrame.getFrameEnd().getValue();
-        current++;
-        if (rows.size() == current) {
-            // scan next row into the buffer
-            ExecRow row = source.next(runtimeContext);
-            if (row != null && Bytes.compareTo(partition, source.getPartition()) == 0) {
-                rows.add(row.getClone());
-            }
-            else {
-                // We are either reach the end of a partition or
-                // the end of a region
-                source.pushBack(row);
-                return;
-            }
-
-        }
-        ExecRow nextRow = rows.get(current);
-        int[] sortColumns = windowContext.getSortColumns();
-        DataValueDescriptor dvd = nextRow.getColumn(sortColumns[0]+1);
-        long value = dvd.getLong();
-
-        // remove rows from window frame if they are out of range
-        Frame f = windowFrame.getFrameStart().getFrame();
-        if (f == Frame.CURRENT_ROW) {
-            remove();
-            start++;
-        }
-        else if (frameStart != Long.MIN_VALUE) {
-            ExecRow startRow = rows.get(start);
-            DataValueDescriptor d = startRow.getColumn(sortColumns[0]+1);
-            long v = d.getLong();
-            while (value + frameStart <= v && start < rows.size()) {
-                remove();
-                start++;
-            }
-        }
-
-        // remove rows from buffer if they are no longer needed
-        if (frameStart == Long.MIN_VALUE ) {
-            // start frame is unbounded, remove the first row in the buffer
-            rows.remove(0);
-            start--;
-            current--;
-            end--;
-        } else {
-
-            int minIndex = (current < start) ? start : current;
-            for (int i = 0; i < minIndex; ++i) {
-                rows.remove(0);
-                start--;
-                current--;
-                end--;
-            }
-        }
-
-        // Add rows to window frame
-        if (end != Long.MAX_VALUE) {
-            boolean done = false;
-            do {
-                nextRow = getNext();
-                if (nextRow != null) {
-                    ExecRow clonedRow = nextRow.getClone();
-                    DataValueDescriptor d = nextRow.getColumn(sortColumns[0] + 1);
-                    long v = d.getLong();
-                    if (value + frameEnd >= v || windowFrame.getFrameEnd().getFrame() == Frame.CURRENT_ROW) {
-                        // If the row is in the range, add it to window
-                        end++;
-                        add(clonedRow);
-                        if (windowFrame.getFrameEnd().getFrame() == Frame.CURRENT_ROW) {
-                            done = true;
-                        }
-                    }
-                    else {
-                        // the row is out of range, push it back
-                        done = true;
-                        source.pushBack(nextRow);
-                    }
-                }
-                else {
-                    // consumed all rows
-                    done = true;
-                }
-            } while (!done);
-        }
-    }
-
-    private ExecRow getNext() throws StandardException, IOException{
-        ExecRow row = null;
-        if (end + 1 < rows.size()) {
-            return rows.get(end+1);
-        }
-        else {
-            // read a row from scanner
-            row = source.next(runtimeContext);
-            if (row == null || Bytes.compareTo(partition, source.getPartition())!=0) {
-                source.pushBack(row);
-                row = null;
-            }
-        }
-        return row;
-    }
-
-    public void moveRowFrame() throws StandardException, IOException{
 
         long frameStart = windowFrame.getFrameStart().getValue();
         long frameEnd = windowFrame.getFrameEnd().getValue();
