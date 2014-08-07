ac0e6e921017d724b3fce9cd7eab8506cb78bd56
Yanan Jian <yanan@Yanans-MacBook-Pro-2.local>
2014-08-07 15:35:23 -0700

    micro changes in SpliceJob

--- structured_derby/src/main/java/com/splicemachine/mrio/api/SpliceJob.java
+++ structured_derby/src/main/java/com/splicemachine/mrio/api/SpliceJob.java
@@ -7,32 +7,35 @@ import java.sql.SQLException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.JobStatus;
+import org.apache.hadoop.mapreduce.Job.JobState;
 
 public class SpliceJob extends Job{
 
 	 private static SQLUtil sqlUtil = SQLUtil.getInstance();
 	 private static Connection conn = null;
-	public SpliceJob() throws IOException {
+	 public SpliceJob() throws IOException {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
-	public SpliceJob(Configuration conf) throws IOException {
+	 public SpliceJob(Configuration conf) throws IOException {
 	    super(conf, null);
 	  }
 
-	  public SpliceJob(Configuration conf, String jobName) throws IOException {
+	 public SpliceJob(Configuration conf, String jobName) throws IOException {
 	    super(conf,jobName);
 	  }
 	  
-	  public void setJobName(String name) throws IllegalStateException {
-		    super.setJobName(name);
-		    
-		    if (conn == null)
+	 @Override
+	 public void submit() throws IOException, InterruptedException, ClassNotFoundException 
+	 {	
+		 if (conn == null)
 				try {
-					conn = sqlUtil.createConn();
+					conn = sqlUtil.createConn(); // Parent transaction starts here
 					System.out.println("created conn");
 					sqlUtil.disableAutoCommit(conn);
+					// TODO:this transaction ID could be wrong (larger than the one we want)
 					String parentTxsID = sqlUtil.getTransactionID(conn);
 					super.getConfiguration().set(SpliceConstants.SPLICE_TRANSACTION_ID, parentTxsID);
 					System.out.println("SpliceJob, created parent TXSID:"+parentTxsID);
@@ -40,7 +43,7 @@ public class SpliceJob extends Job{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-		    
+		 super.submit();
 	 }
 	  
 	  @Override
