05942f1765afda96bc1f9b3b88b6e61f29420c27
Keith Winkler <kwinkler@splicemachine.com>
2014-08-12 15:42:41 -0500

    MoreExecutors: add factory method for single thread scheduled executor

--- src/main/java/com/splicemachine/concurrent/MoreExecutors.java
+++ src/main/java/com/splicemachine/concurrent/MoreExecutors.java
@@ -5,25 +5,39 @@ import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.concurrent.*;
 
 /**
- * @author Scott Fines
- *         Date: 8/5/14
+ * Convenient executor factory methods.
  */
 public class MoreExecutors {
 
-    private MoreExecutors(){}
+    private MoreExecutors() {
+    }
 
+    /**
+     * Single thread ExecutorService with named threads.
+     */
     public static ExecutorService namedSingleThreadExecutor(String nameFormat) {
         ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
         return Executors.newSingleThreadExecutor(factory);
     }
 
-    public static ThreadPoolExecutor namedThreadPool(int coreWorkers,int maxWorkers,
+    /**
+     * Single thread ScheduledExecutorService with named threads.
+     */
+    public static ScheduledExecutorService namedSingleThreadScheduledExecutor(String nameFormat) {
+        ThreadFactory factory = new ThreadFactoryBuilder().setNameFormat(nameFormat).build();
+        return Executors.newScheduledThreadPool(1, factory);
+    }
+
+    /**
+     * Factory for general purpose ThreadPoolExecutor.
+     */
+    public static ThreadPoolExecutor namedThreadPool(int coreWorkers, int maxWorkers,
                                                      String nameFormat,
                                                      long keepAliveSeconds,
-                                                     boolean daemon){
+                                                     boolean daemon) {
         ThreadFactory factory = new ThreadFactoryBuilder().setDaemon(daemon).setNameFormat(nameFormat).build();
-        return new ThreadPoolExecutor(coreWorkers,maxWorkers,keepAliveSeconds,
-                TimeUnit.SECONDS,new LinkedBlockingQueue<Runnable>(),factory);
+        return new ThreadPoolExecutor(coreWorkers, maxWorkers, keepAliveSeconds,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), factory);
     }
 
 }
--- src/test/java/com/splicemachine/concurrent/MoreExecutorsTest.java
+++ src/test/java/com/splicemachine/concurrent/MoreExecutorsTest.java
@@ -1,30 +1,66 @@
 package com.splicemachine.concurrent;
 
-import com.splicemachine.concurrent.MoreExecutors;
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.Random;
+import java.util.concurrent.*;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class MoreExecutorsTest {
 
     @Test
     public void newSingleThreadExecutor_usesThreadWithExpectedName() throws Exception {
-
         ExecutorService executorService = MoreExecutors.namedSingleThreadExecutor("testName-%d");
+        Future<String> threadName = executorService.submit(new GetThreadNameCallable());
+        Future<Boolean> isDaemon = executorService.submit(new IsDaemonCallable());
 
-        Future<String> threadName = executorService.submit(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                return Thread.currentThread().getName();
-            }
-        });
+        assertTrue(threadName.get().matches("testName-\\d"));
+        assertEquals(false, isDaemon.get());
+        executorService.shutdown();
+    }
 
+    @Test
+    public void namedSingleThreadScheduledExecutor() throws Exception {
+        ScheduledExecutorService executorService = MoreExecutors.namedSingleThreadScheduledExecutor("testName-%d");
+        Future<String> threadName = executorService.submit(new GetThreadNameCallable());
+        Future<Boolean> isDaemon = executorService.submit(new IsDaemonCallable());
+
+        assertTrue(threadName.get().matches("testName-\\d"));
+        assertEquals(false, isDaemon.get());
+        executorService.shutdown();
+    }
+
+    @Test
+    public void namedThreadPool() throws Exception {
+        final int CORE_POOL_SIZE = 1 + new Random().nextInt(9);
+        final int MAX_POOL_SIZE = CORE_POOL_SIZE + new Random().nextInt(9);
+        final int KEEP_ALIVE_SEC = new Random().nextInt(100);
+        final boolean IS_DAEMON = new Random().nextBoolean();
+        ThreadPoolExecutor executorService = MoreExecutors.namedThreadPool(CORE_POOL_SIZE, MAX_POOL_SIZE, "testName-%d", KEEP_ALIVE_SEC, IS_DAEMON);
+        Future<String> threadName = executorService.submit(new GetThreadNameCallable());
+        Future<Boolean> isDaemon = executorService.submit(new IsDaemonCallable());
+
+        assertEquals(CORE_POOL_SIZE, executorService.getCorePoolSize());
+        assertEquals(MAX_POOL_SIZE, executorService.getMaximumPoolSize());
+        assertEquals(KEEP_ALIVE_SEC, executorService.getKeepAliveTime(TimeUnit.SECONDS));
+        assertEquals(IS_DAEMON, isDaemon.get());
         assertTrue(threadName.get().matches("testName-\\d"));
         executorService.shutdown();
     }
 
+    private static class GetThreadNameCallable implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            return Thread.currentThread().getName();
+        }
+    }
+
+    private static class IsDaemonCallable implements Callable<Boolean> {
+        @Override
+        public Boolean call() throws Exception {
+            return Thread.currentThread().isDaemon();
+        }
+    }
 }
