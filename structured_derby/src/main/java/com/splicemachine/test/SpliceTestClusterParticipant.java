53ec553c5dd18e060c829afaba646d05d793fe1f
Keith Winkler <kwinkler@splicemachine.com>
2014-08-12 09:43:59 -0500

    DB-1600: make SpliceTestClusterParticipant take memberNumber as a parameter.

--- structured_derby/src/main/java/com/splicemachine/test/SpliceTestClusterParticipant.java
+++ structured_derby/src/main/java/com/splicemachine/test/SpliceTestClusterParticipant.java
@@ -1,53 +1,55 @@
 package com.splicemachine.test;
 
+import com.splicemachine.constants.SpliceConstants;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.MiniHBaseCluster;
+import org.apache.log4j.Logger;
 
 /**
  * Add an additional member to the cluster started with SpliceTestPlatform.
- *
+ * <p/>
  * Although MiniHBaseCluster can be used to create a multi region-server cluster all within a single JVM our system
  * currently requires each member to be in a separate JVM (because of static-state/singletons, SpliceDriver, for instance).
- *
+ * <p/>
  * Running: mvn exec:exec -PspliceClusterMember
  */
 public class SpliceTestClusterParticipant {
 
-    private final int regionServerPort;
-    private final int regionServerInfoPort;
+    private static final Logger LOG = Logger.getLogger(SpliceTestClusterParticipant.class);
+
+    private static final int REGION_SERVER_PORT = 60020;
+    private static final int REGION_SERVER_WEB_PORT = 60030;
+
     private final String hbaseTargetDirectory;
-    private final int derbyPort;
+    private final int memberNumber;
 
     /**
      * MAIN:
      * <p/>
      * arg-1: hbase dir
-     * arg-2: region server port
-     * arg-3: region server info web-ui port
-     * arg-4: derby port
+     * arg-2: cluster member number
      */
     public static void main(String[] args) throws Exception {
         SpliceTestClusterParticipant spliceTestPlatform;
-        if (args.length == 4) {
-            spliceTestPlatform = new SpliceTestClusterParticipant(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]), Integer.parseInt(args[3]));
+        if (args.length == 2) {
+            spliceTestPlatform = new SpliceTestClusterParticipant(args[0], Integer.parseInt(args[1]));
             spliceTestPlatform.start();
         } else {
-            System.out.println("" +
-                    "Splice Test Platform requires three arguments corresponding to the hbase target directory, " +
-                    "region server port and region server info port.  The first region server is brought up via the " +
-                    "test platform on 60010 and 60020.");
+            System.out.println("usage: SpliceTestClusterParticipant [hbase dir] [member number]");
             System.exit(1);
         }
     }
 
-    public SpliceTestClusterParticipant(String hbaseTargetDirectory, int regionServerPort, int regionServerInfoPort, int derbyPort) {
+    public SpliceTestClusterParticipant(String hbaseTargetDirectory, int memberNumber) {
         this.hbaseTargetDirectory = hbaseTargetDirectory;
-        this.regionServerPort = regionServerPort;
-        this.regionServerInfoPort = regionServerInfoPort;
-        this.derbyPort = derbyPort;
+        this.memberNumber = memberNumber;
     }
 
     private void start() throws Exception {
+        int regionServerPort = REGION_SERVER_PORT + memberNumber;
+        int regionServerInfoPort = REGION_SERVER_WEB_PORT + memberNumber;
+        int derbyPort = SpliceConstants.DEFAULT_DERBY_BIND_PORT + memberNumber;
+
         Configuration config = SpliceTestPlatformConfig.create(
                 hbaseTargetDirectory,
                 0,
