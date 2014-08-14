0fc2b44bb5a42c7b2ef5e3e29b9657ec7e738e9c
David Winters <dwinters@splicemachine.com>
2014-08-14 14:42:51 -0700

    DB-1634 Added end-to-end (e2e or IT) tests for admin console using Protractor, which is an Angular wrapper for Selenium.

---
+++ splice_web/protractor.conf.js
@@ -0,0 +1,21 @@
+exports.config = {
+  allScriptsTimeout: 11000,
+
+  specs: [
+    'src/test/webapp/test/e2e/**/*-scenarios.js'
+  ],
+
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  chromeOnly: true,
+
+  baseUrl: 'http://localhost:8080/',
+
+  framework: 'jasmine',
+
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
