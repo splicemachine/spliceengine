40dcc0d0e08108e566de664045d1470107daefe3
Keith Winkler <kwinkler@splicemachine.com>
2014-09-09 10:30:21 -0500

    Added Threads class.

---
+++ src/main/java/com/splicemachine/concurrent/Threads.java
@@ -0,0 +1,26 @@
+package com.splicemachine.concurrent;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Thread utilities.
+ */
+public class Threads {
+
+    private Threads() {
+    }
+
+    /**
+     * Unchecked version of sleep.
+     *
+     * Use when you calling thread being interrupted is unexpected-- thus unchecked exception is appropriate.
+     */
+    public static void sleep(long duration, TimeUnit timeUnit) {
+        try {
+            Thread.sleep(timeUnit.toMillis(duration));
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+}
