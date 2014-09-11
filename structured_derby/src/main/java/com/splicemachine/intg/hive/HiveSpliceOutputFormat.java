c3df203e2fdd12f758b594a801d0a752492d9b50
Walt Koetke <wkoetke@splicemachine.com>
2014-09-11 15:32:37 -0700

    Moving hive components into different package.

---
+++ structured_derby/src/main/java/com/splicemachine/intg/hive/HiveSpliceOutputFormat.java
@@ -0,0 +1,398 @@
+package com.splicemachine.intg.hive;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.ParseException;
+import org.apache.derby.iapi.error.StandardException;
+import org.apache.derby.iapi.sql.ResultColumnDescriptor;
+import org.apache.derby.iapi.sql.ResultDescription;
+import org.apache.derby.iapi.sql.execute.ExecRow;
+import org.apache.derby.iapi.types.DataTypeDescriptor;
+import org.apache.derby.iapi.types.DataValueDescriptor;
+import org.apache.derby.iapi.types.SQLBlob;
+import org.apache.derby.iapi.types.SQLBoolean;
+import org.apache.derby.iapi.types.SQLDouble;
+import org.apache.derby.iapi.types.SQLInteger;
+import org.apache.derby.iapi.types.SQLLongint;
+import org.apache.derby.iapi.types.SQLSmallint;
+import org.apache.derby.iapi.types.SQLVarchar;
+import org.apache.derby.impl.sql.execute.ValueRow;
+import org.apache.hadoop.conf.Configurable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat;
+import org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles;
+import org.apache.hadoop.hbase.mapreduce.TableOutputCommitter;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hive.conf.HiveConf;
+import org.apache.hadoop.hive.ql.io.HiveOutputFormat;
+import org.apache.hadoop.hive.shims.ShimLoader;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.OutputCommitter;
+import org.apache.hadoop.mapreduce.OutputFormat;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Progressable;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.splicemachine.constants.bytes.BytesUtil;
+import com.splicemachine.derby.hbase.SpliceDriver;
+import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
+import com.splicemachine.derby.impl.load.ImportContext;
+import com.splicemachine.derby.impl.load.ImportUtils;
+import com.splicemachine.derby.utils.marshall.BareKeyHash;
+import com.splicemachine.derby.utils.marshall.DataHash;
+import com.splicemachine.derby.utils.marshall.EntryDataHash;
+import com.splicemachine.derby.utils.marshall.HashPrefix;
+import com.splicemachine.derby.utils.marshall.KeyEncoder;
+import com.splicemachine.derby.utils.marshall.KeyPostfix;
+import com.splicemachine.derby.utils.marshall.NoOpDataHash;
+import com.splicemachine.derby.utils.marshall.NoOpPostfix;
+import com.splicemachine.derby.utils.marshall.NoOpPrefix;
+import com.splicemachine.derby.utils.marshall.SaltedPrefix;
+import com.splicemachine.derby.utils.marshall.dvd.DescriptorSerializer;
+import com.splicemachine.derby.utils.marshall.dvd.VersionedSerializers;
+import com.splicemachine.hbase.KVPair;
+import com.splicemachine.hbase.writer.CallBuffer;
+import com.splicemachine.hbase.writer.RecordingCallBuffer;
+import com.splicemachine.hbase.writer.WriteCoordinator;
+import com.splicemachine.mapreduce.HBaseBulkLoadMapper;
+import com.splicemachine.mapreduce.HBaseBulkLoadReducer;
+import com.splicemachine.utils.IntArrays;
+import com.splicemachine.utils.SpliceLogUtils;
+import com.splicemachine.utils.kryo.KryoPool;
+import com.splicemachine.uuid.Snowflake;
+import com.splicemachine.mrio.api.SQLUtil;
+import com.splicemachine.mrio.api.SpliceMRConstants;
+
+import org.apache.hadoop.hive.ql.exec.FileSinkOperator.RecordWriter;
+
+public class HiveSpliceOutputFormat extends OutputFormat implements
+HiveOutputFormat<ImmutableBytesWritable, Put>,
+org.apache.hadoop.mapred.OutputFormat<ImmutableBytesWritable, Put>{
+	private static SQLUtil sqlUtil = null;
+	
+	private static Configuration conf = null;
+	
+	private String spliceTableName;
+	protected static String tableID;
+	private HashMap<List, List> tableStructure;
+	private HashMap<List, List> pks;
+	
+	private HiveSpliceOutputFormat(){
+		super();
+		//sqlUtil = SQLUtil.getInstance();
+		
+	}
+	
+	public Configuration getConf() {
+		return this.conf;
+	}
+	public void setConf(Configuration conf) {
+		this.conf = conf;
+	}
+	
+	@Override
+	public void checkOutputSpecs(JobContext arg0) throws IOException,
+			InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public OutputCommitter getOutputCommitter(TaskAttemptContext arg0)
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return new TableOutputCommitter();
+	}
+	
+	public RecordWriter getRecordWriter()
+			throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		if(conf == null)
+			throw new IOException("Error: Please set Configuration for SpliceOutputFormat");
+		if(sqlUtil == null)
+			sqlUtil = SQLUtil.getInstance(conf.get(SpliceMRConstants.SPLICE_JDBC_STR));
+		spliceTableName = conf.get(TableOutputFormat.OUTPUT_TABLE);
+		
+		tableStructure = sqlUtil.getTableStructure(spliceTableName);
+		pks = sqlUtil.getPrimaryKey(spliceTableName);
+		ArrayList<String> pkColNames = null;
+		ArrayList<String> allColNames = new ArrayList<String>();
+		ArrayList<Integer> allColTypes = new ArrayList<Integer>();
+		Iterator tableiter = tableStructure.entrySet().iterator();
+		Iterator pkiter = pks.entrySet().iterator();
+	    if(tableiter.hasNext()){
+	    	Map.Entry kv = (Map.Entry)tableiter.next();
+	    	allColNames = (ArrayList<String>)kv.getKey(); 
+	    	allColTypes = (ArrayList<Integer>)kv.getValue();
+	    }
+	    if(pkiter.hasNext()){
+	    	Map.Entry kv = (Map.Entry)pkiter.next();
+	    	pkColNames = (ArrayList<String>)kv.getKey(); 	
+	    }
+	    int[]pkCols = new int[pkColNames.size()];
+	    
+	    if(pkColNames == null || pkColNames.size() == 0){
+	    	SpliceRecordWriter spw = new SpliceRecordWriter(null, allColTypes);
+			return spw;
+	    }
+	   
+	    else{
+	    	for (int i = 0; i < pkColNames.size(); i++){
+	    		pkCols[i] = allColNames.indexOf(pkColNames.get(i))+1;
+	    	}
+	    }
+	    
+	    SpliceRecordWriter spw = new SpliceRecordWriter(pkCols, allColTypes);
+		return spw;	
+	}
+	
+	protected static class SpliceRecordWriter implements RecordWriter {
+	     
+		private RecordingCallBuffer callBuffer = null;
+		private static final Snowflake snowflake = new Snowflake((short)1);
+		private int[] pkCols = null;
+		private DescriptorSerializer[] serializers = null;
+		private DataHash rowHash = null;
+		private KeyEncoder keyEncoder = null;
+		private ArrayList<Integer> colTypes = null;
+		private DataValueDescriptor[] rowDesc = null;
+		private String taskID = "";
+		private Connection conn = null;
+		private String childTxsID = "";
+		
+		
+		private void setTaskID(String taskID){
+			this.taskID = taskID;
+		}
+		
+		public SpliceRecordWriter(int[]pkCols, ArrayList colTypes) throws IOException{
+			
+			System.out.println("Initializing SpliceRecordWriter....");
+			if(conf == null)
+				throw new IOException("Error: Please set Configuration for SpliceRecordWriter");
+			try {
+				this.colTypes = colTypes;
+				this.rowDesc = createDVD();
+				this.taskID = taskID;
+				this.pkCols = pkCols;
+				this.keyEncoder =  getKeyEncoder(null);
+				this.rowHash = getRowHash(null);	
+				tableID = sqlUtil.getConglomID(conf.get(TableOutputFormat.OUTPUT_TABLE));	
+				
+			} catch (StandardException e) {
+				// TODO Auto-generated catch block
+				throw new IOException(e.getCause());
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				throw new IOException(e.getCause());
+			} 
+		}
+		
+		
+		private KeyEncoder getKeyEncoder(SpliceRuntimeContext spliceRuntimeContext) throws StandardException {
+			HashPrefix prefix;
+			DataHash dataHash;
+			KeyPostfix postfix = NoOpPostfix.INSTANCE;
+			
+			if(pkCols==null){
+				    prefix = new SaltedPrefix(snowflake.newGenerator(100));
+					dataHash = NoOpDataHash.INSTANCE;
+			}else{
+					int[] keyColumns = new int[pkCols.length];
+					for(int i=0;i<keyColumns.length;i++){
+							keyColumns[i] = pkCols[i] -1;
+							
+					}
+					prefix = NoOpPrefix.INSTANCE;
+					
+					DescriptorSerializer[] serializers = VersionedSerializers.latestVersion(true).getSerializers(rowDesc);
+					dataHash = BareKeyHash.encoder(keyColumns,null, serializers);
+			}
+			
+			return new KeyEncoder(prefix,dataHash,postfix);
+	}
+		
+		private int[] getEncodingColumns(int n) {
+	        int[] columns = IntArrays.count(n);
+
+	        // Skip primary key columns to save space
+	        if (pkCols != null) {
+	        	
+	            for(int pkCol:pkCols) {
+	                columns[pkCol-1] = -1;
+	            }
+	            return columns;
+	        }
+	        else
+	        	return null;
+	        
+	    }
+		
+		private DataHash getRowHash(SpliceRuntimeContext spliceRuntimeContext) throws StandardException {
+			//get all columns that are being set
+			int[] columns = getEncodingColumns(colTypes.size());
+			
+			DescriptorSerializer[] serializers = VersionedSerializers.forVersion("2.0",true).getSerializers(rowDesc);
+			return new EntryDataHash(columns,null,serializers);
+	}
+		
+		private DataValueDescriptor[] createDVD() throws StandardException
+		{
+			DataValueDescriptor dvds[] = new DataValueDescriptor[colTypes.size()];
+			for(int pos = 0; pos < colTypes.size(); pos++){
+				dvds[pos] = DataTypeDescriptor.getBuiltInDataTypeDescriptor(colTypes.get(pos)).getNull();
+			}
+			return dvds;			
+		}
+		
+		@Override
+		public void write(Writable valueWritable)
+				throws IOException {
+			// TODO Auto-generated method stub
+			try {		
+				if(callBuffer == null){
+					conn = sqlUtil.createConn();
+					sqlUtil.disableAutoCommit(conn);
+					
+					childTxsID = sqlUtil.getChildTransactionID(conn, 
+									conf.get(SpliceMRConstants.SPLICE_TRANSACTION_ID), 
+									Long.parseLong(tableID));
+
+					callBuffer = WriteCoordinator.create(conf).writeBuffer(Bytes.toBytes(tableID), 
+									childTxsID, SpliceMRConstants.SPLICE_WRITE_BUFFER_SIZE);	
+				}		
+				ExecRow value = ((ExecRowWritable)valueWritable).get();
+				byte[] key = this.keyEncoder.getKey(value);
+				rowHash.setRow(value);
+				
+				byte[] bdata = rowHash.encode();
+				KVPair kv = new KVPair();
+				kv.setKey(key);
+				kv.setValue(bdata);	
+				callBuffer.add(kv);
+					
+			} catch (StandardException e) {
+				// TODO Auto-generated catch block
+				throw new IOException(e.getCause());
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				throw new IOException(e.getCause());
+			} 
+		}
+
+
+		@Override
+		public void close(boolean abort) throws IOException {
+			// TODO Auto-generated method stub
+			if (!abort) {
+		          //table.flushCommits();
+		        	//Do commit here
+				try {
+					this.callBuffer.flushBuffer();
+					this.callBuffer.close();
+					sqlUtil.commit(conn);
+					
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					try {
+						sqlUtil.rollback(conn);
+						throw new IOException("Exception in RecordWriter.close, "+e.getMessage());
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						throw new IOException("Exception in RecordWriter.close, "+e1.getMessage());
+					}
+				}
+		        }
+		}
+	}
+
+	@Override
+	public void checkOutputSpecs(FileSystem arg0, JobConf jc)
+			throws IOException {
+		 	String spliceTableName = jc.get(SpliceSerDe.SPLICE_TABLE_NAME);
+		 	System.out.println("checking outputspec, writing to Splice table: "+spliceTableName);
+		    jc.set(TableOutputFormat.OUTPUT_TABLE, spliceTableName);
+		    Job job = new Job(jc);
+		    JobContext jobContext = ShimLoader.getHadoopShims().newJobContext(job);
+
+		    try {
+		      checkOutputSpecs(jobContext);
+		    } catch (InterruptedException e) {
+		      throw new IOException(e);
+		    }
+		
+	}
+
+	@Override
+	public org.apache.hadoop.mapred.RecordWriter<ImmutableBytesWritable, Put> getRecordWriter(
+			FileSystem arg0, JobConf arg1, String arg2, Progressable arg3)
+			throws IOException {
+		// TODO Auto-generated method stub
+		throw new RuntimeException("Error: Hive should not invoke this method.");
+	}
+
+	@Override
+	public org.apache.hadoop.hive.ql.exec.FileSinkOperator.RecordWriter getHiveRecordWriter(
+			JobConf jc, Path finalOutPath,
+			Class<? extends Writable> valueClass, boolean isCompressed,
+			Properties tableProperties, Progressable progress)
+			throws IOException {
+		String spliceTableName = jc.get(SpliceSerDe.SPLICE_TABLE_NAME);
+	    jc.set(TableOutputFormat.OUTPUT_TABLE, spliceTableName);
+	    final boolean walEnabled = HiveConf.getBoolVar(
+	        jc, HiveConf.ConfVars.HIVE_HBASE_WAL_ENABLED);
+	    final HTable table = new HTable(HBaseConfiguration.create(jc), spliceTableName);
+	    table.setAutoFlush(false);
+	    RecordWriter rw = null;
+	    try {
+			rw = this.getRecordWriter();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    return rw;
+	}
+
+	@Override
+	public org.apache.hadoop.mapreduce.RecordWriter getRecordWriter(
+			TaskAttemptContext arg0) throws IOException, InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+}
---
+++ structured_derby/src/main/java/com/splicemachine/intg/hive/SpliceSerDe.java
@@ -0,0 +1,224 @@
+package com.splicemachine.intg.hive;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.derby.iapi.error.StandardException;
+import org.apache.derby.iapi.sql.execute.ExecRow;
+import org.apache.derby.iapi.types.DataTypeDescriptor;
+import org.apache.derby.iapi.types.DataValueDescriptor;
+import org.apache.derby.iapi.types.SQLBlob;
+import org.apache.derby.iapi.types.SQLBoolean;
+import org.apache.derby.iapi.types.SQLDouble;
+import org.apache.derby.iapi.types.SQLInteger;
+import org.apache.derby.iapi.types.SQLLongint;
+import org.apache.derby.iapi.types.SQLSmallint;
+import org.apache.derby.iapi.types.SQLVarchar;
+import org.apache.derby.impl.sql.execute.ValueRow;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hive.serde.Constants;
+import org.apache.hadoop.hive.serde2.AbstractSerDe;
+import org.apache.hadoop.hive.serde2.SerDeException;
+import org.apache.hadoop.hive.serde2.SerDeStats;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.typeinfo.StructTypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfo;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoFactory;
+import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.splicemachine.mrio.api.SQLUtil;
+import com.splicemachine.mrio.api.SpliceMRConstants;
+
+public class SpliceSerDe extends AbstractSerDe {
+	 private StructTypeInfo rowTypeInfo;
+	 private ObjectInspector rowOI;
+	 private List<String> colNames;
+	 private List<Integer> colTypes;
+	 public static final String SPLICE_TABLE_NAME = "splice.table.name";
+	 public static final String SPLICE_JDBC_STR = "splice.jdbc";
+	 private List<Object> row = new ArrayList<Object>();
+	 private SQLUtil sqlUtil = null;
+	 static Logger Log = Logger.getLogger(
+			 SpliceSerDe.class.getName());
+
+	 /**
+	  * An initialization function used to gather information about the table.
+	  * Typically, a SerDe implementation will be interested in the list of
+	  * column names and their types. That information will be used to help 
+	  * perform actual serialization and deserialization of data.
+	  */
+	 @Override
+	 public void initialize(Configuration conf, Properties tbl)
+	     throws SerDeException {
+	   // Get a list of the table's column names.
+	   String spliceTableName = tbl.getProperty(SpliceSerDe.SPLICE_TABLE_NAME);
+	   spliceTableName = spliceTableName.trim();
+	   if(sqlUtil == null)
+		   sqlUtil = SQLUtil.getInstance(tbl.getProperty(SpliceMRConstants.SPLICE_JDBC_STR));
+	   getSpliceTableStructure(spliceTableName);
+	  
+	   String colNamesStr = tbl.getProperty(Constants.LIST_COLUMNS);
+	   colNames = Arrays.asList(colNamesStr.split(","));
+	   Log.info("------col names read from property: "+colNamesStr);
+	   // Get a list of TypeInfos for the columns. This list lines up with
+	   // the list of column names.
+	   String colTypesStr = tbl.getProperty(Constants.LIST_COLUMN_TYPES);
+	   List<TypeInfo> colTypes =
+	       TypeInfoUtils.getTypeInfosFromTypeString(colTypesStr);
+	  
+	   rowTypeInfo =
+	       (StructTypeInfo) TypeInfoFactory.getStructTypeInfo(colNames, colTypes);
+	   rowOI =
+	       TypeInfoUtils.getStandardJavaObjectInspectorFromTypeInfo(rowTypeInfo);
+	   for(TypeInfo typeInfo : colTypes)
+	   {
+		   Log.info("----- col Type: "+typeInfo.getTypeName());
+	   }
+	   Log.info("--------Finished initialize");
+	 }
+
+	 
+	 // temporarily use this method to read table structure of Splice, 
+	 // colTypes helps decoding ExecRow, which can be filled in the Hive Row.
+	 // this will cost the inconsistency if reading data out of Splice at the same time DDL.
+	 private void getSpliceTableStructure(String tableName)
+	 {
+		    HashMap<List, List> tableStructure = new HashMap<List, List>();
+		    
+		 	tableStructure = sqlUtil.getTableStructure(tableName);
+			
+	    	Iterator iter = tableStructure.entrySet().iterator();
+	    	if(iter.hasNext())
+	    	{
+	    		Map.Entry kv = (Map.Entry)iter.next();
+	    		colNames = (ArrayList<String>)kv.getKey();
+	    		colTypes = (ArrayList<Integer>)kv.getValue();
+	    	}
+		    	
+	 }
+	 
+	 private void fillRow(DataValueDescriptor[] dvds) throws StandardException
+	 {
+		
+			for(int pos = 0; pos < colTypes.size(); pos++)
+			{
+				switch(colTypes.get(pos))
+				{
+				case java.sql.Types.INTEGER:
+					row.add(dvds[pos].getInt());
+					break;
+				case java.sql.Types.BIGINT:
+					row.add(dvds[pos].getLong());
+					break;
+				case java.sql.Types.SMALLINT:
+					row.add(dvds[pos].getShort());
+					break;
+				case java.sql.Types.BOOLEAN:
+					row.add(dvds[pos].getBoolean());
+					break;
+				case java.sql.Types.DOUBLE:	
+					row.add(dvds[pos].getDouble());
+					break;
+				case java.sql.Types.FLOAT:
+					row.add(dvds[pos].getFloat());
+					break;
+				case java.sql.Types.CHAR:
+					
+				case java.sql.Types.VARCHAR:
+					row.add(dvds[pos].getString());
+					
+					break;
+				case java.sql.Types.BINARY:	
+					
+				default:
+					row.add(dvds[pos].getBytes());
+				}
+			}
+			
+		}
+	 
+	 /**
+	  * This method does the work of deserializing a record into Java objects
+	  * that Hive can work with via the ObjectInspector interface.
+	  */
+	 @Override
+	 public Object deserialize(Writable blob) throws SerDeException {
+	   row.clear();
+	   // Do work to turn the fields in the blob into a set of row fields
+	   Log.debug(Thread.currentThread() .getStackTrace()[1].getMethodName());
+	   ExecRowWritable rowWritable = (ExecRowWritable) blob;
+	   
+	   try {
+		   ExecRow val = rowWritable.get();
+		   if (val == null)
+			   return null;
+		   DataValueDescriptor dvd[] = val.getRowArray();
+		   if(dvd == null || dvd.length == 0)
+			   return row;
+		
+		//row.add(rowWritable.get().getRowArray()[0].getString());
+		   fillRow(dvd);
+		   
+	   } catch (StandardException e) {
+		// TODO Auto-generated catch block
+		  throw new SerDeException("deserialization error, "+e.getCause());
+	   }
+	   return row;
+	 }
+
+	 /**
+	  * Return an ObjectInspector for the row of data
+	  */
+	 @Override
+	 public ObjectInspector getObjectInspector() throws SerDeException {
+		 Log.debug(Thread.currentThread() .getStackTrace()[1].getMethodName());
+	   return rowOI;
+	 }
+
+	 /**
+	  * Unimplemented
+	  */
+	 @Override
+	 public SerDeStats getSerDeStats() {
+		 Log.debug(Thread.currentThread() .getStackTrace()[1].getMethodName());
+	   return null;
+	 }
+
+	 /**
+	  * Return the class that stores the serialized data representation.
+	  */
+	 @Override
+	 public Class<? extends Writable> getSerializedClass() {
+		 Log.debug(Thread.currentThread() .getStackTrace()[1].getMethodName());
+	   return ExecRowWritable.class;
+	 }
+
+	 /**
+	  * This method takes an object representing a row of data from Hive, and
+	  * uses the ObjectInspector to get the data for each column and serialize
+	  * it.
+	  */
+	 @Override
+	 public Writable serialize(Object obj, ObjectInspector oi)
+	     throws SerDeException {
+	   // Take the object and transform it into a serialized representation
+		 Log.debug(Thread.currentThread() .getStackTrace()[1].getMethodName());
+		 DataValueDescriptor[] data = new DataValueDescriptor[]{new SQLInteger(1),
+					new SQLVarchar("abcd")};
+		 ExecRow row = new ValueRow(data.length);
+			row.setRowArray(data);
+		ExecRowWritable rowWritable = new ExecRowWritable(colTypes);
+		rowWritable.set(row);
+		return rowWritable;
+		 
+	 }
+}
