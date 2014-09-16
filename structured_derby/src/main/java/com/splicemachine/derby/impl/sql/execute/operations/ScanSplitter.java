74c86b215d39db1bf419c61b7f698fed57781a41
Scott Fines <scottfines@gmail.com>
2014-09-16 15:26:26 -0500

    Fixing broken SI UT

---
+++ structured_derby/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/ScanSplitter.java
@@ -0,0 +1,8 @@
+package com.splicemachine.derby.impl.sql.execute.operations;
+
+/**
+ * @author Scott Fines
+ *         Date: 9/16/14
+ */
+public interface ScanSplitter {
+}
---
+++ structured_derby/src/main/java/com/splicemachine/test/RandomSelectQueryRunner.java
@@ -0,0 +1,8 @@
+package com.splicemachine.test;
+
+/**
+ * @author Scott Fines
+ *         Date: 9/16/14
+ */
+public class RandomSelectQueryRunner {
+}
