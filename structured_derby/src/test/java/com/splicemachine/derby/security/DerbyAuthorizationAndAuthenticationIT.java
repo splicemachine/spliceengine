1901011ba2c13239d8a1cf62e6d2284dec565b98
Scott Fines <scottfines@gmail.com>
2014-09-23 12:27:37 -0500

    DB-1861: Fixing accidental test breakage

--- structured_derby/src/test/java/com/splicemachine/derby/security/DerbyAuthorizationAndAuthenticationIT.java
+++ structured_derby/src/test/java/com/splicemachine/derby/security/DerbyAuthorizationAndAuthenticationIT.java
@@ -1,14 +1,16 @@
 package com.splicemachine.derby.security;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLNonTransientConnectionException;
+import java.sql.SQLSyntaxErrorException;
+import java.util.concurrent.Callable;
+
 import com.splicemachine.derby.test.framework.*;
 import com.splicemachine.derby.utils.ErrorState;
 import org.junit.*;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
-
-import java.sql.SQLException;
-import java.sql.SQLNonTransientConnectionException;
-import java.util.concurrent.Callable;
 
 //@Ignore()
 public class DerbyAuthorizationAndAuthenticationIT extends SpliceUnitTest {
@@ -22,20 +24,47 @@ public class DerbyAuthorizationAndAuthenticationIT extends SpliceUnitTest {
     protected static final String INSERT_STATEMENT = "insert into %s values ('1','Johnny',1.2,'Detroit')";
     protected static final String INSERT_STATEMENT1Rev2 = "insert into %s values ('2','Johnny',1.2,'Detroit')";
     protected static final String UPDATE_STATEMENT1Rev2 = "update %s set CITY = 'STL' where EMPNUM = '2'";
+    protected static final String ROLE1 = "super_user";
+    protected static final String ROLE2 = "read_only";
+    protected static final String ROLE3 = "app_user";
 
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
     protected static SpliceSchemaWatcher spliceSchemaWatcher1 = new SpliceSchemaWatcher(SCHEMA_NAME+USER1,USER1);
+    protected static SpliceSchemaWatcher spliceSchemaWatcher2 = new SpliceSchemaWatcher(SCHEMA_NAME+USER2,USER2);
+    protected static SpliceSchemaWatcher spliceSchemaWatcher3 = new SpliceSchemaWatcher(SCHEMA_NAME+USER3,USER3);
+    protected static SpliceUserWatcher spliceUserWatcher1 = new SpliceUserWatcher(USER1,PASSWORD1);
+    protected static SpliceUserWatcher spliceUserWatcher2 = new SpliceUserWatcher(USER2,PASSWORD2);
+    protected static SpliceUserWatcher spliceUserWatcher3 = new SpliceUserWatcher(USER3,PASSWORD3);
+    protected static SpliceRoleWatcher spliceRoleWatcher1 = new SpliceRoleWatcher(ROLE1);
+    protected static SpliceRoleWatcher spliceRoleWatcher2 = new SpliceRoleWatcher(ROLE2);
+    protected static SpliceRoleWatcher spliceRoleWatcher3 = new SpliceRoleWatcher(ROLE3);
 
     protected static SpliceTableWatcher staffTableUser1 = new SpliceTableWatcher("STAFF",spliceSchemaWatcher1.schemaName,
             "(EMPNUM   VARCHAR(3) NOT NULL, "+
                     "EMPNAME  VARCHAR(20), "+
                     "GRADE    DECIMAL(4), "+
                     "CITY     VARCHAR(15))", USER1, PASSWORD1);
+    protected static SpliceTableWatcher staffTableUser2 = new SpliceTableWatcher("STAFF",spliceSchemaWatcher2.schemaName,
+            "(EMPNUM   VARCHAR(3) NOT NULL, "+
+                    "EMPNAME  VARCHAR(20), "+
+                    "GRADE    DECIMAL(4), "+
+                    "CITY     VARCHAR(15))", USER2, PASSWORD2);
+    protected static SpliceTableWatcher staffTableUser3 = new SpliceTableWatcher("STAFF",spliceSchemaWatcher3.schemaName,
+            "(EMPNUM   VARCHAR(3) NOT NULL, "+
+                    "EMPNAME  VARCHAR(20), "+
+                    "GRADE    DECIMAL(4), "+
+                    "CITY     VARCHAR(15))", USER3, PASSWORD3);
 
     @ClassRule
     public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
+            .around(spliceUserWatcher1)
+            .around(spliceUserWatcher2)
+            .around(spliceUserWatcher3)
             .around(spliceSchemaWatcher1)
-            .around(staffTableUser1);
+            .around(staffTableUser1)
+            .around(spliceRoleWatcher1)
+            .around(spliceRoleWatcher2)
+            .around(spliceRoleWatcher3);
 
     @Rule public SpliceWatcher methodWatcher = new SpliceWatcher();
 
@@ -182,10 +211,10 @@ public class DerbyAuthorizationAndAuthenticationIT extends SpliceUnitTest {
     public void testGrantSelectAndRevoke() throws Exception {
 
         TestConnection conn = methodWatcher.getOrCreateConnection();
-        conn.execute(String.format("grant select on table %s to %s", staffTableUser1, USER2));
+        conn.execute(String.format("grant select on table %s to %s",staffTableUser1,USER2));
         conn.execute(format("grant select (empname,city) on table %s to %s", staffTableUser1, USER3));
-        user2Conn.count(String.format("select * from %s", staffTableUser1));
-        user3Conn.count(String.format("select empname,city from %s", staffTableUser1));
+        user2Conn.count(String.format("select * from %s",staffTableUser1));
+        user3Conn.count(String.format("select empname,city from %s",staffTableUser1));
         assertFailed(new Callable<Void>() {
             @Override
             public Void call() throws Exception {
@@ -194,8 +223,8 @@ public class DerbyAuthorizationAndAuthenticationIT extends SpliceUnitTest {
             }
         }, ErrorState.AUTH_NO_COLUMN_PERMISSION.getSqlState());
 
-        conn.execute(format("revoke select on table %s from %s", staffTableUser1, USER2));
-        conn.execute(format("revoke select (empname,city) on table %s from %s", staffTableUser1, USER3));
+        conn.execute(format("revoke select on table %s from %s",staffTableUser1,USER2));
+        conn.execute(format("revoke select (empname,city) on table %s from %s",staffTableUser1,USER3));
     }
 
     /*****************************************************************************************************************/
