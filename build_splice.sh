5c617d558b3e6c85f3e93a7640919d8fad218427
Aaron Molitor <amolitor@splicemachine.com>
2014-10-02 11:46:45 -0500

    adding updated splice distribution mgmt, removing outdated instructions and deploy scripts, updating build_splice script with new options for sane vs. insane builds

--- build_splice.sh
+++ build_splice.sh
@@ -1,3 +1,14 @@
-# call with your gpg passphrase as the first param
+#!/bin/bash
 
-ant clean && mkdir -p bin/templates && ant buildsource testing buildjars && cd maven2 && mvn clean install && cd ..
+# Builds derby for splice developers. 
+# Pass in "sane" or "" for typical debug builds
+# Pass in "insane" for a production build
+# see BUILDING.html for more detail on sane vs. insane builds
+
+if [[ "${1}" == "" || "${1}" == "sane" ]]; then
+	ant clean && ant buildsource testing buildjars && cd maven2 && mvn clean install && cd ..
+elif [[ "${1}" == "insane" ]]; then
+	ant clean && ant buildsource testing buildjars -Dsane=false && cd maven2 && mvn clean install -Dsanity=insane && cd ..
+else 
+	echo "	USAGE: ${0} [ sane | insane | NULL ] " 
+fi
