f6d19b5a9e5ea5d1067f81c7c2c60a78ab23abe2
Keith Winkler <kwinkler@splicemachine.com>
2014-10-10 10:11:19 -0500

    MultiRegionIT: cleanup periodically failing IT.

--- structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MultiRegionIT.java
+++ structured_derby/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MultiRegionIT.java
@@ -1,35 +1,44 @@
 package com.splicemachine.derby.impl.sql.execute.operations;
 
-import com.splicemachine.constants.bytes.BytesUtil;
-import com.splicemachine.derby.test.framework.*;
-import com.splicemachine.test.SlowTest;
-
+import com.splicemachine.derby.test.framework.SpliceDataWatcher;
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.derby.test.framework.SpliceTableWatcher;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
 import com.splicemachine.derby.utils.SpliceUtils;
+import com.splicemachine.test.SerialTest;
+import com.splicemachine.test.SlowTest;
+import com.splicemachine.test_dao.StatementHistory;
+import com.splicemachine.test_dao.StatementHistoryDAO;
 import org.apache.hadoop.hbase.HRegionInfo;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.junit.*;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
-@Category(SlowTest.class)
-public class MultiRegionIT extends SpliceUnitTest {
-    public static final String CLASS_NAME = MultiRegionIT.class.getSimpleName().toUpperCase();
+import static java.lang.String.format;
+import static org.junit.Assert.*;
 
-    protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
-    public static final String TABLE_NAME = "TAB";
-    protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);
+/**
+ * SerialTest because it clears the statement history table, SlowTests because it performs manual splits.
+ */
+@Category(value = {SerialTest.class, SlowTest.class})
+public class MultiRegionIT {
 
-    private static String tableDef = "(I INT, D DOUBLE)";
-    protected static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher(TABLE_NAME,CLASS_NAME, tableDef);
+    private static final String SCHEMA_NAME = MultiRegionIT.class.getSimpleName().toUpperCase();
+    private static final SpliceWatcher spliceClassWatcher = new SpliceWatcher(SCHEMA_NAME);
+    private static final String TABLE_NAME = "TAB";
+    private static final SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(SCHEMA_NAME);
+    private static final SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher(TABLE_NAME, SCHEMA_NAME, "(I INT, D DOUBLE)");
 
     @ClassRule
     public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
@@ -39,121 +48,77 @@ public class MultiRegionIT extends SpliceUnitTest {
                 protected void starting(Description description) {
                     PreparedStatement ps;
                     try {
-                        ps = spliceClassWatcher.prepareStatement(
-                                String.format("insert into %s (i, d) values (?, ?)", spliceTableWatcher));
+                        ps = spliceClassWatcher.prepareStatement(format("insert into %s (i, d) values (?, ?)", TABLE_NAME));
                         for (int j = 0; j < 100; ++j) {
                             for (int i = 0; i < 10; i++) {
                                 ps.setInt(1, i);
-                                ps.setDouble(2, i*1.0);
+                                ps.setDouble(2, i * 1.0);
                                 ps.execute();
                             }
                         }
-                        spliceClassWatcher.splitTable(TABLE_NAME, CLASS_NAME, 250);
-                        spliceClassWatcher.splitTable(TABLE_NAME, CLASS_NAME, 500);
-                        spliceClassWatcher.splitTable(TABLE_NAME, CLASS_NAME, 750);
+                        spliceClassWatcher.splitTable(TABLE_NAME, SCHEMA_NAME, 250);
+                        spliceClassWatcher.splitTable(TABLE_NAME, SCHEMA_NAME, 500);
+                        spliceClassWatcher.splitTable(TABLE_NAME, SCHEMA_NAME, 750);
 
-                    }  catch (Exception e) {
+                    } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
                 }
             });
 
     @Rule
-    public SpliceWatcher methodWatcher = new SpliceWatcher();
-    
+    public SpliceWatcher methodWatcher = new SpliceWatcher(SCHEMA_NAME);
+
     @Test
     public void testStddevAndAutoTrace() throws Exception {
-    	Connection conn = methodWatcher.createConnection();
+        Connection conn = methodWatcher.createConnection();
+        StatementHistoryDAO statementHistoryDAO = new StatementHistoryDAO(conn);
 
         conn.createStatement().execute("call SYSCS_UTIL.SYSCS_PURGE_XPLAIN_TRACE()");
+        conn.commit();
+        assertTrue("need more than 3 regions to test auto trace", getNumOfRegions(SCHEMA_NAME, TABLE_NAME) > 3);
 
-    	ResultSet rs = conn.createStatement().executeQuery(
-                String.format("select stddev_pop(i) from %s", this.getTableReference(TABLE_NAME)));
+        Double popValue = methodWatcher.query(format("select stddev_pop(i) from %s", TABLE_NAME));
+        assertEquals(2.8, popValue, .5);
 
-        while(rs.next()){
-        	Assert.assertEquals((int)rs.getDouble(1), 2);
-        }
-        rs.close();
+        Double sampleValue = methodWatcher.query(format("select stddev_samp(i) from %s", TABLE_NAME));
+        assertEquals(2.8, sampleValue, .5);
 
-        rs = conn.createStatement().executeQuery(
-                String.format("select stddev_samp(i) from %s", this.getTableReference(TABLE_NAME)));
-
-        while(rs.next()){
-            Assert.assertEquals((int)rs.getDouble(1), 2);
-        }
-        rs.close();
-
-        Thread.sleep(8000);
-
-        int numRegions = getNumOfRegions(CLASS_NAME, TABLE_NAME);
-
-        rs = conn.createStatement().executeQuery(
-                String.format("select count(*) from sys.sysstatementhistory"));
-
-        int count = 0;
-        if(rs.next()){
-            count = rs.getInt(1);
-        }
-        rs.close();
-
-        if (numRegions > 3) {
-            Assert.assertTrue(count > 0);
-        }
+        StatementHistory hist1 = statementHistoryDAO.findStatement("stddev_samp", 20, TimeUnit.SECONDS);
+        StatementHistory hist2 = statementHistoryDAO.findStatement("stddev_pop", 20, TimeUnit.SECONDS);
+        assertTrue("hist1=" + hist1 + " hist2=" + hist2, hist1 != null && hist2 != null);
     }
 
     @Test
     public void testDistinctCount() throws Exception {
-        Connection conn = methodWatcher.createConnection();
-        ResultSet rs = conn.createStatement().executeQuery(
-                String.format("select count(distinct i) from %s", this.getTableReference(TABLE_NAME)));
+        Long count = methodWatcher.query(format("select count(distinct i) from %s", TABLE_NAME));
+        assertEquals(10, count.intValue());
+    }
 
-        while(rs.next()){
-            Assert.assertEquals((int)rs.getInt(1), 10);
-        }
-        rs.close();
+    @Test
+    public void testAutoTraceOff() throws Exception {
+        Connection conn = methodWatcher.createConnection();
+        StatementHistoryDAO statementHistoryDAO = new StatementHistoryDAO(conn);
+
+        conn.createStatement().execute("call SYSCS_UTIL.SYSCS_PURGE_XPLAIN_TRACE()");
+
+        // turn OFF auto trace
+        conn.createStatement().execute("call SYSCS_UTIL.SYSCS_SET_AUTO_TRACE(0)");
+
+        Double sampleValue = methodWatcher.query(format("select stddev_samp(i) from %s", TABLE_NAME));
+        assertEquals(2.8, sampleValue, .5);
+
+        // turn ON auto trace
+        conn.createStatement().execute("call SYSCS_UTIL.SYSCS_SET_AUTO_TRACE(1)");
+
+        StatementHistory history = statementHistoryDAO.findStatement("stddev_samp", 6, TimeUnit.SECONDS);
+        assertNull("expected not to find it because auto trace was off", history);
     }
 
     private int getNumOfRegions(String schemaName, String tableName) throws Exception {
         long conglomId = spliceClassWatcher.getConglomId(tableName, schemaName);
         HBaseAdmin admin = SpliceUtils.getAdmin();
         List<HRegionInfo> regions = admin.getTableRegions(Bytes.toBytes(Long.toString(conglomId)));
-
         return regions.size();
-    }
-
-    @Test
-    public void testAutoTraceOff() throws Exception {
-        Thread.sleep(5000);
-        Connection conn = methodWatcher.createConnection();
-
-        ResultSet rs =  conn.createStatement().executeQuery("select count(*) from sys.sysstatementhistory");
-        int before = 0;
-        if(rs.next()){
-            before = rs.getInt(1);
-        }
-        rs.close();
-
-        // turn off auto trace
-        conn.createStatement().execute("call SYSCS_UTIL.SYSCS_SET_AUTO_TRACE(0)");
-
-        rs = conn.createStatement().executeQuery(
-                String.format("select stddev_samp(i) from %s", this.getTableReference(TABLE_NAME)));
-
-        while(rs.next()){
-            Assert.assertEquals((int)rs.getDouble(1), 2);
-        }
-        rs.close();
-
-        // turn on auto trace
-        conn.createStatement().execute("call SYSCS_UTIL.SYSCS_SET_AUTO_TRACE(1)");
-
-        Thread.sleep(5000);
-        rs =  conn.createStatement().executeQuery("select count(*) from sys.sysstatementhistory");
-        int after = 0;
-        if(rs.next()){
-            after = rs.getInt(1);
-        }
-        rs.close();
-        Assert.assertTrue(before==after);
     }
 }
