81925c51ca3d534fa99fd36fc3fc51eacff8c952
Aaron Molitor <amolitor@splicemachine.com>
2014-10-13 13:40:00 -0500

    DB-2005 removing splice_web from spliceengine, to make it a standalone project

--- splice_web/src/main/webapp/scripts/controllers/main-controller.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('spliceAdminControllers')
-	.controller('mainController', ['$scope', function ($scope) {
-		// Set the active tab to be highlighted in the top nav bar.
-		$scope.homeTab = 'active';
-	}]);
