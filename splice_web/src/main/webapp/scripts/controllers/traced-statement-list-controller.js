3428a5d2a402fab4c96b14f1123d3e47ac80be59
Aaron Molitor <amolitor@splicemachine.com>
2014-10-13 13:40:00 -0500

    DB-2005 removing splice_web from spliceengine, to make it a standalone project

--- splice_web/src/main/webapp/scripts/controllers/traced-statement-list-controller.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('spliceAdminControllers')
-	.controller('tracedStatementListController', ['$scope', 'tracedStatementListService', function ($scope, tracedStatementListService) {
-		// Set the active tab to be highlighted in the top nav bar.
-		$scope.homeTab = 'active';
-
-		$scope.tracedStatements = tracedStatementListService.query();
-	}]);
