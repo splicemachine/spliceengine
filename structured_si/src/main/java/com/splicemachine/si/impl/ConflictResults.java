9b7505f66e766fbce59e5e4a9f61681358d147b6
Scott Fines <scottfines@gmail.com>
2014-10-17 16:25:49 -0500

    DB-1244: Adding the ability to recognize additive transaction conflicts (and therefore to detect when upserts duplicate records)
    Conflicts:
    	structured_si/src/main/java/com/splicemachine/si/impl/DataStore.java
    	structured_si/src/main/java/com/splicemachine/si/impl/SITransactor.java

--- structured_si/src/main/java/com/splicemachine/si/impl/ConflictResults.java
+++ structured_si/src/main/java/com/splicemachine/si/impl/ConflictResults.java
@@ -1,24 +1,51 @@
 package com.splicemachine.si.impl;
 
+import com.carrotsearch.hppc.LongOpenHashSet;
+
 import java.util.Collections;
 import java.util.Set;
 
 public class ConflictResults {
-		public static final ConflictResults NO_CONFLICT = new ConflictResults(Collections.<Long>emptySet(), Collections.<Long>emptySet(), false);
-		final Set<Long> toRollForward;
-    final Set<Long> childConflicts;
-    final Boolean hasTombstone;
+    public static final ConflictResults NO_CONFLICT=new ConflictResults(LongOpenHashSet.newInstanceWithCapacity(0,0.9f), LongOpenHashSet.from(), false);
+    private LongOpenHashSet toRollForward;
+    LongOpenHashSet childConflicts;
+    private LongOpenHashSet additiveConflicts;
+    boolean hasTombstone;
 
-    public ConflictResults(Set<Long> toRollForward, Set<Long> childConflicts, Boolean hasTombstone) {
+    public ConflictResults(){
+
+    }
+
+    public ConflictResults(LongOpenHashSet toRollForward, LongOpenHashSet childConflicts, Boolean hasTombstone) {
         this.toRollForward = toRollForward;
         this.childConflicts = childConflicts;
         this.hasTombstone = hasTombstone;
     }
 
-	@Override
-	public String toString() {		
-		return super.toString();
-	}
-    
-    
+
+    public void child(long txnId){
+        if(childConflicts==null)
+            childConflicts = LongOpenHashSet.newInstanceWithCapacity(1,0.9f);
+        childConflicts.add(txnId);
+    }
+
+    public void additive(long txnId){
+        if(additiveConflicts==null)
+            additiveConflicts = LongOpenHashSet.newInstanceWithCapacity(1,0.9f);
+        additiveConflicts.add(txnId);
+    }
+
+    public void addRollForward(long dataTransactionId) {
+        if(toRollForward==null)
+            toRollForward = LongOpenHashSet.newInstanceWithCapacity(1,0.9f);
+        toRollForward.add(dataTransactionId);
+    }
+
+    public void tombstone(boolean hasTombstone) {
+        this.hasTombstone = hasTombstone;
+    }
+
+    public boolean hasAdditiveConflicts() {
+        return additiveConflicts!=null;
+    }
 }
