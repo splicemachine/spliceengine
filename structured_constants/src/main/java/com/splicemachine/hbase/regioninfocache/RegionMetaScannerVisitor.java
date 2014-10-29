f0403436fb89c3d796b8983f3ff874a72365fff1
Keith Winkler <kwinkler@splicemachine.com>
2014-10-29 12:38:38 -0500

    DB-2134: Break up functionality into several smaller, simpler, package-private classes. Combine logic for scanning one/all tables into a single class. Add early stop when scanning for a single table. Assert servername poriton of Pair is never null.

---
+++ structured_constants/src/main/java/com/splicemachine/hbase/regioninfocache/RegionMetaScannerVisitor.java
@@ -0,0 +1,77 @@
+package com.splicemachine.hbase.regioninfocache;
+
+import com.google.common.collect.Sets;
+import com.splicemachine.hbase.RegionCacheComparator;
+import org.apache.hadoop.hbase.HRegionInfo;
+import org.apache.hadoop.hbase.ServerName;
+import org.apache.hadoop.hbase.catalog.MetaReader;
+import org.apache.hadoop.hbase.client.MetaScanner;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.Pair;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeMap;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+class RegionMetaScannerVisitor implements MetaScanner.MetaScannerVisitor {
+
+    private static final RegionCacheComparator COMPARATOR = new RegionCacheComparator();
+
+    /* We don't need a concurrent data structure here, but we do need one that does not depend
+     * on hashCode of the keys being consistent with equality, which is not the case for byte arrays */
+    private Map<byte[], SortedSet<Pair<HRegionInfo, ServerName>>> regionPairMap =
+            new TreeMap<byte[], SortedSet<Pair<HRegionInfo, ServerName>>>(Bytes.BYTES_COMPARATOR);
+
+    private byte[] updateTableName;
+
+    /**
+     * Construct an instance that collects information only for the specified table.
+     */
+    RegionMetaScannerVisitor(byte[] updateTableName) {
+        this.updateTableName = updateTableName;
+    }
+
+    @Override
+    public boolean processRow(Result rowResult) throws IOException {
+        Pair<HRegionInfo, ServerName> infoPair = MetaReader.parseCatalogResult(rowResult);
+
+        checkNotNull(infoPair.getFirst(), "never expect HRegionInfo object to be null");
+        checkNotNull(infoPair.getSecond(), "never expect ServerName object to be null");
+
+        HRegionInfo regionInfo = infoPair.getFirst();
+        byte[] currentTableName = regionInfo.getTableName();
+
+        if (updateTableName != null && !Bytes.equals(updateTableName, currentTableName)) {
+            /* We are looking for one specific table. This isn't it.  Should we continue?  If regionPairMap
+             * is empty then we may not have seen our table yet (or its regions were offline), continue. If the map
+             * is not empty however, then we know we can stop as we have scanned past the table we are looking for. */
+            return regionPairMap.isEmpty();
+        }
+
+        if (isRegionAvailable(regionInfo)) {
+            SortedSet<Pair<HRegionInfo, ServerName>> regionsForTable = regionPairMap.get(currentTableName);
+            if (regionsForTable == null) {
+                regionsForTable = Sets.newTreeSet(COMPARATOR);
+                regionPairMap.put(currentTableName, regionsForTable);
+            }
+            regionsForTable.add(infoPair);
+        }
+        return true;
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+    public Map<byte[], SortedSet<Pair<HRegionInfo, ServerName>>> getRegionPairMap() {
+        return regionPairMap;
+    }
+
+    public static boolean isRegionAvailable(HRegionInfo info) {
+        return !info.isOffline() && !info.isSplit() && !info.isSplitParent();
+    }
+}
