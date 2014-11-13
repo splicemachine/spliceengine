13389939813a96d4ac9a95c33223985f12204e76
Keith Winkler <kwinkler@splicemachine.com>
2014-11-13 12:36:57 -0600

    Added script for running all unit and integration tests without building the assembly.

--- cloudera-cdh4.5.0/splice_machine_test/run_all_tests.sh
+++ cloudera-cdh4.5.0/splice_machine_test/run_all_tests.sh
@@ -8,9 +8,6 @@ mvn exec:exec -PspliceZoo > zoo_it.log 2>&1 &
 # Start SpliceTestPlaform in background.
 mvn exec:exec -PspliceFast -DfailTasksRandomly=true > splice_it.log 2>&1 &
 
-# Wait for for splice-JDBC connections to be available.
-mvn exec:java -PspliceWait
-
-# Run all tests
-#mvn verify -Dskip.integration.tests=false -DskipTests=false -Dexcluded.categories=  
-mvn verify -Dskip.integration.tests=false -DskipTests=true -Dexcluded.categories=  
+# Wait for for splice-JDBC connections to be available, then run all tests.
+mvn exec:java -PspliceWait && \
+    mvn verify -Dskip.integration.tests=false -DskipTests=true -Dexcluded.categories=  
