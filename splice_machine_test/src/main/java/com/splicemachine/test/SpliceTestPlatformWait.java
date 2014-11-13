b3c7f3ebdce164baad44d8ee6f0bd1b9cd4e83dc
Keith Winkler <kwinkler@splicemachine.com>
2014-11-13 12:29:51 -0600

    Added script for running all unit and integration tests without building the assembly.

---
+++ splice_machine_test/src/main/java/com/splicemachine/test/SpliceTestPlatformWait.java
@@ -0,0 +1,46 @@
+package com.splicemachine.test;
+
+import com.splicemachine.concurrent.Threads;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.TimeUnit;
+
+import static java.lang.String.format;
+
+/**
+ * Waits for connections on a given host+port to be available.
+ */
+public class SpliceTestPlatformWait {
+
+    private static final long MAX_WAIT_SECS = TimeUnit.SECONDS.toSeconds(60);
+
+    /**
+     * argument 0 - hostname
+     * argument 1 - port
+     */
+    public static void main(String[] arguments) throws IOException {
+
+        String hostname = arguments[0];
+        int port = Integer.valueOf(arguments[1]);
+
+        long startTime = System.currentTimeMillis();
+        long elapsedSecs = 0;
+        while (elapsedSecs < MAX_WAIT_SECS) {
+            try {
+                new Socket(hostname, port);
+                System.out.println("\nStarted\n");
+                System.exit(0);
+            } catch (Exception e) {
+                System.out.println(format("SpliceTestPlatformWait: Not started, still waiting for '%s:%s'. %s of %s seconds elapsed.",
+                        hostname, port, elapsedSecs, MAX_WAIT_SECS));
+                Threads.sleep(1, TimeUnit.SECONDS);
+            }
+            elapsedSecs = (long) ((System.currentTimeMillis() - startTime) / 1000d);
+        }
+
+        System.out.println(format("Waited %s seconds without success", MAX_WAIT_SECS));
+        System.exit(-1);
+    }
+
+}
