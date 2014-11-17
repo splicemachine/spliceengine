a0f6e7634171d62c37eddb714e8fa39b49d57433
John Leach <jleach@SpliceMachines-MacBook-Pro.local>
2014-11-17 09:59:35 -0600

    Pulled forward filter row key

--- splice_si_adapter_94/src/main/java/com/splicemachine/si/impl/region/ActiveTxnFilter.java
+++ splice_si_adapter_94/src/main/java/com/splicemachine/si/impl/region/ActiveTxnFilter.java
@@ -2,6 +2,8 @@ package com.splicemachine.si.impl.region;
 
 import org.apache.hadoop.hbase.KeyValue;
 
+import com.splicemachine.si.impl.TxnUtils;
+
 /**
  * @author Scott Fines
  *         Date: 8/18/14
@@ -16,4 +18,17 @@ public class ActiveTxnFilter extends BaseActiveTxnFilter<KeyValue> {
     public ReturnCode filterKeyValue(KeyValue kv) {
     	return this.internalFilter(kv);
     }
+    
+    @Override
+    public boolean filterRowKey(byte[] buffer, int offset, int length) {
+				/*
+				 * Since the transaction id must necessarily be the begin timestamp in both
+				 * the old and new format, we can filter transactions out based entirely on the
+				 * row key here
+				 */
+        long txnId = TxnUtils.txnIdFromRowKey(buffer, offset, length);
+        boolean withinRange = txnId >= afterTs && txnId <= beforeTs;
+        return !withinRange;
+    }
+    
 }
