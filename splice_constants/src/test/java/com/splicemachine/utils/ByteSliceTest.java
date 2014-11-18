debb702b6cac269befa03abdf5cf513492741846
Keith Winkler <kwinkler@splicemachine.com>
2014-11-18 11:29:35 -0600

    merge: 37a44f8 DB-2189: ByteSlice: add at few tests asserting properties of ByteSlice that existing code depends on

--- splice_constants/src/test/java/com/splicemachine/utils/ByteSliceTest.java
+++ splice_constants/src/test/java/com/splicemachine/utils/ByteSliceTest.java
@@ -2,9 +2,25 @@ package com.splicemachine.utils;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class ByteSliceTest {
+
+    /* Test to ensure that ByteSlice set methods continue to replace buffer rather than mutating existing buffer. */
+    @Test
+    public void set_replacesBuffer_ratherThanUpdatingIt_evenIfNewArrayIsSmaller() {
+        byte[] wrappedBytes = new byte[]{0, 1, 2, 3, 4, 5, 6, 7};
+        ByteSlice byteSlice = ByteSlice.wrap(wrappedBytes);
+        assertSame("array() returns reference to underlying buffer", wrappedBytes, byteSlice.array());
+
+        byte[] bytesBytes = new byte[]{0, 1, 2, 3, 4};
+        byteSlice.set(bytesBytes);
+        assertSame("expected new array reference", bytesBytes, byteSlice.array());
+
+        byte[] bytesBytes2 = new byte[]{0, 1, 2};
+        byteSlice.set(bytesBytes2, 0, 3);
+        assertSame("expected new array reference", bytesBytes2, byteSlice.array());
+    }
 
     @Test
     public void set_zeroLength_zeroOffset() {
@@ -31,6 +47,15 @@ public class ByteSliceTest {
     }
 
     @Test
+    public void getByteCopy() {
+        byte[] bytes = new byte[]{0, 1, 2, 3, 4, 5};
+        ByteSlice byteSlice = ByteSlice.wrap(bytes);
+        byte[] copy = byteSlice.getByteCopy();
+        assertNotSame("should not be the same", bytes, copy);
+        assertArrayEquals("but should be equal", bytes, copy);
+    }
+
+    @Test
     public void toHexString() {
         ByteSlice byteSlice = new ByteSlice();
         byteSlice.set(new byte[]{18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0});
@@ -53,5 +78,13 @@ public class ByteSliceTest {
         assertEquals(-1, byteSlice.find((byte) 120, 0));
     }
 
+    @Test
+    public void reverse() {
+        byte[] bytes = new byte[]{0, 1, 2, 3};
+        ByteSlice byteSlice = ByteSlice.wrap(bytes);
+        byteSlice.reverse();
+        assertSame(bytes, byteSlice.array());
+        assertArrayEquals(new byte[]{-1, -2, -3, -4}, byteSlice.array());
+    }
 
 }
