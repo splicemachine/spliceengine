07a201dad802f368017db19826f8d136cbd86ca1
Keith Winkler <kwinkler@splicemachine.com>
2014-11-19 14:08:21 -0600

    merge: 19a115a Revert "Revert "added post exec hook""

--- splice_machine/src/main/java/com/splicemachine/intg/hive/SpliceStorageHandler.java
+++ splice_machine/src/main/java/com/splicemachine/intg/hive/SpliceStorageHandler.java
@@ -4,11 +4,9 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -17,53 +15,36 @@ import org.apache.hadoop.hive.metastore.MetaStoreUtils;
 import org.apache.hadoop.hive.metastore.api.MetaException;
 import org.apache.hadoop.hive.metastore.api.Table;
 import org.apache.hadoop.hive.metastore.api.hive_metastoreConstants;
-import org.apache.hadoop.hive.ql.hooks.ExecuteWithHookContext;
-import org.apache.hadoop.hive.ql.hooks.HookContext;
-import org.apache.hadoop.hive.ql.hooks.LineageInfo;
-import org.apache.hadoop.hive.ql.hooks.PostExecute;
-import org.apache.hadoop.hive.ql.hooks.ReadEntity;
-import org.apache.hadoop.hive.ql.hooks.WriteEntity;
 import org.apache.hadoop.hive.ql.metadata.DefaultStorageHandler;
 import org.apache.hadoop.hive.ql.metadata.HiveStoragePredicateHandler;
 import org.apache.hadoop.hive.ql.plan.ExprNodeDesc;
 import org.apache.hadoop.hive.ql.plan.TableDesc;
-import org.apache.hadoop.hive.ql.session.SessionState;
 import org.apache.hadoop.hive.serde2.Deserializer;
 import org.apache.hadoop.hive.serde2.SerDe;
 import org.apache.hadoop.mapred.InputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputFormat;
-import org.apache.hadoop.security.UserGroupInformation;
 
 import com.splicemachine.mrio.api.SQLUtil;
 import com.splicemachine.mrio.api.SpliceMRConstants;
 
 public class SpliceStorageHandler extends DefaultStorageHandler
-implements HiveMetaHook, HiveStoragePredicateHandler, PostExecute{
+implements HiveMetaHook, HiveStoragePredicateHandler{
 
 	private Configuration spliceConf;
 	final static public String DEFAULT_PREFIX = "default.";
 	private SQLUtil sqlUtil = null;
-
+	private static Connection parentConn = null;
 	private String parentTxnId = null;
-	boolean performInput = true;
-	private Connection parentConn = null;
 	
 	private String getSpliceTableName(Table tbl)
 	{
-		String tableName = tbl.getParameters().get(SpliceSerDe.SPLICE_INPUT_TABLE_NAME);
-		if(tableName == null){
-			tableName = tbl.getParameters().get(SpliceSerDe.SPLICE_OUTPUT_TABLE_NAME);
-			performInput = false;
-		}
+		String tableName = tbl.getParameters().get(SpliceSerDe.SPLICE_TABLE_NAME);
 		if(tableName == null)
 		{
 			// Note: Have to look at What is in SerdeInfo.
 			// Now I'm just imitating what HBase does
-			if(performInput)
-				tableName = tbl.getSd().getSerdeInfo().getParameters().get(SpliceSerDe.SPLICE_INPUT_TABLE_NAME);
-			else
-				tableName = tbl.getSd().getSerdeInfo().getParameters().get(SpliceSerDe.SPLICE_OUTPUT_TABLE_NAME);
+			tableName = tbl.getSd().getSerdeInfo().getParameters().get(SpliceSerDe.SPLICE_TABLE_NAME);
 		}
 		 if (tableName == null) {
 		        tableName = tbl.getDbName() + "." + tbl.getTableName();
@@ -76,70 +57,36 @@ implements HiveMetaHook, HiveStoragePredicateHandler, PostExecute{
 	}
 	
 	public void configureTableJobProperties(TableDesc tableDesc,
-		    								Map<String, String> jobProperties, boolean isInputJob)
+		    								Map<String, String> jobProperties)
 	{
 		Properties tableProperties = tableDesc.getProperties();
-		String tableName = null;
-		String connStr = tableProperties.getProperty(SpliceSerDe.SPLICE_JDBC_STR);
-		if(sqlUtil == null)
-			sqlUtil = SQLUtil.getInstance(connStr);
-		if(isInputJob)
-			tableName = tableProperties.getProperty(SpliceSerDe.SPLICE_INPUT_TABLE_NAME);
-		else
-	    	tableName = tableProperties.getProperty(SpliceSerDe.SPLICE_OUTPUT_TABLE_NAME);
-	    
+
+	    String tableName = tableProperties.getProperty(SpliceSerDe.SPLICE_TABLE_NAME);
+	    System.out.println("---------- SpliceStorageHandler configureTableJobProperties, "
+	    					+ "tableName getting from tableProperties:"
+	    					+ tableName + "-----------");
 	    if (tableName == null) {
-	      tableName = tableProperties.getProperty(hive_metastoreConstants.META_TABLE_NAME);
+	      tableName =
+	        tableProperties.getProperty(hive_metastoreConstants.META_TABLE_NAME);
 	      
 	      if (tableName.startsWith(DEFAULT_PREFIX)) {
 	          tableName = tableName.substring(DEFAULT_PREFIX.length());
 	        }
 	      System.out.println("=========== SpliceStorageHandler configureTableJobProperties, "
 					+ "tableName getting from hive metastore:"
-					+ tableName + "============"+"isInputJob?"+isInputJob);
+					+ tableName + "============");
 	    }
 	    tableName = tableName.trim();
-	   
-	    if(isInputJob){
-	    	//jobProperties.put(SpliceSerDe.SPLICE_TRANSACTION_ID, sqlUtil.getTransactionID());
-	    	jobProperties.put(SpliceSerDe.SPLICE_INPUT_TABLE_NAME, tableName);
-	    }
-	    else{
-	    	jobProperties.put(SpliceSerDe.SPLICE_OUTPUT_TABLE_NAME, tableName);
-	    	parentTxnId = startWriteJobParentTxn(connStr, tableName);
-	    	jobProperties.put(SpliceSerDe.SPLICE_TRANSACTION_ID, parentTxnId);
-	    }
+	    String connStr = tableProperties.getProperty(SpliceSerDe.SPLICE_JDBC_STR);
+	    jobProperties.put(SpliceSerDe.SPLICE_TABLE_NAME, tableName);
 	    jobProperties.put(SpliceSerDe.SPLICE_JDBC_STR, connStr);
-		
-	}
-	
-	public String startWriteJobParentTxn(String connStr, String tableName){
-		
-		if (sqlUtil == null)
-			sqlUtil = SQLUtil.getInstance(connStr);
-		try {
-			parentConn = sqlUtil.createConn();
-			sqlUtil.disableAutoCommit(parentConn);
-			String pTxsID = sqlUtil.getTransactionID(parentConn);
-			System.out.println("parent TxnID in StorageHandler:" + pTxsID);
-			PreparedStatement ps = parentConn
-					.prepareStatement("call SYSCS_UTIL.SYSCS_ELEVATE_TRANSACTION(?)");
-			ps.setString(1, tableName);
-			ps.executeUpdate();
-			return pTxsID;
-		} catch (SQLException e) {
-			return null;
-		} catch (InstantiationException e) {
-			// TODO Auto-generated catch block
-			return null;
-		} catch (IllegalAccessException e) {
-			// TODO Auto-generated catch block
-			return null;
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			return null;
-		}
-		
+	    if(sqlUtil == null){
+	    	sqlUtil = SQLUtil.getInstance(connStr);	
+	    }
+	    /*if(parentTxnId == null){
+	    	parentTxnId = sqlUtil.getTransactionID();
+		    jobProperties.put(SpliceSerDe.SPLICE_TRANSACTION_ID, parentTxnId);
+	    } */   
 	}
 	
 	@Override
@@ -149,7 +96,7 @@ implements HiveMetaHook, HiveStoragePredicateHandler, PostExecute{
 		System.out.println("SpliceStorageHandler, configureInputJobProperties:"
 				+ "get tableName from tableDesc: "+tableDesc.getTableName());
 		
-	    configureTableJobProperties(tableDesc, jobProperties, true);
+	      configureTableJobProperties(tableDesc, jobProperties);
 	  }
 	
 	@Override
@@ -158,7 +105,7 @@ implements HiveMetaHook, HiveStoragePredicateHandler, PostExecute{
 	    Map<String, String> jobProperties) {
 		System.out.println("SpliceStorageHandler, configureOutputJobProperties:"
 				+ "get tableName from tableDesc: "+tableDesc.getTableName());
-	      configureTableJobProperties(tableDesc, jobProperties, false);
+	      configureTableJobProperties(tableDesc, jobProperties);
 	  }
 	
 	@Override
@@ -263,18 +210,13 @@ implements HiveMetaHook, HiveStoragePredicateHandler, PostExecute{
 	@Override
 	public Class<? extends OutputFormat> getOutputFormatClass() {
 		
-	    return HiveSpliceTableOutputFormat.class;
+	    return HiveSpliceOutputFormat.class;
 	}
 
-	@Override
-	public void run(SessionState sess, Set<ReadEntity> inputs,
-			Set<WriteEntity> outputs, LineageInfo lInfo,
-			UserGroupInformation ugi) throws Exception {
-		// TODO Auto-generated method stub
-		if(parentConn != null)
-			parentConn.commit();
-		System.out.println("Hive hook post exec, commit");
-	}
+    public static void commitParentTxn() throws SQLException{
+        if(parentConn != null)
+            parentConn.commit();
+    }
 
 }
 
