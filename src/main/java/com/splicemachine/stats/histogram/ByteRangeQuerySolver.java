8a5243b60f7f8a7bc685511c46036ddb1426e462
Scott Fines <scottfines@gmail.com>
2014-12-01 10:19:18 -0600

    Creating a ByteRangeQuerySolver abstraction, plus a simple dense implementation

--- src/main/java/com/splicemachine/stats/histogram/ByteRangeQuerySolver.java
+++ src/main/java/com/splicemachine/stats/histogram/ByteRangeQuerySolver.java
@@ -4,5 +4,17 @@ package com.splicemachine.stats.histogram;
  * @author Scott Fines
  *         Date: 12/1/14
  */
-public interface ByteRangeQuerySolver {
+public interface ByteRangeQuerySolver extends RangeQuerySolver<Byte>{
+
+    byte max();
+
+    byte min();
+
+    long equal(byte value);
+
+    long between(byte startValue, byte endValue, boolean inclusiveStart, boolean inclusiveEnd);
+
+    long after(byte n, boolean equals);
+
+    long before(byte n, boolean equals);
 }
