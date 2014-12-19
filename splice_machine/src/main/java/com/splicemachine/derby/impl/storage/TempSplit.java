c04c9f5ddc8a66eeeb2c71e5b88d0a08482ee72e
Walter Koetke <wkoetke@splicemachine.com>
2014-12-18 17:58:26 -0800

    Reintroducing stored procedure to rebuild SPLICE_TEMP from scratch. We purged this awhile back on purpose b/c it's wrongly named and dangerous if misused, but it turns out we really need to rebuild splice_temp in certain circumstances, such as POCs and testing clusters where splice_temp gets abused.
    So in 1.0.1 (or patch on top of 1.0.0), to add this to an existing
    system, run syscs_util.syscs_update_all_system_procedures()
    to add it to the data dictionary, then use it to purge and recreate
    temp table: syscs_util.syscs_split_temp().

--- splice_machine/src/main/java/com/splicemachine/derby/impl/storage/TempSplit.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/storage/TempSplit.java
@@ -1,27 +1,75 @@
 package com.splicemachine.derby.impl.storage;
 
+import java.io.IOException;
 import java.sql.SQLException;
+
+import com.splicemachine.derby.impl.temp.TempTable;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.log4j.Logger;
+
+import com.splicemachine.constants.SpliceConstants;
+import com.splicemachine.utils.SpliceLogUtils;
+import com.splicemachine.utils.SpliceUtilities;
 import org.apache.log4j.Logger;
 
 /**
- * Permanently desupported. Do not use.
+ * Utility to split the TEMP table.
  */
 public class TempSplit {
+	
+	//
+	// IMPORTANT: please be aware this does much more than 'split' the temp table.
+	// In fact, it doesn't do that at all. Rather, it disables and removes
+	// the SPLICE_TEMP table and creates a brand new one with preset region count.
+	// and split keys. Only use this if you know what you are doing.
+	// 
+	
+	private static final Logger LOG = Logger.getLogger(TempSplit.class);
+ 
+	/**
+	 * Split the TEMP table into a fixed number of buckets.
+	 *
+	 * It disables and removes the TEMP table and creates a new one
+	 * with the right split keys.
+	 */
+	public static void SYSCS_SPLIT_TEMP() throws SQLException{
+ 
+		SpliceLogUtils.info(LOG, "Recreating SPLICE_TEMP table from scratch and splitting into preset buckets...");
+		
+        HBaseAdmin admin = SpliceUtilities.getAdmin();
+        try {
+    		SpliceLogUtils.info(LOG, "Recreating SPLICE_TEMP table (step 1 - disable)...");
+            admin.disableTable(SpliceConstants.TEMP_TABLE_BYTES);
+        } catch (IOException e) {
+            SpliceLogUtils.error(LOG,"Unable to disable the TEMP table",e);
+        }
 
-    // This stored procedure that calls this has been purged.
-    // As of version 1.0 (Fuji), it will not automatically be
-    // deleted from the data dictionary of an upgraded database.
-    // Therefore, to be safe, leave this backing class here
-    // but throw runtime exception if invoked.
+        try {
+    		SpliceLogUtils.info(LOG, "Recreating SPLICE_TEMP table (step 2 - delete)...");
+            admin.deleteTable(SpliceConstants.TEMP_TABLE_BYTES);
+        } catch (IOException e) {
+            SpliceLogUtils.error(LOG,"Unable to delete the TEMP table",e);
+        }
+        
+        try {
+    		SpliceLogUtils.info(LOG, "Recreating SPLICE_TEMP table (step 3 - create)...");
+            SpliceUtilities.createTempTable(admin);
+        } catch (IOException e) {
+            SpliceLogUtils.error(LOG,"Unable to create the TEMP table",e);
+            return;
+        }
+        
+        try {
+    		SpliceLogUtils.info(LOG, "Recreating SPLICE_TEMP table (step 4 - balance)...");
+            boolean runs = admin.balancer();
+            if (!runs) {
+                SpliceLogUtils.warn(LOG,"Unable to launch the balancer");
+            }
+        } catch (Exception e) {
+            SpliceLogUtils.error(LOG,"Unable to launch the balancer",e);
+        }
 
-    private static final Logger LOG = Logger.getLogger(TempSplit.class);
-
-    /**
-     * @deprecated No longer supported. No replacement. Throws exception if invoked.
-     */
-    public static void SYSCS_SPLIT_TEMP() throws SQLException{
-
-        throw new UnsupportedOperationException("This procedure is not supported.");
-
-    }
+		SpliceLogUtils.info(LOG, "Done cecreating SPLICE_TEMP table!");
+    	
+     }
 }
