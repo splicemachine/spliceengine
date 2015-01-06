db30891ec503a6bb85bdfe135940b26b3bb664cc
John Leach <jleach@splicemachine.com>
2015-01-06 10:07:28 -0600

    DB-2521 Code Cleanup

--- splice_machine/src/main/java/com/splicemachine/mrio/api/SpliceMRConstants.java
+++ splice_machine/src/main/java/com/splicemachine/mrio/api/SpliceMRConstants.java
@@ -1,11 +1,15 @@
 package com.splicemachine.mrio.api;
+/**
+ * 
+ * Splice Constants...
+ * 
+ *
+ */
 public class SpliceMRConstants {
-
 	public final static String SPLICE_TRANSACTION_ID = "transaction_id";
 	final public static String SPLICE_INPUT_TABLE_NAME = "splice_input_tableName";
 	final public static String SPLICE_OUTPUT_TABLE_NAME = "splice_output_tableName";
 	final protected static String HBASE_OUTPUT_TABLE_NAME = "hbase_output_tableName";
 	final public static String SPLICE_WRITE_BUFFER_SIZE = "splice_write_buffer_size";
-
 	final public static String SPLICE_JDBC_STR = "splice_jdbc";
 }
