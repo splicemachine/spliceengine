3e71ea40afa90ceca7637f9b2d1fc1ac969cc1c9
John Leach <jleach@splicemachine.com>
2015-01-06 10:10:01 -0600

    DB-2521 Code Cleanup

--- splice_machine/src/main/java/com/splicemachine/mrio/api/SpliceTableRecordReader.java
+++ splice_machine/src/main/java/com/splicemachine/mrio/api/SpliceTableRecordReader.java
@@ -1,12 +1,10 @@
 package com.splicemachine.mrio.api;
 
 import java.io.IOException;
-
 import org.apache.derby.iapi.error.StandardException;
 import org.apache.derby.iapi.sql.execute.ExecRow;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.client.HTable;
-import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.mapreduce.InputSplit;
