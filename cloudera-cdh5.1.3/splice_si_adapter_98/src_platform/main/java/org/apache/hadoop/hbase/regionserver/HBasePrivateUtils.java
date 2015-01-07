05e6fbd7c2b67f7ec0751d2a1c0305eb4a7e59cc
John Leach <jleach@splicemachine.com>
2015-01-07 12:04:57 -0600

    DB-2521 Paving way for spark and migrating to cdh5.2 and HDP 2.2

---
+++ cloudera-cdh5.1.3/splice_si_adapter_98/src_platform/main/java/org/apache/hadoop/hbase/regionserver/HBasePrivateUtils.java
@@ -0,0 +1,18 @@
+package org.apache.hadoop.hbase.regionserver;
+
+import org.apache.hadoop.hbase.catalog.CatalogTracker;
+import org.apache.hadoop.hbase.master.MasterServices;
+
+public class HBasePrivateUtils {
+	public static KeyValueSkipListSet getKvset(HStore store) {
+		return store.memstore.kvset;
+	}
+
+	public static KeyValueSkipListSet getSnapshot(HStore store) {
+		return store.memstore.snapshot;
+	}
+
+	public static CatalogTracker getCatalogTracker(MasterServices masterServices) {
+		return masterServices.getCatalogTracker();
+	}
+}
