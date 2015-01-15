468098ac996b34eb67faa130041c05b9425991ad
VladRodionov <vladrodionov@gmail.com>
2015-01-15 14:13:58 -0800

    DB-2405: MapR fixed SI tests
    Conflicts:
    	splice_si_test/src/test/java/com/splicemachine/si/HStoreSetup.java

--- splice_si_test/src/test/java/com/splicemachine/si/HStoreSetup.java
+++ splice_si_test/src/test/java/com/splicemachine/si/HStoreSetup.java
@@ -86,7 +86,14 @@ public class HStoreSetup implements StoreSetup {
 						TransactionTimestamps.setTimestampSource(timestampSource);
 						Configuration configuration = testCluster.getConfiguration();
 						configuration.set("hbase.coprocessor.region.classes", TxnLifecycleEndpoint.class.getName());
-						SpliceConstants.config = configuration;
+    // -> MapR work-around
+    configuration.set(FileSystem.FS_DEFAULT_NAME_KEY,"file:///");
+    configuration.set("fs.default.name", "file:///");
+    configuration.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+    System.setProperty("zookeeper.sasl.client", "false");   
+    System.setProperty("zookeeper.sasl.serverconfig", "fake");
+    // <- MapR work-around
+            SpliceConstants.config = configuration;
             setTestingUtilityPorts(testCluster, basePort);
 
             testCluster.startMiniCluster(1);
