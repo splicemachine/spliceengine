a4c757a733ac6ae218d8ab7efba434a88de1f7f4
Aaron Molitor <amolitor@splicemachine.com>
2015-01-15 17:00:58 -0600

    DB-2418 - adding comments to cleaned up configuration, changing a parameter and adding one

--- splice_machine_test/src/main/java/com/splicemachine/test/SpliceTestPlatformConfig.java
+++ splice_machine_test/src/main/java/com/splicemachine/test/SpliceTestPlatformConfig.java
@@ -12,6 +12,7 @@ import com.splicemachine.si.coprocessors.TimestampMasterObserver;
 import com.splicemachine.si.coprocessors.TxnLifecycleEndpoint;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.io.hfile.CacheConfig;
 
 import java.util.List;
@@ -67,23 +68,24 @@ class SpliceTestPlatformConfig {
         config.setInt("hbase.master.info.port", masterInfoPort);
         config.setInt("hbase.regionserver.port", regionServerPort);
         config.setInt("hbase.regionserver.info.port", regionServerInfoPort);
-        config.setInt("hbase.master.jmx.port", 10102);
+        config.setInt("hbase.master.jmx.port", 10102); // this is set because the HBase master and regionserver are running on the same machine and in the same JVM
         config.setInt(SpliceConstants.DERBY_BIND_PORT, derbyPort);
 
         //
         // Networking -- interfaces
         //
-        String interfaceName = System.getProperty("os.name").contains("Mac") ? "lo0" : "default";
-        config.set("hbase.zookeeper.dns.interface", interfaceName);
-        config.set("hbase.regionserver.dns.interface", interfaceName);
-        config.set("hbase.master.dns.interface", interfaceName);
+        // force use of loop back interface on MacOSX, else don't set it
+        if (System.getProperty("os.name").contains("Mac") ) {
+            String interfaceName = "lo0";
+            config.set("hbase.zookeeper.dns.interface", interfaceName);
+            config.set("hbase.master.dns.interface", interfaceName);
+            config.set("hbase.regionserver.dns.interface", interfaceName);
+        }
 
         //
         // File System
         //
-
-        // MapR Hack, tells it local filesystem
-        config.set("fs.default.name", "file:///");
+        config.set("fs.default.name", "file:///"); // MapR Hack, tells it local filesystem
         // Must allow Cygwin instance to config its own rootURI
         if (!"CYGWIN".equals(hbaseRootDirUri)) {
             config.set("hbase.rootdir", hbaseRootDirUri);
@@ -104,41 +106,41 @@ class SpliceTestPlatformConfig {
         //
         // Compaction Controls
         //
-        config.setLong("hbase.hstore.compaction.min", 5); // Minimum Number of Files for Compaction
-        config.setLong("hbase.hstore.compaction.max", 10);
-        config.setLong("hbase.hstore.compaction.min.size", 16 * MiB);
-        config.setLong("hbase.hstore.compaction.max.size", 248 * MiB);
+        config.setLong("hbase.hstore.compaction.min", 5); // min number of eligible files before we compact
+        config.setLong("hbase.hstore.compaction.max", 10); // max files to be selected for a single minor compaction
+        config.setLong("hbase.hstore.compaction.min.size", 16 * MiB); // store files smaller than this will always be eligible for minor compaction.  HFiles this size or larger are evaluated by hbase.hstore.compaction.ratio to determine if they are eligible
+        config.setLong("hbase.hstore.compaction.max.size", 248 * MiB); // store files larger than this will be excluded from compaction
+        config.setFloat("hbase.hstore.compaction.ratio", 1.25f); // default is 1.2f, at one point we had this set to 0.25f and 25f (which was likely a typo)
 
         //
         // Memstore, store files, splits
         //
-        config.setLong("hbase.hregion.max.filesize", 1024 * MiB);
-        config.setLong("hbase.hregion.memstore.flush.size", 512 * MiB); // Way too high
+        config.setLong(HConstants.HREGION_MAX_FILESIZE, 1024 * MiB); // hbase.hregion.max.filesize
+        config.setLong("hbase.hregion.memstore.flush.size", 128 * MiB); // was 512 MiB
         config.setLong("hbase.hregion.memstore.block.multiplier", 4);
-        config.setFloat("hbase.regionserver.global.memstore.size", 0.25f);
+        config.setFloat("hbase.regionserver.global.memstore.size", 0.25f); // set mem store to 25% of heap
         config.setLong("hbase.hstore.blockingStoreFiles", 20);
-        config.set("hbase.regionserver.region.split.policy", "org.apache.hadoop.hbase.regionserver.ConstantSizeRegionSplitPolicy");
+        config.set("hbase.regionserver.region.split.policy", "org.apache.hadoop.hbase.regionserver.ConstantSizeRegionSplitPolicy"); // change default split policy.  this makes more sense for a standalone/single regionserver
 
         //
         // HFile
         //
-        config.setInt("hfile.index.block.max.size", 16 * 1024); // Aaron
-        config.setFloat("hfile.block.cache.size", 0.25f);
+        config.setInt("hfile.index.block.max.size", 16 * 1024); // 16KiB
+        config.setFloat("hfile.block.cache.size", 0.25f); // set block cache to 25% of heap
         config.setFloat("io.hfile.bloom.error.rate", (float) 0.005);
+        config.setBoolean(CacheConfig.CACHE_BLOOM_BLOCKS_ON_WRITE_KEY, true); // hfile.block.bloom.cacheonwrite
 
         //
         // Misc
         //
-        config.set("hbase.cluster.distributed", "true");
-        config.set("hbase.master.distributed.log.splitting", "false"); // Why?
-        config.setBoolean(CacheConfig.CACHE_BLOOM_BLOCKS_ON_WRITE_KEY, true);
+        config.set("hbase.cluster.distributed", "true");  // don't start zookeeper for us
+        config.set("hbase.master.distributed.log.splitting", "false"); // TODO: explain why we are setting this
 
         //
         // Splice
         //
 
-        //wait 15 seconds before bailing on bad ddl statements
-        config.setLong("splice.ddl.drainingWait.maximum", SECONDS.toMillis(15));
+        config.setLong("splice.ddl.drainingWait.maximum", SECONDS.toMillis(15)); // wait 15 seconds before bailing on bad ddl statements
 
         config.setDouble(SpliceConstants.DEBUG_TASK_FAILURE_RATE, 0.05d);
         config.setBoolean(SpliceConstants.DEBUG_FAIL_TASKS_RANDOMLY, failTasksRandomly);
