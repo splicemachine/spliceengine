16fcc9f6654994858434aef648067f80ccc0f02f
Walter Koetke <wkoetke@splicemachine.com>
2015-01-26 12:17:53 -0800

    DB-2628: Someone turned off this entire test class SpliceAdminIT, with no ignore comment nor jira for turning it back on. Not cool.

--- splice_machine_test/src/test/java/com/splicemachine/derby/utils/SpliceAdminIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/utils/SpliceAdminIT.java
@@ -31,7 +31,6 @@ import com.splicemachine.test.SlowTest;
  *         Date: 12/11/13
  */
 @Category(SerialTest.class)
-@Ignore
 public class SpliceAdminIT {
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
     public static final String CLASS_NAME = SpliceAdminIT.class.getSimpleName().toUpperCase();
