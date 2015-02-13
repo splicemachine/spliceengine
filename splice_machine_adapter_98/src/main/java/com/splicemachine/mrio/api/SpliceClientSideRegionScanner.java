b96f73bdd2637ac1ff4854efb0fcd0fd36ac1198
John Leach <jleach@splicemachine.com>
2015-02-13 13:37:42 -0600

    Vlad's changes

--- splice_machine_adapter_98/src/main/java/com/splicemachine/mrio/api/SpliceClientSideRegionScanner.java
+++ splice_machine_adapter_98/src/main/java/com/splicemachine/mrio/api/SpliceClientSideRegionScanner.java
@@ -20,11 +20,7 @@ import org.apache.hadoop.hbase.regionserver.BaseHRegionUtil;
 import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.hadoop.hbase.regionserver.KeyValueScanner;
 import org.apache.hadoop.hbase.regionserver.RegionScanner;
-<<<<<<< Updated upstream
 
-=======
-import org.apache.log4j.Logger;
->>>>>>> Stashed changes
 /**
  * 
  * A Client Side Region Scanner that reads from store files and incremental
@@ -33,7 +29,6 @@ import org.apache.log4j.Logger;
  * 
  */
 public class SpliceClientSideRegionScanner implements RegionScanner {
-<<<<<<< Updated upstream
 	protected HRegion region;
 	protected RegionScanner scanner;
 
@@ -199,69 +194,4 @@ public class SpliceClientSideRegionScanner implements RegionScanner {
 		list.add(new SpliceMemstoreKeyValueScanner(table.getScanner(memScan)));
 		return list;
 	}
-=======
-    protected static final Logger LOG = Logger.getLogger(SpliceMemstoreKeyValueScanner.class);
-
-  protected HRegion region;
-  protected RegionScanner scanner;
-
-  public SpliceClientSideRegionScanner(Configuration conf, FileSystem fs,
-      Path rootDir, HTableDescriptor htd, HRegionInfo hri, Scan scan, ScanMetrics scanMetrics, List<KeyValueScanner> keyValueScanners) throws IOException {
-    scan.setIsolationLevel(IsolationLevel.READ_UNCOMMITTED);     // region is immutable, set isolation level
-    this.region = HRegion.openHRegion(conf, fs, rootDir, hri, htd, null, null, null);
-    this.scanner = BaseHRegionUtil.getScanner(region, scan, keyValueScanners);
-    region.startRegionOperation();
-  }
-
-@Override
-public boolean next(List<Cell> results) throws IOException {
-	return scanner.next(results);
-}
-
-@Override
-public boolean next(List<Cell> result, int limit) throws IOException {
-	return scanner.next(result,limit);
-}
-
-@Override
-public void close() throws IOException {
-	scanner.close();
-}
-
-@Override
-public HRegionInfo getRegionInfo() {
-	return scanner.getRegionInfo();
-}
-
-@Override
-public boolean isFilterDone() throws IOException {
-	return scanner.isFilterDone();
-}
-
-@Override
-public boolean reseek(byte[] row) throws IOException {
-	return scanner.reseek(row);
-}
-
-@Override
-public long getMaxResultSize() {
-	return scanner.getMaxResultSize();
-}
-
-@Override
-public long getMvccReadPoint() {
-	return scanner.getMvccReadPoint();
-}
-
-@Override
-public boolean nextRaw(List<Cell> result) throws IOException {
-	return scanner.nextRaw(result);
-}
-
-@Override
-public boolean nextRaw(List<Cell> result, int limit) throws IOException {
-	return scanner.nextRaw(result, limit);
-}
-  
->>>>>>> Stashed changes
 }
