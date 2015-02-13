167ec449a7607049661c10433323661108e4b82d
jeffcunningham <jcunningham@splicemachine.com>
2015-02-13 14:47:15 -0600

    DB-2863: second(timestamp) function fails.
    The problem originated when we added the Lazy*DataValueDescriptor classes.
    SQLTimestamp.getSeconds(NumberDataValue source) enforces that the 'source'
    is of type SQLDouble. The 'source' type is now LazyTimestampDataValueDescriptor.
    The fix was to change the enforcement so that the NumberDataValue checks that
    it contains a type double and all went fine. I ran the ITs on 1.0.x and all passed.
    
    Added a simple IT, LazyTimestampDataValueDescriptorTest.

---
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/tester/LazyTimestampDataValueDescriptorTest.java
@@ -0,0 +1,28 @@
+package com.splicemachine.derby.impl.sql.execute.tester;
+
+import org.apache.derby.iapi.types.SQLDouble;
+import org.apache.derby.iapi.types.SQLTimestamp;
+import org.joda.time.DateTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.splicemachine.derby.impl.sql.execute.LazyNumberDataValueDescriptor;
+import com.splicemachine.derby.impl.sql.execute.LazyTimestampDataValueDescriptor;
+
+/**
+ * TODO: Should be more tests here for LazyTimestampDataValueDescriptor. Adding this test for verification of fix for DB-2863.
+ *
+ * @author Jeff Cunningham
+ *         Date: 2/13/15
+ */
+public class LazyTimestampDataValueDescriptorTest {
+
+    @Test
+    public void testGetSeconds() throws Exception {
+        int expected = 32;
+        LazyTimestampDataValueDescriptor timestamp =
+            new LazyTimestampDataValueDescriptor(new SQLTimestamp(new DateTime(2012, 2, 14, 12, 12, expected, 0)));
+        double actual = timestamp.getSeconds(new LazyNumberDataValueDescriptor(new SQLDouble(0.0))).getDouble();
+        Assert.assertEquals(expected, actual, 0.0);
+    }
+}
