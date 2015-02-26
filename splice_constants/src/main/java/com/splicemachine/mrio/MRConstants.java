ee4c3655e7de7ca459be379fc43fe268b732feda
John Leach <jleach@splicemachine.com>
2015-02-26 17:27:16 -0600

    Adding Direct Conglomerate Passing

--- splice_constants/src/main/java/com/splicemachine/mrio/MRConstants.java
+++ splice_constants/src/main/java/com/splicemachine/mrio/MRConstants.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.hbase.KeyValue;
 public class MRConstants {
 	public final static String SPLICE_TRANSACTION_ID = "transaction_id";
 	final public static String SPLICE_INPUT_TABLE_NAME = "splice_input_tableName";
+	final public static String SPLICE_INPUT_CONGLOMERATE = "splice_input_conglomerate";	
 	final public static String SPLICE_OUTPUT_TABLE_NAME = "splice_output_tableName";
 	final public static String HBASE_OUTPUT_TABLE_NAME = "hbase_output_tableName";
 	final public static String SPLICE_WRITE_BUFFER_SIZE = "splice_write_buffer_size";
