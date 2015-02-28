714b206e077748807ba3353a7fe586f25fa2350a
Keith Winkler <kwinkler@splicemachine.com>
2015-02-28 15:46:46 -0800

    DB-2576: Add a method to TableDAO to delete tables recursively, accounting for FKs.

--- splice_machine_test/src/test/java/com/splicemachine/test_dao/TableDAO.java
+++ splice_machine_test/src/test/java/com/splicemachine/test_dao/TableDAO.java
@@ -3,6 +3,9 @@ package com.splicemachine.test_dao;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
+import java.util.List;
+
+import static java.lang.String.format;
 
 /**
  * Manipulate tables.
@@ -16,15 +19,11 @@ public class TableDAO {
     }
 
     /**
-     * Attempts to drop each table in the order passed, ignores any exceptions.
+     * Drop the specified tables.
      */
-    public void deleteTableForce(String... tableNames) {
+    public void drop(String schemaName, String... tableNames) {
         for (String tableName : tableNames) {
-            try {
-                jdbcTemplate.executeUpdate("drop table " + tableName);
-            } catch (Exception e) {
-                /* ignore per method contract */
-            }
+            drop(schemaName, tableName, false);
         }
     }
 
@@ -37,18 +36,53 @@ public class TableDAO {
             ResultSet rs = metaData.getTables(null, schemaName.toUpperCase(), tableName.toUpperCase(), null);
             if (rs.next()) {
                 if (isView) {
-                    jdbcTemplate.executeUpdate(String.format("drop view %s.%s", schemaName.toUpperCase(), tableName.toUpperCase()));
+                    jdbcTemplate.executeUpdate(format("drop view %s.%s", schemaName, tableName));
                 } else {
-                    jdbcTemplate.executeUpdate(String.format("drop table if exists %s.%s", schemaName.toUpperCase(), tableName.toUpperCase()));
+                    jdbcTemplate.executeUpdate(format("drop table if exists %s.%s", schemaName, tableName));
                 }
             }
+        } catch (Exception e) {
+            // Drop failed because of FK constraint.
+            if (e.getMessage().contains("DROP CONSTRAINT") && e.getMessage().contains("is dependent on that object")) {
+                dropTableRecursive(schemaName, tableName);
+            } else {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Delete the specified table, deleting any tables with FKs referencing it first, recursively.
+     */
+    public void dropTableRecursive(String schemaName, String tableName) {
+        try {
+            List<String> dependentTableNames = getDependentTableNames(schemaName, tableName);
+            for (String dependentTableName : dependentTableNames) {
+                dropTableRecursive(schemaName, dependentTableName);
+            }
+            jdbcTemplate.executeUpdate(format("drop table if exists %s.%s", schemaName, tableName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void drop(String schemaName, String tableName) {
-        drop(schemaName, tableName, false);
+    /**
+     * Returns a list of table names in the specified schema that have FKs referencing the specified table.
+     */
+    private List<String> getDependentTableNames(String schemaName, String tableName) {
+        String sql = "" +
+                "select t.tablename " +
+                " from sys.systables t " +
+                "  join sys.SYSCONSTRAINTS c on c.tableid = t.tableid " +
+                "  where c.constraintid in ( " +
+                "    select fk.CONSTRAINTID " +
+                "    from sys.sysforeignkeys fk " +
+                "      join sys.sysconstraints c on fk.keyconstraintid = c.constraintid " +
+                "      join sys.systables      t on t.tableid  = c.tableid " +
+                "      join sys.sysschemas     s on s.schemaid = t.schemaid " +
+                "    where s.SCHEMANAME=? " +
+                "          and t.tablename=?      )";
+        return jdbcTemplate.query(sql, schemaName, tableName);
     }
 
 }
