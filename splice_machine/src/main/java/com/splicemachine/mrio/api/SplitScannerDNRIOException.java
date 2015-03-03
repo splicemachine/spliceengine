2c45caf84b2e2830ae37fa7ad5dd55aca249c769
John Leach <jleach@splicemachine.com>
2015-03-02 19:36:30 -0600

    Add new DNRIO Exception

---
+++ splice_machine/src/main/java/com/splicemachine/mrio/api/SplitScannerDNRIOException.java
@@ -0,0 +1,8 @@
+package com.splicemachine.mrio.api;
+
+import org.apache.hadoop.hbase.DoNotRetryIOException;
+
+public class SplitScannerDNRIOException extends DoNotRetryIOException {
+	private static final long serialVersionUID = -175989532140489323L;
+
+}
