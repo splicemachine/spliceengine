374a6414585e84b2608ed367d7b96b25c2f54d15
John Leach <jleach@splicemachine.com>
2015-03-04 13:43:06 -0600

    Add Parameterized JDBC URL

--- splice_machine_test/src/test/java/com/splicemachine/mrio/api/HiveIntegrationTest.java
+++ splice_machine_test/src/test/java/com/splicemachine/mrio/api/HiveIntegrationTest.java
@@ -8,13 +8,21 @@ import java.sql.Statement;
 import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore
+import com.splicemachine.constants.SpliceConstants;
+import com.splicemachine.derby.test.framework.SpliceNetConnection;
+
 public class HiveIntegrationTest {
 
 	private static String driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
 	
 	static {
 		try {
+
+			// Requires alter table primary key support
+//			System.setProperty("javax.jdo.option.ConnectionURL", SpliceNetConnection.getDefaultLocalURL());
+//			System.setProperty("javax.jdo.option.ConnectionUserName", "splice");
+//			System.setProperty("javax.jdo.option.ConnectionPassword", "admin");
+//			System.setProperty("javax.jdo.option.ConnectionDriverName", SpliceConstants.SPLICE_JDBC_DRIVER);
 			Class.forName(driverName);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -26,7 +34,6 @@ public class HiveIntegrationTest {
 		Connection con = DriverManager.getConnection("jdbc:hive://");
 	    Statement stmt = con.createStatement();
 	    String tableName = "testHiveDriverTable";
-	    stmt.executeQuery("drop table " + tableName);
 	}
 	
 }
