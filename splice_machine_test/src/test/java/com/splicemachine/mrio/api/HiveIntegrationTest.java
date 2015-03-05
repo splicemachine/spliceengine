89488af34a822fb6c9ca73a4ee234f559bd04511
John Leach <jleach@splicemachine.com>
2015-03-05 15:46:56 -0600

    Removing Hive, JDO not enabled

--- splice_machine_test/src/test/java/com/splicemachine/mrio/api/HiveIntegrationTest.java
+++ splice_machine_test/src/test/java/com/splicemachine/mrio/api/HiveIntegrationTest.java
@@ -1,39 +1,31 @@
 package com.splicemachine.mrio.api;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import org.junit.Ignore;
 import org.junit.Test;
-
-import com.splicemachine.constants.SpliceConstants;
-import com.splicemachine.derby.test.framework.SpliceNetConnection;
 
 public class HiveIntegrationTest {
 
 	private static String driverName = "org.apache.hadoop.hive.jdbc.HiveDriver";
 	
 	static {
-		try {
+	//	try {
 
 			// Requires alter table primary key support
 //			System.setProperty("javax.jdo.option.ConnectionURL", SpliceNetConnection.getDefaultLocalURL());
 //			System.setProperty("javax.jdo.option.ConnectionUserName", "splice");
 //			System.setProperty("javax.jdo.option.ConnectionPassword", "admin");
 //			System.setProperty("javax.jdo.option.ConnectionDriverName", SpliceConstants.SPLICE_JDBC_DRIVER);
-			Class.forName(driverName);
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+//			Class.forName(driverName);
+//		} catch (ClassNotFoundException e) {
+//			e.printStackTrace();
+//		}
 	}
 	
 	@Test
 	public void testSimpleQuery() throws SQLException {
-		Connection con = DriverManager.getConnection("jdbc:hive://");
-	    Statement stmt = con.createStatement();
-	    String tableName = "testHiveDriverTable";
+//		Connection con = DriverManager.getConnection("jdbc:hive://");
+//	    Statement stmt = con.createStatement();
+//	    String tableName = "testHiveDriverTable";
 	}
 	
 }
