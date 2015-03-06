a8efea649ade931f5e7310871a78be4f3677c759
Scott Fines <scottfines@gmail.com>
2015-03-06 10:11:21 -0600

    Adding primitive specific methods to the BooleanDistribution

--- src/main/java/com/splicemachine/stats/estimate/BooleanDistribution.java
+++ src/main/java/com/splicemachine/stats/estimate/BooleanDistribution.java
@@ -15,6 +15,10 @@ public class BooleanDistribution implements Distribution<Boolean> {
         this.elements = elements;
     }
 
+    public long selectivity(boolean element){
+        return element? elements.equalsTrue().count(): elements.equalsFalse().count();
+    }
+
     @Override
     public long selectivity(Boolean element) {
         if(element==null) return nullCount;
@@ -23,24 +27,36 @@ public class BooleanDistribution implements Distribution<Boolean> {
         else return elements.equalsFalse().count();
     }
 
-    @Override
-    public long rangeSelectivity(Boolean start, Boolean stop, boolean includeStart, boolean includeStop) {
-        if(start==null){
-            if(stop==null) return elements.equalsTrue().count()+elements.equalsFalse().count();
-            else return rangeSelectivity(Boolean.TRUE,stop,true,includeStop);
-        }else if(stop==null){
-            return rangeSelectivity(start,Boolean.FALSE,includeStart,true);
-        }else
+    public long rangeSelectivity(boolean start, boolean stop, boolean includeStart, boolean includeStop){
         if(start==stop){
-            if(includeStart && includeStop) return elements.equal(start).count();
-            return 0l;
+            if(includeStart && includeStop) return elements.equals(start).count();
+            else return 0l; //empty set has 0 elements
         }else{
-            //since start!=stop, we assume that start<stop, which means that start = TRUE, and stop = FALSE
-            //so we just have to compute the edge components
-            long count = 0;
+            /*
+             * We now have the following states
+             *
+             * start && !stop
+             * !start && stop
+             *
+             * And the second option is invalid, because TRUE comes before FALSE in our arbitrary ordering,
+             * so we only have start && !stop
+             */
+            assert start : "Cannot get range selectivity, stop comes before start!";
+            long count = 0l;
             if(includeStart) count+=elements.equalsTrue().count();
             if(includeStop) count+=elements.equalsFalse().count();
             return count;
         }
+    }
+
+    @Override
+    public long rangeSelectivity(Boolean start, Boolean stop, boolean includeStart, boolean includeStop) {
+        if(start==null){
+            if(stop==null) return elements.equalsTrue().count()+elements.equalsFalse().count();
+            else return rangeSelectivity(true,stop.booleanValue(),true,includeStop);
+        }else if(stop==null){
+            return rangeSelectivity(start.booleanValue(),false,includeStart,true);
+        }else
+            return rangeSelectivity(start.booleanValue(),stop.booleanValue(),includeStart,includeStop);
     }
 }
