3add315715f99b202c9dc50e9e2923b3e540622e
Keith Winkler <kwinkler@splicemachine.com>
2015-03-09 10:35:14 -0500

    DB-2940: Move splice ITs out of "org.apache.derby" package.

---
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/DeleteOperationIT.java
@@ -0,0 +1,222 @@
+package com.splicemachine.derby.impl.sql.execute.operations;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+
+import com.splicemachine.pipeline.exception.ErrorState;
+
+import com.splicemachine.db.iapi.reference.SQLState;
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.splicemachine.derby.test.framework.SpliceDataWatcher;
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.derby.test.framework.SpliceTableWatcher;
+import com.splicemachine.derby.test.framework.SpliceUnitTest;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
+
+/**
+ * @author Scott Fines
+ *         Created: 2/5/13 12:24 PM
+ */
+//@Category(SerialTest.class)
+public class DeleteOperationIT extends SpliceUnitTest {
+	protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
+	public static final String CLASS_NAME = DeleteOperationIT.class.getSimpleName().toUpperCase();
+	private static final Logger LOG = Logger.getLogger(DeleteOperationIT.class);
+	protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);	
+	protected static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher("T",CLASS_NAME,"(v1 int,v2 int)");
+    protected static SpliceTableWatcher spliceTableWatcher2 = new SpliceTableWatcher("a_test",spliceSchemaWatcher.schemaName,"(c1 smallint)");
+	private static final Map<Integer,Integer> initialResults = Maps.newHashMap();
+
+    protected static SpliceTableWatcher spliceTableWatcher4 = new SpliceTableWatcher("customer1",spliceSchemaWatcher.schemaName,"(cust_id int, status boolean)");
+    protected static SpliceTableWatcher spliceTableWatcher5 = new SpliceTableWatcher("shipment",spliceSchemaWatcher.schemaName,"(cust_id int)");
+    protected static SpliceTableWatcher spliceTableWatcher6 = new SpliceTableWatcher("customer2",spliceSchemaWatcher.schemaName,"(cust_id int, status boolean)");
+    protected static SpliceTableWatcher deleteFailWatcher = new SpliceTableWatcher("tdelete",spliceSchemaWatcher.schemaName,"(a smallint)");
+
+    @ClassRule 
+	public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
+		.around(spliceSchemaWatcher)
+		.around(spliceTableWatcher)
+        .around(spliceTableWatcher2)
+        .around(spliceTableWatcher4)
+        .around(spliceTableWatcher5)
+        .around(spliceTableWatcher6)
+            .around(deleteFailWatcher)
+		.around(new SpliceDataWatcher(){
+			@Override
+			protected void starting(Description description) {
+				try {
+					PreparedStatement ps = spliceClassWatcher.prepareStatement(format("insert into %s.T values (?,?)",CLASS_NAME));
+					ps.setInt(1,1);
+					ps.setInt(2,2);
+					ps.executeUpdate();
+					initialResults.put(1,2);
+					ps.setInt(1,3);
+					ps.setInt(2,4);
+					ps.executeUpdate();
+					initialResults.put(3,4);
+
+                    ps = spliceClassWatcher.prepareStatement(format("insert into %s values (?)",spliceTableWatcher2.toString()));
+                    ps.setInt(1,32767);
+                    ps.executeUpdate();
+
+                    // Insert into customer and shipment tables
+                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher4 + " values(?,?)");
+                    ps.setInt(1, 1);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 2);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 3);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 4);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 5);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    
+                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher6 + " values(?,?)");
+                    ps.setInt(1, 1);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 2);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 3);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 4);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    ps.setInt(1, 5);
+                    ps.setBoolean(2, true);
+                    ps.executeUpdate();
+                    
+                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher5 + " values(?)");
+                    ps.setInt(1, 2);
+                    ps.executeUpdate();
+                    ps.setInt(1, 4);
+                    ps.executeUpdate();
+
+            ps = spliceClassWatcher.prepareStatement("insert into "+ deleteFailWatcher+" values(?)");
+            ps.setShort(1, (short) 1);ps.executeUpdate();
+            ps.setShort(1, Short.MAX_VALUE);ps.executeUpdate();
+
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+				finally {
+					spliceClassWatcher.closeAll();
+				}
+			}
+			
+		});
+	
+	@Rule public SpliceWatcher methodWatcher = new SpliceWatcher();
+
+
+	@Test
+	public void testDelete() throws Exception{
+		Statement s = methodWatcher.getStatement();
+		int numDeleted = s.executeUpdate(format("delete from %s where v1 = 1",this.getTableReference("T")));
+		Assert.assertEquals("Incorrect num rows deleted", 1, numDeleted);
+		Map<Integer,Integer> deletedResults = Maps.newHashMap(initialResults);
+		deletedResults.remove(1);
+
+		ResultSet rs = methodWatcher.executeQuery(format("select * from %s where v1 = 1",this.getTableReference("T")));
+		Assert.assertTrue("Rows were returned!",!rs.next());
+
+		rs = methodWatcher.executeQuery(format("select * from %s",this.getTableReference("T")));
+		List<String> results = Lists.newArrayList();
+		while(rs.next()){
+			Integer v1 = rs.getInt(1);
+			Integer v2 = rs.getInt(2);
+			Assert.assertNotNull("v1 not returned!",v1);
+			Assert.assertNotNull("v2 not returned!",v2);
+
+			deletedResults.put(v1,v2);
+			results.add(String.format("v1:%d,v2:%d",v1,v2));
+		}
+		for(String result:results){
+			LOG.info(result);
+		}
+		Assert.assertEquals("Incorrect returned row size!",initialResults.size()-numDeleted,deletedResults.size());
+		for(Integer keptV1:deletedResults.keySet()){
+			Assert.assertTrue("Not in original data set!",initialResults.containsKey(keptV1));
+			Assert.assertEquals("incorrect v2!",initialResults.get(keptV1),deletedResults.get(keptV1));
+		}
+	}
+
+    @Test(expected= SQLException.class,timeout=10000)
+    public void testDeleteWithSumOverflowThrowsError() throws Exception {
+        try{
+            methodWatcher.getStatement().execute("delete from "+spliceTableWatcher2.toString()+" where c1+c1 > 0");
+        }catch(SQLException sql){
+           Assert.assertEquals("Incorrect SQLState for message " + sql.getMessage(),SQLState.LANG_OUTSIDE_RANGE_FOR_DATATYPE,sql.getSQLState());
+            throw sql;
+        }
+    }
+
+	// If you change one of the following 'delete over join' tests,
+    // you probably need to make a similar change to UpdateOperationIT.
+
+    @Test
+    public void testDeleteOverBroadcastJoin() throws Exception {
+    	doTestDeleteOverJoin("BROADCAST", spliceTableWatcher4);
+    }
+
+    @Test
+    public void testDeleteOverMergeSortJoin() throws Exception {
+    	doTestDeleteOverJoin("SORTMERGE", spliceTableWatcher6);
+    }
+
+    private void doTestDeleteOverJoin(String hint, SpliceTableWatcher customerTable) throws Exception {
+    	StringBuffer sb = new StringBuffer(200);
+    	sb.append("delete from %s %s \n");
+    	sb.append("where not exists ( \n");
+		sb.append("  select 1 \n");
+		sb.append("  from %s %s --SPLICE-PROPERTIES joinStrategy=%s \n");                                                                           
+		sb.append("  where %s.cust_id = %s.cust_id \n");
+		sb.append(") \n");
+		String query = String.format(sb.toString(), customerTable, "customer", spliceTableWatcher5, "shipment", hint, "customer", "shipment");
+    	int rows = methodWatcher.executeUpdate(query);
+        Assert.assertEquals("Incorrect number of rows deleted.", 3, rows);
+    }
+
+    @Test(expected = SQLException.class)
+		public void testDeleteThrowsDivideByZero() throws Exception {
+				try{
+						methodWatcher.executeUpdate(String.format("delete from %s where c1/0 = 1",spliceTableWatcher2));
+				}catch(SQLException se){
+						String sqlState = se.getSQLState();
+						Assert.assertEquals("incorrect SQL state!","22012",sqlState);
+						Assert.assertEquals("Incorrect message!","Attempt to divide by zero.",se.getMessage());
+						throw se;
+				}
+		}
+
+    @Test
+    public void testDeleteThrowsOutOfRange() throws Exception {
+        try{
+            methodWatcher.executeUpdate(String.format("delete from %s where a+a > 0",deleteFailWatcher));
+        }catch(SQLException se){
+            Assert.assertEquals("Incorrect SQL state!", ErrorState.LANG_OUTSIDE_RANGE_FOR_DATATYPE.getSqlState(),se.getSQLState());
+        }
+    }
+}
---
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/UnionOperationIT.java
@@ -0,0 +1,254 @@
+package com.splicemachine.derby.impl.sql.execute.operations;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Ordering;
+import com.google.common.collect.Sets;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.homeless.TestUtils;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+import java.sql.ResultSet;
+import java.util.*;
+
+import static com.splicemachine.derby.test.framework.SpliceUnitTest.resultSetSize;
+import static org.junit.Assert.*;
+
+public class UnionOperationIT {
+
+    private static final String CLASS_NAME = UnionOperationIT.class.getSimpleName().toUpperCase();
+    private static final SpliceWatcher spliceClassWatcher = new SpliceWatcher(CLASS_NAME);
+
+    @ClassRule
+    public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
+            .around(new SpliceSchemaWatcher(CLASS_NAME))
+            .around(TestUtils.createFileDataWatcher(spliceClassWatcher, "test_data/UnionOperationIT.sql", CLASS_NAME));
+
+    @Rule
+    public SpliceWatcher methodWatcher = new SpliceWatcher(CLASS_NAME);
+
+    @Test
+    public void testUnionAll() throws Exception {
+        List<String> names = methodWatcher.queryList("select name from ST_MARS UNION ALL select name from ST_EARTH");
+        assertEquals(10, names.size());
+        assertEquals(Ordering.natural().nullsLast().sortedCopy(names),
+                Lists.newArrayList(
+                        "Duncan-Robert", "Mulgrew-Kate", "Nimoy-Leonard", "Nimoy-Leonard", "Patrick", "Ryan-Jeri",
+                        "Shatner-William", "Spiner-Brent", null, null
+                ));
+    }
+
+    @Test
+    public void testUnionOneColumn() throws Exception {
+        List<String> names = methodWatcher.queryList("select name from ST_MARS UNION select name from ST_EARTH");
+        assertEquals(8, names.size());
+        assertEquals(Ordering.natural().nullsLast().sortedCopy(names),
+                Lists.newArrayList(
+                        "Duncan-Robert", "Mulgrew-Kate", "Nimoy-Leonard", "Patrick", "Ryan-Jeri", "Shatner-William",
+                        "Spiner-Brent", null
+                ));
+    }
+
+    /* This needs to use a provider interface for both of its traversals and not use isScan - JL */
+    @Test
+    public void testValuesUnion() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("SELECT TTABBREV, TABLE_TYPE from (VALUES ('T','TABLE'), ('S','SYSTEM TABLE'), ('V', 'VIEW'), ('A', 'SYNONYM')) T (TTABBREV,TABLE_TYPE)");
+        assertTrue(resultSetSize(rs) > 0);
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+        List<Integer> idList = methodWatcher.queryList("select empId from ST_MARS UNION select empId from ST_EARTH");
+        Set<Integer> idSet = Sets.newHashSet(idList);
+        assertEquals(5, idSet.size());
+        assertEquals("Expected no duplicates in query result", idList.size(), idSet.size());
+    }
+
+    @Test
+    public void testUnionNoSort() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select * from ST_MARS UNION select * from ST_EARTH");
+        assertEquals(8, resultSetSize(rs));
+    }
+
+    @Test
+    public void testUnionWithSort() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select * from ST_MARS UNION select * from ST_EARTH order by 1 desc");
+        assertEquals(8, resultSetSize(rs));
+    }
+
+    /* Regression test for Bug 373 */
+    @Test
+    public void testUnionWithWhereClause() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select * from ST_MARS where empId = 6 UNION select * from ST_EARTH where empId=3");
+        assertEquals(2, resultSetSize(rs));
+    }
+
+    /* Regression for Bug 292 */
+    @Test
+    public void testUnionValuesInSubSelect() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select empId from ST_MARS where empId in (select empId from ST_EARTH union all values 1)");
+        assertEquals(5, resultSetSize(rs));
+    }
+
+    @Test
+    public void testValuesFirstInUnionAll() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("values (9,10) union all select a.i, b.i from T2 a, DUPS b union all select b.i, a.i from T2 a, DUPS b");
+        assertEquals(33, resultSetSize(rs));
+    }
+
+    @Test
+    public void testValuesLastInUnionAll() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery(
+                "select a.i, b.i from T2 a, DUPS b union all select b.i, a.i from T2 a, DUPS b union all values (9,10)");
+        assertEquals(33, resultSetSize(rs));
+    }
+
+    // 792
+    @Test
+    public void unionOverScalarAggregate_max() throws Exception {
+        List<Integer> maxList = methodWatcher.queryList("select max(a.i) from T1 a union select max(b.i) from T1 b");
+        assertFalse(maxList.contains(null));
+        assertEquals("union should return 1 rows", 1, maxList.size());
+    }
+
+    // Bug 791
+    @Test
+    public void unionAllOverScalarAggregate_max() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select max(a.i) from T1 a UNION ALL select max(b.i) from T1 b");
+        assertEquals("union all should return 2 rows", 2, resultSetSize(rs));
+    }
+
+    /* bug DB-1304 */
+    @Test
+    public void unionOverScalarAggregate_count() throws Exception {
+        Long count2 = methodWatcher.query("select count(*) from empty_table_1 UNION select count(*) from empty_table_2");
+        Long count3 = methodWatcher.query("select count(*) from empty_table_1 UNION select count(*) from empty_table_2 UNION select count(*) from empty_table_3");
+        Long count4 = methodWatcher.query("select count(*) from empty_table_1 UNION select count(*) from empty_table_2 UNION select count(*) from empty_table_3 UNION select count(*) from empty_table_4");
+        assertEquals(0, count2.intValue());
+        assertEquals(0, count3.intValue());
+        assertEquals(0, count4.intValue());
+    }
+
+    /* bug DB-1304 */
+    @Test
+    public void unionAllOverScalarAggregate_count() throws Exception {
+        List<Long> counts = methodWatcher.queryList("select count(*) from empty_table_1 UNION ALL select count(*) from empty_table_2");
+        assertEquals(Arrays.asList(0L, 0L), counts);
+
+        counts = methodWatcher.queryList("select count(*) from empty_table_1 UNION ALL select count(*) from empty_table_2 UNION ALL select count(*) from empty_table_3");
+        assertEquals(Arrays.asList(0L, 0L, 0L), counts);
+
+        counts = methodWatcher.queryList("select count(*) from empty_table_1 UNION ALL select count(*) from empty_table_2 UNION ALL select count(*) from empty_table_3 UNION ALL select count(*) from empty_table_4");
+        assertEquals(Arrays.asList(0L, 0L, 0L, 0L), counts);
+    }
+
+    /* bug DB-1304 */
+    @Test
+    public void unionAllOverScalarAggregate_countNonZero() throws Exception {
+        long COUNT1 = 1 + new Random().nextInt(9);
+        long COUNT2 = 1 + COUNT1 + new Random().nextInt(9);
+        insert(COUNT1, "insert into empty_table_1 values(100, 200, '')");
+        insert(COUNT2, "insert into empty_table_4 values(100, 200, '')");
+
+        List<Long> counts = methodWatcher.queryList("" +
+                "          select count(*) from empty_table_1 " +
+                "UNION ALL select count(*) from empty_table_2 " +
+                "UNION ALL select count(*) from empty_table_3 " +
+                "UNION ALL select count(*) from empty_table_4");
+        Collections.sort(counts);
+
+        assertEquals(Arrays.asList(0L, 0L, COUNT1, COUNT2), counts);
+
+        methodWatcher.executeUpdate("delete from empty_table_1");
+        methodWatcher.executeUpdate("delete from empty_table_4");
+    }
+
+
+    // Bug 852
+    @Test
+    public void testMultipleUnionsInASubSelect() throws Exception {
+        List<Integer> actual = methodWatcher.queryList(
+                "select i from T1 where exists (select i from T2 where T1.i < i union \n" +
+                        "select i from T2 where 1 = 0 union select i from T2 where T1.i < i union select\n" +
+                        "i from T2 where 1 = 0)"
+        );
+        Collections.sort(actual);
+        assertEquals("Incorrect result contents!", Arrays.asList(1, 2), actual);
+    }
+
+
+    /* Regression test #1 for DB-1038 */
+    @Test
+    public void testUnionDistinctValues() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("values (1,2,3,4) union distinct values (5,6,7,8) union distinct values (9,10,11,12)");
+        int[][] correct = new int[][]{
+                new int[]{1, 2, 3, 4},
+                new int[]{5, 6, 7, 8},
+                new int[]{9, 10, 11, 12}
+        };
+        int[][] actual = new int[correct.length][];
+        int count = 0;
+        while (rs.next()) {
+            int first = rs.getInt(1);
+            int second = rs.getInt(2);
+            int third = rs.getInt(3);
+            int fourth = rs.getInt(4);
+            actual[count] = new int[]{first, second, third, fourth};
+            count++;
+        }
+        for (int i = 0; i < correct.length; i++) {
+            assertArrayEquals("Incorrect value!", correct[i], actual[i]);
+        }
+    }
+
+    /* Regression test #2 for DB-1038 */
+    @Test
+    public void testUnionValues() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("values (1,2,3,4) union values (5,6,7,8) union values (9,10,11,12)");
+        int[][] correct = new int[][]{
+                new int[]{1, 2, 3, 4},
+                new int[]{5, 6, 7, 8},
+                new int[]{9, 10, 11, 12}
+        };
+        int[][] actual = new int[correct.length][];
+        int count = 0;
+        while (rs.next()) {
+            int first = rs.getInt(1);
+            int second = rs.getInt(2);
+            int third = rs.getInt(3);
+            int fourth = rs.getInt(4);
+            actual[count] = new int[]{first, second, third, fourth};
+            count++;
+        }
+        for (int i = 0; i < correct.length; i++) {
+            assertArrayEquals("Incorrect value!", correct[i], actual[i]);
+        }
+    }
+
+    /* Regression test for DB-1026 */
+    @Test
+    public void testMultipleUnionValues() throws Exception {
+        ResultSet rs = methodWatcher.executeQuery("select distinct * from (values 2.0,2.1,2.2,2.2) v1");
+        float[] correct = new float[]{2.0f, 2.1f, 2.2f};
+        float[] actual = new float[correct.length];
+        int count = 0;
+        while (rs.next()) {
+            assertTrue("Too many rows returned!", count < correct.length);
+            float n = rs.getFloat(1);
+            actual[count] = n;
+            count++;
+        }
+
+        assertArrayEquals("Incorrect values, there should be no rounding error present!", correct, actual, 1e-5f);
+    }
+
+    private void insert(long times, String sql) throws Exception {
+        for (long i = 0; i < times; i++) {
+            methodWatcher.executeUpdate(sql);
+        }
+    }
+}
