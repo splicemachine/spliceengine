e9934d4d14b73243241b9e079a80c2953ce9449c
Keith Winkler <kwinkler@splicemachine.com>
2015-03-23 13:06:38 -0500

    DeleteOperationIT: simplify, add test for delete by primary key col, unique index col

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/DeleteOperationIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/DeleteOperationIT.java
@@ -1,222 +1,145 @@
 package com.splicemachine.derby.impl.sql.execute.operations;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
-import java.util.Map;
-
-import com.splicemachine.pipeline.exception.ErrorState;
-
 import com.splicemachine.db.iapi.reference.SQLState;
-import org.apache.log4j.Logger;
-import org.junit.Assert;
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.homeless.TestUtils;
+import com.splicemachine.pipeline.exception.ErrorState;
+import com.splicemachine.test_tools.TableCreator;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.junit.rules.TestRule;
-import org.junit.runner.Description;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-import com.splicemachine.derby.test.framework.SpliceDataWatcher;
-import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
-import com.splicemachine.derby.test.framework.SpliceTableWatcher;
-import com.splicemachine.derby.test.framework.SpliceUnitTest;
-import com.splicemachine.derby.test.framework.SpliceWatcher;
+import java.sql.SQLException;
 
-/**
- * @author Scott Fines
- *         Created: 2/5/13 12:24 PM
- */
-//@Category(SerialTest.class)
-public class DeleteOperationIT extends SpliceUnitTest {
-	protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
-	public static final String CLASS_NAME = DeleteOperationIT.class.getSimpleName().toUpperCase();
-	private static final Logger LOG = Logger.getLogger(DeleteOperationIT.class);
-	protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);	
-	protected static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher("T",CLASS_NAME,"(v1 int,v2 int)");
-    protected static SpliceTableWatcher spliceTableWatcher2 = new SpliceTableWatcher("a_test",spliceSchemaWatcher.schemaName,"(c1 smallint)");
-	private static final Map<Integer,Integer> initialResults = Maps.newHashMap();
+import static com.splicemachine.test_tools.Rows.row;
+import static com.splicemachine.test_tools.Rows.rows;
+import static org.junit.Assert.assertEquals;
 
-    protected static SpliceTableWatcher spliceTableWatcher4 = new SpliceTableWatcher("customer1",spliceSchemaWatcher.schemaName,"(cust_id int, status boolean)");
-    protected static SpliceTableWatcher spliceTableWatcher5 = new SpliceTableWatcher("shipment",spliceSchemaWatcher.schemaName,"(cust_id int)");
-    protected static SpliceTableWatcher spliceTableWatcher6 = new SpliceTableWatcher("customer2",spliceSchemaWatcher.schemaName,"(cust_id int, status boolean)");
-    protected static SpliceTableWatcher deleteFailWatcher = new SpliceTableWatcher("tdelete",spliceSchemaWatcher.schemaName,"(a smallint)");
+public class DeleteOperationIT {
 
-    @ClassRule 
-	public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
-		.around(spliceSchemaWatcher)
-		.around(spliceTableWatcher)
-        .around(spliceTableWatcher2)
-        .around(spliceTableWatcher4)
-        .around(spliceTableWatcher5)
-        .around(spliceTableWatcher6)
-            .around(deleteFailWatcher)
-		.around(new SpliceDataWatcher(){
-			@Override
-			protected void starting(Description description) {
-				try {
-					PreparedStatement ps = spliceClassWatcher.prepareStatement(format("insert into %s.T values (?,?)",CLASS_NAME));
-					ps.setInt(1,1);
-					ps.setInt(2,2);
-					ps.executeUpdate();
-					initialResults.put(1,2);
-					ps.setInt(1,3);
-					ps.setInt(2,4);
-					ps.executeUpdate();
-					initialResults.put(3,4);
+    private static final String SCHEMA = DeleteOperationIT.class.getSimpleName().toUpperCase();
+    private static SpliceWatcher spliceClassWatcher = new SpliceWatcher(SCHEMA);
 
-                    ps = spliceClassWatcher.prepareStatement(format("insert into %s values (?)",spliceTableWatcher2.toString()));
-                    ps.setInt(1,32767);
-                    ps.executeUpdate();
+    @ClassRule
+    public static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(SCHEMA);
 
-                    // Insert into customer and shipment tables
-                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher4 + " values(?,?)");
-                    ps.setInt(1, 1);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 2);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 3);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 4);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 5);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    
-                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher6 + " values(?,?)");
-                    ps.setInt(1, 1);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 2);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 3);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 4);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    ps.setInt(1, 5);
-                    ps.setBoolean(2, true);
-                    ps.executeUpdate();
-                    
-                    ps = spliceClassWatcher.prepareStatement("insert into " + spliceTableWatcher5 + " values(?)");
-                    ps.setInt(1, 2);
-                    ps.executeUpdate();
-                    ps.setInt(1, 4);
-                    ps.executeUpdate();
+    @BeforeClass
+    public static void createSharedTables() throws Exception {
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table T (v1 int primary key, v2 int, v3 int unique)")
+                .withInsert("insert into T values(?,?,?)")
+                .withRows(rows(row(1, 10, 100), row(2, 20, 200), row(3, 30, 300))).create();
 
-            ps = spliceClassWatcher.prepareStatement("insert into "+ deleteFailWatcher+" values(?)");
-            ps.setShort(1, (short) 1);ps.executeUpdate();
-            ps.setShort(1, Short.MAX_VALUE);ps.executeUpdate();
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table a_test (c1 smallint)")
+                .withInsert("insert into a_test values(?)")
+                .withRows(rows(row(32767))).create();
 
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-				finally {
-					spliceClassWatcher.closeAll();
-				}
-			}
-			
-		});
-	
-	@Rule public SpliceWatcher methodWatcher = new SpliceWatcher();
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table customer1 (cust_id int, status boolean)")
+                .withInsert("insert into customer1 values(?,?)")
+                .withRows(rows(row(1, true), row(2, true), row(3, true), row(4, true), row(5, true))).create();
 
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table customer2 (cust_id int, status boolean)")
+                .withInsert("insert into customer2 values(?,?)")
+                .withRows(rows(row(1, true), row(2, true), row(3, true), row(4, true), row(5, true))).create();
 
-	@Test
-	public void testDelete() throws Exception{
-		Statement s = methodWatcher.getStatement();
-		int numDeleted = s.executeUpdate(format("delete from %s where v1 = 1",this.getTableReference("T")));
-		Assert.assertEquals("Incorrect num rows deleted", 1, numDeleted);
-		Map<Integer,Integer> deletedResults = Maps.newHashMap(initialResults);
-		deletedResults.remove(1);
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table shipment (cust_id int)")
+                .withInsert("insert into shipment values(?)")
+                .withRows(rows(row(2), row(4))).create();
 
-		ResultSet rs = methodWatcher.executeQuery(format("select * from %s where v1 = 1",this.getTableReference("T")));
-		Assert.assertTrue("Rows were returned!",!rs.next());
+        new TableCreator(spliceClassWatcher.getOrCreateConnection())
+                .withCreate("create table tdelete (a smallint)")
+                .withInsert("insert into tdelete values(?)")
+                .withRows(rows(row((short) 1), row(Short.MAX_VALUE))).create();
 
-		rs = methodWatcher.executeQuery(format("select * from %s",this.getTableReference("T")));
-		List<String> results = Lists.newArrayList();
-		while(rs.next()){
-			Integer v1 = rs.getInt(1);
-			Integer v2 = rs.getInt(2);
-			Assert.assertNotNull("v1 not returned!",v1);
-			Assert.assertNotNull("v2 not returned!",v2);
+    }
 
-			deletedResults.put(v1,v2);
-			results.add(String.format("v1:%d,v2:%d",v1,v2));
-		}
-		for(String result:results){
-			LOG.info(result);
-		}
-		Assert.assertEquals("Incorrect returned row size!",initialResults.size()-numDeleted,deletedResults.size());
-		for(Integer keptV1:deletedResults.keySet()){
-			Assert.assertTrue("Not in original data set!",initialResults.containsKey(keptV1));
-			Assert.assertEquals("incorrect v2!",initialResults.get(keptV1),deletedResults.get(keptV1));
-		}
-	}
+    @Rule
+    public SpliceWatcher methodWatcher = new SpliceWatcher(SCHEMA);
 
-    @Test(expected= SQLException.class,timeout=10000)
+    @Test
+    public void testDelete() throws Exception {
+        // Delete using primary key col
+        assertEquals("Incorrect num rows deleted", 1, methodWatcher.executeUpdate("delete from T where v1 = 1"));
+        assertEquals("" +
+                        "V1 |V2 |V3  |\n" +
+                        "-------------\n" +
+                        " 2 |20 |200 |\n" +
+                        " 3 |30 |300 |",
+                TestUtils.FormattedResult.ResultFactory.toString(methodWatcher.executeQuery("select * from T")));
+
+        // Delete using unique index col
+        assertEquals("Incorrect num rows deleted", 1, methodWatcher.executeUpdate("delete from T where v3 = 300"));
+        assertEquals("" +
+                        "V1 |V2 |V3  |\n" +
+                        "-------------\n" +
+                        " 2 |20 |200 |",
+                TestUtils.FormattedResult.ResultFactory.toString(methodWatcher.executeQuery("select * from T")));
+
+        // Delete using normal col
+        assertEquals("Incorrect num rows deleted", 1, methodWatcher.executeUpdate("delete from T where v2 = 20"));
+        assertEquals("", TestUtils.FormattedResult.ResultFactory.toString(methodWatcher.executeQuery("select * from T")));
+    }
+
+    @Test(expected = SQLException.class, timeout = 10000)
     public void testDeleteWithSumOverflowThrowsError() throws Exception {
-        try{
-            methodWatcher.getStatement().execute("delete from "+spliceTableWatcher2.toString()+" where c1+c1 > 0");
-        }catch(SQLException sql){
-           Assert.assertEquals("Incorrect SQLState for message " + sql.getMessage(),SQLState.LANG_OUTSIDE_RANGE_FOR_DATATYPE,sql.getSQLState());
+        try {
+            methodWatcher.getStatement().execute("delete from a_test where c1+c1 > 0");
+        } catch (SQLException sql) {
+            assertEquals("Incorrect SQLState for message " + sql.getMessage(), SQLState.LANG_OUTSIDE_RANGE_FOR_DATATYPE, sql.getSQLState());
             throw sql;
         }
     }
 
-	// If you change one of the following 'delete over join' tests,
+    // If you change one of the following 'delete over join' tests,
     // you probably need to make a similar change to UpdateOperationIT.
 
     @Test
     public void testDeleteOverBroadcastJoin() throws Exception {
-    	doTestDeleteOverJoin("BROADCAST", spliceTableWatcher4);
+        doTestDeleteOverJoin("BROADCAST", "customer1");
     }
 
     @Test
     public void testDeleteOverMergeSortJoin() throws Exception {
-    	doTestDeleteOverJoin("SORTMERGE", spliceTableWatcher6);
+        doTestDeleteOverJoin("SORTMERGE", "customer2");
     }
 
-    private void doTestDeleteOverJoin(String hint, SpliceTableWatcher customerTable) throws Exception {
-    	StringBuffer sb = new StringBuffer(200);
-    	sb.append("delete from %s %s \n");
-    	sb.append("where not exists ( \n");
-		sb.append("  select 1 \n");
-		sb.append("  from %s %s --SPLICE-PROPERTIES joinStrategy=%s \n");                                                                           
-		sb.append("  where %s.cust_id = %s.cust_id \n");
-		sb.append(") \n");
-		String query = String.format(sb.toString(), customerTable, "customer", spliceTableWatcher5, "shipment", hint, "customer", "shipment");
-    	int rows = methodWatcher.executeUpdate(query);
-        Assert.assertEquals("Incorrect number of rows deleted.", 3, rows);
+    private void doTestDeleteOverJoin(String hint, String customerTable) throws Exception {
+        StringBuffer sb = new StringBuffer(200);
+        sb.append("delete from %s %s \n");
+        sb.append("where not exists ( \n");
+        sb.append("  select 1 \n");
+        sb.append("  from %s %s --SPLICE-PROPERTIES joinStrategy=%s \n");
+        sb.append("  where %s.cust_id = %s.cust_id \n");
+        sb.append(") \n");
+        String query = String.format(sb.toString(), customerTable, "customer", "shipment", "shipment", hint, "customer", "shipment");
+        int rows = methodWatcher.executeUpdate(query);
+        assertEquals("Incorrect number of rows deleted.", 3, rows);
     }
 
     @Test(expected = SQLException.class)
-		public void testDeleteThrowsDivideByZero() throws Exception {
-				try{
-						methodWatcher.executeUpdate(String.format("delete from %s where c1/0 = 1",spliceTableWatcher2));
-				}catch(SQLException se){
-						String sqlState = se.getSQLState();
-						Assert.assertEquals("incorrect SQL state!","22012",sqlState);
-						Assert.assertEquals("Incorrect message!","Attempt to divide by zero.",se.getMessage());
-						throw se;
-				}
-		}
+    public void testDeleteThrowsDivideByZero() throws Exception {
+        try {
+            methodWatcher.executeUpdate("delete from a_test where c1/0 = 1");
+        } catch (SQLException se) {
+            String sqlState = se.getSQLState();
+            assertEquals("incorrect SQL state!", "22012", sqlState);
+            assertEquals("Incorrect message!", "Attempt to divide by zero.", se.getMessage());
+            throw se;
+        }
+    }
 
     @Test
     public void testDeleteThrowsOutOfRange() throws Exception {
-        try{
-            methodWatcher.executeUpdate(String.format("delete from %s where a+a > 0",deleteFailWatcher));
-        }catch(SQLException se){
-            Assert.assertEquals("Incorrect SQL state!", ErrorState.LANG_OUTSIDE_RANGE_FOR_DATATYPE.getSqlState(),se.getSQLState());
+        try {
+            methodWatcher.executeUpdate(String.format("delete from tdelete where a+a > 0"));
+        } catch (SQLException se) {
+            assertEquals("Incorrect SQL state!", ErrorState.LANG_OUTSIDE_RANGE_FOR_DATATYPE.getSqlState(), se.getSQLState());
         }
     }
 }
