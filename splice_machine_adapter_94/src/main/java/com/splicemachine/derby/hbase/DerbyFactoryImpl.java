7df4adc4e5cac77be8901766901867c5a1ca9e5c
David Winters <dwinters@splicemachine.com>
2015-04-07 11:43:09 -0700

    DB-2916: Fixing further 0.94 compilation bustage.

--- splice_machine_adapter_94/src/main/java/com/splicemachine/derby/hbase/DerbyFactoryImpl.java
+++ splice_machine_adapter_94/src/main/java/com/splicemachine/derby/hbase/DerbyFactoryImpl.java
@@ -79,6 +79,7 @@ import com.splicemachine.hase.debug.HBaseEntryPredicateFilter;
 import com.splicemachine.hbase.HBaseRegionLoads;
 import com.splicemachine.hbase.ThrowIfDisconnected;
 import com.splicemachine.hbase.jmx.JMXUtils;
+import com.splicemachine.job.JobStatusLogger;
 import com.splicemachine.mrio.api.core.MemStoreFlushAwareScanner;
 import com.splicemachine.mrio.api.core.MemstoreAware;
 import com.splicemachine.mrio.api.core.SpliceRegionScanner;
@@ -135,8 +136,8 @@ public class DerbyFactoryImpl implements DerbyFactory<SparseTxn> {
 	@Override
 	public BaseJobControl getJobControl(CoprocessorJob job, String jobPath,
 			SpliceZooKeeperManager zkManager, int maxResubmissionAttempts,
-			JobMetrics jobMetrics) {
-		return new JobControl(job,jobPath,zkManager,maxResubmissionAttempts,jobMetrics);
+			JobMetrics jobMetrics, JobStatusLogger jobStatusLogger) {
+		return new JobControl(job,jobPath,zkManager,maxResubmissionAttempts,jobMetrics,jobStatusLogger);
 	}
 
 	@Override
