19c6d05af1425b49b51eefa9a3c239e3b6583631
Scott Fines <scottfines@gmail.com>
2015-04-15 10:56:54 -0400

    Fixing XPlainTrace test to remove optimizer dependency.

--- splice_machine_test/src/test/java/com/splicemachine/management/XPlainTrace1IT.java
+++ splice_machine_test/src/test/java/com/splicemachine/management/XPlainTrace1IT.java
@@ -350,7 +350,7 @@ public class XPlainTrace1IT extends BaseXplainIT {
         Assert.assertEquals("Table scan has incorrect number of iterations",count,child.getIterations());
         String info = child.getInfo();
 //        System.out.println(info);
-        Assert.assertTrue("Incorrect info: info="+info,info.compareToIgnoreCase("Scan filter:(T1.I[1:1] = T2.I[2:1]), table:XPLAINTRACE1IT.TAB2")==0);
+        Assert.assertTrue("Incorrect info: info="+info,info.compareToIgnoreCase("Scan filter:(T1.I[1:1] = T2.I[2:1])")==0);
     }
 
 
