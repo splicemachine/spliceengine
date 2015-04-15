dd2bde68a37d4352d43fcff82ff98d30d968a470
Scott Fines <scottfines@gmail.com>
2015-04-15 09:00:45 -0400

    Correcting faulty XPlain test so that it accurately reflects expected output

--- splice_machine/src/main/java/com/splicemachine/derby/management/XPlainTrace.java
+++ splice_machine/src/main/java/com/splicemachine/derby/management/XPlainTrace.java
@@ -3,10 +3,8 @@ package com.splicemachine.derby.management;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.splicemachine.derby.impl.sql.catalog.SpliceXplainUtils;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.ResultSet;
-import java.sql.Statement;
+
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -42,13 +40,13 @@ public class XPlainTrace {
 
 
     protected XPlainTrace() {
-        xPlainTreeNodeMap = new HashMap<Long, XPlainTreeNode>(10);
+        xPlainTreeNodeMap = new HashMap<>(10);
         sequenceId = 0;
     }
 
     public XPlainTrace(long sId, int mode, String format) throws SQLException {
         this.statementId = sId;
-        xPlainTreeNodeMap = new HashMap<Long, XPlainTreeNode>(10);
+        xPlainTreeNodeMap = new HashMap<>(10);
         sequenceId = 0;
         connection = SpliceXplainUtils.getDefaultConn();
         this.mode = mode;
@@ -139,8 +137,7 @@ public class XPlainTrace {
 
     private void populateLoopsMap(XPlainTreeNode node, NavigableMap<Integer,
             List<XPlainTreeNode>> loopsMap, int level) {
-        if (node.isIndexScanOperation() || node.isTableScanOperation() || node.isProjectRestrictOperation())
-        {
+        if (node.isIndexScanOperation() || node.isTableScanOperation() || node.isProjectRestrictOperation()) {
             Deque<XPlainTreeNode> children = node.getChildren();
             if (children != null && children.size() > 0 &&
                 children.getFirst().getOperationType().compareToIgnoreCase("ScrollInsensitive") == 0) {
@@ -211,44 +208,34 @@ public class XPlainTrace {
     }
 
     private void populateMetrics() throws SQLException, IllegalAccessException{
-        ResultSet rs = getTaskHistory();
-        try{
-            HashMap<String, XPlainTreeNode> regionMap = new HashMap<String, XPlainTreeNode>();
-            while (rs.next()) {
-                int index = rs.findColumn("OPERATIONID");
-                Long operationId = rs.getLong(index);
-                XPlainTreeNode node = xPlainTreeNodeMap.get(operationId);
-                if (node != null) {
-                    // For detailed view of the execution plan, show metrics for each region
-                    if (mode == 1) {
-                        if (node.isTableScanOperation()) {
-                            XPlainTreeNode child = new XPlainTreeNode(statementId, operationId);
-                            child.setOperationType("RegionScan");
-                            node.addLastChild(child);
-                            node = child;
+        try(PreparedStatement ps=getTaskHistoryStatement()){
+            try(ResultSet rs=ps.executeQuery()){
+                while(rs.next()){
+                    int index=rs.findColumn("OPERATIONID");
+                    Long operationId=rs.getLong(index);
+                    XPlainTreeNode node=xPlainTreeNodeMap.get(operationId);
+                    if(node!=null){
+                        // For detailed view of the execution plan, show metrics for each region
+                        if(mode==1){
+                            if(node.isTableScanOperation()){
+                                XPlainTreeNode child=new XPlainTreeNode(statementId,operationId);
+                                child.setOperationType("RegionScan");
+                                node.addLastChild(child);
+                                node=child;
+                            }
                         }
+                        node.setAttributes(rs);
                     }
-                    node.setAttributes(rs);
                 }
             }
-        }finally{
-            rs.close();
         }
     }
 
-    private ResultSet getTaskHistory() throws SQLException{
-
-        StringBuilder query = new StringBuilder("select * from ");
-        query.append(SCHEMANAME)
-                .append(".")
-                .append(taskTableName)
-                .append(" where statementId=")
-                .append(statementId);
-
-        Statement s = connection.createStatement();
-        ResultSet rs = s.executeQuery(query.toString());
-
-        return rs;
+    private PreparedStatement getTaskHistoryStatement() throws SQLException{
+        String table = SCHEMANAME+"."+taskTableName;
+        PreparedStatement ps = connection.prepareStatement("select * from "+table+" where statementId=?");
+        ps.setLong(1,statementId);
+        return ps;
     }
 
     /*
@@ -286,39 +273,35 @@ public class XPlainTrace {
      */
     private boolean populateTreeNodeMap() throws SQLException {
         int count = 0;
-        ResultSet rs = getOperationHistory();
-        try{
-            while (rs.next()) {
-                count++;
-                Long operationId = rs.getLong(1);
-                XPlainTreeNode node = new XPlainTreeNode(statementId, operationId);
-                node.setParentOperationId(rs.getLong(2));
-                node.setOperationType(rs.getString(3));
-                node.setRightChildOp(rs.getBoolean(4));
-                node.setInfo(rs.getString(5));
-                xPlainTreeNodeMap.put(operationId, node);
+        try(PreparedStatement ps=getOperationHistoryStatement()){
+            try(ResultSet rs=ps.executeQuery()){
+                while(rs.next()){
+                    count++;
+                    Long operationId=rs.getLong(1);
+                    XPlainTreeNode node=new XPlainTreeNode(statementId,operationId);
+                    node.setParentOperationId(rs.getLong(2));
+                    node.setOperationType(rs.getString(3));
+                    node.setRightChildOp(rs.getBoolean(4));
+                    node.setInfo(rs.getString(5));
+                    xPlainTreeNodeMap.put(operationId,node);
+                }
             }
-        }finally{
-            rs.close();
         }
         return count != 0;
     }
 
-    private ResultSet getOperationHistory() throws SQLException{
-
-        StringBuilder query =
-                new StringBuilder("select operationId, parent_operation_id, operation_type, is_right_child_op, info from ");
-
-        query.append(SCHEMANAME)
-                .append(".")
-                .append(operationTableName)
-                .append(" where statementId=")
-                .append(statementId);
-
-        Statement s = connection.createStatement();
-        ResultSet rs = s.executeQuery(query.toString());
-
-        return rs;
+    private PreparedStatement getOperationHistoryStatement() throws SQLException{
+        String table = SCHEMANAME+"."+operationTableName;
+        PreparedStatement ps=connection.prepareStatement("select " +
+                "operationId" +
+                ", parent_operation_id" +
+                ", operation_type" +
+                ", is_right_child_op" +
+                ", info " +
+                "from "+table +
+                " where statementId = ?");
+        ps.setLong(1,statementId);
+        return ps;
     }
 
     /*
