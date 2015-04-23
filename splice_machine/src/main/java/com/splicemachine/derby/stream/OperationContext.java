ac13bed8cac6a3003dacc7991837218843d46c51
John Leach <jleach@splicemachine.com>
2015-04-23 14:06:35 -0500

    3210 Add Interface

---
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/OperationContext.java
@@ -0,0 +1,24 @@
+package com.splicemachine.derby.stream;
+
+import com.splicemachine.db.iapi.sql.Activation;
+import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
+import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
+import com.splicemachine.si.api.TxnView;
+import org.apache.spark.Accumulator;
+
+import java.io.Externalizable;
+
+/**
+ * Created by jleach on 4/17/15.
+ */
+public interface OperationContext<Op extends SpliceOperation> extends Externalizable {
+    void prepare();
+    void reset();
+    Op getOperation();
+    Activation getActivation();
+    SpliceRuntimeContext getSpliceRuntimeContext();
+    TxnView getTxn();
+    void recordRead();
+    void recordFilter();
+    void recordWrite();
+}
