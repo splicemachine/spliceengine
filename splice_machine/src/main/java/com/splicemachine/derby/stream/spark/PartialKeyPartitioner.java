c9d70fe35cfb81a5d562d9649d475889fdc0dce1
John Leach <jleach@splicemachine.com>
2015-04-28 11:10:07 -0500

    Adding Partial Key Partitioner

---
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/spark/PartialKeyPartitioner.java
@@ -0,0 +1,54 @@
+package com.splicemachine.derby.stream.spark;
+
+import com.splicemachine.db.iapi.sql.execute.ExecRow;
+import org.apache.spark.Partitioner;
+import org.apache.spark.util.Utils;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+/**
+ * Created by jleach on 4/27/15.
+ */
+public class PartialKeyPartitioner extends Partitioner implements Externalizable {
+    protected int[] keyColumns;
+    protected int numPartitions;
+
+    public PartialKeyPartitioner() {
+
+    }
+
+    public PartialKeyPartitioner(int[] keyColumns, int numPartitions) {
+        this.keyColumns = keyColumns;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeInt(numPartitions);
+        out.writeInt(keyColumns.length);
+        for (int key: keyColumns)
+            out.writeInt(key);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        numPartitions = in.readInt();
+        keyColumns = new int[in.readInt()];
+        for (int i =0; i< keyColumns.length;i++)
+            keyColumns[i] = in.readInt();
+    }
+
+    @Override
+    public int numPartitions() {
+        return numPartitions;
+    }
+
+    @Override
+    public int getPartition(Object o) {
+        if (o == null)
+            return 0;
+        return Utils.nonNegativeMod(((ExecRow)o).hashCode(keyColumns), numPartitions);
+    }
+}
