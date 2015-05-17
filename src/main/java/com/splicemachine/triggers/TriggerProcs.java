96980b016c1b2ca353c2c5f726185f033eb1557e
jeffcunningham <jcunningham@splicemachine.com>
2015-05-17 16:47:40 -0500

    Moved other stored procedures used in splice tests to this repo.

--- src/main/java/com/splicemachine/triggers/TriggerProcs.java
+++ src/main/java/com/splicemachine/triggers/TriggerProcs.java
@@ -42,6 +42,34 @@ public class TriggerProcs {
     }
 
     /**
+     * A stored procedure which gets called from a ROW level trigger.<br/>
+     * Updates an "audit" table with the name of the user who initiated the trigger,
+     * the timestamp of execution and, if row trigger, the new transition value and the old transition value.
+     * @param schema the name of the schema in which the table to update lives
+     * @param table the name of the "audit" table
+     * @param newColValue the value of the new transition variable
+     * @param oldColValue the value of the old transition variable
+     * @throws SQLException
+     */
+    public static void proc_call_audit_with_transition(String schema, String table,
+                                                       Integer newColValue, Integer oldColValue) throws SQLException {
+        Connection conn = null;
+        Statement statement = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:default:connection");
+            String username = conn.getMetaData().getUserName();
+            statement = conn.createStatement();
+            statement.execute(
+                String.format("insert into %s.%s (username,insert_time,new_id,old_id) values ('%s',CURRENT_TIMESTAMP, %s, %s)",
+                                            schema,table, username, newColValue, oldColValue));
+        } finally {
+            closeQuietly(statement);
+            closeQuietly(conn);
+        }
+
+    }
+
+    /**
      * A stored procedure which gets called from a trigger.<br/>
      * Updates an "audit" table with the name of the user who initiated the trigger
      * and the timestamp of execution.
