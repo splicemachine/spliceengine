dd4d53be8e6afc4c50a4b0a9b2328fbd2a831962
Keith Winkler <kwinkler@splicemachine.com>
2015-05-18 11:41:00 -0500

    DB-3307: RegionServerCallBuffer: white space formatting and doc

--- splice_machine/src/main/java/com/splicemachine/pipeline/callbuffer/RegionServerCallBuffer.java
+++ splice_machine/src/main/java/com/splicemachine/pipeline/callbuffer/RegionServerCallBuffer.java
@@ -23,152 +23,158 @@ import java.util.concurrent.Future;
  * <em>Please Note:</em> This data structure also contains a table name.  So there will be multiple instances of this class
  * for each region server, since each table and region server combination will have an instance of this class.
  */
-class RegionServerCallBuffer implements CallBuffer<Pair<byte[],RegionCallBuffer>> {
-		private static final Logger LOG = Logger.getLogger(RegionServerCallBuffer.class);
-		private ServerName serverName;
-		private final Writer writer;
+class RegionServerCallBuffer implements CallBuffer<Pair<byte[], RegionCallBuffer>> {
 
-		/**
-		 * Map of all the call buffers for each region on this region server.
-		 * The map is keyed by the starting row key for the regions.
-		 * And the value (entry) stored in the map is the buffer of calls for the region.
-		 * These calls are HBase RPC calls.
-		 * Regions are basically defined by their table name and the starting row key for the region.
-		 * If you know that information, you can get the call buffer to the region.
-		 */
-		private NavigableMap<byte[],RegionCallBuffer> buffers;
-		private final List<Future<WriteStats>> outstandingRequests = Lists.newArrayList();
-		private final MergingWriteStats writeStats;
-		private WriteConfiguration writeConfiguration;
-		private byte[] tableName;
-		private final TxnView txn;
+    private static final Logger LOG = Logger.getLogger(RegionServerCallBuffer.class);
 
-		public RegionServerCallBuffer(byte[] tableName,
-																	TxnView txn,
-																	WriteConfiguration writeConfiguration,
-																	ServerName serverName,
-																	Writer writer,
-																	final MergingWriteStats writeStats) {
-				this.txn = txn;
-				this.writeConfiguration = writeConfiguration;
-				this.tableName = tableName;
-				this.writeStats = writeStats;
-				this.serverName = serverName;
-				this.writer = writer;
-				this.buffers = new TreeMap<>(Bytes.BYTES_COMPARATOR);
-		}
+    private final ServerName serverName;
+    private final Writer writer;
 
-	    /**
-	     * Add a buffer of region calls to this region server's call buffers.
-	     * @param element a pair that consists of the starting row key and the call buffer for the region
-	     */
-		@Override
-		public void add(Pair<byte[],RegionCallBuffer> element) throws Exception {
-				SpliceLogUtils.trace(LOG, "add %s", element);
-				buffers.put(element.getFirst(), element.getSecond());
-		}
+    /**
+     * Map of all the call buffers for each region on this region server.
+     * The map is keyed by the starting row key for the regions.
+     * And the value (entry) stored in the map is the buffer of calls for the region.
+     * These calls are HBase RPC calls.
+     * Regions are basically defined by their table name and the starting row key for the region.
+     * If you know that information, you can get the call buffer to the region.
+     */
+    private final NavigableMap<byte[], RegionCallBuffer> buffers;
+    private final List<Future<WriteStats>> outstandingRequests = Lists.newArrayList();
+    private final MergingWriteStats writeStats;
+    private final WriteConfiguration writeConfiguration;
+    private final byte[] tableName;
+    private final TxnView txn;
 
-		@Override
-		public void addAll(Pair<byte[],RegionCallBuffer>[] elements) throws Exception {
-				for(Pair<byte[],RegionCallBuffer> element:elements)
-						add(element);
-		}
+    public RegionServerCallBuffer(byte[] tableName,
+                                  TxnView txn,
+                                  WriteConfiguration writeConfiguration,
+                                  ServerName serverName,
+                                  Writer writer,
+                                  final MergingWriteStats writeStats) {
+        this.txn = txn;
+        this.writeConfiguration = writeConfiguration;
+        this.tableName = tableName;
+        this.writeStats = writeStats;
+        this.serverName = serverName;
+        this.writer = writer;
+        this.buffers = new TreeMap<>(Bytes.BYTES_COMPARATOR);
+    }
 
-		public void remove(byte[] key) throws Exception {
-				this.buffers.remove(key);
-		}
+    /**
+     * Add a buffer of region calls to this region server's call buffers.
+     *
+     * @param element a pair that consists of the starting row key and the call buffer for the region
+     */
+    @Override
+    public void add(Pair<byte[], RegionCallBuffer> element) throws Exception {
+        SpliceLogUtils.trace(LOG, "add %s", element);
+        buffers.put(element.getFirst(), element.getSecond());
+    }
+
+    @Override
+    public void addAll(Pair<byte[], RegionCallBuffer>[] elements) throws Exception {
+        for (Pair<byte[], RegionCallBuffer> element : elements)
+            add(element);
+    }
+
+    public void remove(byte[] key) throws Exception {
+        this.buffers.remove(key);
+    }
 
 
-		@Override
-		public void addAll(Iterable<Pair<byte[],RegionCallBuffer>> elements) throws Exception {
-				for(Pair<byte[],RegionCallBuffer> element:elements){
-						add(element);
-				}
-		}
+    @Override
+    public void addAll(Iterable<Pair<byte[], RegionCallBuffer>> elements) throws Exception {
+        for (Pair<byte[], RegionCallBuffer> element : elements) {
+            add(element);
+        }
+    }
 
-		private void flushBufferCheckPrevious() throws Exception {
-				Iterator<Future<WriteStats>> futureIterator = outstandingRequests.iterator();
-				while(futureIterator.hasNext()){
-						Future<WriteStats> future = futureIterator.next();
-						if(future.isDone()){
-								WriteStats retStats = future.get();//check for errors
-								//if it gets this far, it succeeded--strip the reference
-								futureIterator.remove();
-								writeStats.merge(retStats);
-						}
-				}
-		}
+    private void flushBufferCheckPrevious() throws Exception {
+        Iterator<Future<WriteStats>> futureIterator = outstandingRequests.iterator();
+        while (futureIterator.hasNext()) {
+            Future<WriteStats> future = futureIterator.next();
+            if (future.isDone()) {
+                WriteStats retStats = future.get();//check for errors
+                //if it gets this far, it succeeded--strip the reference
+                futureIterator.remove();
+                writeStats.merge(retStats);
+            }
+        }
+    }
 
-		public BulkWrites getBulkWrites() throws Exception {
-				Set<Entry<byte[], RegionCallBuffer>> entries = this.buffers.entrySet();
-				Collection<BulkWrite> bws = new ArrayList<>(entries.size());
-				for (Entry<byte[], RegionCallBuffer> regionEntry: this.buffers.entrySet()) {
-						RegionCallBuffer value = regionEntry.getValue();
-						if(value.isEmpty()) continue;
-						bws.add(value.getBulkWrite());
-						value.flushBuffer(); // zero out
-				}
-				if(LOG.isTraceEnabled())
-						SpliceLogUtils.trace(LOG, "flushing %d entries", bws.size());
-				return new BulkWrites(bws,this.txn,this.buffers.lastKey());
-		}
+    public BulkWrites getBulkWrites() throws Exception {
+        Set<Entry<byte[], RegionCallBuffer>> entries = this.buffers.entrySet();
+        Collection<BulkWrite> bws = new ArrayList<>(entries.size());
+        for (Entry<byte[], RegionCallBuffer> regionEntry : this.buffers.entrySet()) {
+            RegionCallBuffer value = regionEntry.getValue();
+            if (value.isEmpty()) continue;
+            bws.add(value.getBulkWrite());
+            value.flushBuffer(); // zero out
+        }
+        return new BulkWrites(bws, this.txn, this.buffers.lastKey());
+    }
 
-		@Override
-		public void flushBuffer() throws Exception {
-				SpliceLogUtils.trace(LOG, "flushBuffer %s",this.serverName);
-				if (writer == null) // No Op Buffer
-						return;
-				if(buffers.size()<=0)
-						return;
-				flushBufferCheckPrevious();
-				BulkWrites bulkWrites = getBulkWrites();
-				if (bulkWrites.numEntries()!=0)
-						outstandingRequests.add(writer.write(tableName,bulkWrites, writeConfiguration));
-		}
+    /**
+     * Send buffered BulkWrites and check for (but do not wait for) writes in progress.
+     */
+    @Override
+    public void flushBuffer() throws Exception {
+        SpliceLogUtils.trace(LOG, "flushBuffer %s", this.serverName);
+        if (writer == null || buffers.isEmpty()) {
+            return;
+        }
+        flushBufferCheckPrevious();
+        BulkWrites bulkWrites = getBulkWrites();
+        if (bulkWrites.numEntries() != 0) {
+            Future<WriteStats> write = writer.write(tableName, bulkWrites, writeConfiguration);
+            outstandingRequests.add(write);
+        }
+    }
 
-		@Override
-		public void close() throws Exception {
-				if (writer == null) // No Op Buffer
-						return;
-				flushBuffer();
-				//make sure all outstanding buffers complete before returning
-				for(Future<WriteStats> outstandingCall:outstandingRequests){
-						WriteStats retStats = outstandingCall.get();//wait for errors and/or completion
-						if (LOG.isTraceEnabled())
-								SpliceLogUtils.trace(LOG, "close returning stats %s",retStats);
-						writeStats.merge(retStats);
-				}
-		}
+    @Override
+    public void close() throws Exception {
+        if (writer == null) {// No Op Buffer
+            return;
+        }
+        flushBuffer();
+        //make sure all outstanding buffers complete before returning
+        for (Future<WriteStats> outstandingCall : outstandingRequests) {
+            WriteStats retStats = outstandingCall.get();//wait for errors and/or completion
+            if (LOG.isTraceEnabled())
+                SpliceLogUtils.trace(LOG, "close returning stats %s", retStats);
+            writeStats.merge(retStats);
+        }
+    }
 
-		public int getHeapSize() {
-				int heapSize = 0;
-				for(Entry<byte[], RegionCallBuffer> element:buffers.entrySet())
-						heapSize+=element.getValue().getHeapSize();
-				return heapSize;
-		}
+    public int getHeapSize() {
+        int heapSize = 0;
+        for (Entry<byte[], RegionCallBuffer> element : buffers.entrySet())
+            heapSize += element.getValue().getHeapSize();
+        return heapSize;
+    }
 
-		public int getKVPairSize() {
-				int size = 0;
-				for(Entry<byte[], RegionCallBuffer> element:buffers.entrySet())
-						size+=element.getValue().getBufferSize();
-				return size;
-		}
+    public int getKVPairSize() {
+        int size = 0;
+        for (Entry<byte[], RegionCallBuffer> element : buffers.entrySet())
+            size += element.getValue().getBufferSize();
+        return size;
+    }
 
-		public Writer getWriter() {
-				return writer;
-		}
+    public Writer getWriter() {
+        return writer;
+    }
 
-		public ServerName getServerName() {
-				return serverName;
-		}
+    public ServerName getServerName() {
+        return serverName;
+    }
 
-		@Override
-		public PreFlushHook getPreFlushHook() {
-				return null;
-		}
+    @Override
+    public PreFlushHook getPreFlushHook() {
+        return null;
+    }
 
-		@Override
-		public WriteConfiguration getWriteConfiguration() {
-				return writeConfiguration;
-		}
+    @Override
+    public WriteConfiguration getWriteConfiguration() {
+        return writeConfiguration;
+    }
 }
