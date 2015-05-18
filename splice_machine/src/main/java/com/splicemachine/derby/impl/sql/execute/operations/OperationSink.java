645cf60d13d0b84e47187b1bf74bfb500d23ba4c
Keith Winkler <kwinkler@splicemachine.com>
2015-05-18 13:06:48 -0500

    DB-3307: TableOperationSink: refactor for readability.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/OperationSink.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/OperationSink.java
@@ -2,11 +2,15 @@ package com.splicemachine.derby.impl.sql.execute.operations;
 
 import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
 import com.splicemachine.derby.stats.TaskStats;
+
 /**
  * Instances are invoked by SinkTask to perform the actual operation-specific Sinking logic.
  */
 public interface OperationSink {
 
+    /**
+     * A given instance's sink method is only called once.
+     */
     TaskStats sink(SpliceRuntimeContext spliceRuntimeContext) throws Exception;
 
 }
