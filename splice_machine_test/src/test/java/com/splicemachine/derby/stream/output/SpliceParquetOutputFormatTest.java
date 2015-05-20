2c1f1f16289dafc7a96f650060d336e717cea48a
John Leach <jleach@splicemachine.com>
2015-05-20 12:26:55 -0500

    Parquet info

--- splice_machine_test/src/test/java/com/splicemachine/derby/stream/output/SpliceParquetOutputFormatTest.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/stream/output/SpliceParquetOutputFormatTest.java
@@ -4,10 +4,15 @@ import com.splicemachine.db.iapi.sql.execute.ExecRow;
 import com.splicemachine.derby.stream.BaseStreamTest;
 import com.splicemachine.derby.test.framework.SpliceUnitTest;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hive.ql.io.ParquetFileStorageFormatDescriptor;
 import org.apache.spark.sql.columnar.BINARY;
+import org.junit.Ignore;
 import org.junit.Test;
+import parquet.hadoop.ParquetFileReader;
 import parquet.hadoop.ParquetWriter;
 import parquet.hadoop.metadata.CompressionCodecName;
+import parquet.hadoop.metadata.ParquetMetadata;
 import parquet.schema.MessageType;
 import parquet.schema.OriginalType;
 import parquet.schema.Types;
@@ -17,6 +22,7 @@ import java.io.IOException;
 /**
  * Created by jleach on 5/15/15.
  */
+@Ignore
 public class SpliceParquetOutputFormatTest extends BaseStreamTest {
     protected String baseParquetDirectory = SpliceUnitTest.getBaseDirectory()+"/target/parquet";
 
@@ -30,6 +36,9 @@ public class SpliceParquetOutputFormatTest extends BaseStreamTest {
             writer.write(tenRowsTwoDuplicateRecords.get(i));
         }
         writer.close();
+
+        ParquetMetadata readFooter = ParquetFileReader.readFooter(HBaseConfiguration.create(), new Path(baseParquetDirectory + "/foo"));
+
     }
 
 }
