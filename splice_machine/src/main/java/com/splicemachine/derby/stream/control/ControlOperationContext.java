ab763e919a50b674946a2ed9107a69ada64d4e06
John Leach <jleach@splicemachine.com>
2015-05-21 08:36:29 -0500

    Add Serde to Control Operation Context

--- splice_machine/src/main/java/com/splicemachine/derby/stream/control/ControlOperationContext.java
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/control/ControlOperationContext.java
@@ -2,9 +2,16 @@ package com.splicemachine.derby.stream.control;
 
 import com.splicemachine.db.iapi.error.StandardException;
 import com.splicemachine.db.iapi.sql.Activation;
+import com.splicemachine.derby.hbase.SpliceObserverInstructions;
 import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
+import com.splicemachine.derby.iapi.sql.execute.SpliceOperationContext;
+import com.splicemachine.derby.impl.spark.SpliceSpark;
+import com.splicemachine.derby.jdbc.SpliceTransactionResourceImpl;
 import com.splicemachine.derby.stream.iapi.OperationContext;
+import com.splicemachine.si.api.TransactionOperations;
 import com.splicemachine.si.api.TxnView;
+import com.splicemachine.utils.SpliceLogUtils;
+import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
@@ -13,16 +20,30 @@ import java.io.ObjectOutput;
  * Created by jleach on 4/17/15.
  */
 public class ControlOperationContext<Op extends SpliceOperation> implements OperationContext<Op> {
-        protected Op spliceOperation;
         long rowsRead;
         long rowsFiltered;
         long rowsWritten;
+        public SpliceObserverInstructions soi;
+        public SpliceTransactionResourceImpl impl;
+        public Activation activation;
+        public SpliceOperationContext context;
+        public Op op;
+        public TxnView txn;
+        protected static Logger LOG = Logger.getLogger(ControlOperationContext.class);
 
-        public ControlOperationContext() {
+
+
+    public ControlOperationContext() {
         }
 
         protected ControlOperationContext(Op spliceOperation) {
-            this.spliceOperation = spliceOperation;
+            this.op = spliceOperation;
+            this.activation = op.getActivation();
+            try {
+                this.txn = spliceOperation.getCurrentTransaction();
+            } catch (StandardException se) {
+                throw new RuntimeException(se);
+            }
             rowsRead = 0;
             rowsFiltered=0;
             rowsWritten = 0;
@@ -33,13 +54,37 @@ public class ControlOperationContext<Op extends SpliceOperation> implements Oper
 
         @Override
         public void writeExternal(ObjectOutput out) throws IOException {
-            throw new RuntimeException("Control Side Should Never Be Serialized");
+            if (soi == null)
+                soi = SpliceObserverInstructions.create(op.getActivation(), op);
+            out.writeObject(soi);
+            out.writeObject(op);
+            TransactionOperations.getOperationFactory().writeTxn(txn, out);
        }
 
         @Override
         public void readExternal(ObjectInput in)
                 throws IOException, ClassNotFoundException {
-            throw new RuntimeException("Control Side Should Never Be Serialized");
+            SpliceSpark.setupSpliceStaticComponents();
+            soi = (SpliceObserverInstructions) in.readObject();
+            op = (Op) in.readObject();
+            txn = TransactionOperations.getOperationFactory().readTxn(in);
+            boolean prepared = false;
+            try {
+                impl = new SpliceTransactionResourceImpl();
+                impl.prepareContextManager();
+                prepared = true;
+                impl.marshallTransaction(txn);
+                activation = soi.getActivation(impl.getLcc());
+                context = SpliceOperationContext.newContext(activation);
+                op.init(context);
+                readExternalInContext(in);
+            } catch (Exception e) {
+                SpliceLogUtils.logAndThrowRuntime(LOG, e);
+            } finally {
+                if (prepared) {
+                    impl.resetContextManager();
+                }
+            }
         }
 
     @Override
@@ -54,18 +99,18 @@ public class ControlOperationContext<Op extends SpliceOperation> implements Oper
 
     @Override
     public Op getOperation() {
-        return spliceOperation;
+        return op;
     }
 
     @Override
     public Activation getActivation() {
-        return spliceOperation.getActivation();
+        return op.getActivation();
     }
 
     @Override
     public TxnView getTxn() {
         try {
-            return spliceOperation.getCurrentTransaction();
+            return op.getCurrentTransaction();
         } catch (StandardException se) {
             throw new RuntimeException(se);
         }
