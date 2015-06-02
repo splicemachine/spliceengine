2dad46fd6acffd7b1825e1e0090f3d4fce3a3f33
Walter Koetke <wkoetke@splicemachine.com>
2015-06-02 16:59:19 -0400

    DB-3291: Preserve splice-derby.log file by renaming it upon startup, so we don't lose content.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/services/streams/ConfiguredStream.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/services/streams/ConfiguredStream.java
@@ -1,9 +1,17 @@
 package com.splicemachine.derby.impl.services.streams;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.log4j.Logger;
+
 import com.splicemachine.db.iapi.reference.Property;
 import com.splicemachine.db.iapi.services.property.PropertyUtil;
 import com.splicemachine.db.iapi.services.stream.HeaderPrintWriter;
 import com.splicemachine.db.impl.services.stream.SingleStream;
+import com.splicemachine.derby.management.StatementManager;
 
 /**
  * @author Scott Fines
@@ -11,6 +19,9 @@ import com.splicemachine.db.impl.services.stream.SingleStream;
  */
 public class ConfiguredStream extends SingleStream {
 
+    private static Logger LOG = Logger.getLogger(ConfiguredStream.class);
+	private static String SPLICE_DERBY_LOG = "splice-derby.log";
+	
     @Override
     protected HeaderPrintWriter makeStream() {
         String errorFileLocation = PropertyUtil.getSystemProperty(Property.ERRORLOG_FILE_PROPERTY);
@@ -24,7 +35,7 @@ public class ConfiguredStream extends SingleStream {
             String hbaseLogDir = PropertyUtil.getSystemProperty("hbase.log.dir");
             if(hbaseLogDir==null)
                 hbaseLogDir="."; //if you don't know where it goes, make it relative to here
-            String logFileName = PropertyUtil.getSystemProperty("splice.log.file","splice-derby.log");
+            String logFileName = PropertyUtil.getSystemProperty("splice.log.file",SPLICE_DERBY_LOG);
 
             errorFileLocation = hbaseLogDir+"/"+logFileName;
             System.setProperty(Property.ERRORLOG_FILE_PROPERTY,errorFileLocation);
@@ -32,4 +43,36 @@ public class ConfiguredStream extends SingleStream {
 
         return super.makeStream();
     }
+
+    protected boolean archiveLogFileIfNeeded(File logFile) {
+		boolean archived = false;
+		String logFileName = logFile.getName();
+		if (!SPLICE_DERBY_LOG.equalsIgnoreCase(logFileName)) {
+			return false;
+		}
+		if (logFile.exists()) {
+			int maxFileCount = PropertyUtil.getSystemInt("splice.log.file.max", 100);
+			boolean validTarget = false;
+			Path targetPath = null;
+			String baseName = logFileName + ".%s";
+			Path logFilePath = logFile.toPath();
+			for (int i = 1; !validTarget && i < maxFileCount; i++) {
+				targetPath = logFilePath.resolveSibling(String.format(baseName, i));
+				if (!Files.exists(targetPath)) {
+					validTarget = true;
+				}
+			}
+			try {
+				if (validTarget) {
+					Files.move(logFilePath, targetPath);
+					archived = true;
+				}
+			} catch (IOException ioe) {
+				archived = false;
+				LOG.error(String.format("Exception trying to archive log file %s. We will proceed without archiving it.",
+					logFileName), ioe);
+			}
+		}
+		return archived;
+	}
 }
