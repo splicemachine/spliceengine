1e8ac4f577a05ea82839743f8dfc335360ff7b44
John Leach <jleach@splicemachine.com>
2015-06-09 16:34:57 -0500

    Add Merge Iterators

---
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/iterator/merge/AbstractMergeJoinIterator.java
@@ -0,0 +1,117 @@
+package com.splicemachine.derby.stream.iterator.merge;
+
+import com.splicemachine.db.iapi.error.StandardException;
+import com.splicemachine.db.iapi.sql.execute.ExecRow;
+import com.splicemachine.derby.impl.sql.execute.operations.JoinUtils;
+import com.splicemachine.derby.impl.sql.execute.operations.LocatedRow;
+import com.splicemachine.derby.impl.sql.execute.operations.MergeJoinOperation;
+import com.splicemachine.derby.stream.iterator.merge.MergeOuterJoinIterator;
+import org.apache.log4j.Logger;
+import org.sparkproject.guava.common.collect.PeekingIterator;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public abstract class AbstractMergeJoinIterator implements Iterator<LocatedRow>, Iterable<LocatedRow> {
+    private static final Logger LOG = Logger.getLogger(MergeOuterJoinIterator.class);
+    final Iterator<LocatedRow> leftRS;
+    final PeekingIterator<LocatedRow> rightRS;
+    final int[] joinKeys;
+    protected List<ExecRow> currentRights = new ArrayList<ExecRow>();
+    protected LocatedRow left;
+    protected Iterator<ExecRow> currentRightIterator;
+    protected LocatedRow currentLocatedRow;
+    protected MergeJoinOperation mergeJoinOperation;
+
+    /**
+     * MergeJoinRows constructor. Note that keys for left & right sides
+     * are the join keys on which each side is sorted (not all of the
+     * join keys).
+     *
+     * @param leftRS        Iterator for left side rows
+     * @param rightRS       Iterator for right side rows
+     * @param leftKeys      Join key(s) on which left side is sorted
+     * @param rightKeys     Join Key(s) on which right side is sorted
+     */
+    public AbstractMergeJoinIterator(Iterator<LocatedRow> leftRS,
+                                     PeekingIterator<LocatedRow> rightRS,
+                                     int[] leftKeys, int[] rightKeys,
+                                     MergeJoinOperation mergeJoinOperation) {
+        this.mergeJoinOperation = mergeJoinOperation;
+        this.leftRS = leftRS;
+        this.rightRS = rightRS;
+        assert(leftKeys.length == rightKeys.length);
+        joinKeys = new int[leftKeys.length * 2];
+        for (int i = 0, s = leftKeys.length; i < s; i++){
+            // add keys indices for left & right like [L1, R1, L2, R2, ...],
+            // incremented to work with 1-based ExecRow.getColumn()
+            joinKeys[i * 2] = leftKeys[i] + 1;
+            joinKeys[i * 2 + 1] = rightKeys[i] + 1;
+        }
+    }
+
+    private int compare(ExecRow left, ExecRow right) throws StandardException {
+        for (int i = 0, s = joinKeys.length; i < s; i = i + 2) {
+            int result = left.getColumn(joinKeys[i])
+                    .compare(right.getColumn(joinKeys[i + 1]));
+            if (result != 0) {
+                return result;
+            }
+        }
+        return 0;
+    }
+
+    protected Iterator<ExecRow> rightsForLeft(ExecRow left) throws IOException, StandardException {
+        if (currentRights.size() > 0 // Check to see if we've already collected the right rows
+                && compare(left, currentRights.get(0)) == 0){ // that match this left
+            return currentRights.iterator();
+        }
+        // If not, look for the ones that do
+        currentRights = new ArrayList<ExecRow>();
+        while (rightRS.hasNext()){
+            int comparison = compare(left, rightRS.peek().getRow());
+            if (comparison == 0) { // if matches left, add to buffer
+                currentRights.add(rightRS.next().getRow().getClone());
+            } else if (comparison < 0) { // if is greater than left, stop
+                break;
+            } else {
+                // if is less than left, read next right
+            }
+        }
+        return currentRights.iterator();
+    }
+
+    @Override
+    public Iterator<LocatedRow> iterator() {
+        return this;
+    }
+
+    @Override
+    public abstract boolean hasNext();
+
+    @Override
+    public LocatedRow next() {
+        return currentLocatedRow;
+    }
+
+    @Override
+    public void remove() {
+        throw new RuntimeException("Not Supported");
+    }
+
+    protected LocatedRow mergeRows(LocatedRow leftRow, ExecRow rightRow) {
+        try {
+            ExecRow execRow = JoinUtils.getMergedRow(leftRow.getRow(), rightRow == null ? mergeJoinOperation.getEmptyRow() : rightRow,
+                    mergeJoinOperation.wasRightOuterJoin,
+                    mergeJoinOperation.getExecutionFactory().getValueRow(
+                            mergeJoinOperation.getRightNumCols() + mergeJoinOperation.getLeftNumCols()));
+            LocatedRow lr = new LocatedRow(leftRow.getRowLocation(), execRow);
+            mergeJoinOperation.setCurrentLocatedRow(lr);
+            return lr;
+        } catch (Exception e ) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/iterator/merge/MergeOuterJoinIterator.java
@@ -0,0 +1,61 @@
+package com.splicemachine.derby.stream.iterator.merge;
+
+import com.splicemachine.db.iapi.sql.execute.ExecRow;
+import com.splicemachine.derby.impl.sql.execute.operations.LocatedRow;
+import com.splicemachine.derby.impl.sql.execute.operations.MergeJoinOperation;
+import org.apache.log4j.Logger;
+import org.sparkproject.guava.common.collect.PeekingIterator;
+import java.util.Iterator;
+
+public class MergeOuterJoinIterator extends AbstractMergeJoinIterator {
+    private static final Logger LOG = Logger.getLogger(MergeOuterJoinIterator.class);
+    /**
+     * MergeJoinRows constructor. Note that keys for left & right sides
+     * are the join keys on which each side is sorted (not all of the
+     * join keys).
+     *
+     * @param leftRS        Iterator for left side rows
+     * @param rightRS       Iterator for right side rows
+     * @param leftKeys      Join key(s) on which left side is sorted
+     * @param rightKeys     Join Key(s) on which right side is sorted
+     */
+    public MergeOuterJoinIterator(Iterator<LocatedRow> leftRS,
+                                  PeekingIterator<LocatedRow> rightRS,
+                                  int[] leftKeys, int[] rightKeys,
+                                  MergeJoinOperation mergeJoinOperation) {
+        super(leftRS,rightRS,leftKeys,rightKeys,mergeJoinOperation);
+    }
+
+    @Override
+    public boolean hasNext() {
+        try {
+            while (currentRightIterator.hasNext()) {
+                ExecRow right = currentRightIterator.next();
+                currentLocatedRow = mergeRows(left, right);
+                if (mergeJoinOperation.getRestriction().apply(currentLocatedRow.getRow())) {
+                    return true;
+                }
+            }
+            while (leftRS.hasNext()) {
+                left = leftRS.next();
+                currentRightIterator = rightsForLeft(left.getRow());
+                boolean returnedRows = false;
+                while (currentRightIterator.hasNext()) {
+                    currentLocatedRow = mergeRows(left, currentRightIterator.next());
+                    if (mergeJoinOperation.getRestriction().apply(currentLocatedRow.getRow())) {
+                        returnedRows = true;
+                        return true;
+                    }
+                }
+                if (!returnedRows) {
+                    currentLocatedRow = mergeRows(left, null);
+                    return true;
+                }
+            }
+            return false;
+        }  catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
