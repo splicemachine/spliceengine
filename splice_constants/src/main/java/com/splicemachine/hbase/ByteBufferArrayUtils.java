c8bcc08405735cb24a0787150bbf50b76d1809dc
John Leach <jleach@splicemachine.com>
2015-06-10 11:06:28 -0500

    Deprecation Cleanup

--- splice_constants/src/main/java/com/splicemachine/hbase/ByteBufferArrayUtils.java
+++ splice_constants/src/main/java/com/splicemachine/hbase/ByteBufferArrayUtils.java
@@ -12,43 +12,43 @@ public class ByteBufferArrayUtils {
     public static boolean matchingFamily(KeyValue keyValue, byte[] family) {
     	if (family==null||keyValue==null || family.length != keyValue.getFamilyLength())
     		return false;
-    	return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getFamilyOffset(), family, 0, keyValue.getFamilyLength());
+    	return ArrayUtil.equals(keyValue.getFamilyArray(), keyValue.getFamilyOffset(), family, 0, keyValue.getFamilyLength());
     }
     
     public static boolean matchingQualifier(KeyValue keyValue, byte[] qualifier) {
     	if (qualifier==null||keyValue==null||qualifier.length != keyValue.getQualifierLength())
     		return false;
-    	return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getQualifierOffset(), qualifier, 0, keyValue.getQualifierLength());
+    	return ArrayUtil.equals(keyValue.getQualifierArray(), keyValue.getQualifierOffset(), qualifier, 0, keyValue.getQualifierLength());
     }
 
     public static boolean matchingValue(KeyValue keyValue, byte[] value) {
     	if (value==null||keyValue==null || value.length != keyValue.getValueLength())
     		return false;
-    	return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getValueOffset(), value, 0, keyValue.getValueLength());   	
+    	return ArrayUtil.equals(keyValue.getValueArray(), keyValue.getValueOffset(), value, 0, keyValue.getValueLength());
     }
 
 	public static boolean matchingFamilyKeyValue(KeyValue keyValue, KeyValue other) {
 		if (keyValue==null||other==null || keyValue.getFamilyLength()!=other.getFamilyLength())
 			return false;
-		return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getFamilyOffset(), other.getBuffer(), other.getFamilyOffset(), other.getFamilyLength());
+		return ArrayUtil.equals(keyValue.getFamilyArray(), keyValue.getFamilyOffset(), other.getFamilyArray(), other.getFamilyOffset(), other.getFamilyLength());
 	}
 
 	public static boolean matchingQualifierKeyValue(KeyValue keyValue, KeyValue other) {
 		if (keyValue==null||other==null || keyValue.getQualifierLength()!=other.getQualifierLength())
 			return false;
-		return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getQualifierOffset(), other.getBuffer(), other.getQualifierOffset(), other.getQualifierLength());
+		return ArrayUtil.equals(keyValue.getQualifierArray(), keyValue.getQualifierOffset(), other.getQualifierArray(), other.getQualifierOffset(), other.getQualifierLength());
 	}
 
 	public static boolean matchingRowKeyValue(KeyValue keyValue, KeyValue other) {
 		if (keyValue==null||other==null || keyValue.getRowLength()!=other.getRowLength())
 			return false;
-		return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getRowOffset(), other.getBuffer(), other.getRowOffset(), other.getRowLength());
+		return ArrayUtil.equals(keyValue.getRowArray(), keyValue.getRowOffset(), other.getRowArray(), other.getRowOffset(), other.getRowLength());
 	}
 
     public static boolean matchingValueKeyValue(KeyValue keyValue, KeyValue other) {
 		if (keyValue==null||other==null || keyValue.getValueLength()!=other.getValueLength())
 			return false;
-		return ArrayUtil.equals(keyValue.getBuffer(), keyValue.getValueOffset(), other.getBuffer(), other.getValueOffset(), other.getValueLength());
+		return ArrayUtil.equals(keyValue.getValueArray(), keyValue.getValueOffset(), other.getValueArray(), other.getValueOffset(), other.getValueLength());
     }
 
 	
