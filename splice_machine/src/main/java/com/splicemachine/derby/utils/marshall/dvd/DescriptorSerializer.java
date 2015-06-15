558501099b662441de586807aa53788840a32417
Keith Winkler <kwinkler@splicemachine.com>
2015-06-15 15:41:40 -0500

    DB-3370: javadoc: document what "scalar" actually means in the context of our encoding

--- splice_machine/src/main/java/com/splicemachine/derby/utils/marshall/dvd/DescriptorSerializer.java
+++ splice_machine/src/main/java/com/splicemachine/derby/utils/marshall/dvd/DescriptorSerializer.java
@@ -10,42 +10,48 @@ import java.io.IOException;
 
 /**
  * @author Scott Fines
- * Date: 4/2/14
+ *         Date: 4/2/14
  */
 public interface DescriptorSerializer extends Closeable {
-		void encode(MultiFieldEncoder fieldEncoder,DataValueDescriptor dvd, boolean desc) throws StandardException;
 
-		byte[] encodeDirect(DataValueDescriptor dvd, boolean desc) throws StandardException;
+    void encode(MultiFieldEncoder fieldEncoder, DataValueDescriptor dvd, boolean desc) throws StandardException;
 
-		void decode(MultiFieldDecoder fieldDecoder,DataValueDescriptor destDvd, boolean desc) throws StandardException;
+    byte[] encodeDirect(DataValueDescriptor dvd, boolean desc) throws StandardException;
 
-		void decodeDirect(DataValueDescriptor dvd, byte[] data, int offset, int length, boolean desc) throws StandardException;
+    void decode(MultiFieldDecoder fieldDecoder, DataValueDescriptor destDvd, boolean desc) throws StandardException;
 
-		boolean isScalarType();
+    void decodeDirect(DataValueDescriptor dvd, byte[] data, int offset, int length, boolean desc) throws StandardException;
 
-		boolean isFloatType();
+    boolean isScalarType();
 
-		boolean isDoubleType();
+    boolean isFloatType();
 
-		public static interface Factory<T extends DescriptorSerializer>{
+    boolean isDoubleType();
 
-				T newInstance();
-				/**
-				 * @param dvd the dvd to encode/decode
-				 * @return true if this serializer can be used to encode/decode this DataValueDescriptor
-				 */
-				boolean applies(DataValueDescriptor dvd);
+    interface Factory<T extends DescriptorSerializer> {
 
-				/**
-				 *
-				 * @param typeFormatId the Derby type format id for the descriptor
-				 * @return true if this serializer can be used to encode/decode DataValueDescriptors with
-				 * this typeFormatId
-				 */
-				boolean applies(int typeFormatId);
+        T newInstance();
 
-				boolean isScalar();
-				boolean isFloat();
-				boolean isDouble();
-		}
+        /**
+         * @param dvd the dvd to encode/decode
+         * @return true if this serializer can be used to encode/decode this DataValueDescriptor
+         */
+        boolean applies(DataValueDescriptor dvd);
+
+        /**
+         * @param typeFormatId the Derby type format id for the descriptor
+         * @return true if this serializer can be used to encode/decode DataValueDescriptors with
+         * this typeFormatId
+         */
+        boolean applies(int typeFormatId);
+
+        /**
+         * Here "scalar" means the type is ultimately encoded as a variable length integer.
+         */
+        boolean isScalar();
+
+        boolean isFloat();
+
+        boolean isDouble();
+    }
 }
--- splice_machine/src/main/java/com/splicemachine/derby/utils/marshall/dvd/TypeProvider.java
+++ splice_machine/src/main/java/com/splicemachine/derby/utils/marshall/dvd/TypeProvider.java
@@ -5,9 +5,13 @@ package com.splicemachine.derby.utils.marshall.dvd;
  *         Date: 4/9/14
  */
 public interface TypeProvider {
-		boolean isScalar(int keyFormatId);
 
-		boolean isFloat(int keyFormatId);
+    /**
+     * Here "scalar" means the type is ultimately encoded as a variable length integer.
+     */
+    boolean isScalar(int keyFormatId);
 
-		boolean isDouble(int keyFormatId);
+    boolean isFloat(int keyFormatId);
+
+    boolean isDouble(int keyFormatId);
 }
