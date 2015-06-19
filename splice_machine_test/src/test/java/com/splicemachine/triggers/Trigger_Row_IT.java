92d82a02e39ab4750f6da76e9fecce289240faca
Keith Winkler <kwinkler@splicemachine.com>
2015-06-19 11:49:08 -0500

    DB-3391: Trigger_Row_IT: cleanup

--- splice_machine_test/src/test/java/com/splicemachine/triggers/Trigger_Row_IT.java
+++ splice_machine_test/src/test/java/com/splicemachine/triggers/Trigger_Row_IT.java
@@ -11,6 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Timestamp;
 
+import static java.lang.System.currentTimeMillis;
 import static org.junit.Assert.*;
 
 /**
@@ -38,9 +39,6 @@ public class Trigger_Row_IT {
     public static void createSharedTables() throws Exception {
         classWatcher.executeUpdate("create table T (a int, b int, c int)");
         classWatcher.executeUpdate("create table RECORD (text varchar(99))");
-        classWatcher.executeUpdate("create table cascade1 (a int)");
-        classWatcher.executeUpdate("create table cascade2 (a int, name varchar(20), t timestamp)");
-        classWatcher.executeUpdate("create table cascade3 (a int, name varchar(20), t timestamp)");
     }
 
     /* Each test starts with same table state */
@@ -50,11 +48,6 @@ public class Trigger_Row_IT {
         classWatcher.executeUpdate("delete from T");
         classWatcher.executeUpdate("delete from RECORD");
         classWatcher.executeUpdate("insert into T values(1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5),(6,6,6)");
-
-//        triggerDAO.drop("cascade1_insert", "cascade2_insert");
-        classWatcher.executeUpdate("delete from cascade1");
-        classWatcher.executeUpdate("delete from cascade2");
-        classWatcher.executeUpdate("delete from cascade3");
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -65,7 +58,7 @@ public class Trigger_Row_IT {
 
     @Test
     public void afterUpdate() throws Exception {
-        methodWatcher.executeUpdate(tb.after().update().on("T").row().then("INSERT INTO RECORD VALUES('after-update')").build());
+        createTrigger(tb.after().update().on("T").row().then("INSERT INTO RECORD VALUES('after-update')"));
 
         // when - update all rows
         long updateCount1 = methodWatcher.executeUpdate("update T set c = c + 1");
@@ -90,10 +83,10 @@ public class Trigger_Row_IT {
     /* Trigger on subset of columns */
     @Test
     public void afterUpdateOfColumns() throws Exception {
-        methodWatcher.executeUpdate(tb.after().update().of("a,c").on("T").row().then("INSERT INTO RECORD VALUES('after-update')").build());
+        createTrigger(tb.after().update().of("a,c").on("T").row().then("INSERT INTO RECORD VALUES('after-update')"));
 
         // when - update non trigger col
-         methodWatcher.executeUpdate("update T set b = 100");
+        methodWatcher.executeUpdate("update T set b = 100");
         assertRecordCount("after-update", 0);
 
         // when -- update trigger col 1
@@ -107,7 +100,7 @@ public class Trigger_Row_IT {
 
     @Test
     public void afterInsert() throws Exception {
-        methodWatcher.executeUpdate(tb.after().insert().on("T").row().then("INSERT INTO RECORD VALUES('after-insert')").build());
+        createTrigger(tb.after().insert().on("T").row().then("INSERT INTO RECORD VALUES('after-insert')"));
 
         // when - insert over select
         long insertCount1 = methodWatcher.executeUpdate("insert into T select * from T");
@@ -126,7 +119,7 @@ public class Trigger_Row_IT {
 
     @Test
     public void afterDelete() throws Exception {
-        methodWatcher.executeUpdate(tb.after().delete().on("T").row().then("INSERT INTO RECORD VALUES('after-delete')").build());
+        createTrigger(tb.after().delete().on("T").row().then("INSERT INTO RECORD VALUES('after-delete')"));
 
         // when - delete
         long deleteCount1 = methodWatcher.executeUpdate("delete from T where a >=4");
@@ -156,7 +149,7 @@ public class Trigger_Row_IT {
     public void afterInsert_sinkingTriggerAction() throws Exception {
         methodWatcher.executeUpdate("insert into RECORD values('aaa')");
 
-        methodWatcher.executeUpdate(tb.after().insert().on("T").row().then("insert into RECORD select * from RECORD").build());
+        createTrigger(tb.after().insert().on("T").row().then("insert into RECORD select * from RECORD"));
 
         // when - insert over select
         methodWatcher.executeUpdate("insert into T values(1,1,1)");
@@ -173,44 +166,45 @@ public class Trigger_Row_IT {
     public void afterUpdate_sinkingTriggerAction() throws Exception {
         methodWatcher.executeUpdate("insert into RECORD values('aaa')");
 
-        methodWatcher.executeUpdate(tb.after().update().on("T").row().then("insert into RECORD select * from RECORD").build());
+        createTrigger(tb.after().update().on("T").row().then("insert into RECORD select * from RECORD"));
 
         // when - update all rows
         methodWatcher.executeUpdate("update T set c = c + 1");
-        // this should actually be 2^6
+        // this would be 2^6 if our trigger actions executed sequentially
         assertRecordCount("aaa", 7);
     }
 
+    // DB-3354: Nested triggers produce null transition variables.
     @Test
     public void afterInsertCascadingTriggers() throws Exception {
-        // DB-3354: Nested triggers produce null transition variables.
-        methodWatcher.executeUpdate(tb.named("cascade1_insert").after().insert().on("cascade1").referencing("NEW as NEW").row().
-            then("insert into cascade2(a, name, t)\n" +
-                     "values (NEW.a, 'cascade1_insert', CURRENT_TIMESTAMP)").build());
-        methodWatcher.executeUpdate(tb.named("cascade2_insert").after().insert().on("cascade2").referencing("NEW as NEW").row().
-            then("insert into cascade3(a, name, t)\n" +
-                     "values (NEW.a, 'cascade2_insert', CURRENT_TIMESTAMP)").build());
+        methodWatcher.executeUpdate("create table cascade1 (a int)");
+        methodWatcher.executeUpdate("create table cascade2 (a int, name varchar(20), t timestamp)");
+        methodWatcher.executeUpdate("create table cascade3 (a int, name varchar(20), t timestamp)");
 
+        createTrigger(tb.named("cascade1_insert").after().insert().on("cascade1").referencing("NEW as NEW").row().
+                then("insert into cascade2(a, name, t) values (NEW.a, 'cascade1_insert', CURRENT_TIMESTAMP)"));
+        createTrigger(tb.named("cascade2_insert").after().insert().on("cascade2").referencing("NEW as NEW").row().
+                then("insert into cascade3(a, name, t) values (NEW.a, 'cascade2_insert', CURRENT_TIMESTAMP)"));
+
+        // when -- fire the trigger
         methodWatcher.executeUpdate("insert into cascade1 (a) values (1)");
 
-        Timestamp ts2 = null;
-        ResultSet rs = methodWatcher.executeQuery("select * from cascade2");
-        while (rs.next()) {
-            assertEquals(1, rs.getInt(1));
-            assertEquals("cascade1_insert", rs.getString(2));
-            ts2 = rs.getTimestamp(3);
-            assertNotNull(ts2);
-        }
+        // then -- verify content of table 1
+        assertEquals(1, methodWatcher.query("select a from cascade1"));
 
-        Timestamp ts3 = null;
-        rs = methodWatcher.executeQuery("select * from cascade3");
-        while (rs.next()) {
-            assertEquals(1, rs.getInt(1));
-            assertEquals("cascade2_insert", rs.getString(2));
-            ts3 = rs.getTimestamp(3);
-            assertNotNull(ts3);
-        }
+        // then -- verify content of table 2
+        assertEquals(1, methodWatcher.query("select a from cascade2"));
+        assertEquals("cascade1_insert", methodWatcher.query("select name from cascade2"));
+        Timestamp ts2 = methodWatcher.query("select t from cascade2");
+
+        // then -- verify content of table 3
+        assertEquals(1, methodWatcher.query("select a from cascade3"));
+        assertEquals("cascade2_insert", methodWatcher.query("select name from cascade3"));
+        Timestamp ts3 = methodWatcher.query("select t from cascade3");
+
         assertTrue(ts2.before(ts3));
+        assertTrue("ts should be close to current time", Math.abs(currentTimeMillis() - ts2.getTime()) < 3000);
+        assertTrue("ts should be close to current time", Math.abs(currentTimeMillis() - ts3.getTime()) < 3000);
     }
 
 
@@ -222,19 +216,19 @@ public class Trigger_Row_IT {
 
     @Test
     public void beforeUpdate() throws Exception {
-        methodWatcher.executeUpdate(tb.before().update().on("T").row().then("select 1/0 from sys.systables").build());
+        createTrigger(tb.before().update().on("T").row().then("select 1/0 from sys.systables"));
         assertQueryFails("update T set b = b * 2 where a <= 4", "Attempt to divide by zero.");
     }
 
     @Test
     public void beforeInsert() throws Exception {
-        methodWatcher.executeUpdate(tb.before().insert().on("T").row().then("select 1/0 from sys.systables").build());
+        createTrigger(tb.before().insert().on("T").row().then("select 1/0 from sys.systables"));
         assertQueryFails("insert into T values(8,8,8)", "Attempt to divide by zero.");
     }
 
     @Test
     public void beforeDelete() throws Exception {
-        methodWatcher.executeUpdate(tb.before().delete().on("T").row().then("select 1/0 from sys.systables").build());
+        createTrigger(tb.before().delete().on("T").row().then("select 1/0 from sys.systables"));
         assertQueryFails("delete from T where c = 6", "Attempt to divide by zero.");
     }
 
@@ -253,8 +247,8 @@ public class Trigger_Row_IT {
          * the trigger to test if it fired when that effect is also rolled back.  The SET_GLOBAL_DATABASE_PROPERTY
          * stored procedure is non transactional.
          */
-        methodWatcher.executeUpdate(tb.named("constraintTrig1").after().insert().on("T2").row()
-                .then("call syscs_util.SYSCS_SET_GLOBAL_DATABASE_PROPERTY('triggerRowItPropA', 'someValue')").build());
+        createTrigger(tb.named("constraintTrig1").after().insert().on("T2").row()
+                .then("call syscs_util.SYSCS_SET_GLOBAL_DATABASE_PROPERTY('triggerRowItPropA', 'someValue')"));
 
         // when - insert over select
         assertQueryFails("insert into T2 select * from T2", "The statement was aborted because it would have caused a duplicate key value in a unique or primary key constraint or unique index identified by 'T_INDEX1' defined on 'T2'.");
@@ -273,8 +267,8 @@ public class Trigger_Row_IT {
         methodWatcher.executeUpdate("create table T3(a int, constraint T_INDEX2 unique(a))");
         methodWatcher.executeUpdate("insert into T3 values(1),(2),(3)");
 
-        methodWatcher.executeUpdate(tb.named("constraintTrig2").after().update().on("T3").row()
-                .then("call syscs_util.SYSCS_SET_GLOBAL_DATABASE_PROPERTY('triggerRowItPropB', 'someValue')").build());
+        createTrigger(tb.named("constraintTrig2").after().update().on("T3").row()
+                .then("call syscs_util.SYSCS_SET_GLOBAL_DATABASE_PROPERTY('triggerRowItPropB', 'someValue')"));
 
         // when - update
         assertQueryFails("update T3 set a=1 where a=3", "The statement was aborted because it would have caused a duplicate key value in a unique or primary key constraint or unique index identified by 'T_INDEX2' defined on 'T3'.");
@@ -295,17 +289,17 @@ public class Trigger_Row_IT {
     @Test
     public void multipleRowAndStatementTriggersOnOneTable() throws Exception {
         // given - six row triggers on same table.
-        methodWatcher.executeUpdate(tb.named("u_1").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u1')").build());
-        methodWatcher.executeUpdate(tb.named("u_2").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u2')").build());
-        methodWatcher.executeUpdate(tb.named("i_1").after().insert().on("T").row().then("INSERT INTO RECORD VALUES('i1')").build());
-        methodWatcher.executeUpdate(tb.named("i_2").after().insert().on("T").row().then("INSERT INTO RECORD VALUES('i2')").build());
-        methodWatcher.executeUpdate(tb.named("d_1").after().delete().on("T").row().then("INSERT INTO RECORD VALUES('d1')").build());
-        methodWatcher.executeUpdate(tb.named("d_2").after().delete().on("T").row().then("INSERT INTO RECORD VALUES('d2')").build());
+        createTrigger(tb.named("u_1").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u1')"));
+        createTrigger(tb.named("u_2").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u2')"));
+        createTrigger(tb.named("i_1").after().insert().on("T").row().then("INSERT INTO RECORD VALUES('i1')"));
+        createTrigger(tb.named("i_2").after().insert().on("T").row().then("INSERT INTO RECORD VALUES('i2')"));
+        createTrigger(tb.named("d_1").after().delete().on("T").row().then("INSERT INTO RECORD VALUES('d1')"));
+        createTrigger(tb.named("d_2").after().delete().on("T").row().then("INSERT INTO RECORD VALUES('d2')"));
 
         // given - three statement triggers
-        methodWatcher.executeUpdate(tb.named("u_stat").after().update().on("T").statement().then("INSERT INTO RECORD VALUES('statement-1')").build());
-        methodWatcher.executeUpdate(tb.named("i_stat").after().insert().on("T").statement().then("INSERT INTO RECORD VALUES('statement-2')").build());
-        methodWatcher.executeUpdate(tb.named("d_stat").after().delete().on("T").statement().then("INSERT INTO RECORD VALUES('statement-3')").build());
+        createTrigger(tb.named("u_stat").after().update().on("T").statement().then("INSERT INTO RECORD VALUES('statement-1')"));
+        createTrigger(tb.named("i_stat").after().insert().on("T").statement().then("INSERT INTO RECORD VALUES('statement-2')"));
+        createTrigger(tb.named("d_stat").after().delete().on("T").statement().then("INSERT INTO RECORD VALUES('statement-3')"));
 
         // when - update
         methodWatcher.executeUpdate("update T set c = 0 where c=1 or c=2 or c=3");
@@ -331,7 +325,7 @@ public class Trigger_Row_IT {
         assertEquals(6, ps.executeUpdate());
 
         // given - define the trigger after ps is created/compiled
-        methodWatcher.executeUpdate(tb.named("u_1").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u1')").build());
+        createTrigger(tb.named("u_1").after().update().on("T").row().then("INSERT INTO RECORD VALUES('u1')"));
 
         // when - execute ps again
         assertEquals(6, ps.executeUpdate());
@@ -362,6 +356,10 @@ public class Trigger_Row_IT {
         for (long i : values) {
             assertTrue(i > 0);
         }
+    }
+
+    private void createTrigger(TriggerBuilder tb) throws Exception {
+        methodWatcher.executeUpdate(tb.build());
     }
 
 }
