cbf7ae7fde660a177613e7a741c9125708cd3db2
John Leach <jleach@splicemachine.com>
2015-06-29 13:37:25 -0500

    NLJ Changed to MSJ

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/JoinSelectionIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/JoinSelectionIT.java
@@ -201,7 +201,7 @@ public class JoinSelectionIT extends SpliceUnitTest  {
             if (count == 2) {
     			String row = rs.getString(1);
     			String joinStrategy = row.substring(row.indexOf(PLAN_LINE_LEADER)+PLAN_LINE_LEADER.length(),row.indexOf(JOIN_STRATEGY_TERMINATOR));
-    			Assert.assertEquals(NESTED_LOOP_JOIN, joinStrategy);
+    			Assert.assertEquals(MERGE_SORT_JOIN, joinStrategy);
             	break;
             }
         }    
