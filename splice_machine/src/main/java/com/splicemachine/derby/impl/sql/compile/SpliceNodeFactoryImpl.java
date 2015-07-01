0d9726c0d43acbf6411d07430f5098b2085d39be
jeffcunningham <jcunningham@splicemachine.com>
2015-07-01 17:29:19 -0500

    DB-3496: ast manip. Fixed IT bustage by removing splice copy of OrderByNode.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceNodeFactoryImpl.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceNodeFactoryImpl.java
@@ -415,7 +415,7 @@ public class SpliceNodeFactoryImpl extends NodeFactory implements ModuleControl,
 		  	return C_NodeNames.SCROLL_INSENSITIVE_RESULT_SET_NODE_NAME;
 
 		  case C_NodeTypes.ORDER_BY_NODE:
-		  	return "com.splicemachine.derby.impl.sql.compile.OrderByNode";
+              return C_NodeNames.ORDER_BY_NODE_NAME;
 
 		  case C_NodeTypes.DISTINCT_NODE:
 		  	return C_NodeNames.DISTINCT_NODE_NAME;
