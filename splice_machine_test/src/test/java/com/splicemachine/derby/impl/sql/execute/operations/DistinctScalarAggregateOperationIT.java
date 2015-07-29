31d0aa50c7c3ec290e2228b407b231bd88f677e9
Jun Yuan <jyuan@splicemachine.com>
2015-07-29 08:08:19 -0700

    DB-3294: IT for distinct count order by

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/DistinctScalarAggregateOperationIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/DistinctScalarAggregateOperationIT.java
@@ -119,7 +119,7 @@ public class DistinctScalarAggregateOperationIT extends SpliceUnitTest {
 
     @Test
     public void testDistinctCountOrderBy() throws Exception {
-        Assert.assertEquals(4L, methodWatcher.query("select count(distinct score) from" + this.getPaddedTableReference("ORDERSUMMARY")));
+        Assert.assertEquals(4L, methodWatcher.query("select count(distinct score) from " + this.getPaddedTableReference("ORDERSUMMARY") + "order by 1"));
     }
 
     @Test
