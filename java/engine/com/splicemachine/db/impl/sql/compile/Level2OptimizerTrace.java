4af6902b68b0b5776532e3286bc26fef7473243a
jeffcunningham <jcunningham@splicemachine.com>
2015-07-31 14:29:50 -0500

    The push I made for a98c8d1 caused a silent failure in building derbyclient.jar. The build didn't fail but derbyclient.jar was empty.
    The problem was me adding guava dependency transitive to client classes which caused
    the derbyclientjar ant target to fail silently because it couldn't find guava in its
    classpath.
    
    Added a failonerror property to derbyclientjar build target.
    Removed guava dependency from client classes dependencies.

--- java/engine/com/splicemachine/db/impl/sql/compile/Level2OptimizerTrace.java
+++ java/engine/com/splicemachine/db/impl/sql/compile/Level2OptimizerTrace.java
@@ -5,7 +5,6 @@ import com.splicemachine.db.iapi.sql.compile.*;
 import com.splicemachine.db.iapi.sql.conn.LanguageConnectionContext;
 import com.splicemachine.db.iapi.sql.dictionary.ConglomerateDescriptor;
 import com.splicemachine.db.iapi.sql.dictionary.IndexRowGenerator;
-import com.splicemachine.db.impl.ast.PredicateUtils;
 
 /**
  * @author Scott Fines
@@ -131,9 +130,9 @@ public class Level2OptimizerTrace implements OptimizerTrace{
             case JOIN_NODE_PREDICATE_MANIPULATION:
                 String preds = null;
                 if (objectParam2 instanceof PredicateList) {
-                    preds = PredicateUtils.predListToString.apply((PredicateList)objectParam2);
+                    preds = OperatorToString.toString((PredicateList) objectParam2);
                 } else {
-                    preds = PredicateUtils.predToString.apply((Predicate)objectParam2);
+                    preds = OperatorToString.toString((Predicate) objectParam2);
                 }
                 traceString = "Predicate manipulation in join nodeL: "+objectParam1+": "+preds;
                 break;
@@ -159,7 +158,7 @@ public class Level2OptimizerTrace implements OptimizerTrace{
                 break;
             case ADDING_UNORDERED_OPTIMIZABLE:
                 traceString="Adding unordered optimizable, # of predicates = "+intParam1+": "+
-                    PredicateUtils.predListToString.apply((PredicateList)objectParam1);
+                    OperatorToString.toString((PredicateList) objectParam1);
                 break;
             case CHANGING_ACCESS_PATH_FOR_TABLE:
                 traceString="Changing access path for table "+intParam1;
@@ -190,7 +189,7 @@ public class Level2OptimizerTrace implements OptimizerTrace{
                 break;
             case CALLING_NEXT_ACCESS_PATH:
                 traceString="Calling nextAccessPath() for base table "+ objectParam1 +" with "+intParam1+" predicates: "+
-                PredicateUtils.predListToString.apply((PredicateList) objectParam2);
+                OperatorToString.toString((PredicateList) objectParam2);
                 break;
             case TABLE_LOCK_OVER_THRESHOLD:
                 traceString=lockModeThreshold("MODE_TABLE","greater",doubleParam,intParam1);
@@ -277,7 +276,7 @@ public class Level2OptimizerTrace implements OptimizerTrace{
                 throw new IllegalStateException("Unexpected Trace flag: "+ flag);
         }
         assert traceString!=null: "TraceString expected to be non-null";
-        trace(flag.level(),traceString);
+        trace(flag.level(), traceString);
     }
 
     @Override
@@ -350,5 +349,4 @@ public class Level2OptimizerTrace implements OptimizerTrace{
     private String bestCost(){
         return "Best cost = "+optimizer.bestCost+"\n";
     }
-
 }
