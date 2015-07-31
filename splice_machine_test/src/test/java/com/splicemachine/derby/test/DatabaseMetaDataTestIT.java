a8458bf6e06f393748d4847035f4616a6009cac8
dmustafin <dmustafin@lohika.com>
2015-07-31 11:44:23 -0500

    update version

--- splice_machine_test/src/test/java/com/splicemachine/derby/test/DatabaseMetaDataTestIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/test/DatabaseMetaDataTestIT.java
@@ -1,24 +1,20 @@
 package com.splicemachine.derby.test;
 
 import com.splicemachine.derby.test.framework.SpliceWatcher;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 
 import java.sql.DatabaseMetaData;
 
-/**
- * Created by dmustafin on 7/30/15.
- */
-public class DatabaseMetaDataTest {
+public class DatabaseMetaDataTestIT {
 
     @Rule
     public SpliceWatcher methodWatcher = new SpliceWatcher();
 
 
     @Test
-    public void testVersion() throws Exception {
+    public void testVersionAndProductName() throws Exception {
         DatabaseMetaData dmd = methodWatcher.createConnection().getMetaData();
-        Assert.assertEquals("Apache Derby", dmd.getDatabaseProductName());
+        Assert.assertEquals("Splice Machine", dmd.getDatabaseProductName());
+        Assert.assertEquals("10.9.2.2 - (1)", dmd.getDatabaseProductVersion());
     }
 }
