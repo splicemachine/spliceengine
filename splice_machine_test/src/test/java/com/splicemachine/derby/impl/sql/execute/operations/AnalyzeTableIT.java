22fb304c91bb3b5448e9866e6324350b4e385f10
Jun Yuan <jyuan@splicemachine.com>
2015-08-03 09:22:01 -0700

    DB-3611: analyze requires insert privilege on a table

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/AnalyzeTableIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/AnalyzeTableIT.java
@@ -4,7 +4,10 @@ import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
 import com.splicemachine.derby.test.framework.SpliceWatcher;
 import org.junit.*;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLSyntaxErrorException;
+import java.sql.Statement;
 /**
  * Created by jyuan on 7/30/15.
  */
@@ -19,12 +22,21 @@ public class AnalyzeTableIT {
     public SpliceWatcher methodWatcher = new SpliceWatcher(SCHEMA);
 
     @BeforeClass
-    public static void createTables() throws Exception {
+    public static void setup() throws Exception {
         classWatcher.executeUpdate("create table T1 (I INT)");
         classWatcher.executeUpdate("create table T2 (I INT)");
 
         classWatcher.executeUpdate("insert into T1 values 1, 2, 3");
         classWatcher.executeUpdate("insert into T2 values 1, 2, 3, 4, 5, 6, 7, 8, 9");
+
+        classWatcher.execute("call syscs_util.syscs_create_user('analyzeuser', 'passwd')");
+        classWatcher.execute("grant execute on procedure syscs_util.collect_table_statistics to analyzeuser");
+        classWatcher.execute("grant execute on procedure syscs_util.collect_schema_statistics to analyzeuser");
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        classWatcher.execute("call syscs_util.syscs_drop_user('analyzeuser')");
     }
 
     @Test
@@ -45,5 +57,49 @@ public class AnalyzeTableIT {
             count++;
         }
         Assert.assertEquals(2, count);
+    }
+
+    @Test
+    public void testDefaultSchema() throws Exception {
+        Connection connection = methodWatcher.getOrCreateConnection();
+        Statement statement = connection.createStatement();
+        statement.execute("set schema " + SCHEMA);
+        ResultSet rs = statement.executeQuery("Analyze table t1");
+        int count = 0;
+        while (rs.next()) {
+            ++count;
+            String schema = rs.getString(1);
+            Assert.assertTrue(schema.compareToIgnoreCase(SCHEMA) == 0);
+        }
+        Assert.assertEquals(1, count);
+    }
+
+    @Test
+    public void testAnalyzeTablePrivilege() throws Exception {
+        try {
+            Connection connection = methodWatcher.createConnection("analyzeuser", "passwd");
+            Statement statement = connection.createStatement();
+            statement.execute("analyze table AnalyzeTableIT.T2");
+        }
+        catch (SQLSyntaxErrorException e) {
+            String expected = "User 'ANALYZEUSER' does not have INSERT permission on table 'ANALYZETABLEIT'.'T2'.";
+            String message = e.getMessage();
+            Assert.assertTrue(message.compareTo(expected)==0);
+        }
+    }
+
+    @Test
+    public void testAnalyzeSchemaPrivilege() throws Exception {
+
+        try {
+            Connection connection = methodWatcher.createConnection("analyzeuser", "passwd");
+            Statement statement = connection.createStatement();
+            statement.execute("analyze schema AnalyzeTableIT");
+        }
+        catch (SQLSyntaxErrorException e) {
+            String expected = "User 'ANALYZEUSER' does not have INSERT permission on table";
+            String message = e.getMessage();
+            Assert.assertTrue(message.contains(expected));
+        }
     }
 }
