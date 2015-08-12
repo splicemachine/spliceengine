c8d2c235135e2111a73e25bc69bb826c21d14f43
Jun Yuan <jyuan@splicemachine.com>
2015-08-12 14:07:36 -0700

    DB-3664: fix cost estimate for distinct scalar aggregate operation

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceLevel2OptimizerImpl.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceLevel2OptimizerImpl.java
@@ -171,7 +171,7 @@ public class SpliceLevel2OptimizerImpl extends Level2OptimizerImpl{
     @Override
     public AggregateCostController newAggregateCostController(GroupByList groupingList,List<AggregateNode> aggregateVector){
         if(groupingList==null||groupingList.size()<=0) //we are a scalar aggregate
-            return new TempScalarAggregateCostController();
+            return new TempScalarAggregateCostController(aggregateVector);
         else //we are a grouped aggregate
         return new TempGroupedAggregateCostController(groupingList);
     }
