fbaed1b47140938dba92edc0743b09315c3a3c5f
John Leach <jleach@splicemachine.com>
2015-08-18 13:55:01 -0500

    DB-3678 Pointing to the correct client library for generic exception creation

--- java/client/com/splicemachine/db/client/am/Statement.java
+++ java/client/com/splicemachine/db/client/am/Statement.java
@@ -22,8 +22,6 @@ package com.splicemachine.db.client.am;
 
 import java.sql.SQLException;
 import java.util.Arrays;
-
-import com.splicemachine.db.impl.jdbc.Util;
 import com.splicemachine.db.shared.common.reference.SQLState;
 import com.splicemachine.db.shared.common.sanity.SanityManager;
 
@@ -210,7 +208,7 @@ public class Statement implements java.sql.Statement, StatementCallbackInterface
 
     @Override
     public <T> T unwrap(Class<T> iface) throws SQLException{
-        throw Util.notImplemented("unwrap");
+        throw SQLExceptionFactory.notImplemented("unwrap");
     }
 
     private void resetStatement() {
