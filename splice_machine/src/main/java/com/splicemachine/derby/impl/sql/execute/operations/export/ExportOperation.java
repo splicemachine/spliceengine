6fba568457689c2a7810357d345b41b260f34748
dmustafin <dmustafin@lohika.com>
2015-09-03 09:14:40 -0500

    Throw an exception if needed

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/export/ExportOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/export/ExportOperation.java
@@ -3,6 +3,7 @@ package com.splicemachine.derby.impl.sql.execute.operations.export;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
+import com.splicemachine.db.iapi.reference.SQLState;
 import com.splicemachine.derby.hbase.SpliceObserverInstructions;
 import com.splicemachine.derby.iapi.sql.execute.*;
 import com.splicemachine.derby.iapi.storage.RowProvider;
@@ -65,6 +66,11 @@ public class ExportOperation extends SpliceBaseOperation implements SinkingOpera
                            String fieldSeparator,
                            String quoteCharacter) throws StandardException {
         super(activation, rsNumber, 0d, 0d);
+
+        if (replicationCount <= 0) {
+            throw StandardException.newException(SQLState.EXPORT_PARAMETER_IS_WRONG);
+        }
+
         this.source = source;
         this.sourceColumnDescriptors = sourceColumnDescriptors;
         this.exportParams = new ExportParams(exportPath, compression, replicationCount, encoding, fieldSeparator, quoteCharacter);
