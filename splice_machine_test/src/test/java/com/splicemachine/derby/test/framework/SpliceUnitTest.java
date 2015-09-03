1ebb5de470ebd8b139c1c64632b5f2ceb739b581
John Leach <jleach@splicemachine.com>
2015-09-03 07:48:59 -0700

    DB-3409 Test for Subquery Unrolling Query 2

--- splice_machine_test/src/test/java/com/splicemachine/derby/test/framework/SpliceUnitTest.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/test/framework/SpliceUnitTest.java
@@ -107,6 +107,20 @@ public class SpliceUnitTest {
             resultSet.next();
         Assert.assertTrue("failed query: " + query + " -> " + resultSet.getString(1),resultSet.getString(1).contains(contains));
     }
+
+    protected void queryDoesNotContainString(String query, String notContains,SpliceWatcher methodWatcher) throws Exception {
+        ResultSet resultSet = methodWatcher.executeQuery(query);
+        while (resultSet.next())
+            Assert.assertFalse("failed query: " + query + " -> " + resultSet.getString(1), resultSet.getString(1).contains(notContains));
+    }
+
+
+    protected void rowContainsQuery(int level, ResultSet resultSet, String contains,SpliceWatcher methodWatcher) throws Exception {
+        for (int i = 0; i< level;i++)
+            resultSet.next();
+        Assert.assertTrue("failed query: " + resultSet.getString(1),resultSet.getString(1).contains(contains));
+    }
+
     protected void rowManyContainsQuery(int level, String query, SpliceWatcher methodWatcher,String... contains) throws Exception {
         ResultSet resultSet = methodWatcher.executeQuery(query);
         for (int i = 0; i< level;i++)
