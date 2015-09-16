887e981d8c67c42ecc4ed62ab875dab3f489a3b8
Walter Koetke <wkoetke@splicemachine.com>
2015-09-16 12:03:08 -0700

    DB-3861: Added IT that fails due to this issue.

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/CheckConstraintIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/CheckConstraintIT.java
@@ -15,6 +15,7 @@ import com.splicemachine.test_tools.TableCreator;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -44,7 +45,8 @@ public class CheckConstraintIT extends SpliceUnitTest {
     @SuppressWarnings("unchecked")
 	public static void createData(Connection conn, String schemaName) throws Exception {
 
-        new TableCreator(conn) // maybe insert some base 'good' rows too
+        // Table2 has check constraints upon creation
+        new TableCreator(conn)
             .withCreate(
             	"create table table1 ( " +
                 "id int not null primary key constraint id_ge_ck check (id >= 1000), " +
@@ -57,6 +59,21 @@ public class CheckConstraintIT extends SpliceUnitTest {
             .withRows(rows(
                 row(1000, 200, 90, 79, "ok", "good1")))
             .create();
+
+        // Table2 has no check constraints initially
+        new TableCreator(conn)
+        .withCreate(
+        	"create table table2 ( " +
+            "id int not null primary key, " +
+            "i_gt int, " +
+            "i_eq int, " +
+            "i_lt int, " +
+            "v_neq varchar(32), " +
+            "v_in varchar(32))")
+        .withInsert("insert into table2 values (?, ?, ?, ?, ?, ?)")
+        .withRows(rows(
+            row(1000, 200, 90, 79, "ok", "good1")))
+        .create();
 
         conn.commit();
     }
@@ -103,6 +120,37 @@ public class CheckConstraintIT extends SpliceUnitTest {
     		MSG_START_DEFAULT, "V_NEQ_CK");
 
     	verifyViolation("insert into table1 values (1005, 101, 90, 85, 'ok', 'notsogood')", // col 6 bad
+    		MSG_START_DEFAULT, "V_IN_CK");
+
+    }
+
+    @Ignore("DB-3681 targeted for Lassen")
+    public void testSingleInsertsAfterAlterTable() throws Exception {
+        methodWatcher.executeUpdate("alter table table2 add constraint id_ge_ck check (id >= 1000)");
+        methodWatcher.executeUpdate("alter table table2 add constraint i_gt_ck check (i_gt > 100)");
+        methodWatcher.executeUpdate("alter table table2 add constraint i_eq_ck check (i_eq = 90)");
+        methodWatcher.executeUpdate("alter table table2 add constraint i_lt_ck check (i_lt < 80)");
+        methodWatcher.executeUpdate("alter table table2 add constraint v_neq_ck check (v_neq <> 'bad')");
+        methodWatcher.executeUpdate("alter table table2 add constraint v_in_ck check (v_in in ('good1', 'good2', 'good3'))");
+
+        verifyNoViolation("insert into table2 values (1001, 101, 90, 79, 'ok', 'good2')", 1); // all good
+
+    	verifyViolation("insert into table2 values (999, 101, 90, 79, 'ok', 'good1')", // col 1 (PK) bad
+    		MSG_START_DEFAULT, "ID_GE_CK");
+
+    	verifyViolation("insert into table2 values (1002, 25, 90, 79, 'ok', 'good1')", // col 2 bad
+    		MSG_START_DEFAULT, "I_GT_CK");
+
+    	verifyViolation("insert into table2 values (1003, 101, 901, 79, 'ok', 'good1')", // col 3 bad
+    		MSG_START_DEFAULT, "I_EQ_CK");
+
+    	verifyViolation("insert into table2 values (1004, 101, 90, 85, 'ok', 'good2')", // col 4 bad
+    		MSG_START_DEFAULT, "I_LT_CK");
+
+    	verifyViolation("insert into table2 values (1005, 101, 90, 85, 'bad', 'good3')", // col 5 bad
+    		MSG_START_DEFAULT, "V_NEQ_CK");
+
+    	verifyViolation("insert into table2 values (1005, 101, 90, 85, 'ok', 'notsogood')", // col 6 bad
     		MSG_START_DEFAULT, "V_IN_CK");
 
     }
