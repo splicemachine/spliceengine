fb7f7d9bf470cbe8171f8cff9547c420fea2a9dd
Alexander Stybaev <astybaev@splicemachine.com>
2015-09-21 09:01:40 -0500

    DB-3779 Subquery flattening: BETWEEN operator prevents flattening. Merge ITs

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/SubqueryExistsFlatteningIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/SubqueryExistsFlatteningIT.java
@@ -4,9 +4,9 @@ import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
 import com.splicemachine.derby.test.framework.SpliceWatcher;
 import org.junit.*;
 
-public class Subquery_InListFlattening_IT {
+public class SubqueryExistsFlatteningIT {
 
-    private static final String SCHEMA = Subquery_InListFlattening_IT.class.getSimpleName();
+    private static final String SCHEMA = SubqueryExistsFlatteningIT.class.getSimpleName();
 
     @ClassRule
     public static SpliceSchemaWatcher schemaWatcher = new SpliceSchemaWatcher(SCHEMA);
@@ -22,42 +22,20 @@ public class Subquery_InListFlattening_IT {
     @BeforeClass
     public static void createSharedTables() throws Exception {
         classWatcher.executeUpdate("create table A(a1 int, a2 int)");
-        classWatcher.executeUpdate("create table B(b1 int, b2 int)");
         classWatcher.executeUpdate("create table C(name varchar(20), surname varchar(20))");
         classWatcher.executeUpdate("insert into A values(0,0),(1,10),(2,20),(3,30),(4,40),(5,50)");
-        classWatcher.executeUpdate("insert into B values(0,0),(0,0),(1,10),(1,10),(2,20),(2,20),(3,30),(3,30),(4,40),(4,40),(5,50),(5,50)");
         classWatcher.executeUpdate("insert into C values('Jon', 'Snow'),('Eddard', 'Stark'),('Robb', 'Stark'), ('Jon', 'Arryn')");
-    }
-
-    @Test
-    public void simple() throws Exception {
-        // subquery reads different table
-        SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from A where a1 in (select b1 from B where b2 > 20)", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
-                        "A1 |A2 |\n" +
-                        "--------\n" +
-                        " 3 |30 |\n" +
-                        " 4 |40 |\n" +
-                        " 5 |50 |"
-        );
-        // subquery reads same table
-        SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from A where a1 in (select a1 from A ai where ai.a2 > 20)", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
-                        "A1 |A2 |\n" +
-                        "--------\n" +
-                        " 3 |30 |\n" +
-                        " 4 |40 |\n" +
-                        " 5 |50 |"
-        );
     }
 
     @Test
     @Ignore
     public void testBetweenFlattenAndNode() throws Exception {
         SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from A where a1 between 0 and 10 and a1 in (select a1 from A ai where ai.a2 > 20)", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
+                "select * from A where a1 between 1 and 10 and exists (select a1 from A ai where ai.a2 > 20)", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
                         "A1 |A2 |\n" +
                         "--------\n" +
+                        " 1 |10 |\n" +
+                        " 2 |20 |\n" +
                         " 3 |30 |\n" +
                         " 4 |40 |\n" +
                         " 5 |50 |"
@@ -68,7 +46,7 @@ public class Subquery_InListFlattening_IT {
     @Ignore
     public void testBetweenNotFlattenOrNode() throws Exception {
         SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from A where a1 between 0 and 4 or a1 in (select a1 from A ai where ai.a2 > 20)", 1, "" +
+                "select * from A where a1 between 1 and 4 or exists (select a1 from A ai where ai.a2 > 20)", 1, "" +
                         "A1 |A2 |\n" +
                         "--------\n" +
                         " 0 | 0 |\n" +
@@ -84,10 +62,12 @@ public class Subquery_InListFlattening_IT {
     @Ignore
     public void testLikeFlattenAndNode() throws Exception {
         SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from C where surname like 'S%' and surname in (select surname from C where name = 'Jon')", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
-                        "NAME | SURNAME |\n" +
-                        "----------------\n" +
-                        " Jon |  Snow   |"
+                "select * from C where surname like 'S%' and exists (select surname from C where name = 'Jon')", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
+                        "NAME  | SURNAME |\n" +
+                        "------------------\n" +
+                        "Eddard |  Stark  |\n" +
+                        "  Jon  |  Snow   |\n" +
+                        " Robb  |  Stark  |"
         );
     }
 
@@ -95,14 +75,44 @@ public class Subquery_InListFlattening_IT {
     @Ignore
     public void testLikeNotFlattenOrNode() throws Exception {
         SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
-                "select * from C where surname like 'A%' or surname in (select surname from C where name = 'Robb')", 1, "" +
+                "select * from C where surname like 'A%' or exists (select surname from C where name = 'Robb')", 1, "" +
                         "NAME  | SURNAME |\n" +
                         "------------------\n" +
                         "Eddard |  Stark  |\n" +
                         "  Jon  |  Arryn  |\n" +
+                        "  Jon  |  Snow   |\n" +
                         " Robb  |  Stark  |"
         );
     }
 
+    @Test
+    @Ignore
+    public void testSimplePredicateFlattenAndNode() throws Exception {
+        SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
+                "select * from A where a1 > 0 and a1 <= 10 and exists (select a1 from A ai where ai.a2 > 20)", ZERO_SUBQUERY_NODES_IN_PLAN, "" +
+                        "A1 |A2 |\n" +
+                        "--------\n" +
+                        " 1 |10 |\n" +
+                        " 2 |20 |\n" +
+                        " 3 |30 |\n" +
+                        " 4 |40 |\n" +
+                        " 5 |50 |"
+        );
+    }
 
+    @Test
+    @Ignore
+    public void testSimplePredicateNotFlattenOrNode() throws Exception {
+        SubqueryITUtil.assertUnorderedResult(methodWatcher.getOrCreateConnection(),
+                "select * from A where a1 >= 1 and a1 <= 4 or exists (select a1 from A ai where ai.a2 > 20)", 1, "" +
+                        "A1 |A2 |\n" +
+                        "--------\n" +
+                        " 0 | 0 |\n" +
+                        " 1 |10 |\n" +
+                        " 2 |20 |\n" +
+                        " 3 |30 |\n" +
+                        " 4 |40 |\n" +
+                        " 5 |50 |"
+        );
+    }
 }
