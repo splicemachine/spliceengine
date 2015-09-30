1e4ff31e5b2a20c73851f8d9823fecb6fc4c4c3c
jeffcunningham <jcunningham@splicemachine.com>
2015-09-30 16:17:38 -0500

    DB-3920: LAST_VALUE() window function. Basic case now working. See WindowFunctionIT#testLastValueFunction().

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/function/LastValueFunction.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/function/LastValueFunction.java
@@ -11,8 +11,12 @@ import com.splicemachine.db.iapi.types.DataValueDescriptor;
  *         Date: 9/30/15
  */
 public class LastValueFunction extends SpliceGenericWindowFunction {
+    // TODO: JC - implementing FIRST_VALUE is trivial.
+    boolean lastValue;
+
     public WindowFunction setup(ClassFactory cf, String aggregateName, DataTypeDescriptor returnType ) {
         super.setup( cf, aggregateName, returnType );
+        lastValue = aggregateName.equals("LAST_VALUE");
         return this;
     }
 
@@ -36,17 +40,14 @@ public class LastValueFunction extends SpliceGenericWindowFunction {
 
     @Override
     protected void calculateOnRemove(WindowChunk chunk, DataValueDescriptor[] dvds) throws StandardException {
-        DataValueDescriptor input = dvds[0];
-        if (input != null && ! input.isNull()) {
-            chunk.setResult(input);
-        }
+       // nothing to do here we've already set all the results. For LAST_VALUE(), result is just the last item we added.
     }
 
     @Override
     public DataValueDescriptor getResult() throws StandardException {
+        // for LAST_VALUE(), we just get the last result. For FIRST_VALUE(), it's the first.
         WindowChunk last = chunks.get(chunks.size()-1);
-        DataValueDescriptor result = last.getResult();
-        return result;
+        return last.getResult();
     }
 
     @Override
