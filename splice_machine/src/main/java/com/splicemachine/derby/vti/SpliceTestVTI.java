025dd98df06d1ceb5021a78895bf50595940271a
John Leach <jleach@splicemachine.com>
2015-10-08 10:18:03 -0500

    Minor Fixes

---
+++ splice_machine/src/main/java/com/splicemachine/derby/vti/SpliceTestVTI.java
@@ -0,0 +1,25 @@
+package com.splicemachine.derby.vti;
+
+import com.splicemachine.db.iapi.error.StandardException;
+import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
+import com.splicemachine.derby.impl.sql.execute.operations.LocatedRow;
+import com.splicemachine.derby.stream.iapi.DataSet;
+import com.splicemachine.derby.stream.iapi.DataSetProcessor;
+import com.splicemachine.derby.stream.utils.StreamUtils;
+import com.splicemachine.derby.vti.iapi.DatasetProvider;
+
+/**
+ * Created by jleach on 10/7/15.
+ */
+public class SpliceTestVTI implements DatasetProvider {
+
+    public static SpliceTestVTI getSpliceTestVTI() {
+        return new SpliceTestVTI();
+    }
+
+    @Override
+    public <Op extends SpliceOperation> DataSet<LocatedRow> getDataSet(DataSetProcessor dsp) throws StandardException {
+       return StreamUtils.controlDataSetProcessor.getEmpty();
+    }
+
+}
