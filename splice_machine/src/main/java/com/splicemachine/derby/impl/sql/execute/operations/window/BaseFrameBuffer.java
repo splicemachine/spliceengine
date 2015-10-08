41328556195daa98cabe490181df8fd8548fcc8e
jeffcunningham <jcunningham@splicemachine.com>
2015-10-08 13:20:04 -0500

    Refactoring in prep for frame result buffering.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/BaseFrameBuffer.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/window/BaseFrameBuffer.java
@@ -2,6 +2,7 @@ package com.splicemachine.derby.impl.sql.execute.operations.window;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.splicemachine.db.iapi.error.StandardException;
@@ -20,15 +21,15 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
     protected final long frameEnd;
     private final WindowAggregator[] aggregators;
     private final ExecRow templateRow;
+    private ResultBuffer resultBuffer;
 
     protected int start;
     protected int end;
     protected int current;
     protected List<ExecRow> rows;
-    protected PartitionAwarePushBackIterator<ExecRow> source;
+    protected PartitionAwarePushBackIterator<ExecRow> partitionIter;
     protected byte[] partition;
     protected boolean endOfPartition;
-    protected int[] sortColumns;
 
     public static WindowFrameBuffer createFrameBuffer(SpliceRuntimeContext runtimeContext,
                                                       WindowAggregator[] aggregators,
@@ -40,7 +41,7 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
         FrameDefinition.FrameMode frameMode = frameDefinition.getFrameMode();
         if (frameMode == FrameDefinition.FrameMode.ROWS) {
             return new PhysicalGroupFrameBuffer(
-                    runtimeContext, aggregators, source, frameDefinition, sortColumns, templateRow);
+                    runtimeContext, aggregators, source, frameDefinition, templateRow);
         }
         else {
             return new LogicalGroupFrameBuffer(
@@ -52,12 +53,10 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
                             WindowAggregator[] aggregators,
                             PartitionAwarePushBackIterator<ExecRow> source,
                             FrameDefinition frameDefinition,
-                            int[] sortColumns,
                             ExecRow templateRow) throws StandardException {
         this.runtimeContext = runtimeContext;
         this.aggregators = aggregators;
-        this.source = source;
-        this.sortColumns = sortColumns;
+        this.partitionIter = source;
         this.templateRow = templateRow;
 
         for (WindowAggregator aggregator: this.aggregators) {
@@ -69,6 +68,7 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
         this.frameStart = frameDefinition.getFrameStart().getValue();
         this.frameEnd = frameDefinition.getFrameEnd().getValue();
         this.rows = new ArrayList<>();
+        this.resultBuffer = new ResultBuffer();
     }
 
     @Override
@@ -78,14 +78,15 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
 
         if (row == null) {
             // This is the end of one partition, peek the next row
-            row = source.next(runtimeContext);
+            row = partitionIter.next(runtimeContext);
             if (row == null) {
-                // This is the end of the region
+                // This is the end of the region/frame
+                finishFrame();
                 return null;
             }
             else {
                 // init a new window buffer for the next partition
-                source.pushBack(row);
+                partitionIter.pushBack(row);
                 this.reset();
                 row = this.next();
             }
@@ -93,21 +94,6 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
 
         this.move();
 
-        return row;
-    }
-
-    protected ExecRow next() throws IOException, StandardException{
-        if (current >= rows.size()) {
-            return null;
-        }
-        ExecRow row = rows.get(current);
-        for (WindowAggregator aggregator : aggregators) {
-            // For current row  and window, evaluate the window function
-            int aggregatorColumnId = aggregator.getFunctionColumnId();
-            int resultColumnId = aggregator.getResultColumnId();
-            SpliceGenericWindowFunction function = (SpliceGenericWindowFunction) templateRow.getColumn(aggregatorColumnId).getObject();
-            row.setColumn(resultColumnId, function.getResult().cloneValue(false));
-        }
         return row;
     }
 
@@ -126,7 +112,23 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
         }
     }
 
-    protected void reset() throws StandardException, IOException {
+    private ExecRow next() throws IOException, StandardException{
+        if (current >= rows.size()) {
+            return null;
+        }
+        ExecRow row = rows.get(current);
+        for (WindowAggregator aggregator : aggregators) {
+            // For current row  and window, evaluate the window function
+            int aggregatorColumnId = aggregator.getFunctionColumnId();
+            int resultColumnId = aggregator.getResultColumnId();
+            SpliceGenericWindowFunction function = (SpliceGenericWindowFunction) templateRow.getColumn(aggregatorColumnId).getObject();
+            row.setColumn(resultColumnId, function.getResult().cloneValue(false));
+            this.resultBuffer.bufferResult(row);
+        }
+        return row;
+    }
+
+    private void reset() throws StandardException, IOException {
         rows = new ArrayList<>();
 
         // Initialize window functions
@@ -138,9 +140,65 @@ abstract public class BaseFrameBuffer implements WindowFrameBuffer{
             aggregator.initialize(templateRow);
         }
 
+        // reset result buffer
+        this.resultBuffer.reset();
+
         // initializes frame buffer
         loadFrame();
     }
 
+    private void finishFrame() throws StandardException {
+        for (WindowAggregator aggregator : aggregators) {
+            SpliceGenericWindowFunction cachedAggregator = aggregator.getCachedAggregator();
+            if (cachedAggregator != null) {
+                cachedAggregator.finishFrame();
+            }
+        }
+        resultBuffer.setFinished();
+    }
+
     abstract protected void loadFrame() throws IOException, StandardException;
+
+    private static class ResultBuffer implements Iterator<ExecRow> {
+        private final List<ExecRow> results = new ArrayList<>();
+        private Iterator<ExecRow> resultItr;
+        boolean finished;
+
+        void bufferResult(ExecRow resultRow) {
+            results.add(resultRow);
+        }
+
+        boolean isFinished() {
+            return finished;
+        }
+
+        void reset() {
+            resultItr = null;
+            results.clear();
+            finished = false;
+        }
+
+        public void setFinished() {
+            finished = true;
+            resultItr = results.iterator();
+        }
+
+        @Override
+        public boolean hasNext() {
+            return (resultItr != null && resultItr.hasNext());
+        }
+
+        @Override
+        public ExecRow next() {
+            if (resultItr == null) {
+                return null;
+            }
+            return resultItr.next();
+        }
+
+        @Override
+        public void remove() {
+            // not implemented
+        }
+    }
 }
