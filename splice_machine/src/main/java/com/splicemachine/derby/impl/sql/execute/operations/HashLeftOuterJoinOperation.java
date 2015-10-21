f857fa26d9dec02f834b9af6f8162068fcb2da9c
Keith Winkler <kwinkler@splicemachine.com>
2015-10-21 09:40:51 -0500

    DB-4003: NestedLoopJoinOperation: formatting, dead code, warnings

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/HashLeftOuterJoinOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/HashLeftOuterJoinOperation.java
@@ -1,47 +1,39 @@
 package com.splicemachine.derby.impl.sql.execute.operations;
 
-import java.util.ArrayList;
-
-import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
 import com.splicemachine.db.iapi.error.StandardException;
 import com.splicemachine.db.iapi.services.loader.GeneratedMethod;
 import com.splicemachine.db.iapi.sql.Activation;
-import com.splicemachine.db.iapi.sql.execute.NoPutResultSet;
+import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
+import com.splicemachine.utils.SpliceLogUtils;
 import org.apache.log4j.Logger;
 
-import com.splicemachine.utils.SpliceLogUtils;
-
 public class HashLeftOuterJoinOperation extends NestedLoopLeftOuterJoinOperation {
-	private static Logger LOG = Logger.getLogger(HashLeftOuterJoinOperation.class);
-	static {
-		nodeTypes = new ArrayList<NodeType>();
-		nodeTypes.add(NodeType.MAP);	
-		nodeTypes.add(NodeType.SCROLL);
-	}
-	
-	public HashLeftOuterJoinOperation() {
-		super();
-	}
-	
-	public HashLeftOuterJoinOperation (
-			SpliceOperation leftResultSet,
-			int leftNumCols,
-			SpliceOperation rightResultSet,
-			int rightNumCols,
-			Activation activation,
-			GeneratedMethod restriction,
-			int resultSetNumber,
-			GeneratedMethod emptyRowFun,
-			boolean wasRightOuterJoin,
-		    boolean oneRowRightSide,
-		    boolean notExistsRightSide,
-			double optimizerEstimatedRowCount,
-			double optimizerEstimatedCost,
-			String userSuppliedOptimizerOverrides) throws StandardException {
-		super(leftResultSet, leftNumCols, rightResultSet, rightNumCols, activation, restriction, resultSetNumber, emptyRowFun, wasRightOuterJoin,
-				oneRowRightSide, notExistsRightSide, optimizerEstimatedRowCount, optimizerEstimatedCost, userSuppliedOptimizerOverrides);
-		this.isHash = true;
-		SpliceLogUtils.trace(LOG, "instantiate");
-		recordConstructorTime(); 
-	}
+    private static Logger LOG = Logger.getLogger(HashLeftOuterJoinOperation.class);
+
+    public HashLeftOuterJoinOperation() {
+        super();
+    }
+
+    public HashLeftOuterJoinOperation(
+            SpliceOperation leftResultSet,
+            int leftNumCols,
+            SpliceOperation rightResultSet,
+            int rightNumCols,
+            Activation activation,
+            GeneratedMethod restriction,
+            int resultSetNumber,
+            GeneratedMethod emptyRowFun,
+            boolean wasRightOuterJoin,
+            boolean oneRowRightSide,
+            boolean notExistsRightSide,
+            double optimizerEstimatedRowCount,
+            double optimizerEstimatedCost,
+            String userSuppliedOptimizerOverrides) throws StandardException {
+        super(leftResultSet, leftNumCols, rightResultSet, rightNumCols, activation, restriction, resultSetNumber, emptyRowFun, wasRightOuterJoin,
+                oneRowRightSide, notExistsRightSide, optimizerEstimatedRowCount, optimizerEstimatedCost, userSuppliedOptimizerOverrides);
+        this.isHash = true;
+        SpliceLogUtils.trace(LOG, "instantiate");
+        recordConstructorTime();
+    }
+
 }
--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/NestedLoopLeftOuterJoinOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/NestedLoopLeftOuterJoinOperation.java
@@ -1,6 +1,7 @@
 package com.splicemachine.derby.impl.sql.execute.operations;
 
 import java.io.IOException;
+
 import com.splicemachine.derby.iapi.sql.execute.SpliceOperationContext;
 import com.splicemachine.derby.iapi.sql.execute.SpliceRuntimeContext;
 import com.splicemachine.derby.impl.SpliceMethod;
@@ -15,128 +16,128 @@ import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
 import com.splicemachine.utils.SpliceLogUtils;
 
 public class NestedLoopLeftOuterJoinOperation extends NestedLoopJoinOperation {
-		private static Logger LOG = Logger.getLogger(NestedLoopLeftOuterJoinOperation.class);
-        protected SpliceMethod<ExecRow> emptyRowFun;
-		protected Qualifier[][] qualifierProbe;
-		public int emptyRightRowsReturned = 0;
 
-		public NestedLoopLeftOuterJoinOperation() {
-				super();
-		}
+    private static final Logger LOG = Logger.getLogger(NestedLoopLeftOuterJoinOperation.class);
 
-		public NestedLoopLeftOuterJoinOperation(
-						SpliceOperation leftResultSet,
-						int leftNumCols,
-						SpliceOperation rightResultSet,
-						int rightNumCols,
-						Activation activation,
-						GeneratedMethod restriction,
-						int resultSetNumber,
-						GeneratedMethod emptyRowFun,
-						boolean wasRightOuterJoin,
-						boolean oneRowRightSide,
-						boolean notExistsRightSide,
-						double optimizerEstimatedRowCount,
-						double optimizerEstimatedCost,
-						String userSuppliedOptimizerOverrides) throws StandardException {
-				super(leftResultSet, leftNumCols, rightResultSet, rightNumCols,
-								activation, restriction, resultSetNumber,oneRowRightSide, notExistsRightSide,
-								optimizerEstimatedRowCount, optimizerEstimatedCost,userSuppliedOptimizerOverrides);
-				SpliceLogUtils.trace(LOG, "instantiate");
-				this.emptyRowFunMethodName = (emptyRowFun == null) ? null : emptyRowFun.getMethodName();
-				this.wasRightOuterJoin = wasRightOuterJoin;
-				try {
-						init(SpliceOperationContext.newContext(activation));
-				} catch (IOException e) {
-						throw Exceptions.parseException(e);
-				}
-				recordConstructorTime();
-		}
+    protected SpliceMethod<ExecRow> emptyRowFun;
+    public int emptyRightRowsReturned = 0;
 
-		@Override
-		public ExecRow nextRow(SpliceRuntimeContext spliceRuntimeContext) throws StandardException, IOException {
-				SpliceLogUtils.trace(LOG, "nextRow");
-                if (rightResultSetUniqueSequenceID == null) {
-                    rightResultSetUniqueSequenceID = rightResultSet.getUniqueSequenceID();
-                }
+    public NestedLoopLeftOuterJoinOperation() {
+        super();
+    }
 
-                if (nestedLoopIterator == null) {
-						if ( (leftRow = leftResultSet.nextRow(spliceRuntimeContext)) == null) {
-								mergedRow = null;
-								setCurrentRow(mergedRow);
-								return mergedRow;
-						} else {
-								nestedLoopIterator = new NestedLoopLeftOuterIterator(leftRow,isHash,spliceRuntimeContext, true, false);
-								rowsSeenLeft++;
-								return nextRow(spliceRuntimeContext);
-						}
-				}
-				if(!nestedLoopIterator.hasNext()){
-						nestedLoopIterator.close();
+    public NestedLoopLeftOuterJoinOperation(
+            SpliceOperation leftResultSet,
+            int leftNumCols,
+            SpliceOperation rightResultSet,
+            int rightNumCols,
+            Activation activation,
+            GeneratedMethod restriction,
+            int resultSetNumber,
+            GeneratedMethod emptyRowFun,
+            boolean wasRightOuterJoin,
+            boolean oneRowRightSide,
+            boolean notExistsRightSide,
+            double optimizerEstimatedRowCount,
+            double optimizerEstimatedCost,
+            String userSuppliedOptimizerOverrides) throws StandardException {
+        super(leftResultSet, leftNumCols, rightResultSet, rightNumCols,
+                activation, restriction, resultSetNumber, oneRowRightSide, notExistsRightSide,
+                optimizerEstimatedRowCount, optimizerEstimatedCost, userSuppliedOptimizerOverrides);
+        SpliceLogUtils.trace(LOG, "instantiate");
+        this.emptyRowFunMethodName = (emptyRowFun == null) ? null : emptyRowFun.getMethodName();
+        this.wasRightOuterJoin = wasRightOuterJoin;
+        try {
+            init(SpliceOperationContext.newContext(activation));
+        } catch (IOException e) {
+            throw Exceptions.parseException(e);
+        }
+        recordConstructorTime();
+    }
 
-						if ( (leftRow = leftResultSet.nextRow(spliceRuntimeContext)) == null) {
-								mergedRow = null;
-								setCurrentRow(mergedRow);
-								return mergedRow;
-						} else {
-								nestedLoopIterator = new NestedLoopLeftOuterIterator(leftRow,isHash,spliceRuntimeContext, true, false);
-								rowsSeenLeft++;
-								return nextRow(spliceRuntimeContext);
-						}
-				}
+    @Override
+    public ExecRow nextRow(SpliceRuntimeContext spliceRuntimeContext) throws StandardException, IOException {
+        SpliceLogUtils.trace(LOG, "nextRow");
+        if (rightResultSetUniqueSequenceID == null) {
+            rightResultSetUniqueSequenceID = rightResultSet.getUniqueSequenceID();
+        }
 
-				SpliceLogUtils.trace(LOG, "nextRow loop iterate next ");
-				ExecRow next = nestedLoopIterator.next();
-				SpliceLogUtils.trace(LOG,"nextRow returning %s",next);
-				setCurrentRow(next);
-				rowsReturned++;
-//		mergedRow=null;
-				return next;
+        if (nestedLoopIterator == null) {
+            if ((leftRow = leftResultSet.nextRow(spliceRuntimeContext)) == null) {
+                mergedRow = null;
+                setCurrentRow(null);
+                return null;
+            } else {
+                nestedLoopIterator = new NestedLoopLeftOuterIterator(leftRow, isHash, spliceRuntimeContext, true, false);
+                rowsSeenLeft++;
+                return nextRow(spliceRuntimeContext);
+            }
+        }
+        if (!nestedLoopIterator.hasNext()) {
+            nestedLoopIterator.close();
 
-		}
+            if ((leftRow = leftResultSet.nextRow(spliceRuntimeContext)) == null) {
+                mergedRow = null;
+                setCurrentRow(null);
+                return null;
+            } else {
+                nestedLoopIterator = new NestedLoopLeftOuterIterator(leftRow, isHash, spliceRuntimeContext, true, false);
+                rowsSeenLeft++;
+                return nextRow(spliceRuntimeContext);
+            }
+        }
 
-		@Override
-		public void init(SpliceOperationContext context) throws StandardException, IOException {
-				SpliceLogUtils.trace(LOG, "init");
-            super.init(context);
-            emptyRightRowsReturned = 0;
-            emptyRowFun = (emptyRowFunMethodName == null) ? null : new SpliceMethod<ExecRow>(emptyRowFunMethodName,activation);
-		}
+        SpliceLogUtils.trace(LOG, "nextRow loop iterate next ");
+        ExecRow next = nestedLoopIterator.next();
+        SpliceLogUtils.trace(LOG, "nextRow returning %s", next);
+        setCurrentRow(next);
+        rowsReturned++;
+        return next;
 
-		@Override
-		public String prettyPrint(int indentLevel) {
-				return "LeftOuter"+super.prettyPrint(indentLevel);
-		}
+    }
+
+    @Override
+    public void init(SpliceOperationContext context) throws StandardException, IOException {
+        SpliceLogUtils.trace(LOG, "init");
+        super.init(context);
+        emptyRightRowsReturned = 0;
+        emptyRowFun = (emptyRowFunMethodName == null) ? null : new SpliceMethod<ExecRow>(emptyRowFunMethodName, activation);
+    }
+
+    @Override
+    public String prettyPrint(int indentLevel) {
+        return "LeftOuter" + super.prettyPrint(indentLevel);
+    }
 
     @Override
     protected NestedLoopIterator createNestedLoopIterator(ExecRow leftRow, boolean hash, SpliceRuntimeContext spliceRuntimeContext, boolean showStatementInfo, boolean cloneResults) throws StandardException, IOException {
         return new NestedLoopLeftOuterIterator(leftRow, hash, spliceRuntimeContext, showStatementInfo, cloneResults);
     }
 
-    private class NestedLoopLeftOuterIterator extends NestedLoopIterator{
-				private boolean seenRow = false;
+    private class NestedLoopLeftOuterIterator extends NestedLoopIterator {
+        private boolean seenRow = false;
 
-				NestedLoopLeftOuterIterator(ExecRow leftRow, boolean hash,SpliceRuntimeContext context, boolean showStatementInfo, boolean cloneResults) throws StandardException, IOException {
-						super(leftRow, hash, true, rightResultSetUniqueSequenceID, context, showStatementInfo, cloneResults);
-				}
+        NestedLoopLeftOuterIterator(ExecRow leftRow, boolean hash, SpliceRuntimeContext context, boolean showStatementInfo, boolean cloneResults) throws StandardException, IOException {
+            super(leftRow, hash, true, rightResultSetUniqueSequenceID, context, showStatementInfo, cloneResults);
+        }
 
-				@Override
-				protected void nonNullRight() {
-						seenRow=true;
-				}
+        @Override
+        protected void nonNullRight() {
+            seenRow = true;
+        }
 
-				@Override
-				protected ExecRow getEmptyRightRow() throws StandardException {
-						if (seenRow) {
-								SpliceLogUtils.trace(LOG, "already has seen row and no right result");
-								probeResultSet.setCurrentRow(null);
-								emptyRightRowsReturned++;
-								close();
-								return null;
-						}
-						rightRow = (ExecRow) emptyRowFun.invoke();
-						seenRow=true;
-						return rightRow;
-				}
-		}
+        @Override
+        protected ExecRow getEmptyRightRow() throws StandardException {
+            if (seenRow) {
+                SpliceLogUtils.trace(LOG, "already has seen row and no right result");
+                probeResultSet.setCurrentRow(null);
+                emptyRightRowsReturned++;
+                close();
+                return null;
+            }
+            rightRow = emptyRowFun.invoke();
+            seenRow = true;
+            return rightRow;
+        }
+    }
 }
