983f48f11650d541066bd5630618464485196a8a
Keith Winkler <kwinkler@splicemachine.com>
2015-10-26 12:12:55 -0500

    DB-4001: move union subquery flattening tests into their own classes

--- splice_machine_test/src/test/java/com/splicemachine/subquery/Subquery_Flattening_NotExists_IT.java
+++ splice_machine_test/src/test/java/com/splicemachine/subquery/Subquery_Flattening_NotExists_IT.java
@@ -23,7 +23,7 @@ public class Subquery_Flattening_NotExists_IT {
 
     @BeforeClass
     public static void createSharedTables() throws Exception {
-        classWatcher.executeUpdate("create table EMPTY_TABLE(e1 int, e2 int)");
+        classWatcher.executeUpdate("create table EMPTY_TABLE(e1 int, e2 int, e3 int, e4 int)");
         classWatcher.executeUpdate("create table A(a1 int, a2 int)");
         classWatcher.executeUpdate("create table B(b1 int, b2 int)");
         classWatcher.executeUpdate("create table C(c1 int, c2 int)");
@@ -509,133 +509,6 @@ public class Subquery_Flattening_NotExists_IT {
                 " 7 |70 |");
     }
 
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    //
-    // union
-    //
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    @Test
-    public void union_unCorrelated() throws Exception {
-        // union of empty tables
-        String sql = "select * from A where NOT exists(select 1 from EMPTY_TABLE union select 1 from EMPTY_TABLE)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "A1 |A2 |\n" +
-                "--------\n" +
-                " 0 | 0 |\n" +
-                " 1 |10 |\n" +
-                " 2 |20 |\n" +
-                " 3 |30 |\n" +
-                " 4 |40 |\n" +
-                " 5 |50 |\n" +
-                " 7 |70 |");
-
-        // union one non-empty first subquery
-        sql = "select count(*) from A where NOT exists(select 1 from C union select 1 from EMPTY_TABLE)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-        // union one non-empty second subquery
-        sql = "select count(*) from A where NOT exists(select 1 from EMPTY_TABLE union select 1 from C)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-        // union, three unions
-        sql = "select count(*) from A where NOT exists(select 1 from EMPTY_TABLE union select 1 from C union select 1 from D)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-        // non-empty tables, where subquery predicates eliminate all rows
-        sql = "select count(*) from A where NOT exists(select 1 from C where c1 > 999999 union select 1 from D where d1 < -999999)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 7 |");
-
-        // non-empty tables, where subquery predicates eliminate all rows in ONE table
-        sql = "select count(*) from A where NOT exists(select 1 from C where c1 > 999999 union select 1 from D where d1 = 0)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-
-        // unions with column references
-        sql = "select count(*) from A where NOT exists(select c1,c2 from C union select d1,d2 from D)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-        // unions referencing all columns
-        sql = "select count(*) from A where NOT exists(select * from C union select * from D)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-
-        // union same table
-        sql = "select count(*) from A where NOT exists(select * from A union select * from A)";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "1 |\n" +
-                "----\n" +
-                " 0 |");
-    }
-
-    @Test
-    public void union_correlated() throws Exception {
-        // union of empty tables
-        String sql = "select count(*) from A where NOT exists(select 1 from EMPTY_TABLE where e1=a1 union select 1 from EMPTY_TABLE where e1=a1)";
-        assertUnorderedResult(conn(), sql, 1, "" +
-                        "1 |\n" +
-                        "----\n" +
-                        " 7 |");
-
-        // union one non-empty first subquery
-        sql = "select * from A where NOT exists(select 1 from C where c1=a1 union select 1 from EMPTY_TABLE where e1=a1)";
-        assertUnorderedResult(conn(), sql, 1, "" +
-                "A1 |A2 |\n" +
-                "--------\n" +
-                " 0 | 0 |\n" +
-                " 7 |70 |");
-
-        // union one non-empty second subquery
-        sql = "select * from A where NOT exists(select 1 from EMPTY_TABLE where e1=a1 union select 1 from C where c1=a1)";
-        assertUnorderedResult(conn(), sql, 1, "" +
-                "A1 |A2 |\n" +
-                "--------\n" +
-                " 0 | 0 |\n" +
-                " 7 |70 |");
-
-        // union no non-empty
-        sql = "select * from A where NOT exists(select 1 from D where d1=a1 union select 1 from C where c1=a1)";
-        assertUnorderedResult(conn(), sql, 1, "");
-
-        // union same table
-        sql = "select * from A where NOT exists(select 1 from D where d1=a1 union select 1 from D where d2=a2)";
-        assertUnorderedResult(conn(), sql, 1, "" +
-                "A1 |A2 |\n" +
-                "--------\n" +
-                " 2 |20 |\n" +
-                " 4 |40 |");
-    }
-
-    @Test
-    public void union_unionsInFromListOfNotExistsSubquery() throws Exception {
-        String sql = "select * from B where NOT exists(" +
-                "select * from (select c1 r from C union select d1 r from D) foo where foo.r=b1" +
-                ")";
-        assertUnorderedResult(conn(), sql, ZERO_SUBQUERY_NODES, "" +
-                "B1  | B2  |\n" +
-                "------------\n" +
-                "NULL |NULL |");
-    }
 
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
