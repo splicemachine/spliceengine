30a2763ea40bffb7dbe401f5f7d12924440039f2
Scott Fines <scottfines@gmail.com>
2015-10-27 08:52:23 -0500

    DB-4040: Correcting startup race condition.
    It's possible for multiple simultaneously starting RegionServers to both
    attempt the upgrade simultaneously. Our concurrency controls will force
    one to win and the other to lose; in that case, we were attempting to
    add a column which had just been created by another server. This isn't
    actually a problem, but it will throw an exception. Since all we care
    about is that it was created (and not who created it) the exception is
    as good as success here.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/LassenUpgradeScript.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/LassenUpgradeScript.java
@@ -4,12 +4,14 @@ import com.splicemachine.db.catalog.UUID;
 import com.splicemachine.db.catalog.types.DefaultInfoImpl;
 import com.splicemachine.db.iapi.error.StandardException;
 import com.splicemachine.db.iapi.sql.dictionary.ColumnDescriptor;
+import com.splicemachine.db.iapi.sql.dictionary.ColumnDescriptorList;
 import com.splicemachine.db.iapi.sql.dictionary.DataDictionary;
 import com.splicemachine.db.iapi.sql.dictionary.TableDescriptor;
 import com.splicemachine.db.iapi.store.access.TransactionController;
 import com.splicemachine.db.iapi.types.DataTypeDescriptor;
 import com.splicemachine.db.iapi.types.SQLBoolean;
 import com.splicemachine.derby.impl.sql.catalog.SpliceDataDictionary;
+import com.splicemachine.pipeline.exception.ErrorState;
 import com.splicemachine.utils.SpliceUtilities;
 
 import java.sql.Types;
@@ -45,6 +47,25 @@ public class LassenUpgradeScript extends UpgradeScriptBase {
         TableDescriptor sysColumns = sdd.getTableDescriptor("SYSCOLUMNS",sdd.getSystemSchemaDescriptor(),tc);
         UUID defaultUuid = sdd.getUUIDFactory().createUUID();
 
+        addCollectStatsColumn(tc,template_column,dtd,sysColumns,defaultUuid);
+    }
+
+    private void addCollectStatsColumn(TransactionController tc,
+                                       SQLBoolean template_column,
+                                       DataTypeDescriptor dtd,
+                                       TableDescriptor sysColumns,
+                                       UUID defaultUuid) throws StandardException{
+        ColumnDescriptorList columnDescriptorList=sysColumns.getColumnDescriptorList();
+        for(ColumnDescriptor cd:columnDescriptorList){
+            /*
+             * Quick check: If we can already see the column, no ned to add it in. This may happen
+             * if another region server is starting simultaneously to us, and managed to get here first
+             */
+            if("COLLECTSTATS".equalsIgnoreCase(cd.getColumnName())){
+                return; //no need to add the collect stats column
+            }
+        }
+
         ColumnDescriptor cd = new ColumnDescriptor("COLLECTSTATS",10,
                 dtd,
                 template_column,
@@ -53,11 +74,31 @@ public class LassenUpgradeScript extends UpgradeScriptBase {
                 defaultUuid,
                 0,
                 0);
-        sdd.addDescriptor(cd,sysColumns, DataDictionary.SYSCOLUMNS_CATALOG_NUM,false,tc);
 
-        sysColumns.getColumnDescriptorList().add(cd);
+        try{
+            /*
+             * There is a cluster race condition here; when multiple RegionServers are starting up, one
+             * may get here before the other, resulting in both attempting to create the same column. Our transaction
+             * system (and the internal DD consistency controls) should prevent that, causing this to throw a
+             * "LANG_OBJECT_ALREADY_EXISTS_IN_OBJECT" error back at us. This really is just saying that we
+             * tried to create the same column twice. Since the column already exists and that's what we care about,
+             * we are just as happy to get the error and return here as we would be if the call succeeded.
+             *
+             * Of course, if we get a goofy error that doesn't match up with what we expect, we should report that.
+             */
+            sdd.addDescriptor(cd,sysColumns,DataDictionary.SYSCOLUMNS_CATALOG_NUM,false,tc);
+        }catch(StandardException se){
+            if(ErrorState.LANG_OBJECT_ALREADY_EXISTS_IN_OBJECT.getSqlState().equals(se.getSQLState())){
+                /*
+                 * This occurred because another region server already added it in, no need to worry about it then.
+                 * Just add it in to the CDL to make sure that it's present.
+                 */
+                columnDescriptorList.add(cd);
+                return;
+            }else throw se;
+        }
+
+        columnDescriptorList.add(cd);
         sdd.updateSYSCOLPERMSforAddColumnToUserTable(sysColumns.getUUID(), tc);
     }
-
-
 }
