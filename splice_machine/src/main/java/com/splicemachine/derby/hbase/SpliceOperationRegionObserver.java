bf2b3bb4c4d452de015efb39ce56bdfee3380b1c
John Leach <jleach@splicemachine.com>
2015-11-05 11:30:37 -0600

    Code Removal and Cleanup

--- splice_machine/src/main/java/com/splicemachine/derby/hbase/SpliceOperationRegionObserver.java
+++ splice_machine/src/main/java/com/splicemachine/derby/hbase/SpliceOperationRegionObserver.java
@@ -108,41 +108,9 @@ public class SpliceOperationRegionObserver extends BaseRegionObserver {
         }
     }
 
-    /**
-     * Override the postScannerOpen to wrap the scan with the SpliceOperationRegionScanner.  This allows for cases
-     * where the hbase scanner will be limited (ProjectRestrictOperation) or where new records would be added (LeftOuterJoin).
-     */
-    @Override
-    public RegionScanner postScannerOpen(ObserverContext<RegionCoprocessorEnvironment> e, Scan scan,RegionScanner s) throws IOException {
-        if (scan.getAttribute(SPLICE_OBSERVER_INSTRUCTIONS) != null){
-            SpliceLogUtils.trace(LOG, "postScannerOpen called, wrapping SpliceOperationRegionScanner");
-            if (scan.getCaching() < 0) // Async Scanner is corrupting this value..
-                scan.setCaching(SpliceConstants.DEFAULT_CACHE_SIZE);
-            HRegion region = e.getEnvironment().getRegion();
-            return super.postScannerOpen(e, scan, derbyFactory.getOperationRegionScanner(s,scan, region,getTxnRegion(region)));
-        }
-        return super.postScannerOpen(e, scan, s);
-    }
-
     @Override
     public void postScannerClose(ObserverContext<RegionCoprocessorEnvironment> e, InternalScanner s) throws IOException {
         super.postScannerClose(e, s);
-    }
-
-    @Override
-    public boolean preScannerNext(ObserverContext<RegionCoprocessorEnvironment> e, InternalScanner s, List<Result> results, int limit, boolean hasMore) throws IOException {
-        if(s instanceof SpliceBaseOperationRegionScanner){
-            ((SpliceBaseOperationRegionScanner)s).setupBatch();
-        }
-        return super.preScannerNext(e, s, results, limit, hasMore);
-    }
-
-    @Override
-    public boolean postScannerNext(ObserverContext<RegionCoprocessorEnvironment> e, InternalScanner s, List<Result> results, int limit, boolean hasMore) throws IOException {
-        if(s instanceof SpliceBaseOperationRegionScanner){
-            ((SpliceBaseOperationRegionScanner)s).cleanupBatch();
-        }
-        return super.postScannerNext(e, s, results, limit, hasMore);
     }
 
     /******************************************************************************************************************/
