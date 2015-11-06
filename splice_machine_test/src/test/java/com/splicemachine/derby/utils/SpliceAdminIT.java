23df89e5d3111471db12905d8f66bf008c592a88
John Leach <jleach@splicemachine.com>
2015-11-06 12:18:59 -0600

    Remove Kill Statement for now

--- splice_machine_test/src/test/java/com/splicemachine/derby/utils/SpliceAdminIT.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/utils/SpliceAdminIT.java
@@ -422,30 +422,5 @@ public class SpliceAdminIT {
         DbUtils.closeQuietly(rs);
     }
 
-    @Test
-    public void testKillStatement() throws Exception {
-    	// Simple test (better than the nothing we've had up till now)
-    	// to test that procedure exists and that it throw error
-    	// if you provide bogus statementUuid argument.
-        CallableStatement cs = methodWatcher.prepareCall("call SYSCS_UTIL.SYSCS_KILL_STATEMENT(12345)");
-        ResultSet rs = null;
-        try {
-            rs = cs.executeQuery();
-        } catch (Exception e) {
-            Assert.assertTrue(e.getCause().getCause().getMessage().contains("statementUuid"));
-            return;
-        } finally {
-            DbUtils.closeQuietly(rs);
-        }
-        Assert.fail("Expected exception due to invalid statementUuid");
-    }
     
-    @Test
-    public void testEmptyGlobalStatementCache() throws Exception {
-    	// Placeholder - not much here other than making sure it is here and does not blow up.
-        CallableStatement cs = methodWatcher.prepareCall("call SYSCS_UTIL.SYSCS_EMPTY_GLOBAL_STATEMENT_CACHE()");
-        int count = cs.executeUpdate();
-        Assert.assertEquals(0, count);
-        DbUtils.closeQuietly(cs);
-    }
 }
