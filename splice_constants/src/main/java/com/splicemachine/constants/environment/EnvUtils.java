6b1e7f550eff93387fd278bc1859518474e7446f
John Leach <jleach@splicemachine.com>
2015-11-18 22:53:55 -0600

    Remove RegionCache Implementation

--- splice_constants/src/main/java/com/splicemachine/constants/environment/EnvUtils.java
+++ splice_constants/src/main/java/com/splicemachine/constants/environment/EnvUtils.java
@@ -3,45 +3,50 @@ package com.splicemachine.constants.environment;
 import com.splicemachine.constants.SpliceConstants;
 import com.splicemachine.constants.SpliceConstants.TableEnv;
 import com.splicemachine.utils.SpliceLogUtils;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.regionserver.HRegion;
 import org.apache.log4j.Logger;
 
 public class EnvUtils {
 	private static Logger LOG = Logger.getLogger(EnvUtils.class);
-	private static final long FIRST_USER_TABLE_NUMBER = 1800;
+	private static final long FIRST_USER_TABLE_NUMBER = 1328;
 
     public static TableEnv getTableEnv(RegionCoprocessorEnvironment e) {
-        return EnvUtils.getTableEnv(e.getRegion().getTableDesc().getNameAsString());
+        return EnvUtils.getTableEnv(e.getRegion().getTableDesc().getTableName());
     }
 
-    public static TableEnv getTableEnv(String tableName) {
+    public static TableEnv getTableEnv(TableName tableName) {
         SpliceLogUtils.trace(LOG,"Checking table environment for %s",tableName);
-		if (tableName.equals(SpliceConstants.TRANSACTION_TABLE))
-			return TableEnv.TRANSACTION_TABLE;
-		else if (tableName.equals("-ROOT-"))
-			return TableEnv.ROOT_TABLE;
-		else if (isMetaOrNamespaceTable(tableName))
-			return TableEnv.META_TABLE;
-		else {
+        if (!tableName.getNamespaceAsString().equals(SpliceConstants.spliceNamespace))
+            if (tableName.getQualifierAsString().equals("-ROOT-"))
+                return TableEnv.ROOT_TABLE;
+            else if (isMetaOrNamespaceTable(tableName))
+                return TableEnv.META_TABLE;
+            else {
+                return TableEnv.HBASE_TABLE;
+            }
+        else if (tableName.getQualifierAsString().equals(SpliceConstants.TRANSACTION_TABLE))
+            return TableEnv.TRANSACTION_TABLE;
+        else {
 			try {
-				long tableNumber = Long.parseLong(tableName);			
-				if (tableNumber < FIRST_USER_TABLE_NUMBER) 
+				long tableNumber = Long.parseLong(tableName.getQualifierAsString());
+				if (tableNumber <= FIRST_USER_TABLE_NUMBER)
 					return TableEnv.DERBY_SYS_TABLE;
 			} catch (Exception e) {
                 SpliceLogUtils.debug(LOG,tableName+" is not a number");
 			}
-			
 			return TableEnv.USER_TABLE;
 		}
 	}
 
-    public static boolean isMetaOrNamespaceTable(String tableName) {
-        return "hbase:meta".equals(tableName)
-                || "meta".equals(tableName)
-                || "hbase:namespace".equals(tableName)
-                || "namespace".equals(tableName)
-                || ".META.".equals(tableName);
+    public static boolean isMetaOrNamespaceTable(TableName tableName) {
+        String qualifier = tableName.getQualifierAsString();
+        return "hbase:meta".equals(qualifier)
+                || "meta".equals(qualifier)
+                || "hbase:namespace".equals(qualifier)
+                || "namespace".equals(qualifier)
+                || ".META.".equals(qualifier);
     }
 
     public static String getRegionId(HRegion region) {
