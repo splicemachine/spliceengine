a61bf53392f80254bd70388cafc78df45ab57de9
Walter Koetke <wkoetke@splicemachine.com>
2015-12-16 17:39:40 -0800

    Cleaning and formatting only, prior to real changes.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/CallStatementOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/CallStatementOperation.java
@@ -17,152 +17,140 @@ import java.io.ObjectOutput;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-/**
- *
- *
- */
-
 public class CallStatementOperation extends NoRowsOperation {
-        private static Logger LOG = Logger.getLogger(CallStatementOperation.class);
-		private String methodName;
-		private SpliceMethod<Object> methodCall;
-        protected static final String NAME = CallStatementOperation.class.getSimpleName().replaceAll("Operation","");
+    private static Logger LOG = Logger.getLogger(CallStatementOperation.class);
+	private String methodName;
+	private SpliceMethod<Object> methodCall;
+    protected static final String NAME = CallStatementOperation.class.getSimpleName().replaceAll("Operation","");
 
-		@Override
-		public String getName() {
-				return NAME;
-		}
+	@Override
+	public String getName() {
+		return NAME;
+	}
 
-		
-		public CallStatementOperation(GeneratedMethod methodCall,Activation a) throws StandardException  {
-				super(a);
-				methodName = (methodCall!= null) ? methodCall.getMethodName() : null;
-				this.methodCall = new SpliceMethod<Object>(methodName,activation);
-				recordConstructorTime();
-		}
+	public CallStatementOperation(GeneratedMethod methodCall,Activation a) throws StandardException  {
+		super(a);
+		methodName = (methodCall!= null) ? methodCall.getMethodName() : null;
+		this.methodCall = new SpliceMethod<Object>(methodName,activation);
+		recordConstructorTime();
+	}
 
-		@Override
-		public void readExternal(ObjectInput in) throws IOException,
-						ClassNotFoundException {
-				super.readExternal(in);
-				methodName = in.readUTF();
-		}
+	@Override
+	public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+		super.readExternal(in);
+		methodName = in.readUTF();
+	}
 
+	@Override
+	public void init(SpliceOperationContext context) throws StandardException, IOException {
+		super.init(context);
+		methodCall = new SpliceMethod<Object>(methodName,activation);
+	}
 
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		super.writeExternal(out);
+		out.writeUTF(methodName);
+	}
 
-		@Override
-		public void init(SpliceOperationContext context) throws StandardException, IOException {
-				super.init(context);
-				methodCall = new SpliceMethod<Object>(methodName,activation);
-		}
+	@Override
+	public int[] getRootAccessedCols(long tableNumber) {
+		return null;
+	}
 
-		@Override
-		public void writeExternal(ObjectOutput out) throws IOException {
-				super.writeExternal(out);
-				out.writeUTF(methodName);
-		}
+	@Override
+	public boolean isReferencingTable(long tableNumber) {
+		return false;
+	}
 
-		@Override
-		public int[] getRootAccessedCols(long tableNumber) {
-				return null;
-		}
+    public void call() throws StandardException{
+        SpliceLogUtils.trace(LOG, "open");
+        setup();
+        if(timer==null)
+            timer = Metrics.newTimer();
 
-		@Override
-		public boolean isReferencingTable(long tableNumber) {
-				return false;
-		}
-
-
-        public void call() throws StandardException{
-            SpliceLogUtils.trace(LOG, "open");
-            setup();
-            if(timer==null)
-                timer = Metrics.newTimer();
-
-            timer.startTiming();
-            Object invoked = methodCall.invoke();
-            ResultSet[][] dynamicResults = activation.getDynamicResults();
-            if(dynamicResults==null) {
-                timer.stopTiming();
-                stopExecutionTime = System.currentTimeMillis();
-                return;
-            }
-
-
+        timer.startTiming();
+        Object invoked = methodCall.invoke();
+        ResultSet[][] dynamicResults = activation.getDynamicResults();
+        if(dynamicResults==null) {
             timer.stopTiming();
             stopExecutionTime = System.currentTimeMillis();
+            return;
         }
 
-				@Override
-				public void close() {
-						SpliceLogUtils.trace(LOG, "close for CallStatement, StatementContext=%s",
-										activation.getLanguageConnectionContext().getStatementContext());
-						if (!isOpen)
-								return;
-                        if (1!=2)
-                            return;
+        timer.stopTiming();
+        stopExecutionTime = System.currentTimeMillis();
+    }
 
-						ResultSet[][] dynamicResults = activation.getDynamicResults();
-						if (dynamicResults != null) {
+    @Override
+    public void close() {
+		SpliceLogUtils.trace(LOG, "close for CallStatement, StatementContext=%s",
+			activation.getLanguageConnectionContext().getStatementContext());
+		if (!isOpen)
+			return;
+        if (1!=2)
+            return;
 
-								ConnectionContext jdbcContext = null;
+		ResultSet[][] dynamicResults = activation.getDynamicResults();
+		if (dynamicResults != null) {
 
-								for (int i = 0; i < dynamicResults.length; i++)
-								{
-										ResultSet[] param = dynamicResults[i];
-										ResultSet drs = null;
-										if (param != null) drs = param[0];
+			ConnectionContext jdbcContext = null;
 
-										// Can be null if the procedure never set this parameter
-										// or if the dynamic results were processed by JDBC (EmbedStatement).
-										if (drs == null)
-												continue;
+			for (int i = 0; i < dynamicResults.length; i++)
+			{
+				ResultSet[] param = dynamicResults[i];
+				ResultSet drs = null;
+				if (param != null) drs = param[0];
 
-										if (jdbcContext == null)
-												jdbcContext = (ConnectionContext)activation.getLanguageConnectionContext().getContextManager().getContext(ConnectionContext.CONTEXT_ID);
+				// Can be null if the procedure never set this parameter
+				// or if the dynamic results were processed by JDBC (EmbedStatement).
+				if (drs == null)
+				    continue;
 
-										try {
-												// Is this a valid, open dynamic result set for this connection?
-												if (!jdbcContext.processInaccessibleDynamicResult(drs))
-														continue;
+				if (jdbcContext == null)
+					jdbcContext = (ConnectionContext)activation.getLanguageConnectionContext().getContextManager().getContext(ConnectionContext.CONTEXT_ID);
 
-												drs.close();
+				try {
+					// Is this a valid, open dynamic result set for this connection?
+					if (!jdbcContext.processInaccessibleDynamicResult(drs))
+							continue;
 
-										} catch (SQLException e) {
-												SpliceLogUtils.error(LOG, e);
-										} finally {
-												// Remove any reference to the ResultSet to allow
-												// it and any associated resources to be garbage collected.
-												param[0] = null;
-										}
-								}
-						}
-
-						try {
-								int staLength = (subqueryTrackingArray == null) ? 0 : subqueryTrackingArray.length;
-
-								for (int index = 0; index < staLength; index++)
-								{
-										if (subqueryTrackingArray[index] == null || subqueryTrackingArray[index].isClosed())
-												continue;
-
-										subqueryTrackingArray[index].close();
-								}
-
-								isOpen = false;
-
-								if (activation.isSingleExecution())
-										activation.close();
-						} catch (Exception e) {
-								SpliceLogUtils.error(LOG, e);
-						}
-
+					drs.close();
+				} catch (SQLException e) {
+					SpliceLogUtils.error(LOG, e);
+				} finally {
+					// Remove any reference to the ResultSet to allow
+					// it and any associated resources to be garbage collected.
+					param[0] = null;
 				}
-
-		@Override
-		public String prettyPrint(int indentLevel) {
-				return "CallStatement"+super.prettyPrint(indentLevel);
+			}
 		}
+
+		try {
+        	int staLength = (subqueryTrackingArray == null) ? 0 : subqueryTrackingArray.length;
+        
+        	for (int index = 0; index < staLength; index++)
+        	{
+    			if (subqueryTrackingArray[index] == null || subqueryTrackingArray[index].isClosed())
+					continue;
+    
+    			subqueryTrackingArray[index].close();
+        	}
+        
+        	isOpen = false;
+        
+        	if (activation.isSingleExecution())
+        		activation.close();
+		} catch (Exception e) {
+			SpliceLogUtils.error(LOG, e);
+		}
+
+    }
+
+	@Override
+	public String prettyPrint(int indentLevel) {
+	    return "CallStatement"+super.prettyPrint(indentLevel);
+	}
 
     @Override
     public String toString() {
@@ -170,15 +158,14 @@ public class CallStatementOperation extends NoRowsOperation {
     }
 
     @Override
-        public DataSet<LocatedRow> getDataSet(DataSetProcessor dsp) throws StandardException {
-            call();
+    public DataSet<LocatedRow> getDataSet(DataSetProcessor dsp) throws StandardException {
+        call();
         registerCloseable(new AutoCloseable() {
             @Override
             public void close() throws Exception {
                 this.close();
             }
         });
-            return dsp.getEmpty();
-        }
+        return dsp.getEmpty();
     }
-
+}
