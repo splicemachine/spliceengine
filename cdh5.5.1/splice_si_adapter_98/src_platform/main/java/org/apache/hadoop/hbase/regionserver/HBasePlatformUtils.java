65da515e9e5f83a1cb581ab5d908d8878014eceb
Aaron Molitor <amolitor@splicemachine.com>
2015-12-21 18:49:04 +0000

    adding cdh5.5.1 dir

---
+++ cdh5.5.1/splice_si_adapter_98/src_platform/main/java/org/apache/hadoop/hbase/regionserver/HBasePlatformUtils.java
@@ -0,0 +1,35 @@
+package org.apache.hadoop.hbase.regionserver;
+
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.util.Counter;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.NavigableSet;
+
+public class HBasePlatformUtils{
+	public static void updateWriteRequests(HRegion region, long numWrites) {
+		Counter writeRequestsCount = region.writeRequestsCount;
+		if (writeRequestsCount != null)
+			writeRequestsCount.add(numWrites);
+	}
+
+	public static void updateReadRequests(HRegion region, long numReads) {
+		Counter readRequestsCount = region.readRequestsCount;
+		if (readRequestsCount != null)
+			readRequestsCount.add(numReads);
+	}
+
+	public static NavigableSet<Cell> getKvset(HStore store) {
+		return ((DefaultMemStore) store.memstore).cellSet;
+	}
+
+	public static NavigableSet<Cell> getSnapshot(HStore store) {
+		return ((DefaultMemStore) store.memstore).snapshot;
+	}
+
+    public static Map<byte[],Store> getStores(HRegion region) {
+        return region.getStores();
+    }
+}
