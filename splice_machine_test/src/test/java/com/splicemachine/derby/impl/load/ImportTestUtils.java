0906a750ecc55871d6d73bc3bbcc0647153d8d8d
jeffcunningham <jcunningham@splicemachine.com>
2016-01-11 11:24:16 -0600

    DB-3957: Test bulk import. Re-added deleted import ITs. 62 tests - 20 are @Ignored with Jira issue numbers.

--- splice_machine_test/src/test/java/com/splicemachine/derby/impl/load/ImportTestUtils.java
+++ splice_machine_test/src/test/java/com/splicemachine/derby/impl/load/ImportTestUtils.java
@@ -1,10 +1,17 @@
 package com.splicemachine.derby.impl.load;
 
+import static org.junit.Assert.assertTrue;
+
 import com.splicemachine.db.iapi.error.StandardException;
 import com.splicemachine.db.iapi.sql.execute.ExecRow;
 import com.splicemachine.db.iapi.types.DataValueDescriptor;
+import com.splicemachine.derby.test.framework.SpliceUnitTest;
+import com.splicemachine.homeless.TestUtils;
+
 import org.junit.Assert;
 
+import java.io.File;
+import java.sql.SQLException;
 import java.util.Comparator;
 
 /**
@@ -19,16 +26,45 @@ public class ImportTestUtils {
         return new ExecRowComparator(columnPosition);
     }
 
-		public static void assertRowsEquals(ExecRow correctRow, ExecRow actualRow)  {
-				DataValueDescriptor [] correctRowArray = correctRow.getRowArray();
-				DataValueDescriptor [] actualRowArray = actualRow.getRowArray();
-				for(int dvdPos=0;dvdPos<correctRow.nColumns();dvdPos++){
-						Assert.assertEquals("Incorrect column at position " + dvdPos,
-										correctRowArray[dvdPos],
-										actualRowArray[dvdPos]);
+    public static void assertRowsEquals(ExecRow correctRow, ExecRow actualRow)  {
+        DataValueDescriptor [] correctRowArray = correctRow.getRowArray();
+        DataValueDescriptor [] actualRowArray = actualRow.getRowArray();
+        for(int dvdPos=0;dvdPos<correctRow.nColumns();dvdPos++){
+            Assert.assertEquals("Incorrect column at position " + dvdPos,
+                                correctRowArray[dvdPos],
+                                actualRowArray[dvdPos]);
 
-				}
-		}
+        }
+    }
+
+    public static String printMsgSQLState(String testName, SQLException e) {
+        // useful for debugging import errors
+        StringBuilder buf =new StringBuilder(testName);
+        buf.append("\n");
+        int i =1;
+        SQLException child = e;
+        while (child != null) {
+            buf.append(i++).append(" ").append(child.getSQLState()).append(" ")
+               .append(child.getLocalizedMessage()).append("\n");
+            child = child.getNextException();
+        }
+        return buf.toString();
+    }
+
+    public static File createBadLogDirectory(String schemaName) {
+        File badImportLogDirectory = new File(SpliceUnitTest.getBaseDirectory()+"/target/BAD/"+schemaName);
+        if (badImportLogDirectory.exists()) {
+            //noinspection ConstantConditions
+            for (File file : badImportLogDirectory.listFiles()) {
+                assertTrue("Couldn't create "+file,file.delete());
+            }
+            assertTrue("Couldn't create "+badImportLogDirectory,badImportLogDirectory.delete());
+        }
+        assertTrue("Couldn't create "+badImportLogDirectory,badImportLogDirectory.mkdirs());
+        assertTrue("Failed to create "+badImportLogDirectory,badImportLogDirectory.exists());
+        return badImportLogDirectory;
+    }
+
 
     private static class ExecRowComparator implements Comparator<ExecRow> {
         private final int colNumber;
