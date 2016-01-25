ffe54c768c7df863c00c3938d7c1d835d6e4635d
Scott Fines <scottfines@gmail.com>
2016-01-25 13:28:50 -0600

    Deleting unnecessary platform directories

--- hdp2.2.4/splice_machine_adapter_98/src_platform/main/java/org/apache/hadoop/hbase/replication/ReplicationEndpoint.java
+++
@@ -1,9 +0,0 @@
-package org.apache.hadoop.hbase.replication;
-
-/**
- * No-op tag class so that we can be convertible between hbase 0.98 and Hbase 1.0
- * @author Scott Fines
- *         Date: 5/13/15
- */
-public class ReplicationEndpoint{
-}
