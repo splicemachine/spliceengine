d5d55d62165a6ae80a3f8fff88e5d035a61bbc63
Walter Koetke <wkoetke@splicemachine.com>
2016-02-04 13:20:18 -0700

    Manual integration of 9afd969 from master_dataset to k2_refactor. (John Leach) Fixing Drop Schema to follow drop table conventions and invalidate the new schema cache

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/actions/DropSchemaConstantOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/actions/DropSchemaConstantOperation.java
@@ -1,5 +1,6 @@
 package com.splicemachine.derby.impl.sql.execute.actions;
 
+import com.splicemachine.ddl.DDLMessage;
 import com.splicemachine.derby.ddl.DDLUtils;
 import com.splicemachine.derby.impl.store.access.SpliceTransactionManager;
 import com.splicemachine.db.iapi.error.StandardException;
@@ -56,7 +57,8 @@ public class DropSchemaConstantOperation extends DDLConstantOperation {
         SpliceTransactionManager tc = (SpliceTransactionManager)lcc.getTransactionExecute();
         SchemaDescriptor sd = dd.getSchemaDescriptor(schemaName, tc, true);
         sd.drop(lcc, activation);
-        DDLUtils.notifyMetadataChangeAndWait(ProtoUtil.createDropSchema(tc.getActiveStateTxn().getTxnId(), schemaName));
+        DDLMessage.DDLChange ddlChange = ProtoUtil.createDropSchema(tc.getActiveStateTxn().getTxnId(), schemaName);
+        tc.prepareDataDictionaryChange(DDLUtils.notifyMetadataChange(ddlChange));
     }
 
 }
