10950bcabc651c2ffb9f6ffbdf7214e72425e035
Scott Fines <scottfines@gmail.com>
2016-02-17 09:10:25 -0600

    moving over an @Ignore

--- splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/IndexSelectivityIT.java
+++ splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/IndexSelectivityIT.java
@@ -87,6 +87,7 @@ public class IndexSelectivityIT extends SpliceUnitTest {
     }
 
     @Test
+    @Ignore("DB-4272 DB-4440 Intermittent failures; expected IndexScan[TS_LOW_CARDINALITY_IX_1 got IndexScan[TS_LOW_CARDINALITY_IX_5")
     public void testCoveringIndexScan() throws Exception {
         rowContainsQuery(3,"explain select c1 from ts_low_cardinality where c1 = 1","IndexScan[TS_LOW_CARDINALITY_IX_1",methodWatcher);
         rowContainsQuery(3,"explain select c1,c2 from ts_low_cardinality where c1 = 1","IndexScan[TS_LOW_CARDINALITY_IX_3",methodWatcher);
