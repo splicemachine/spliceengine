152ee7f88f6ca246f612a537d5abef82fec7e9ae
Scott Fines <scottfines@gmail.com>
2016-04-09 12:50:57 -0500

    DB-4874: Fixing mem_engine to compile properly

--- mem_engine/src/main/java/com/splicemachine/derby/impl/sql/LocalOlapClient.java
+++ mem_engine/src/main/java/com/splicemachine/derby/impl/sql/LocalOlapClient.java
@@ -1,9 +1,16 @@
 package com.splicemachine.derby.impl.sql;
 
+import com.splicemachine.EngineDriver;
+import com.splicemachine.derby.iapi.sql.olap.DistributedJob;
 import com.splicemachine.derby.iapi.sql.olap.OlapClient;
 import com.splicemachine.derby.iapi.sql.olap.OlapResult;
+import com.splicemachine.derby.iapi.sql.olap.OlapStatus;
+import com.splicemachine.si.impl.driver.SIDriver;
 
+import javax.annotation.Nonnull;
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author Scott Fines
@@ -18,12 +25,64 @@ public class LocalOlapClient implements OlapClient{
 
     private LocalOlapClient(){ }
 
+
     @Override
-    public <R extends OlapResult> R submitOlapJob(OlapJobRequest jobRequest) throws IOException {
+    public <R extends OlapResult> R execute(@Nonnull DistributedJob jobRequest) throws IOException, TimeoutException{
+        Status status=new Status();
+        Callable<Void> callable=jobRequest.toCallable(status,SIDriver.driver().getClock(),Long.MAX_VALUE);
         try {
-            return callable.call();
+            callable.call();
         } catch (Exception e) {
             throw new IOException(e);
+        }
+        return (R)status.getResult();
+    }
+
+    @Override public void shutdown(){ }
+
+    /* ****************************************************************************************************************/
+    /*private helper stuff*/
+    private static class Status implements OlapStatus{
+        private OlapResult result;
+
+        @Override
+        public State checkState(){
+            return result==null? State.RUNNING:State.COMPLETE;
+        }
+
+        @Override
+        public OlapResult getResult(){
+            return result;
+        }
+
+        @Override
+        public void cancel(){
+
+        }
+
+        @Override
+        public boolean isAvailable(){
+            return true;
+        }
+
+        @Override
+        public boolean markSubmitted(){
+            return true;
+        }
+
+        @Override
+        public void markCompleted(OlapResult result){
+            this.result = result;
+        }
+
+        @Override
+        public boolean markRunning(){
+            return true;
+        }
+
+        @Override
+        public boolean isRunning(){
+            return result==null;
         }
     }
 }
