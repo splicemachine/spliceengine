bcef1d6c0a465ab41439c54c385093c32046c48f
John Leach <jleach@splicemachine.com>
2016-05-13 09:20:26 -0500

    Adding Yarn-Config Copy

--- assembly/src/main/resources/meta/splice_env.sh
+++ assembly/src/main/resources/meta/splice_env.sh
@@ -19,12 +19,20 @@ MYLIBDIR=${PARCELS_ROOT}/${PARCEL_DIRNAME}/lib/splice
 APPENDSTRING=`echo ${MYLIBDIR}/*.jar | sed 's/ /:/g'`
 echo "appending '$APPENDSTRING' to HBASE_CLASSPATH"
 
-echo "listening out config dir '$HBASE_CONF_DIR'"
-
 if [ -z $HBASE_CLASSPATH ] ; then
     export HBASE_CLASSPATH=$APPENDSTRING
 else
     export HBASE_CLASSPATH="$HBASE_CLASSPATH:$APPENDSTRING"
 fi
+
+echo "Copying yarn-site.xml to hbase directory"
+
+if [ -f "/etc/hadoop/conf.cloudera.yarn/yarn-site.xml" ] ; then
+    cp "/etc/hadoop/conf.cloudera.yarn/yarn-site.xml" "$CONF_DIR"
+else
+    echo "Could not find yarn-site.xml, make sure to deploy yarn client in UI" >&2
+    exit 1
+fi
+
 echo "Set HBASE_CLASSPATH to '$HBASE_CLASSPATH'"
 echo "splice_env.sh successfully executed at `date`"
