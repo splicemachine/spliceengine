af758f5a9582716ba842cbd4fc0a501aa5138cce
Scott Fines <scottfines@gmail.com>
2016-06-02 10:18:18 -0500

    convenience fix for sqlshell to make it easy to run

--- sqlshell.sh
+++ sqlshell.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
+DIR=$(pwd)
+cd splice_machine; rlwrap mvn exec:java ; cd ${DIR}
 
-HBASE_HOME=~/apps/hbase
-
-REPO_HOME="/Users/scottfines/.m2/repository"
-VERSION="2.0.0.k2_refactor-SNAPSHOT"
-CLASSPATH="${REPO_HOME}/com/splicemachine/db-project/db-client/${VERSION}/db-client-${VERSION}.jar:${REPO_HOME}/com/splicemachine/db-project/db-engine/${VERSION}/db-engine-${VERSION}.jar:${REPO_HOME}/com/splicemachine/db-project/db-tools-ij/2.0.0.1-SNAPSHOT/db-tools-ij-2.0.0.1-SNAPSHOT.jar"
-ARGS="-cp $CLASSPATH"
-#ARGS="$ARGS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8072"
-ARGS="$ARGS -Dhbase.log.dir=\".\" -Dsplice.log.file=\"client.log\""
-ARGS="$ARGS -Dij.connection.splice=jdbc:splice://localhost:1527/splicedb;user=splice;password=admin"
-
-rlwrap java $ARGS com.splicemachine.db.tools.ij $@
-#rlwrap java -cp $CLASSPATH -Dhbase.log.dir="." -Dsplice.log.file="client.log" -Dij.connection.splice="jdbc:derby://localhost:1527/splicedb;user=splice;password=admin" com.splicemachine.db.tools.ij $@
