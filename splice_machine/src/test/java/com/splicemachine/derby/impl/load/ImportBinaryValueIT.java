48a5c422c9fbe765cb28a3a5014418bbd9ed5fda
John Leach <jleach@splicemachine.com>
2016-06-18 08:34:41 -0700

    Fix IT Contamination

--- splice_machine/src/test/java/com/splicemachine/derby/impl/load/ImportBinaryValueIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/load/ImportBinaryValueIT.java
@@ -22,7 +22,7 @@ import java.sql.PreparedStatement;
 public class ImportBinaryValueIT extends SpliceUnitTest {
 
     protected static SpliceWatcher spliceClassWatcher = new SpliceWatcher();
-    public static final String CLASS_NAME = ImportDefaultValueIT.class.getSimpleName().toUpperCase();
+    public static final String CLASS_NAME = ImportBinaryValueIT.class.getSimpleName().toUpperCase();
     protected static SpliceSchemaWatcher schema = new SpliceSchemaWatcher(CLASS_NAME);
 
     protected static SpliceTableWatcher blobTale = new SpliceTableWatcher("BLOB_TABLE", schema.schemaName, "(VAL1 BLOB)");
