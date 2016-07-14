be973d62dbd81f37ea49b90691b469d84bcc38b9
Aaron Molitor <amolitor@splicemachine.com>
2016-07-13 21:27:04 -0500

    updating copyright on splice files

--- splice_si_api/src/main/java/com/splicemachine/si/api/data/package-info.java
+++ splice_si_api/src/main/java/com/splicemachine/si/api/data/package-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012 - 2016 Splice Machine, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+ * this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 /**
  * Defines the interfaces used by the SI module to access the underlying data store (i.e. HBase). Defining these as
  * interfaces allows an alternate, lightweight implementation to be used instead of HBase for rapid automated unit
