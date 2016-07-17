e7a5bac4ea025ad1e39700300a2862b132d32d09
Gene Davis <gdavis@splicemachine.com>
2016-07-17 14:10:47 -0700

    check for presence of rlwrap in case they don't have it

--- sqlshell.sh
+++ sqlshell.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 DIR=$(pwd)
-cd splice_machine; rlwrap mvn exec:java ; cd ${DIR}
+
+if hash rlwrap 2>/dev/null; then
+    echo -en "\n ========= rlwrap detected and enabled.  Use up and down arrow keys to scroll through command line history. ======== \n\n"
+    RLWRAP=rlwrap
+else
+    echo -en "\n ========= rlwrap not detected.  Consider installing for command line history capabilities. ========= \n\n"
+    RLWRAP=
+fi
+
+echo "Running Splice Machine SQL shell"
+echo "For help: \"splice> help;\""
+
+cd splice_machine ; ${RLWRAP} mvn exec:java ; cd ${DIR}
 
