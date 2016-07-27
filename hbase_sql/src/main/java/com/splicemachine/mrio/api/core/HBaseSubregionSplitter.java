e314379c2fc69795fa1a5a17cf5a24b6bd99d96e
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2016-07-27 17:15:45 +0200

    SPLICE-726 Use ZeroCopyByteString

--- hbase_sql/src/main/java/com/splicemachine/mrio/api/core/HBaseSubregionSplitter.java
+++ hbase_sql/src/main/java/com/splicemachine/mrio/api/core/HBaseSubregionSplitter.java
@@ -16,6 +16,7 @@
 package com.splicemachine.mrio.api.core;
 
 import com.google.protobuf.ByteString;
+import com.google.protobuf.ZeroCopyLiteralByteString;
 import com.splicemachine.access.HConfiguration;
 import com.splicemachine.access.hbase.HBaseTableInfoFactory;
 import com.splicemachine.coprocessor.SpliceMessage;
@@ -73,9 +74,9 @@ public class HBaseSubregionSplitter implements SubregionSplitter{
                                 }
 
                                 SpliceMessage.SpliceSplitServiceRequest message = SpliceMessage.SpliceSplitServiceRequest.newBuilder()
-                                        .setBeginKey(ByteString.copyFrom(scanStartRow))
-                                        .setEndKey(ByteString.copyFrom(scanStopRow))
-                                        .setRegionEndKey(ByteString.copyFrom(stopKey)).build();
+                                        .setBeginKey(ZeroCopyLiteralByteString.wrap(scanStartRow))
+                                        .setEndKey(ZeroCopyLiteralByteString.wrap(scanStopRow))
+                                        .setRegionEndKey(ZeroCopyLiteralByteString.wrap(stopKey)).build();
 
                                 BlockingRpcCallback<SpliceMessage.SpliceSplitServiceResponse> rpcCallback = new BlockingRpcCallback<>();
                                 instance.computeSplits(controller, message, rpcCallback);
