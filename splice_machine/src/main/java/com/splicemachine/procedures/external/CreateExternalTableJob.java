7d147d628bb6119b808803a108c00bb27c0a6935
jfilali <filali.jebril@gmail.com>
2016-12-13 17:22:31 -0800

    SPLICE-1163 : Make sure we have the correct permission for external tables: 1) When someone create a table, make sure if the file doesn't exist to verify if the parent file( aka the directory where you want to store that file ) is writable . 2) When inserting something, make sure the file is writable 3) Found some bug related to compression that came up with those changes

--- splice_machine/src/main/java/com/splicemachine/procedures/external/CreateExternalTableJob.java
+++ splice_machine/src/main/java/com/splicemachine/procedures/external/CreateExternalTableJob.java
@@ -6,6 +6,7 @@ import com.splicemachine.derby.iapi.sql.olap.OlapStatus;
 import com.splicemachine.derby.iapi.sql.olap.SuccessfulOlapResult;
 import com.splicemachine.derby.stream.iapi.DistributedDataSetProcessor;
 import com.splicemachine.derby.stream.output.WriteReadUtils;
+import com.splicemachine.pipeline.ErrorState;
 import com.splicemachine.si.impl.driver.SIDriver;
 import com.splicemachine.utils.IntArrays;
 
@@ -46,7 +47,13 @@ public class CreateExternalTableJob implements Callable<Void> {
 
         // look at the file, if it doesn't exist create it.
         if(!SIDriver.driver().fileSystem().getPath(request.getLocation()).toFile().exists()){
-            dsp.createEmptyExternalFile(execRow, IntArrays.count(execRowTypeFormatIds.length), request.getPartitionBy(),  request.getStoredAs(), request.getLocation());
+
+            //throws a error if the parent doesn't have the permission.
+            if(!SIDriver.driver().fileSystem().getPath(request.getLocation()).getParent().toFile().canWrite()){
+                throw  ErrorState.CANNOT_WRITE_AT_LOCATION.newException(request.getLocation());
+            }
+
+            dsp.createEmptyExternalFile(execRow, IntArrays.count(execRowTypeFormatIds.length), request.getPartitionBy(),  request.getStoredAs(), request.getLocation(),request.getCompression());
         }
 
         jobStatus.markCompleted(new SuccessfulOlapResult());
