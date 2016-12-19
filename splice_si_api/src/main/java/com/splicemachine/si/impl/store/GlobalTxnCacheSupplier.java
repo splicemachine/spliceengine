c5fc0463ed169759d05868582625c55c132e7fad
John Leach <jleach@splicemachine.com>
2016-12-19 17:03:42 -0600

    Foo

---
+++ splice_si_api/src/main/java/com/splicemachine/si/impl/store/GlobalTxnCacheSupplier.java
@@ -0,0 +1,13 @@
+package com.splicemachine.si.impl.store;
+
+import com.splicemachine.si.api.txn.TxnSupplier;
+
+/**
+ * Created by jleach on 12/19/16.
+ */
+public class GlobalTxnCacheSupplier implements TxnSupplier {
+
+
+
+
+}
