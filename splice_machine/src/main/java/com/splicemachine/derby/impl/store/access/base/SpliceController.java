d45e1bfe913f7bf5f93665f2f6b6ab1d2914e3ca
John Leach <jleach@splicemachine.com>
2017-01-23 09:34:57 -0600

    SPLICE-1290 Cleaning up e.prinstackTrace, sorry for the delay Daniel

--- splice_machine/src/main/java/com/splicemachine/derby/impl/store/access/base/SpliceController.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/store/access/base/SpliceController.java
@@ -201,7 +201,6 @@ public abstract class SpliceController implements ConglomerateController{
             }
             return true;
         }catch(Exception e){
-            e.printStackTrace();
             throw Exceptions.parseException(e);
         } finally {
             if (rowDecoder !=null)
