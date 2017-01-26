43d2e909dab7194e8b4399da8d9e02c9d7e7ee3e
Aaron Molitor <amolitor@splicemachine.com>
2017-01-25 21:47:37 -0600

    updaing license to AGPLv3

--- hbase_storage/hbase0.98/src/main/java/com/splicemachine/access/hbase/Hbase98TxnNetworkLayer.java
+++ hbase_storage/hbase0.98/src/main/java/com/splicemachine/access/hbase/Hbase98TxnNetworkLayer.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2012 - 2017 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.splicemachine.access.hbase;
 
 import com.splicemachine.si.coprocessor.TxnMessage;
