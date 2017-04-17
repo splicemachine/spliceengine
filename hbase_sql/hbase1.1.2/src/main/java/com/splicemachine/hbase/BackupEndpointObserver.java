a414e02ec995cb72ae467d4c2002b748e3e17ec2
Jun Yuan <jyuan@splicemachine.com>
2017-04-17 16:10:17 -0700

    DB-5912: fix incremental backup hang

--- hbase_sql/hbase1.1.2/src/main/java/com/splicemachine/hbase/BackupEndpointObserver.java
+++ hbase_sql/hbase1.1.2/src/main/java/com/splicemachine/hbase/BackupEndpointObserver.java
@@ -111,6 +111,32 @@ public class BackupEndpointObserver extends BackupBaseRegionObserver implements 
     }
 
     @Override
+    public void preSplit(ObserverContext<RegionCoprocessorEnvironment> c, byte[] splitRow) throws IOException {
+        if (LOG.isDebugEnabled())
+            SpliceLogUtils.debug(LOG, "BackupEndpointObserver.preSplit()");
+
+        BackupUtils.waitForBackupToComplete(tableName, regionName, path);
+        isSplitting = true;
+        super.preSplit(c, splitRow);
+    }
+
+    @Override
+    public void postRollBackSplit(ObserverContext<RegionCoprocessorEnvironment> ctx) throws IOException {
+        if (LOG.isDebugEnabled())
+            SpliceLogUtils.debug(LOG, "BackupEndpointObserver.postRollBackSplit()");
+
+        isSplitting = false;
+        super.postRollBackSplit(ctx);
+    }
+
+    @Override
+    public void postSplit(ObserverContext<RegionCoprocessorEnvironment> regionCoprocessorEnvironmentObserverContext, Region region, Region region2) throws IOException {
+        if (LOG.isDebugEnabled())
+            SpliceLogUtils.debug(LOG, "BackupEndpointObserver.postSplit()");
+        isSplitting = false;
+    }
+
+    @Override
     public void postSplit(ObserverContext<RegionCoprocessorEnvironment> e, HRegion l, HRegion r) throws IOException {
         if (LOG.isDebugEnabled())
             SpliceLogUtils.debug(LOG, "BackupEndpointObserver.postSplit()");
@@ -153,6 +179,23 @@ public class BackupEndpointObserver extends BackupBaseRegionObserver implements 
     }
 
     @Override
+    public void postFlush(ObserverContext<RegionCoprocessorEnvironment> e) throws IOException {
+        SpliceLogUtils.info(LOG, "Flushing region %s.%s", tableName, regionName);
+        if (!BackupUtils.isSpliceTable(namespace, tableName))
+            return;
+        isFlushing = false;
+    }
+
+    @Override
+    public InternalScanner preFlush(ObserverContext<RegionCoprocessorEnvironment> e, Store store, InternalScanner scanner) throws IOException {
+        if (LOG.isDebugEnabled())
+            SpliceLogUtils.debug(LOG, "BackupEndpointObserver.preFlush()");
+        BackupUtils.waitForBackupToComplete(tableName, regionName, path);
+        isFlushing = true;
+        return super.preFlush(e,store,scanner);
+    }
+
+    @Override
     public void postFlush(ObserverContext<RegionCoprocessorEnvironment> e, Store store, StoreFile resultFile) throws IOException {
         // Register HFiles for incremental backup
         SpliceLogUtils.info(LOG, "Flushing region %s.%s", tableName, regionName);
@@ -165,8 +208,5 @@ public class BackupEndpointObserver extends BackupBaseRegionObserver implements 
         } catch (Exception ex) {
             throw new IOException(ex);
         }
-    }
-
-    public void postSplit(ObserverContext<RegionCoprocessorEnvironment> regionCoprocessorEnvironmentObserverContext, Region region, Region region2) throws IOException {
     }
 }
