37b46cb1843134a28510dc7f1c04452262e4d184
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2017-05-03 12:44:38 +0200

    Revert default sec mechanism

--- db-client/src/main/java/com/splicemachine/db/jdbc/ClientBaseDataSource.java
+++ db-client/src/main/java/com/splicemachine/db/jdbc/ClientBaseDataSource.java
@@ -308,7 +308,7 @@ public abstract class ClientBaseDataSource implements Serializable, Referenceabl
 
     //  Default security mechanism is USER_ONLY_SECURITY.
     public final static short propertyDefault_securityMechanism =
-                                    (short) NetConfiguration.SECMEC_KERSEC;
+                                    (short) NetConfiguration.SECMEC_USRIDONL;
 
     
     // We use the NET layer constants to avoid a mapping for the NET driver.
@@ -393,7 +393,7 @@ public abstract class ClientBaseDataSource implements Serializable, Referenceabl
             // return (short)NetConfiguration.SECMEC_USRSSBPWD;
          // ----------------------
          */
-         return (short)NetConfiguration.SECMEC_KERSEC;
+         return (short)NetConfiguration.SECMEC_USRIDPWD;
 
     }
 
