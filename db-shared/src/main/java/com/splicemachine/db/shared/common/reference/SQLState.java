eb5bee181e68e8858f6ecb1449d22d6a96a24d51
jyuanca <jyuan@splicemachine.com>
2017-05-09 11:16:55 -0700

    DB-5898: clean up timeout backup (#692)

--- db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
+++ db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
@@ -1951,6 +1951,7 @@ public interface SQLState {
 	String BACKUP_DOESNOT_EXIST                                    = "BR012";
 	String PARENT_BACKUP_MISSING                                   = "BR013";
 	String EXISTS_CONCURRENT_BACKUP                                = "BR014";
+	String BACKUP_TIMEOUT                                          = "BR016";
 
 
 	String ROW_FORMAT_NOT_ALLOWED_WITH_PARQUET					   	= "EXT01";
