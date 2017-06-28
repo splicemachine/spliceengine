8090b7294f4fc569942c9da5d801689a178463dc
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2017-06-28 17:23:05 +0200

    SPLICE-1716 Upgrade OlapServer to Netty 4

--- hbase_sql/src/main/java/com/splicemachine/olap/OlapStatusHandler.java
+++ hbase_sql/src/main/java/com/splicemachine/olap/OlapStatusHandler.java
@@ -14,10 +14,9 @@
 
 package com.splicemachine.olap;
 
-import com.splicemachine.olap.OlapMessage;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
 import org.apache.log4j.Logger;
-import org.jboss.netty.channel.ChannelHandlerContext;
-import org.jboss.netty.channel.MessageEvent;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,6 +24,7 @@ import java.util.concurrent.TimeUnit;
  * @author Scott Fines
  *         Date: 4/1/16
  */
+@ChannelHandler.Sharable
 public class OlapStatusHandler extends AbstractOlapHandler{
     private static final Logger LOG = Logger.getLogger(OlapStatusHandler.class);
 
@@ -33,13 +33,12 @@ public class OlapStatusHandler extends AbstractOlapHandler{
     }
 
     @Override
-    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception{
-        OlapMessage.Command cmd = (OlapMessage.Command)e.getMessage();
+    protected void channelRead0(ChannelHandlerContext ctx, OlapMessage.Command cmd) throws Exception {
         if (LOG.isTraceEnabled()) {
             LOG.trace("Received " + cmd);
         }
         if(cmd.getType()!=OlapMessage.Command.Type.STATUS){
-            ctx.sendUpstream(e);
+            ctx.fireChannelRead(cmd);
             return;
         }
         OlapJobStatus status = jobRegistry.getStatus(cmd.getUniqueName());
@@ -55,12 +54,11 @@ public class OlapStatusHandler extends AbstractOlapHandler{
         }
 
 
-        writeResponse(e,cmd.getUniqueName(),status);
+        writeResponse(ctx.channel(),cmd.getUniqueName(),status);
 
         if (LOG.isTraceEnabled()) {
             LOG.trace("Status " + status);
         }
 
-        super.messageReceived(ctx,e);
     }
 }
