e2a20662a709afd5629c6f0a4dc037a7f3441d5d
Mandar Pimpale <mpimpale@splicemachine.com>
2017-08-01 11:08:36 -0700

    Revert "DB-5974 Reconnect to OlapServer in case of failure (#971)"
    This reverts commit bd6433e1f3d57e123b4e8a5e0882bdaa71d17333.

--- hbase_sql/src/main/java/com/splicemachine/derby/lifecycle/HEngineSqlEnv.java
+++ hbase_sql/src/main/java/com/splicemachine/derby/lifecycle/HEngineSqlEnv.java
@@ -17,7 +17,6 @@ package com.splicemachine.derby.lifecycle;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.google.common.net.HostAndPort;
 import com.splicemachine.SqlExceptionFactory;
 import com.splicemachine.access.api.DatabaseVersion;
 import com.splicemachine.access.api.SConfiguration;
@@ -113,13 +112,13 @@ public class HEngineSqlEnv extends EngineSqlEnvironment{
         int port = config.getOlapServerBindPort();
         int retries = config.getOlapClientRetries();
         HBaseConnectionFactory hbcf = HBaseConnectionFactory.getInstance(config);
-        JobExecutor onl = new AsyncOlapNIOLayer(() -> {
-            try {
-                return HostAndPort.fromParts(hbcf.getMasterServer().getHostname(), port);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        },retries);
+        String host;
+        try {
+            host = hbcf.getMasterServer().getHostname();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        JobExecutor onl = new AsyncOlapNIOLayer(host,port,retries);
         return new TimedOlapClient(onl,timeoutMillis);
     }
 
