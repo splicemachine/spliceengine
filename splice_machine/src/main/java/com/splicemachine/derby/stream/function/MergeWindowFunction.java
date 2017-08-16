d9eb3ba92670c7bcba7402dfffe8b132f79fcd34
Yi Xia <yxia@splicemachine.com>
2017-08-15 17:59:06 -0700

    DB-6164 honor Null ordering specification in Spark and Control path for Window function (#1011)

--- splice_machine/src/main/java/com/splicemachine/derby/stream/function/MergeWindowFunction.java
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/function/MergeWindowFunction.java
@@ -24,7 +24,10 @@ import com.splicemachine.derby.stream.window.WindowFrameBuffer;
 import scala.Tuple2;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
 
 import static java.util.Collections.sort;
 
@@ -48,7 +51,10 @@ public class MergeWindowFunction<Op extends WindowOperation> extends SpliceFlatM
             partitionRows.add(lr);
         }
         WindowContext windowContext = operationContext.getOperation().getWindowContext();
-        sort(partitionRows, new LocatedRowComparator(windowContext.getKeyColumns(), windowContext.getKeyOrders()));
+
+        sort(partitionRows, new LocatedRowComparator(windowContext.getKeyColumns(),
+                                                     windowContext.getKeyOrders(),
+                                                     windowContext.getNullOrderings()));
 
         // window logic
         final WindowFrameBuffer frameBuffer = BaseFrameBuffer.createFrameBuffer(
@@ -66,8 +72,8 @@ public class MergeWindowFunction<Op extends WindowOperation> extends SpliceFlatM
     private class LocatedRowComparator implements Comparator<ExecRow> {
         private final ColumnComparator rowComparator;
 
-        public LocatedRowComparator(int[] keyColumns, boolean[] keyOrders) {
-            this.rowComparator = new ColumnComparator(keyColumns, keyOrders, true);
+        public LocatedRowComparator(int[] keyColumns, boolean[] keyOrders, boolean[] nullOrders) {
+            this.rowComparator = new ColumnComparator(keyColumns, keyOrders, nullOrders);
         }
 
         @Override
