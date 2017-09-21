5803f43feb50b7bab196142cb3d6e05cdf205f39
Yi Xia <yxia@splicemachine.com>
2017-09-21 08:17:54 -0700

    DB-6231 enhance MultiProbeIndexScan to apply for cases where inlist is not the leading index/PK column. (#1093)

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/BinaryRelationalOperatorNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/BinaryRelationalOperatorNode.java
@@ -1010,10 +1010,7 @@ public class BinaryRelationalOperatorNode
         }
     }
 
-    @Override
-    public boolean compareWithKnownConstant(Optimizable optTable,boolean considerParameters){
-        ValueNode node;
-        node=keyColumnOnLeft(optTable)?rightOperand:leftOperand;
+    private boolean isKnownConstant(ValueNode node, boolean considerParameters) {
         if (node instanceof CastNode)
             node = ((CastNode) node).castOperand;
 
@@ -1023,6 +1020,17 @@ public class BinaryRelationalOperatorNode
                             (((ParameterNode)node).getDefaultValue()!=null));
         }else{
             return node instanceof ConstantNode;
+        }
+    }
+
+    @Override
+    public boolean compareWithKnownConstant(Optimizable optTable,boolean considerParameters){
+        ValueNode node;
+        if (optTable != null) {
+            node = keyColumnOnLeft(optTable) ? rightOperand : leftOperand;
+            return isKnownConstant(node, considerParameters);
+        } else {
+            return (isKnownConstant(rightOperand, considerParameters) || isKnownConstant(leftOperand, considerParameters));
         }
     }
 
