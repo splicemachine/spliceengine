e06857c13f9976ce37333cc27f5d1459b897bf9e
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2017-10-19 15:22:46 +0200

    Revert "DB-6274 Fix build with missing imports"
    This reverts commit c658135a0783719145b366e50efe0ff523e4a1aa.

--- hbase_storage/hbase1.1.2.2.5/src/main/java/com/splicemachine/access/client/MemstoreKeyValueScanner.java
+++ hbase_storage/hbase1.1.2.2.5/src/main/java/com/splicemachine/access/client/MemstoreKeyValueScanner.java
@@ -16,7 +16,6 @@ package com.splicemachine.access.client;
 
 import com.splicemachine.utils.SpliceLogUtils;
 import org.apache.hadoop.hbase.Cell;
-import org.apache.hadoop.hbase.DoNotRetryIOException;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.ResultScanner;
