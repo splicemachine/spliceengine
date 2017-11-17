749d037fe28d0f8b2ed488541cbdf9f200c02ea6
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2017-11-17 19:27:41 +0100

    SPLICE-1948 Increase test timeout

--- splice_spark/src/test/scala/com/splicemachine/spark/splicemachine/SparkStreamingTest.scala
+++ splice_spark/src/test/scala/com/splicemachine/spark/splicemachine/SparkStreamingTest.scala
@@ -281,7 +281,7 @@ private class ListenableQueue[A] extends Queue[A] {
     synchronized {
       latch = new CountDownLatch(count - size)
     }
-    if (!latch.await(10, TimeUnit.SECONDS))
+    if (!latch.await(1, TimeUnit.MINUTES))
       throw new TimeoutException("Timeout reached")
   }
 }
