ab776bde09be97717a322a5944a6d2ae8d2277f5
jyuanca <jyuan@splicemachine.com>
2017-11-16 20:37:32 -0800

    DB-6373: set rowlocation for index lookup (#1219)

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/IndexRowReader.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/IndexRowReader.java
@@ -131,6 +131,7 @@ public class IndexRowReader implements Iterator<ExecRow>, Iterable<ExecRow>{
             //merge the results
             ExecRow nextScannedRow=next.getFirst();
             DataResult nextFetchedData=next.getSecond();
+            byte[] rowKey = nextFetchedData.key();
             if(entryDecoder==null)
                 entryDecoder=new EntryDecoder();
             for(DataCell kv : nextFetchedData){
@@ -138,6 +139,7 @@ public class IndexRowReader implements Iterator<ExecRow>, Iterable<ExecRow>{
                 rowDecoder.set(kv.valueArray(),kv.valueOffset(),kv.valueLength());
                 rowDecoder.decode(nextScannedRow);
             }
+            nextScannedRow.setKey(rowKey);
             heapRowToReturn=nextScannedRow;
             indexRowToReturn=nextScannedRow;
             return true;
