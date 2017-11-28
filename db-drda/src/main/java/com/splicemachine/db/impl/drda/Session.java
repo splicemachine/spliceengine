ec7dbc86964bd62525fdecf2cc4cd2272ca5b827
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2017-11-28 19:52:19 +0100

    DB-6342 Monitor/kill running operations for all nodes (#1237)

--- db-drda/src/main/java/com/splicemachine/db/impl/drda/Session.java
+++ db-drda/src/main/java/com/splicemachine/db/impl/drda/Session.java
@@ -84,6 +84,8 @@ class Session
                                                         // message if tracing fails.
         private boolean enableOutboundCompression;
 
+	private RemoteUser remoteUser;
+
 	protected boolean canCompress()
  	{
  		return enableOutboundCompression;
@@ -313,5 +315,13 @@ class Session
 		s += database.buildRuntimeInfo(indent,localLangUtil);
 		s += "\n";
 		return s;
+	}
+	
+	public RemoteUser getRemoteUser() {
+		return remoteUser;
+	}
+
+	public void setRemoteUser(RemoteUser remoteUser) {
+		this.remoteUser = remoteUser;
 	}
 }
