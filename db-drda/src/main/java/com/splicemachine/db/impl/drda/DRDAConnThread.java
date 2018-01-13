356fa7f26fb3a358651e953b3d98db80208aefb8
Changli Liu <33394239+changli6@users.noreply.github.com>
2018-01-13 08:22:32 +0800

    SPLICE-1998 Modify splice 2.5 log file's time stamp format to ISO8601 (#1349)

--- db-drda/src/main/java/com/splicemachine/db/impl/drda/DRDAConnThread.java
+++ db-drda/src/main/java/com/splicemachine/db/impl/drda/DRDAConnThread.java
@@ -48,6 +48,7 @@ import java.util.Date;
 import java.util.Properties;
 import java.util.TimeZone;
 import java.util.Vector;
+import java.text.SimpleDateFormat;
 
 import com.splicemachine.db.catalog.SystemProcedures;
 import com.splicemachine.db.iapi.error.StandardException;
@@ -205,6 +206,10 @@ class DRDAConnThread extends Thread {
     private static final byte[] nullSQLState = { ' ', ' ', ' ', ' ', ' ' };
     private static final byte[] errD4_D6 = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; // 12x0 
     private static final byte[] warn0_warnA = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };  // 11x ' '
+
+
+	// SPLICE-1998 Make splice log time stamp be compliant with Hadoop ISO8601
+	private String timeStampFormat = "yyyy-MM-dd HH:mm:ss,SSS";
 
     private final static String AUTHENTICATION_PROVIDER_BUILTIN_CLASS =
     "com.splicemachine.db.impl.jdbc.authentication.BasicAuthenticationServiceImpl";
@@ -8436,7 +8441,7 @@ class DRDAConnThread extends Thread {
      * @throws DRDAProtocolException
      */
     private void invalidClient(String prdid) throws DRDAProtocolException {
-        Monitor.logMessage(new Date()
+        Monitor.logMessage(new SimpleDateFormat(timeStampFormat).format(new Date())
                 + " : "
                 + server.localizeMessage("DRDA_InvalidClient.S",
                         new String[] { prdid }));
