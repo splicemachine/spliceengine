86819f8413f7657ccd9ab7ebf0cfd4f0a889aa74
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2018-02-01 05:47:04 -0800

    SPLICE-2008 Fix license comment

--- hbase_storage/src/main/java/com/splicemachine/si/impl/server/SimpleCompactionContext.java
+++ hbase_storage/src/main/java/com/splicemachine/si/impl/server/SimpleCompactionContext.java
@@ -8,7 +8,7 @@
  * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License aprivate AtomicLong with Splice Machine.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
  * If not, see <http://www.gnu.org/licenses/>.
  *
  */
