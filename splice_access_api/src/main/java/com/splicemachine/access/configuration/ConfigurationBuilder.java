e8d3318e785acb43c2b865131fad42424133a69c
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2018-02-05 20:46:21 -0800

    SPLICE-175 External Olap server (2.5) (#1400)
    * SPLICE-175 External Olap server
    
    * SPLICE-175 Set libraryPath for olapServer
    
    * SPLICE-175 Use hbase.master.hostname when olap server is embedded

--- splice_access_api/src/main/java/com/splicemachine/access/configuration/ConfigurationBuilder.java
+++ splice_access_api/src/main/java/com/splicemachine/access/configuration/ConfigurationBuilder.java
@@ -108,8 +108,14 @@ public class ConfigurationBuilder {
     public int olapClientWaitTime;
     public int olapClientTickTime;
     public int olapServerBindPort;
+    public String olapServerStagingDir;
+    public boolean olapServerExternal;
     public int olapServerThreads;
     public int olapServerTickLimit;
+    public int olapServerSubmitAttempts;
+    public int olapServerMemory;
+    public int olapServerMemoryOverhead;
+    public int olapServerVirtualCores;
     public int partitionserverJmxPort;
     public int partitionserverPort;
     public long broadcastRegionMbThreshold;
