88b60ad4515331574274733ef3c8d774aa18e78d
Mandar Pimpale <mpimpale@splicemachine.com>
2018-03-21 12:26:30 -0700

    DB-6636: add configurable setting to map ldap group to splice user

--- splice_machine/src/main/java/com/splicemachine/derby/impl/db/SpliceDatabase.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/db/SpliceDatabase.java
@@ -243,17 +243,21 @@ public class SpliceDatabase extends BasicDatabase{
         String authenticationLDAPSearchBase = config.getAuthenticationLdapSearchbase();
         String authenticationLDAPSearchFilter = config.getAuthenticationLdapSearchfilter();
         String authenticationLDAPServer = config.getAuthenticationLdapServer();
+        String authenticationLDAPMapGroupAttr = config.getAuthenticationLdapMapGroupAttr();
         SpliceLogUtils.info(LOG,"using LDAP to authorize Splice Machine with "+
-                        "{ldap={searchAuthDN=%s,searchBase=%s, searchFilter=%s}}",
+                        "{ldap={searchAuthDN=%s,searchBase=%s, searchFilter=%s, mapGroupAttr=%s}}",
                 authenticationLDAPSearchAuthDN,
                 authenticationLDAPSearchBase,
-                authenticationLDAPSearchFilter);
+                authenticationLDAPSearchFilter,
+                authenticationLDAPMapGroupAttr);
         System.setProperty("derby.authentication.provider", Property.AUTHENTICATION_PROVIDER_LDAP);
         System.setProperty("derby.authentication.ldap.searchAuthDN",authenticationLDAPSearchAuthDN);
         System.setProperty("derby.authentication.ldap.searchAuthPW",authenticationLDAPSearchAuthPW);
         System.setProperty("derby.authentication.ldap.searchBase",authenticationLDAPSearchBase);
         System.setProperty("derby.authentication.ldap.searchFilter",authenticationLDAPSearchFilter);
         System.setProperty("derby.authentication.server",authenticationLDAPServer);
+        System.setProperty("derby.authentication.ldap.mapGroupAttr",authenticationLDAPMapGroupAttr);
+
     }
 
     private void configureCustomAuth(SConfiguration configuration){
