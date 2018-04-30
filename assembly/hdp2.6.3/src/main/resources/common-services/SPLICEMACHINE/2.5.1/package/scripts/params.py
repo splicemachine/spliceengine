de2b57785e7c82cd763513ead1826ddacd916bbe
John Leach <jleach@splicemachine.com>
2018-04-30 11:25:15 -0500

    DB-6979 Automate Install Process: easier than all the possible issues manually editing config files

--- assembly/hdp2.6.3/src/main/resources/common-services/SPLICEMACHINE/2.5.1/package/scripts/params.py
+++ assembly/hdp2.6.3/src/main/resources/common-services/SPLICEMACHINE/2.5.1/package/scripts/params.py
@@ -18,9 +18,14 @@ Ambari Agent
 import os
 import re
 from resource_management import *
+from resource_management.libraries.functions import format
 from resource_management.libraries.functions.default import default
-#from resource_management.libraries.functions.version import format_hdp_stack_version
 from resource_management.libraries.script.script import Script
+from resource_management.libraries.functions import stack_select
+from resource_management.libraries.functions import conf_select
+from resource_management.libraries.resources.hdfs_resource import HdfsResource
+from resource_management.libraries.functions import get_kinit_path
+from resource_management.libraries.functions.get_not_managed_resources import get_not_managed_resources
 
 # server configurations
 config = Script.get_config()
@@ -45,3 +50,34 @@ stack_version_unformatted = str(config['hostLevelParams']['stack_version'])
 # e.g. 2.3.0.0-2130
 full_version = default("/commandParams/version", None)
 hdp_version = full_version
+
+hdfs_site = config['configurations']['hdfs-site']
+default_fs = config['configurations']['core-site']['fs.defaultFS']
+hdfs_user_keytab = config['configurations']['hadoop-env']['hdfs_user_keytab']
+hdfs_user = config['configurations']['hadoop-env']['hdfs_user']
+hdfs_principal_name = config['configurations']['hadoop-env']['hdfs_principal_name']
+hadoop_bin_dir = stack_select.get_hadoop_dir("bin")
+hadoop_conf_dir = conf_select.get_hadoop_conf_dir()
+security_enabled = config['configurations']['cluster-env']['security_enabled']
+kinit_path_local = get_kinit_path(default('/configurations/kerberos-env/executable_search_paths', None))
+dfs_type = default("/commandParams/dfs_type", "")
+
+
+import functools
+#create partial functions with common arguments for every HdfsResource call
+#to create/delete hdfs directory/file/copyfromlocal we need to call params.HdfsResource in code
+HdfsResource = functools.partial(
+        HdfsResource,
+        user=hdfs_user,
+        hdfs_resource_ignore_file = "/var/lib/ambari-agent/data/.hdfs_resource_ignore",
+        security_enabled = security_enabled,
+        keytab = hdfs_user_keytab,
+        kinit_path_local = kinit_path_local,
+        hadoop_bin_dir = hadoop_bin_dir,
+        hadoop_conf_dir = hadoop_conf_dir,
+        principal_name = hdfs_principal_name,
+        hdfs_site = hdfs_site,
+        default_fs = default_fs,
+        immutable_paths = get_not_managed_resources(),
+        dfs_type = dfs_type
+)
