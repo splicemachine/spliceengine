4ea49ee6fbcff5676531d64d99ed72732cd5e886
Murray Brown <mbrown@splicemachine.com>
2018-05-03 16:32:55 -0700

    DB-6986: hide user and password from process commandline

--- assembly/common/src/main/resources/bin/sqlshell.sh
+++ assembly/common/src/main/resources/bin/sqlshell.sh
@@ -212,7 +212,8 @@ else
         KERBEROS+=";keytab=${KEYTAB}"
       fi
    fi
-   IJ_SYS_ARGS+=" -Dij.connection.splice=jdbc:splice://${HOST}:${PORT}/splicedb;user=${USER};password=${PASS}${SSL}${KERBEROS}"
+   IJ_SYS_ARGS+=" -Dij.connection.splice=jdbc:splice://${HOST}:${PORT}/splicedb${SSL}${KERBEROS}"
+   export JAVA_TOOL_OPTIONS="-Dij.user=$USER -Dij.password=$PASS"
 fi
 
 
@@ -233,4 +234,4 @@ fi
 
 message "Running Splice Machine SQL shell"
 message "For help: \"splice> help;\""
-${RLWRAP} java ${GEN_SYS_ARGS} ${SSL_ARGS} ${IJ_SYS_ARGS}  com.splicemachine.db.tools.ij ${SCRIPT}
+${RLWRAP} java ${GEN_SYS_ARGS} ${SSL_ARGS} ${IJ_SYS_ARGS}  com.splicemachine.db.tools.ij ${SCRIPT} 2> >(grep -v "^Picked up JAVA_TOOL_OPTIONS:" >&2)
