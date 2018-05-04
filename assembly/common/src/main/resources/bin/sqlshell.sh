26badb7af4c71cd5c9a9373852df363ac38d45c5
Murray Brown <mbrown@splicemachine.com>
2018-05-03 21:21:20 -0700

    Whitespace

--- assembly/common/src/main/resources/bin/sqlshell.sh
+++ assembly/common/src/main/resources/bin/sqlshell.sh
@@ -23,81 +23,81 @@ message() {
 }
 
 show_help() {
-        echo "Splice Machine SQL client wrapper script"
-        echo "Usage: $(basename $BASH_SOURCE) [-U url|-h host] [-p port] [-u username] [-s password] [-S] [-P] [-w width] [-f scriptfile] [-o output] [-q]"
-        echo -e "\t-U full JDBC URL for Splice Machine database"
-        echo -e "\t-h IP address or hostname of Splice Machine (HBase RegionServer)"
-        echo -e "\t-p Port which Splice Machine is listening on, defaults to 1527"
-        echo -e "\t-u username for Splice Machine database"
-        echo -e "\t-s password for Splice Machine database"
-        echo -e "\t-S use ssl=basic on connection"
-        echo -e "\t-P prompt for password"
-        echo -e "\t-w width of output lines. defaults to 128"
-        echo -e "\t-f sql file to be executed"
-        echo -e "\t-o file for output"
-        echo -e "\t-q quiet mode"
+   echo "Splice Machine SQL client wrapper script"
+   echo "Usage: $(basename $BASH_SOURCE) [-U url|-h host] [-p port] [-u username] [-s password] [-S] [-P] [-w width] [-f scriptfile] [-o output] [-q]"
+   echo -e "\t-U full JDBC URL for Splice Machine database"
+   echo -e "\t-h IP address or hostname of Splice Machine (HBase RegionServer)"
+   echo -e "\t-p Port which Splice Machine is listening on, defaults to 1527"
+   echo -e "\t-u username for Splice Machine database"
+   echo -e "\t-s password for Splice Machine database"
+   echo -e "\t-S use ssl=basic on connection"
+   echo -e "\t-P prompt for password"
+   echo -e "\t-w width of output lines. defaults to 128"
+   echo -e "\t-f sql file to be executed"
+   echo -e "\t-o file for output"
+   echo -e "\t-q quiet mode"
 }
 
 # Process command line args
 while getopts "U:h:p:u:s:SPw:f:o:q" opt; do
-    case $opt in
-        U)
-                URL="${OPTARG}"
-                ;;
-        h)
-                HOST="${OPTARG}"
-                ;;
-        p)
-                PORT="${OPTARG}"
-                ;;
-        u)
-                USER="${OPTARG}"
-                ;;
-        s)
-                PASS="${OPTARG}"
-                ;;
-        S)
-                SECURE=1
-                ;;
-        P)
-                PROMPT=1
-                ;;
-        w)
-                WIDTH="${OPTARG}"
-                ;;
-        f)
-                SCRIPT="${OPTARG}"
-                ;;
-        o)
-                OUTPUT="${OPTARG}"
-                ;;
-        q)
-                QUIET=1
-                ;;
-        \?)
-                show_help
-                exit 1
-                ;;
-    esac
+   case $opt in
+      U)
+         URL="${OPTARG}"
+         ;;
+      h)
+         HOST="${OPTARG}"
+         ;;
+      p)
+         PORT="${OPTARG}"
+         ;;
+      u)
+         USER="${OPTARG}"
+         ;;
+      s)
+         PASS="${OPTARG}"
+         ;;
+      S)
+         SECURE=1
+         ;;
+      P)
+         PROMPT=1
+         ;;
+      w)
+         WIDTH="${OPTARG}"
+         ;;
+      f)
+         SCRIPT="${OPTARG}"
+         ;;
+      o)
+         OUTPUT="${OPTARG}"
+         ;;
+      q)
+         QUIET=1
+         ;;
+      \?)
+         show_help
+         exit 1
+         ;;
+   esac
 done
 
 # check for jdk1.8 and exit if not found
 if ( type -p java >/dev/null); then
-    _java=java
+   _java=java
 elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    _java="$JAVA_HOME/bin/java"
+   _java="$JAVA_HOME/bin/java"
 else
-    echo "Error: no java found. $0 requires java."
-    show_help
-    exit 1
+   echo "Error: no java found. $0 requires java."
+   show_help
+   exit 1
 fi
 
 jversion=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
 message "java version ${jversion}\n"
 if [[ "$jversion" < "1.8" ]]; then
-    echo "Error: java is older than 1.8.  $0 requires java 1.8"
-    show_help
-    exit 1
+   echo "Error: java is older than 1.8.  $0 requires java 1.8"
+   show_help
+   exit 1
 fi
 
 # set hbase lib dir here to keep it in one place.
@@ -142,11 +142,11 @@ else
 fi
 
 if hash rlwrap 2>/dev/null; then
-    message "\n ========= rlwrap detected and enabled.  Use up and down arrow keys to scroll through command line history. ======== \n"
-    RLWRAP=rlwrap
+   message "\n ========= rlwrap detected and enabled.  Use up and down arrow keys to scroll through command line history. ======== \n"
+   RLWRAP=rlwrap
 else
-    message "\n ========= rlwrap not detected.  Consider installing for command line history capabilities. ========= \n"
-    RLWRAP=
+   message "\n ========= rlwrap not detected.  Consider installing for command line history capabilities. ========= \n"
+   RLWRAP=
 fi
 
 message "Running Splice Machine SQL shell"
