7a69b7d20cd28cdc4e7835e0b74f2d0d756c6bf4
perryluopl <35046794+perryluopl@users.noreply.github.com>
2018-05-04 21:53:18 -0700

    DB-6923 pom file to build stand alone version (2.5) (#1778)

---
+++ assembly/standalone/template/bin/clean.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+source ${BASE_DIR}/bin/functions.sh
+
+DB_DIR="${BASE_DIR}"/db
+
+# Clean the Splice Machine database
+
+# if server still running, fail - must stop first
+SPID=$(ps -ef | awk '/SpliceTestPlatform|SpliceSinglePlatform|SpliceTestClusterParticipant/ && !/awk/ {print $2}')
+ZPID=$(ps -ef | awk '/zoo/ && !/awk/ {print $2}')
+YPID=$(ps -ef | awk '/spliceYarn|SpliceTestYarnPlatform|CoarseGrainedScheduler|ExecutorLauncher/ && !/awk/ {print $2}')
+KPID=$(ps -ef | awk '/TestKafkaCluster/ && !/awk/ {print $2}')
+if [[ -n ${SPID} || -n ${ZPID} ]] || [[ -n ${YPID} || -n ${KPID} ]]; then
+     echo "Splice still running and must be shut down. Run stopdb.sh"
+     exit 1;
+fi
+
+echo "Cleaning the Splice Machine database..."
+
+/bin/rm -rf "${BASE_DIR}"/log/target/SpliceTestYarnPlatform
+/bin/rm -rf "${DB_DIR}"/zookeeper
+/bin/rm -rf "${DB_DIR}"/hbase
+# TODO do I have to do this?
+# /bin/rm -rf "${BASE_DIR}"/lib/yarn-site.xml
+
+echo "done."
