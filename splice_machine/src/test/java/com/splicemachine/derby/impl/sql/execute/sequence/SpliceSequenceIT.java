d85e74bd2f085790f6494a3abd288482b4a379e0
John Leach <jleach@splicemachine.com>
2018-05-16 18:23:34 -0500

    SPLICE-2171 Issue Granting Privileges to Sequences (#1846)

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/sequence/SpliceSequenceIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/sequence/SpliceSequenceIT.java
@@ -14,16 +14,18 @@
 
 package com.splicemachine.derby.impl.sql.execute.sequence;
 
-import com.splicemachine.derby.test.framework.SpliceDataWatcher;
-import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
-import com.splicemachine.derby.test.framework.SpliceTableWatcher;
-import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.derby.test.framework.*;
+import org.junit.Assert;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -31,6 +33,12 @@ import static org.junit.Assert.assertTrue;
 public class SpliceSequenceIT {
 
     private static final String SCHEMA = SpliceSequenceIT.class.getSimpleName().toUpperCase();
+    private static final String USER = SCHEMA+"_USER";
+    private static final String PASSWORD = "password";
+    private static final String SEQUENCE = SCHEMA+"."+"testseq";
+
+
+
     private static SpliceWatcher spliceClassWatcher=new SpliceWatcher();
 
     @ClassRule
@@ -40,11 +48,13 @@ public class SpliceSequenceIT {
     public SpliceWatcher methodWatcher = new SpliceWatcher(SCHEMA);
 
     private static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher("FOO",SCHEMA,"(col1 bigint)");
+    private static SpliceUserWatcher spliceUserWatcher = new SpliceUserWatcher(USER,PASSWORD);
 
     @ClassRule
     public static TestRule chain= RuleChain.outerRule(spliceClassWatcher)
             .around(spliceSchemaWatcher)
             .around(spliceTableWatcher)
+            .around(spliceUserWatcher)
             .around(new SpliceDataWatcher() {
                 @Override
                 protected void starting(Description description) {
@@ -74,86 +84,18 @@ public class SpliceSequenceIT {
 
         methodWatcher.executeUpdate("drop sequence SMALLSEQ restrict");
     }
-/*
-    @Test
-    public void testSparkSequenceGenerationWithCreateAndDrops() throws Exception {
-        for (int i=0;i<10;i++){
-            methodWatcher.executeUpdate("create sequence FOOSEQ");
-            Integer first = methodWatcher.query("select next value for FOOSEQ from foo");
-            assertEquals(first + 1, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 2, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 3, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 4, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 5, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 6, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 1000, methodWatcher.query("select next value for FOOSEQ from foo --splice-properties useSpark=true"));
-            // Spark by default allocates a 1K increment in the spark memory space
-            // the next OLTP query will go to the table to get the sequence value which should start after the allocated entry...
-            // from Spark.  Since Spark and Control run in different memory spaces, this is testing
-            // whether they allocate their correct amounts...
-            assertEquals(first + 7, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 8, methodWatcher.query("select next value for FOOSEQ from foo"));
-            assertEquals(first + 9, methodWatcher.query("select next value for FOOSEQ from foo"));
-            // Verifying the Spark Buffer is still available.  This test would not work if there are several spark nodes.
-            assertEquals(first + 1001, methodWatcher.query("select next value for FOOSEQ from foo --splice-properties useSpark=true"));
-            assertEquals(first + 1002, methodWatcher.query("select next value for FOOSEQ from foo --splice-properties useSpark=true"));
-            methodWatcher.executeUpdate("drop sequence FOOSEQ restrict");
-        }
-    }
 
     @Test
-    public void testSparkSequenceGenerationWithCreateAndDropsStartsWith10000 () throws Exception {
-        for (int i=0;i<10;i++){
-            methodWatcher.executeUpdate("create sequence FOOSEQ2 start with 10000");
-            Integer first = methodWatcher.query("select next value for FOOSEQ2 from foo");
-            assertEquals(first,new Integer(10000));
-            assertEquals(first + 1, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 2, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 3, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 4, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 5, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 6, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 1000, methodWatcher.query("select next value for FOOSEQ2 from foo --splice-properties useSpark=true"));
-            // Spark by default allocates a 1K increment in the spark memory space
-            // the next OLTP query will go to the table to get the sequence value which should start after the allocated entry...
-            // from Spark.  Since Spark and Control run in different memory spaces, this is testing
-            // whether they allocate their correct amounts...
-            assertEquals(first + 7, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 8, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            assertEquals(first + 9, methodWatcher.query("select next value for FOOSEQ2 from foo"));
-            // Verifying the Spark Buffer is still available.  This test would not work if there are several spark nodes.
-            assertEquals(first + 1001, methodWatcher.query("select next value for FOOSEQ2 from foo --splice-properties useSpark=true"));
-            assertEquals(first + 1002, methodWatcher.query("select next value for FOOSEQ2 from foo --splice-properties useSpark=true"));
-            methodWatcher.executeUpdate("drop sequence FOOSEQ2 restrict");
-        }
+    public void testGrantPrivileges() throws Exception {
+        try {methodWatcher.executeUpdate("drop sequence "+SEQUENCE + " restrict");} catch (Exception e) {}
+        methodWatcher.executeUpdate("create sequence "+SEQUENCE);
+        methodWatcher.executeUpdate(String.format("grant usage on SEQUENCE %s to %s",SEQUENCE,USER));
+        Connection connection = methodWatcher.createConnection(USER,PASSWORD);
+        ResultSet rs = connection.prepareStatement("values (next value for "+SEQUENCE+")").executeQuery();
+        assertTrue(rs.next());
+        assertEquals(-2147483648,rs.getInt(1));
+        rs.close();
+        connection.close();
     }
-
-    @Test
-    public void testSparkSequenceGenerationWithCreateAndDropsStartsWith10000IncrementsBy10 () throws Exception {
-        for (int i=0;i<10;i++){
-            methodWatcher.executeUpdate("create sequence FOOSEQ3 start with 10000 increment by 10");
-            Integer first = methodWatcher.query("select next value for FOOSEQ3 from foo");
-            assertEquals(first,new Integer(10000));
-            assertEquals(first + 10, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 20, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 30, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 40, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 50, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 60, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 1000, methodWatcher.query("select next value for FOOSEQ3 from foo --splice-properties useSpark=true"));
-            // Spark by default allocates a 1K increment in the spark memory space
-            // the next OLTP query will go to the table to get the sequence value which should start after the allocated entry...
-            // from Spark.  Since Spark and Control run in different memory spaces, this is testing
-            // whether they allocate their correct amounts...
-            assertEquals(first + 70, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 80, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            assertEquals(first + 90, methodWatcher.query("select next value for FOOSEQ3 from foo"));
-            // Verifying the Spark Buffer is still available.  This test would not work if there are several spark nodes.
-            assertEquals(first + 1010, methodWatcher.query("select next value for FOOSEQ3 from foo --splice-properties useSpark=true"));
-            assertEquals(first + 1020, methodWatcher.query("select next value for FOOSEQ3 from foo --splice-properties useSpark=true"));
-            methodWatcher.executeUpdate("drop sequence FOOSEQ3 restrict");
-        }
-    }
-*/
 
 }
