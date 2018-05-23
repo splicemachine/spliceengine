ce0752e78462b34cd97013b92595d82790661e86
Bin Wang <wbin00@gmail.com>
2018-05-23 08:17:39 +0800

    DB-6954 Fix starting kafka cluster (#1802)
    * DB-6954 Fix starting TestKafkaCluster
    
    * DB-6954 Clean up TestKafkaCluster.java

--- hbase_sql/src/test/java/com/splicemachine/kafka/TestKafkaCluster.java
+++ hbase_sql/src/test/java/com/splicemachine/kafka/TestKafkaCluster.java
@@ -14,39 +14,29 @@
 
 package com.splicemachine.kafka;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 import kafka.server.KafkaConfig;
 import kafka.server.KafkaServerStartable;
-import kafka.utils.TestUtils;
-import org.apache.kafka.common.protocol.SecurityProtocol;
-import scala.Option;
 
 public class TestKafkaCluster {
-    KafkaServerStartable kafkaServer;
+    private KafkaServerStartable kafkaServer;
 
-    public TestKafkaCluster(String connectString) throws Exception {
+    public TestKafkaCluster(String connectString) {
         KafkaConfig config = getKafkaConfig(connectString);
         kafkaServer = new KafkaServerStartable(config);
         kafkaServer.startup();
     }
 
     public static void main(String [] args) throws Exception {
-        TestKafkaCluster cluster;
         if (args.length==1)
-            cluster = new TestKafkaCluster(args[0]);
+            new TestKafkaCluster(args[0]);
         else
             throw new RuntimeException("No zookeper local");
     }
 
     private static KafkaConfig getKafkaConfig(final String zkConnectString) {
-        final Option<File> noFile = scala.Option.apply(null);
-        final Option<SecurityProtocol> noInterBrokerSecurityProtocol = scala.Option.apply(null);
-        scala.collection.Iterator<Properties> propsI =
-                TestUtils.createBrokerConfigs(1, zkConnectString, true, false, noInterBrokerSecurityProtocol, noFile, null, false, false, false, true, null).iterator();
-        assert propsI.hasNext();
-        Properties props = propsI.next();
+        Properties props = new Properties();
         assert props.containsKey("zookeeper.connect");
         props.put("zookeeper.connect", zkConnectString);
         props.put("port","9092");
