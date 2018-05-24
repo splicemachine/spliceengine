b606308fc9e6cd7376df738abcd4a9f38dc2bb5f
Changli Liu <cliu@splicemachine.com>
2018-05-24 23:46:39 +0800

    SPLICE-1743 Check snapshot name validity

--- db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
+++ db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
@@ -2005,6 +2005,7 @@ public interface SQLState {
 	String ACCESSING_S3_EXCEPTION                                   = "EXT34";
 	String SNAPSHOT_EXISTS											= "SNA01";
 	String SNAPSHOT_NOT_EXISTS										= "SNA02";
+	String SNAPSHOT_NAME_ILLEGAL									= "SNA03";
 
     String SPLITKEY_EQUALS_STARTKEY                                 = "TS001";
 	String NO_PRIMARY_KEY											= "TS002";
