f86e70e4356b43028a109a65569a888693d22cfe
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2018-06-14 11:00:40 +0200

    SPLICE-2185 Restore cleanupOnError behaviour

--- db-engine/src/main/java/com/splicemachine/db/impl/jdbc/TransactionResourceImpl.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/jdbc/TransactionResourceImpl.java
@@ -521,7 +521,9 @@ public final class TransactionResourceImpl
 			SanityManager.ASSERT(cm != null, "cannot cleanup on error with null context manager");
 
         //DERBY-4856 thread dump
-        return cm.cleanupOnError(e, diagActive);
+        boolean result = cm.cleanupOnError(e, diagActive);
+        csf.removeContext(cm);
+        return result;
 	}
 
 	boolean isIdle()
