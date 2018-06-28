a03b3606bc733d2e8d7e2fe1669bcb5b24af51a7
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2018-06-28 12:22:06 +0200

    SPLICE-2131 More bytes for random name and append timestamp
    Address review by reducing chance of collision in directory name

--- splice_spark/src/main/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
+++ splice_spark/src/main/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
@@ -259,9 +259,9 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
   }
 
   def getRandomName(): String = {
-    val name = new Array[Byte](20)
+    val name = new Array[Byte](32)
     new SecureRandom().nextBytes(name)
-    Bytes.toHex(name)
+    Bytes.toHex(name)+"-"+System.nanoTime()
   }
 
   /**
