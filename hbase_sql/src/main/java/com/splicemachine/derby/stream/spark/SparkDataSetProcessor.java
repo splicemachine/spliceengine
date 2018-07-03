0da2887bc2c296bf32972a90384df115d25eeae6
Changli Liu <cliu@splicemachine.com>
2018-07-03 20:29:22 +0800

    DB-6809 External textfile table support TERMINATED ESCAPED charater

--- hbase_sql/src/main/java/com/splicemachine/derby/stream/spark/SparkDataSetProcessor.java
+++ hbase_sql/src/main/java/com/splicemachine/derby/stream/spark/SparkDataSetProcessor.java
@@ -562,8 +562,14 @@ public class SparkDataSetProcessor implements DistributedDataSetProcessor, Seria
         try {
             Dataset<Row> table = null;
             try {
+                HashMap<String, String> options = new HashMap<String, String>();
+
                 // spark-2.2.0: commons-lang3-3.3.2 does not support 'XXX' timezone, specify 'ZZ' instead
-                table = SpliceSpark.getSession().read().option("timestampFormat", "yyyy-MM-dd'T'HH:mm:ss.SSSZZ").csv(location);
+                options.put("timestampFormat","yyyy-MM-dd'T'HH:mm:ss.SSSZZ");
+                options.put("escape", characterDelimiter);
+                options.put("sep", columnDelimiter);
+
+                table = SpliceSpark.getSession().read().options(options).csv(location);
 
                 if (op == null) {
                     // stats collection scan
