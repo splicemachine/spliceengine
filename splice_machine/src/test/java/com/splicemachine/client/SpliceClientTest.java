d588ac1d02fa8bd632a8dcd99fb32ae7cd180297
Bin Wang <wbin00@gmail.com>
2018-08-09 23:43:07 +0800

    DB-7297: Remove password from JDBC url in order to not log it (#2152)
    * DB-7297: Remove password from JDBC url in order to not log it
    
    * cleanup code
    
    * Fix testing jdbc url

--- splice_machine/src/test/java/com/splicemachine/client/SpliceClientTest.java
+++ splice_machine/src/test/java/com/splicemachine/client/SpliceClientTest.java
@@ -1,5 +1,6 @@
 package com.splicemachine.client;
 
+import com.splicemachine.db.client.am.SqlException;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,17 +8,12 @@ import static org.junit.Assert.*;
 public class SpliceClientTest {
 
     @Test
-    public void testMaskJDBCPassword() {
-        String raw = "No suitable driver found for jdbc:splice://dwbdtest1r1w3.wellpoint" +
-                ".com:1527/splicedb;user=af29891;password=XXXXX";
-        String masked = "No suitable driver found for jdbc:splice://dwbdtest1r1w3.wellpoint" +
-                ".com:1527/splicedb;user=af29891;password=[Omitted]";
-        assertEquals(masked, SpliceClient.maskJDBCPassword(raw));
+    public void testParseJDBCPassword() throws SqlException {
+        String password = "Abd98*@80EFg";
+        String raw = "jdbc:splice://localhost.com:1527/splicedb;user=af29891;password=" + password;
+        assertEquals(password, SpliceClient.parseJDBCPassword(raw));
 
-        raw = "No suitable driver found for jdbc:splice://dwbdtest1r1w3.wellpoint" +
-                ".com:1527/splicedb;password=XXXXX;user=af29891";
-        masked = "No suitable driver found for jdbc:splice://dwbdtest1r1w3.wellpoint" +
-                ".com:1527/splicedb;password=[Omitted];user=af29891";
-        assertEquals(masked, SpliceClient.maskJDBCPassword(raw));
+        raw = "jdbc:splice://localhost:1527/splicedb;password=" + password + ";user=af29891";
+        assertEquals(password, SpliceClient.parseJDBCPassword(raw));
     }
 }
