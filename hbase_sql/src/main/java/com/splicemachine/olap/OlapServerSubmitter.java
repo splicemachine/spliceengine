dde370f2a1b26b4db287efd29f3d854019f3b831
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2018-08-09 20:10:14 +0200

    DB-6417 Full standalone cluster: KDC, HDFS, HBase, Yarn, Spark (2.5) (#2133)
    * DB-6417 Full standalone cluster: KDC, HDFS, HBase, Yarn, Spark (#1792)
    
    * DB-6417 Start HBase with the current user

--- hbase_sql/src/main/java/com/splicemachine/olap/OlapServerSubmitter.java
+++ hbase_sql/src/main/java/com/splicemachine/olap/OlapServerSubmitter.java
@@ -66,6 +66,8 @@ import scala.runtime.BoxedUnit;
 import java.io.File;
 import java.io.IOException;
 import java.net.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
 import java.util.Collections;
 import java.util.HashMap;
