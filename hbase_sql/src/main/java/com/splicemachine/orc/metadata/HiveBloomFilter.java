1f1e869ca3312567480d7896539fc03e43d10588
Oleg Mazurov <omazurov@splicemachine.com>
2018-08-16 13:28:55 -0700

    Removed unwanted dependency on hive-common-1.2.1.spark2

--- hbase_sql/src/main/java/com/splicemachine/orc/metadata/HiveBloomFilter.java
+++ hbase_sql/src/main/java/com/splicemachine/orc/metadata/HiveBloomFilter.java
@@ -13,25 +13,13 @@
  */
 package com.splicemachine.orc.metadata;
 
-import com.google.common.primitives.Longs;
-import org.apache.hive.common.util.BloomFilter;
-
 import java.util.List;
 
-public class HiveBloomFilter extends BloomFilter
+public class HiveBloomFilter
 {
     // constructor that allows deserialization of a long list into the actual hive bloom filter
     public HiveBloomFilter(List<Long> bits, int numBits, int numHashFunctions)
     {
-        this.bitSet = new BitSet(Longs.toArray(bits));
-        this.numBits = numBits;
-        this.numHashFunctions = numHashFunctions;
-    }
-
-    public HiveBloomFilter(BloomFilter bloomFilter)
-    {
-        this.bitSet = new BitSet(bloomFilter.getBitSet().clone());
-        this.numBits = bloomFilter.getBitSize();
-        this.numHashFunctions = bloomFilter.getNumHashFunctions();
+        // This is a stub
     }
 }
