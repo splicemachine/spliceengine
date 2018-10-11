660ea524d080993a7ed8a72d9a3f3538dc541df0
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2018-10-11 19:17:36 +0200

    DB-7541 Add export_binary operation and use it for internalDf

--- db-engine/src/main/java/com/splicemachine/db/impl/ast/AbstractSpliceVisitor.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/ast/AbstractSpliceVisitor.java
@@ -851,6 +851,11 @@ public abstract class AbstractSpliceVisitor implements ISpliceVisitor {
         return defaultVisit(node);
     }
 
+    @Override
+    public Visitable visit(BinaryExportNode node) throws StandardException {
+        return defaultVisit(node);
+    }
+
 	@Override
 	public Visitable visit(OrderByNode node) throws StandardException {
         return defaultVisit(node);
