0b1e9dc04da81a03b31ae068899fc9cb03d55929
jyuanca <jyuan@splicemachine.com>
2018-10-11 19:24:09 -0700

    DB-7472: fix a race condition in TableDescriptor(2.7) (#2295)

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/catalog/DataDictionaryImpl.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/catalog/DataDictionaryImpl.java
@@ -1433,13 +1433,13 @@ public abstract class DataDictionaryImpl extends BaseDataDictionary{
     /**
      * This method can get called from the DataDictionary cache.
      *
-     * @param tableKey The TableKey of the table
+     * @param tableId The UUID of the table
      * @return The descriptor for the table, null if the table does
      * not exist.
      * @throws StandardException Thrown on failure
      */
-    TableDescriptor getUncachedTableDescriptor(TableKey tableKey) throws StandardException{
-        return getTableDescriptorIndex1Scan(tableKey.getTableName(),tableKey.getSchemaId().toString());
+    public TableDescriptor getUncachedTableDescriptor(UUID tableId) throws StandardException{
+        return getTableDescriptorIndex2Scan(tableId.toString());
     }
 
     /**
@@ -1522,7 +1522,8 @@ public abstract class DataDictionaryImpl extends BaseDataDictionary{
                 getColumnDescriptorsScan(td);
                 getConglomerateDescriptorsScan(td);
                 markSystemTablesAsVersion1(td);
-            }
+                td.getHeapConglomerateId(); // populate heapConglomerateId
+             }
         }
 
         return td;
