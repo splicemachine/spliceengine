0bbc673dd3340acf954ac8fb71f9b18de27a5696
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2018-10-31 11:05:10 +0100

    DB-7566 Broadcast credentials for Spark adapter once per app (#2368)
    * DB-7566 Broadcast credentials for Spark adapter once per app
    
    * DB-7566 Apply changes to Spark 2.3

--- splice_spark/src/main/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
+++ splice_spark/src/main/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
@@ -49,6 +49,17 @@ import org.apache.spark.scheduler.{SparkListener, SparkListenerApplicationEnd}
 
 object Holder extends Serializable {
   @transient lazy val log = Logger.getLogger(getClass.getName)
+
+  var broadcastCredentials: Broadcast[SerializableWritable[Credentials]] = null
+
+  def broadcastCreds(credentials: Credentials) : Unit = this.synchronized {
+    if (broadcastCredentials != null)
+      return
+
+      SpliceSpark.logCredentialsInformation(credentials)
+    broadcastCredentials = SpliceSpark.getContext.broadcast(new SerializableWritable(credentials))
+    SpliceSpark.setCredentials(broadcastCredentials)
+  }
 }
 
 class SplicemachineContext(options: Map[String, String]) extends Serializable {
@@ -59,7 +70,6 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
   }
 
   @transient var credentials = UserGroupInformation.getCurrentUser().getCredentials()
-  var broadcastCredentials: Broadcast[SerializableWritable[Credentials]] = null
   JdbcDialects.registerDialect(new SplicemachineDialect)
 
   private[this] def initConnection() = {
@@ -110,8 +120,7 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
 
           // Add it to credentials and broadcast them
           credentials.addToken(getUniqueAlias(token), token)
-          broadcastCreds
-          SpliceSpark.setCredentials(broadcastCredentials)
+          Holder.broadcastCreds(credentials)
 
           Holder.log.debug(f"Broadcasted credentials")
 
@@ -123,11 +132,6 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
 
       initConnection()
     }
-  }
-
-  def broadcastCreds = {
-    SpliceSpark.logCredentialsInformation(credentials)
-    broadcastCredentials = SpliceSpark.getContext.broadcast(new SerializableWritable(credentials))
   }
 
   def tableExists(schemaTableName: String): Boolean = {
