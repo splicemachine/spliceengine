e7b05d04ad1258097fb383f066e6b7b60da0f8db
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2018-11-22 00:35:33 +0100

    DB-7641 Rollforward OLTP writes after delay (#2423)

--- pipeline_api/src/main/java/com/splicemachine/pipeline/config/ForwardingWriteConfiguration.java
+++ pipeline_api/src/main/java/com/splicemachine/pipeline/config/ForwardingWriteConfiguration.java
@@ -58,6 +58,11 @@ public class ForwardingWriteConfiguration implements WriteConfiguration {
     }
 
     @Override
+    public boolean rollForward() {
+        return delegate.rollForward();
+    }
+
+    @Override
     public int getMaximumRetries() {
         return delegate.getMaximumRetries();
     }
