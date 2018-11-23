b3f136e97d84acb2b86a49e03df106a59ed12e2e
Changli Liu <cliu@splicemachine.com>
2018-11-23 21:58:22 +0800

    SPLICE-1958 Bind column table name in UpdateNode and add new IT

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/UpdateOperationIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/UpdateOperationIT.java
@@ -261,6 +261,15 @@ public class UpdateOperationIT {
                         "-------------\n" +
                         " 101 |94114 |",
                 TestUtils.FormattedResult.ResultFactory.toString(rs));
+
+        updated = methodWatcher.getStatement().executeUpdate("update b1 X set (num, zip) =(select LOCATION.num+2, LOCATION.zip from LOCATION where LOCATION.zip = X.zip)");
+        assertEquals("Incorrect num rows updated!", 1, updated);
+        rs = methodWatcher.executeQuery("select num, zip from b1");
+        assertEquals("" +
+                        "NUM | ZIP  |\n" +
+                        "-------------\n" +
+                        " 102 |94114 |",
+                TestUtils.FormattedResult.ResultFactory.toString(rs));
     }
 
     /* Regression test for Bug 682. */
