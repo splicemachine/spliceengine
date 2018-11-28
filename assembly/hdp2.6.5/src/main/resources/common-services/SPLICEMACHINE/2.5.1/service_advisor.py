de9dbdc702a16c17db212a735e4c0a9262015f8f
Brandon Lo <brandonklo94@gmail.com>
2018-11-28 14:10:29 -0800

    DB-7645 Add hdp2.6.5 support

--- assembly/hdp2.6.5/src/main/resources/common-services/SPLICEMACHINE/2.5.1/service_advisor.py
+++ assembly/hdp2.6.5/src/main/resources/common-services/SPLICEMACHINE/2.5.1/service_advisor.py
@@ -54,17 +54,21 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
         hbase_env = services["configurations"]["hbase-env"]["properties"]
         if "content" in hbase_env:
           content = hbase_env["content"]
-          HBASE_CLASSPATH_PREFIX = "export HBASE_CLASSPATH_PREFIX=/var/lib/splicemachine/*:/usr/hdp/2.6.3.0-235/spark2/jars/*"
+          HBASE_CLASSPATH_PREFIX = "export HBASE_CLASSPATH_PREFIX=/var/lib/splicemachine/*:/usr/hdp/2.6.3.0-235/spark2/jars/*:/usr/hdp/2.6.3.0-235/hadoop/lib/ranger-hdfs-plugin-impl/*"
           HBASE_MASTER_OPTS = "export HBASE_MASTER_OPTS=\"${HBASE_MASTER_OPTS} -D"+ " -D".join(self.getMasterDashDProperties()) + "\""
           HBASE_REGIONSERVER_OPTS = "export HBASE_REGIONSERVER_OPTS=\"${HBASE_REGIONSERVER_OPTS} -D"+ " -D".join(self.getRegionServerDashDProperties()) + "\""
+          HBASE_CONF_DIR = "export HBASE_CONF_DIR=${HBASE_CONF_DIR}:/etc/splicemachine/conf/"
+
           if "splicemachine" not in content:
             print "Updating Hbase Env Items"
             HBASE_CLASSPATH_PREFIX = "#Add Splice Jars to HBASE_PREFIX_CLASSPATH\n" + HBASE_CLASSPATH_PREFIX
             HBASE_MASTER_OPTS = "#Add Splice Specific Information to HBase Master\n" + HBASE_MASTER_OPTS
             HBASE_REGIONSERVER_OPTS = "#Add Splice Specific Information to Region Server\n" + HBASE_REGIONSERVER_OPTS
+            HBASE_CONF_DIR = "#Add Splice Specific Information to Region Server\n" + HBASE_CONF_DIR
             content = "\n\n".join((content, HBASE_CLASSPATH_PREFIX))
             content = "\n\n".join((content, HBASE_MASTER_OPTS))
             content = "\n\n".join((content, HBASE_REGIONSERVER_OPTS))
+            content = "\n\n".join((content, HBASE_CONF_DIR))
             print "content: " + content
             putHbaseEnvProperty = self.putProperty(configurations, "hbase-env", services)
             putHbaseEnvProperty("content", content)
@@ -170,7 +174,7 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
 
   def getYarnSiteDesiredValues(self):
       yarn_site_desired_values = {
-          "hdp.version" : "2.6.3.0-235"
+          "hdp.version" : "2.6.5.0-292"
 
       }
       return yarn_site_desired_values
@@ -230,7 +234,8 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
         "splice.txn.concurrencyLevel" : "4096",
         "splice.olap_server.memory" : "8192",
         "splice.olap_server.memoryOverhead" : "2048",
-        "splice.olap_server.virtualCores" : "2"
+        "splice.olap_server.virtualCores" : "2",
+        "splice.authorization.scheme" : "NATIVE"
     }
     return hbase_site_desired_values
 
@@ -264,8 +269,8 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
         "splice.spark.yarn.am.extraLibraryPath=/usr/hdp/current/hadoop-client/lib/native",
         "splice.spark.yarn.am.waitTime=10s",
         "splice.spark.yarn.executor.memoryOverhead=2048",
-        "splice.spark.yarn.am.extraJavaOptions=-Dhdp.version=2.6.3.0-235",
-        "splice.spark.driver.extraJavaOptions=-Dhdp.version=2.6.3.0-235",
+        "splice.spark.yarn.am.extraJavaOptions=-Dhdp.version=2.6.5.0-292",
+        "splice.spark.driver.extraJavaOptions=-Dhdp.version=2.6.5.0-292",
         "splice.spark.driver.extraLibraryPath=/usr/hdp/current/hadoop-client/lib/native",
         "splice.spark.driver.extraClassPath=/usr/hdp/current/hbase-regionserver/conf:/usr/hdp/current/hbase-regionserver/lib/htrace-core-3.1.0-incubating.jar",
         "splice.spark.ui.retainedJobs=100",
@@ -281,10 +286,10 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
         "splice.spark.local.dir=/tmp",
         "splice.spark.executor.userClassPathFirst=true",
         "splice.spark.driver.userClassPathFirst=true",
-        "splice.spark.executor.extraJavaOptions=-Dhdp.version=2.6.3.0-235",
+        "splice.spark.executor.extraJavaOptions=-Dhdp.version=2.6.5.0-292",
         "splice.spark.executor.extraLibraryPath=/usr/hdp/current/hadoop-client/lib/native",
         "splice.spark.executor.extraClassPath=/usr/hdp/current/hbase-regionserver/conf:/usr/hdp/current/hbase-regionserver/lib/htrace-core-3.1.0-incubating.jar:/var/lib/splicemachine/*:/usr/hdp/2.6.3.0-235/spark2/jars/*:/usr/hdp/current/hbase-master/lib/*",
-        "splice.spark.yarn.jars=/usr/hdp/2.6.3.0-235/spark2/jars/*"
+        "splice.spark.yarn.jars=/usr/hdp/2.6.5.0-292/spark2/jars/*"
       ]
     return dashDProperties
 
