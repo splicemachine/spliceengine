98c24a1f0e3a20509965b813c94c2852eae37a21
jyuanca <jyuan@splicemachine.com>
2019-01-17 08:56:28 -0800

    DB-7476: Make bulk import sampling rate a database property (#2585)
    * DB-7476: Make bulk import sampling rate a database property
    
    * DB-7476: incorporate review comments

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/GenericStatement.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/GenericStatement.java
@@ -447,7 +447,8 @@ public class GenericStatement implements Statement{
             cc.setSelectivityEstimationIncludingSkewedDefault(selectivityEstimationIncludingSkewedDefault);
 
             /* check if the optimization to do projection pruning is enabled or not */
-            String projectionPruningOptimizationString = PropertyUtil.getCachedDatabaseProperty(lcc, Property.PROJECTION_PRUNING_DISABLED);
+            String projectionPruningOptimizationString = PropertyUtil.getCachedDatabaseProperty(lcc,
+                    Property.PROJECTION_PRUNING_DISABLED);
             // if database property is not set, treat it as false
             Boolean projectionPruningOptimizationDisabled = false;
             try {
