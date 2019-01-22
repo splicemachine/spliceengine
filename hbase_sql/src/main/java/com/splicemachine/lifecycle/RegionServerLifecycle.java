4cd57c3585a9de2b678a1ce556d1459d9480963f
Bin Wang <bin.wang@mail.binwang.me>
2019-01-22 10:21:52 +0800

    DB-7806 Handle more exceptions when boot region server and fix master boot timing

--- hbase_sql/src/main/java/com/splicemachine/lifecycle/RegionServerLifecycle.java
+++ hbase_sql/src/main/java/com/splicemachine/lifecycle/RegionServerLifecycle.java
@@ -31,6 +31,8 @@ import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.RegionOfflineException;
 import org.apache.hadoop.hbase.client.RetriesExhaustedException;
+import org.apache.hadoop.hbase.ipc.CallTimeoutException;
+import org.apache.hadoop.hbase.ipc.RemoteWithExtrasException;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.ipc.RemoteException;
 import org.apache.log4j.Logger;
@@ -144,6 +146,8 @@ public class RegionServerLifecycle implements DistributedDerbyStartup{
                 if (PleaseHoldException.class.getName().equals(re.getClassName()))
                     return true;
             }
+            return (e.getCause() instanceof IOException && e.getCause().getCause() instanceof CallTimeoutException) ||
+                    e.getCause() instanceof RemoteWithExtrasException;
         }
         return false;
     }
