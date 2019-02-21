42a7d9d2971356133eb7cefc2868ab33de2e3545
Jun Yuan <jyuan@splicemachine.com>
2019-02-20 16:41:37 -0800

    DB-7933: exclude external tables for table backup

--- db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
+++ db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
@@ -1979,6 +1979,7 @@ public interface SQLState {
 	String INCREMENTAL_NOT_ENABLED								   = "BR021";
 	String POINT_IN_TIME_TOO_SMALL                                 = "BR022";
 	String RESTORE_TXNID_TOO_LARGE								   = "BR023";
+	String NO_BACKUP_EXT_TABLE									   = "BR024";
 
 	String ROW_FORMAT_NOT_ALLOWED_WITH_PARQUET					   	= "EXT01";
 	String ROW_FORMAT_NOT_ALLOWED_WITH_ORC						   	= "EXT02";
