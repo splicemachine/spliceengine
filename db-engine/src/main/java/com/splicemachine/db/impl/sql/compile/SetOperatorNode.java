593bd3effa635a01882659f10bbf649292d477d7
Changli Liu <cliu@splicemachine.com>
2019-04-25 11:59:59 +0800

    Revert "DB-8200 fix NPE and wrong result for nested loop join with UNION oper… (#2704)"
    This reverts commit 0963b4b5e027b0c348098dbdde37a950c2e494bd.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SetOperatorNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SetOperatorNode.java
@@ -337,7 +337,7 @@ abstract class SetOperatorNode extends TableOperatorNode
 
 		JBitSet tableNums = new JBitSet(getReferencedTableMap().size());
 		BaseTableNumbersVisitor btnVis =
-			new BaseTableNumbersVisitor(tableNums, true);
+			new BaseTableNumbersVisitor(tableNums);
 
 		// Check the left child.
 		leftResultSet.accept(btnVis);
