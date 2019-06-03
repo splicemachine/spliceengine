cdce43fdf6db8f6a85533864c9b298db942b3faf
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2019-06-03 11:56:09 +0200

    DB-8394 Remove extra wait for Spark queries

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/SpliceBaseOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/SpliceBaseOperation.java
@@ -203,16 +203,13 @@ public abstract class SpliceBaseOperation implements SpliceOperation, ScopeNamed
             if(LOG_CLOSE.isTraceEnabled() && isOpen)
                 LOG_CLOSE.trace(String.format("closing operation %s",this));
             if (remoteQueryClient != null) {
-                if (!isKilled && !isTimedout) {
-                    // wait for completion, it should be quick
-                    try {
-                        remoteQueryClient.waitForCompletion(1, TimeUnit.MINUTES);
-                    } catch (InterruptedException | TimeoutException | ExecutionException e) {
-                        // ignore, continue closing
-                        LOG.warn("Exception while waiting for remote query client to wrap up", e);
-                    }
+                if (isKilled || isTimedout) {
+                    // interrupt it
+                    remoteQueryClient.interrupt();
+                } else {
+                    // close gracefully
+                    remoteQueryClient.close();
                 }
-                remoteQueryClient.close();
             }
             synchronized (this) {
                 isOpen=false;
