f80d1641833e513874ac766314f22a7aae020220
Changli Liu <33394239+changli6@users.noreply.github.com>
2019-06-07 10:14:13 +0800

    SPLICE-2341 Builtin Function CHR implementation (2.7) (#2750)

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
@@ -1360,6 +1360,14 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                             .isDeterministic(true).ownerClass(SpliceStringFunctions.class.getCanonicalName())
                             .varchar("S", Limits.DB2_VARCHAR_MAXWIDTH)
                             .varchar("REGEXP", Limits.DB2_VARCHAR_MAXWIDTH)
+                            .build(),
+                    Procedure.newBuilder().name("CHR")
+                            .numOutputParams(0)
+                            .numResultSets(0)
+                            .sqlControl(RoutineAliasInfo.NO_SQL)
+                            .returnType(DataTypeDescriptor.getCatalogType(Types.CHAR,1))
+                            .isDeterministic(true).ownerClass(SpliceStringFunctions.class.getCanonicalName())
+                            .integer("I")
                             .build()
             		)
             );
