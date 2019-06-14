62838a632db03be81ef9e4fbf508ab2f5064ff7a
Bin Wang <bin.wang@mail.binwang.me>
2019-06-14 21:59:29 +0800

    fix second constraint IT

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/CheckConstraintIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/actions/CheckConstraintIT.java
@@ -117,7 +117,7 @@ public class CheckConstraintIT extends SpliceUnitTest {
             verifyViolation(s,format("insert into %s values (1005, 101, 90, 79, 'bad', 'good3')",tableName), // col 5 bad
                     MSG_START_DEFAULT,"V_NEQ_CK1",schemaWatcher.schemaName,tableName);
 
-            verifyViolation(s,format("insert into %s values (1005, 101, 90, 85, 'ok', 'notsogood')",tableName), // col 6 bad
+            verifyViolation(s,format("insert into %s values (1005, 101, 90, 79, 'ok', 'notsogood')",tableName), // col 6 bad
                     MSG_START_DEFAULT,"V_IN_CK1",schemaWatcher.schemaName,tableName);
         }
     }
