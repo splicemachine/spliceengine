262ea5f012e9f715b5d37110c3ee9b4555e7b3b7
Bin Wang <bin.wang@mail.binwang.me>
2019-08-02 13:53:32 +0800

    Fix match multiple times and using substring for append

--- splice_encoding/src/test/java/com/splicemachine/utils/MaskPatternLayoutTest.java
+++ splice_encoding/src/test/java/com/splicemachine/utils/MaskPatternLayoutTest.java
@@ -18,11 +18,40 @@ public class MaskPatternLayoutTest {
     }
 
     @Test
+    public void testMatchPatternMultipleTimes() {
+        Pattern maskPattern = Pattern.compile("([0-9]+)");
+        String maskedMessage = MaskPatternLayout.maskMessage("1234 1234", maskPattern, maskString);
+        Assert.assertEquals(maskString + " " + maskString, maskedMessage);
+    }
+
+    @Test
+    public void testMatchPatternStart() {
+        Pattern maskPattern = Pattern.compile("([0-9]+)");
+        String maskedMessage = MaskPatternLayout.maskMessage("1234 efg", maskPattern, maskString);
+        Assert.assertEquals(maskString+ " efg", maskedMessage);
+    }
+
+    @Test
+    public void testMatchPatternEnd() {
+        Pattern maskPattern = Pattern.compile("abc ([0-9]+)");
+        String maskedMessage = MaskPatternLayout.maskMessage("abc 1234", maskPattern, maskString);
+        Assert.assertEquals("abc " + maskString, maskedMessage);
+    }
+
+    @Test
     public void testMultiMatchPattern() {
         Pattern maskPattern = Pattern.compile("([0-9]+)(?:[A-Z]+)([a-z]+)");
         String maskedMessage = MaskPatternLayout.maskMessage("abc 1234ABCefg", maskPattern,
                 maskString);
         Assert.assertEquals("abc " + maskString + "ABC" + maskString, maskedMessage);
+    }
+
+    @Test
+    public void testMultiMatchPatternContinue() {
+        Pattern maskPattern = Pattern.compile("([0-9]+)([a-z]+)");
+        String maskedMessage = MaskPatternLayout.maskMessage("abc 1234efg", maskPattern,
+                maskString);
+        Assert.assertEquals("abc " + maskString + maskString, maskedMessage);
     }
 
     @Test
