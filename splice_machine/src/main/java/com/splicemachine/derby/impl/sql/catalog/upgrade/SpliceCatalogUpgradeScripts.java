fd91105751276575b5bb7029db13b0c07e67e6ea
Mark Sirek <msirek@splicemachine.com>
2019-08-29 00:07:14 -0700

    Revert "Revert "DB-8570: invalidate dictionary cache after updating system procedures(2.8) (#2841)" (#2851)"
    This reverts commit 3c8498d83f4140795fbb1642e67813ddbbc6977c.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
@@ -71,7 +71,8 @@ public class SpliceCatalogUpgradeScripts{
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1909), new UpgradeScriptForReplication(sdd, tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1917), new UpgradeScriptForMultiTenancy(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1924), new UpgradeScriptToAddPermissionViewsForMultiTenancy(sdd,tc));
-
+        // Two system procedures are moved, so we need to run base script to update all system procedures
+        scripts.put(new Splice_DD_Version(sdd,2,8,0, 1928), new UpgradeScriptBase(sdd,tc));
     }
 
     public void run() throws StandardException{
