9d9ca02c0db32c96e90b71d16ad14685b760b308
Jun Yuan <jyuan@splicemachine.com>
2019-09-25 11:35:16 -0700

    DB-8574: do not roll back timeout txns on replication slaves

--- assembly/hdp3.1.0/src/main/resources/common-services/SPLICEMACHINE/2.5.1/service_advisor.py
+++ assembly/hdp3.1.0/src/main/resources/common-services/SPLICEMACHINE/2.5.1/service_advisor.py
@@ -239,7 +239,7 @@ class SPLICEMACHINE251ServiceAdvisor(service_advisor.ServiceAdvisor):
         "hbase.replication.source.service" : "com.splicemachine.replication.SpliceReplication",
         "hbase.replication.sink.service" : "com.splicemachine.replication.SpliceReplication",
         "hbase.bucketcache.ioengine" : "",
-        "hbase.regionserver.replication.handler.count":"20"
+        "hbase.regionserver.replication.handler.count":"40"
     }
     return hbase_site_desired_values
 
