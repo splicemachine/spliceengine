985651a7c23ed4c69fba0781a32efb5748a7f193
yxia92 <yxia@splicemachine.com>
2019-11-06 08:28:13 -0800

    DB-8098 address review comments

--- db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
+++ db-shared/src/main/java/com/splicemachine/db/shared/common/reference/SQLState.java
@@ -1045,7 +1045,7 @@ public interface SQLState {
 	String LANG_INVALID_SESSION_PROPERTY                               = "42Y86";
 	String LANG_INVALID_SESSION_PROPERTY_VALUE                         = "42Y87";
 	String LANG_INVALID_FORCED_NO_SUBQUERY_FLATTEN_VALUE               = "42Y88";
-	String LANG_INVALID_SUBQUYERY_PROPERTY                             = "42Y89";
+	String LANG_INVALID_SUBQUERY_PROPERTY                              = "42Y89";
 	String LANG_STMT_NOT_UPDATABLE                                     = "42Y90";
 	String LANG_NO_SPS_USING_IN_TRIGGER                                = "42Y91";
 	String LANG_TRIGGER_BAD_REF_MISMATCH                               = "42Y92";
