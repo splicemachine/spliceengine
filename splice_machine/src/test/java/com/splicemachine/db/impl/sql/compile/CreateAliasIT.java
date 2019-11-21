4563c91e144d4b49b6d40a97abcb4440b3d7d6ff
yxia92 <yxia@splicemachine.com>
2019-11-21 11:57:22 -0800

    DB-8854 add IT to test the invalidation of distributed cache

--- splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/CreateAliasIT.java
+++ splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/CreateAliasIT.java
@@ -3,26 +3,30 @@ package com.splicemachine.db.impl.sql.compile;
 import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
 import com.splicemachine.derby.test.framework.SpliceUnitTest;
 import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.derby.test.framework.TestConnection;
+import com.splicemachine.test.HBaseTest;
 import com.splicemachine.test_tools.TableCreator;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.experimental.categories.Category;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static com.splicemachine.test_tools.Rows.row;
 import static com.splicemachine.test_tools.Rows.rows;
 import static org.junit.Assert.assertEquals;
 
+@Category(HBaseTest.class)
 public class CreateAliasIT extends SpliceUnitTest {
 
     private static final String SCHEMA_NAME = CreateAliasIT.class.getSimpleName().toUpperCase();
     private static final SpliceWatcher spliceClassWatcher = new SpliceWatcher(SCHEMA_NAME);
     private static final SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(SCHEMA_NAME);
+
+    String remoteURLTemplate = "jdbc:splice://localhost:1528/splicedb;create=true;user=%s;password=%s";
 
     @Rule
     public SpliceWatcher methodWatcher = new SpliceWatcher(SCHEMA_NAME);
@@ -82,5 +86,34 @@ public class CreateAliasIT extends SpliceUnitTest {
         methodWatcher.executeUpdate("drop alias t3");
 
         // SHOW ALIASES is also working but cannot test through query since it is an ij command
+    }
+
+    @Test
+    public void testDropAliasInvalidateDistributedCache() throws Exception {
+        // step 1: create alias on RS0
+        methodWatcher.executeUpdate("create alias a4 for t1");
+        // step 2: select on RS1 to populate the cache
+        TestConnection rs1Conn = spliceClassWatcher.createConnection(remoteURLTemplate, "splice", "admin");
+        rs1Conn.query("select c1 from a4");
+        rs1Conn.commit();
+
+        // step 3: drop the alias on RS0
+        methodWatcher.executeUpdate("drop alias a4");
+
+        // step 4: check that we can re-use the same alias name on RS1
+        try {
+            rs1Conn.execute("create alias a4 for t1");
+        } catch (SQLException e) {
+            Assert.fail("fail to re-create alias a4!");
+        }
+
+        // step 5: check that we can use the re-created alias on RS0
+        ResultSet rs = methodWatcher.executeQuery("select c1 from a4");
+        rs.next();
+        assertEquals(1, rs.getInt("C1"));
+        rs.close();
+
+        rs1Conn.close();
+        methodWatcher.executeUpdate("drop alias a4");
     }
 }
