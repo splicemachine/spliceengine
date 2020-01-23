de5ea89408f8d8b764e10f6add694bcf752cf34e
Oleg Mazurov <omazurov@splicemachine.com>
2020-01-22 20:34:19 -0800

    SPLICE-1806 NullPointerException in PipingCallBuffer (#3149)

--- hbase_storage/src/main/java/com/splicemachine/storage/ClientPartition.java
+++ hbase_storage/src/main/java/com/splicemachine/storage/ClientPartition.java
@@ -14,8 +14,6 @@
 
 package com.splicemachine.storage;
 
-import com.splicemachine.si.api.data.TxnOperationFactory;
-import com.splicemachine.si.impl.driver.SIDriver;
 import org.spark_project.guava.base.Function;
 import com.splicemachine.si.data.HExceptionFactory;
 import com.splicemachine.si.impl.HNotServingRegion;
@@ -35,7 +33,6 @@ import com.splicemachine.utils.Pair;
 import org.apache.hadoop.hbase.*;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.client.coprocessor.Batch;
-import org.apache.hadoop.hbase.protobuf.generated.AdminProtos;
 import javax.annotation.concurrent.NotThreadSafe;
 import java.io.IOException;
 import java.io.InterruptedIOException;
@@ -56,13 +53,13 @@ public class ClientPartition extends SkeletonHBaseClientPartition{
     private final Table table;
     private final Connection connection;
     private final Clock clock;
-    private PartitionInfoCache partitionInfoCache;
+    private PartitionInfoCache<TableName> partitionInfoCache;
 
     public ClientPartition(Connection connection,
                            TableName tableName,
                            Table table,
                            Clock clock,
-                           PartitionInfoCache partitionInfoCache){
+                           PartitionInfoCache<TableName> partitionInfoCache){
         assert tableName!=null:"Passed in tableName is null";
         this.tableName=tableName;
         this.table=table;
@@ -155,30 +152,24 @@ public class ClientPartition extends SkeletonHBaseClientPartition{
             List<Partition> partitions;
             if (!refresh) {
                 partitions = partitionInfoCache.getIfPresent(tableName);
-                if (partitions == null) {
-                    partitions = formatPartitions(getAllRegionLocations(false));
-                    assert partitions!=null:"partitions are null";
-                    partitionInfoCache.put(tableName, partitions);
+                if (partitions != null) {
+                    return partitions;
                 }
-                return partitions;
             }
-            partitions = formatPartitions(getAllRegionLocations(true));
-            partitionInfoCache.invalidate(tableName);
-            assert partitions!=null:"partitions are null";
-            partitionInfoCache.put(tableName,partitions);
+            List<HRegionLocation> tableLocations = ((ClusterConnection) connection).locateRegions(tableName, !refresh, false);
+            partitions = new ArrayList<>(tableLocations.size());
+            for (HRegionLocation location : tableLocations){
+                if (location.getServerName() == null) {     // ensure no offline regions
+                    continue;
+                }
+                HRegionInfo regionInfo = location.getRegionInfo();
+                partitions.add(new RangedClientPartition(this, regionInfo, new RLServer(location)));
+            }
+            partitionInfoCache.put(tableName, partitions);
             return partitions;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    }
-
-    private List<Partition> formatPartitions(List<HRegionLocation> tableLocations) {
-        List<Partition> partitions=new ArrayList<>(tableLocations.size());
-        for(HRegionLocation location : tableLocations){
-            HRegionInfo regionInfo=location.getRegionInfo();
-            partitions.add(new RangedClientPartition(this,regionInfo,new RLServer(location)));
-        }
-        return partitions;
     }
 
     @Override
@@ -286,33 +277,6 @@ public class ClientPartition extends SkeletonHBaseClientPartition{
 
     public Table unwrapDelegate(){
         return table;
-    }
-
-
-    private List<HRegionLocation> getAllRegionLocations(boolean refresh) throws IOException {
-        if (refresh)
-           ((ClusterConnection) connection).clearRegionCache(tableName);
-        RegionLocator regionLocator = null;
-        boolean retry = false;
-        do {
-            retry = false;
-            regionLocator = connection.getRegionLocator(tableName);
-            for (HRegionLocation location: regionLocator.getAllRegionLocations()) {
-                ServerName serverName = location.getServerName();
-                if (serverName == null) {
-                    retry = true;
-                    try {
-                        Thread.sleep(100);
-                    }
-                    catch (InterruptedException e) {
-                        throw new IOException(e);
-                    }
-                    break;
-                }
-            }
-
-        }while (retry);
-        return regionLocator.getAllRegionLocations();
     }
 
     @Override
