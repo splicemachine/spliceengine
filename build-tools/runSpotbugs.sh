18266bdfc4d48dcf17b977b361b134ba2b6e2575
Arnaud Lacurie <alacurie@splicemachine.com>
2020-02-18 16:13:26 +0100

    DB-9162 Replace outdated findbugs with spotbugs

---
+++ build-tools/runSpotbugs.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#This script runs spotbugs and display errors related to files modified by this PR
+#Sample usage:
+# ./runSpotbugs cdh5.14.0 master
+
+display_usage() {
+   echo -e "\nUsage:\n./runSpotbugs <platform> <base_branch>\n"
+}
+if [  $# -ne 2 ]
+then
+   display_usage
+   exit 1
+fi
+
+platform=$1
+branch=$2
+
+out_file=$(mktemp)
+
+errors=
+
+echo "Running spotbugs..."
+time mvn spotbugs:check -Pcore,$platform,mem,ee --fail-never | tee out_file
+for file in $(git diff $(git merge-base $branch HEAD)..HEAD --name-only); do
+    if new_errors="$(grep "$(basename $file)" out_file)"
+    then
+        errors+=$'\n'
+        errors+="$new_errors"
+    fi
+done
+rm out_file
+
+count=$(grep -c "ERROR" <(echo "$errors"))
+
+echo ""
+echo "#######################################################"
+echo "$count specific errors to fix before this PR can be merged:"
+echo "$errors"
+echo "#######################################################"
+echo ""
+echo "To reproduce locally, run ./build-tools/runSpotbugs.sh $platform $branch"
+
+exit $count
