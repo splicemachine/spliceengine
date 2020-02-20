7b2716bc7e1a66b2e77dfe14711154aa54224daf
Yi Xia <yxia@splicemachine.com>
2020-02-20 13:50:39 -0800

    DB-9108 address review comments

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/export/ExportPermissionCheckTest.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/export/ExportPermissionCheckTest.java
@@ -29,6 +29,8 @@ import org.junit.rules.TemporaryFolder;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.stringContainsInOrder;
 import static org.junit.Assert.assertEquals;
 
 @Category(ArchitectureIndependent.class)
@@ -53,9 +55,8 @@ public class ExportPermissionCheckTest {
         try {
             permissionCheck.verify();
         } catch (Exception e) {
-            Assert.assertTrue(e.getMessage(), e.getMessage().contains("IOException '/ExportPermissionCheckTest' when accessing directory")
-                    || e.getMessage().contains("IOException '/ExportPermissionCheckTest: Read-only file system' when accessing directory"));
-
+            assertThat(e.getMessage(),
+                    stringContainsInOrder("IOException '/ExportPermissionCheckTest", "' when accessing directory"));
         }
     }
 
