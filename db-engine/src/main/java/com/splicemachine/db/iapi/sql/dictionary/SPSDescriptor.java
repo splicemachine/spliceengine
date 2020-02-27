92fcc862f4ce0d30f7172fbef37d85e7fc2e6870
Daniel Gómez Ferro <dgomezferro@splicemachine.com>
2020-02-27 19:22:37 +0100

    DB-8893 Fix merge (#3259)

--- db-engine/src/main/java/com/splicemachine/db/iapi/sql/dictionary/SPSDescriptor.java
+++ db-engine/src/main/java/com/splicemachine/db/iapi/sql/dictionary/SPSDescriptor.java
@@ -904,7 +904,7 @@ public class SPSDescriptor extends TupleDescriptor implements UniqueSQLObjectDes
         DataDictionary dd = getDataDictionary();
 
         String role = lcc.getReplicationRole();
-        if (dd.isReadOnlyUpgrade() || role.compareToIgnoreCase("SLAVE") == 0) ||
+        if (dd.isReadOnlyUpgrade() || role.compareToIgnoreCase("SLAVE") == 0 ||
                 lcc.getSessionProperties().getProperty(SessionProperties.PROPERTYNAME.SNAPSHOT_TIMESTAMP) != null)
             return;
 
