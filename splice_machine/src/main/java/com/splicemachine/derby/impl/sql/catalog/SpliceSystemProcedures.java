69043f2d7bae276de8f7c35848f49f4cc3d62f95
Jun Yuan <jyuan@splicemachine.com>
2020-03-03 15:45:30 -0800

    DB-8990: fix slave timestamp

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
@@ -1352,7 +1352,6 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                             .smallint("peerId")
                             .varchar("clusterKey", 32672)
                             .arg("enabled", DataTypeDescriptor.getBuiltInDataTypeDescriptor(Types.BOOLEAN).getCatalogType())
-                            .arg("isSerial", DataTypeDescriptor.getBuiltInDataTypeDescriptor(Types.BOOLEAN).getCatalogType())
                             .ownerClass(ReplicationSystemProcedure.class.getCanonicalName())
                             .build();
                     procedures.add(addPeer);
@@ -1505,6 +1504,15 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                             .ownerClass(ReplicationSystemProcedure.class.getCanonicalName())
                             .build();
                     procedures.add(dumpUnreplicatedWals);
+
+                    Procedure replicationEnabled = Procedure.newBuilder().name("REPLICATION_ENABLED")
+                            .numOutputParams(0)
+                            .catalog("schemaName")
+                            .catalog("tableName")
+                            .numResultSets(1)
+                            .ownerClass(ReplicationSystemProcedure.class.getCanonicalName())
+                            .build();
+                    procedures.add(replicationEnabled);
                 }  // End key == sysUUID
 
             } // End iteration through map keys (schema UUIDs)
