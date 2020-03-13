c5d0f45dd67b784b4edc0596c8ea8509075693f0
Joe Panko <jpanko1@earthlink.net>
2020-03-13 15:35:00 -0500

    Added Kafka path to HBase classpath prepend.

--- assembly/cdh5.13.3/src/main/resources/meta/splicemachine_env.sh
+++ assembly/cdh5.13.3/src/main/resources/meta/splicemachine_env.sh
@@ -21,7 +21,7 @@ MYLIBDIR=${PARCELS_ROOT}/${PARCEL_DIRNAME}/lib
     exit 1
 }
 
-PREPENDSTRING=`echo ${MYLIBDIR}/*.jar ${PARCELS_ROOT}/SPARK2/lib/spark2/jars/*.jar | sed 's/ /:/g'`
+PREPENDSTRING=`echo ${MYLIBDIR}/*.jar ${PARCELS_ROOT}/SPARK2/lib/spark2/jars/*.jar ${PARCELS_ROOT}/SPARK2/lib/spark2/kafka-0.10/*.jar | sed 's/ /:/g'`
 echo "prepending $PREPENDSTRING to HBASE_CLASSPATH_PREFIX"
 if [ -z $HBASE_CLASSPATH_PREFIX ] ; then
     export HBASE_CLASSPATH_PREFIX="${PREPENDSTRING}"
