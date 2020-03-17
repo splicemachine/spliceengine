be3b3064872f6f6ab50243ea3bbb06bb5a2261f3
Changli Liu <33394239+changli6@users.noreply.github.com>
2020-03-18 00:30:35 +0800

    DB-9028 Fix ClassCastException when query contains round() (2.8) (#3299)

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/OperatorToString.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/OperatorToString.java
@@ -963,12 +963,14 @@ public class OperatorToString {
                         // for the ROUND function.
                         // ADD_MONTHS returns incorrect results on
                         // Spark for old dates.
+                        // The ROUND function came from Splice package (uppercase) and java.lang.StrictMath (lowercase),
+                        // we need to handle it especially.
                         if (methodName.equals("MONTH_BETWEEN") ||
                             methodName.equals("REGEXP_LIKE")   ||
                             methodName.equals("ADD_MONTHS")    ||
                             methodName.equals("ADD_YEARS")     ||
                             methodName.equals("ADD_DAYS")      ||
-                            methodName.equals("ROUND"))
+                            methodName.equalsIgnoreCase("ROUND"))
                             throwNotImplementedError();
                         else if (methodName.equals("toDegrees"))
                             methodName = "degrees";
