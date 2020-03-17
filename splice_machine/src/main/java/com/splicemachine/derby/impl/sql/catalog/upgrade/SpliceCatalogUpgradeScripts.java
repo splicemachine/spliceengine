19e77252ec4af7c0abf3b5fd23b10275d0ae4433
Changli Liu <cliu@splicemachine.com>
2020-03-18 01:32:05 +0800

    Add upgrade script to remove unused index

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
@@ -80,8 +80,8 @@ public class SpliceCatalogUpgradeScripts{
         scripts.put(new Splice_DD_Version(sdd,3,1,0, 1941), new UpgradeScriptForTableColumnViewInSYSIBM(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,3,1,0, 1948), new UpgradeScriptBase(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,3,1,0, 1948), new UpgradeScriptForAddDefaultToColumnViewInSYSIBM(sdd,tc));
+        scripts.put(new Splice_DD_Version(sdd,3,1,0, 1951), new UpgradeScriptForRemoveUnusedIndexInSYSFILESTable(sdd,tc));
     }
-
     public void run() throws StandardException{
 
         // Set the current version to upgrade from.
