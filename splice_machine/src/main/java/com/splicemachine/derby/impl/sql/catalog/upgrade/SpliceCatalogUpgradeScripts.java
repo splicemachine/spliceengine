b897ed8993b581638bc82cdb74a886a469301c51
Changli Liu <33394239+changli6@users.noreply.github.com>
2020-03-31 00:50:09 +0800

    DB-6376 Allow importing more than one JAR in a schema (2.8) (#3319)
    * DB-6376 Allow importing more than one JAR in a schema
    
    * Add upgrade script to remove unused index
    
    * Update upgrade version

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
@@ -78,6 +78,7 @@ public class SpliceCatalogUpgradeScripts{
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1941), new UpgradeScriptForTableColumnViewInSYSIBM(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1948), new UpgradeScriptBase(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1948), new UpgradeScriptForAddDefaultToColumnViewInSYSIBM(sdd,tc));
+        scripts.put(new Splice_DD_Version(sdd,2,8,0, 1953), new UpgradeScriptForRemoveUnusedIndexInSYSFILESTable(sdd,tc));
     }
 
     public void run() throws StandardException{
