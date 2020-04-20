3846cf51c6e29b211584f71a56b94913819eb497
yxia92 <yxia@splicemachine.com>
2020-04-20 10:36:17 -0700

    DB-9401 fix spotbugs

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/FromTable.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/FromTable.java
@@ -45,6 +45,7 @@ import com.splicemachine.db.iapi.types.DataValueDescriptor;
 import com.splicemachine.db.iapi.types.SQLChar;
 import com.splicemachine.db.iapi.util.JBitSet;
 import com.splicemachine.db.iapi.util.StringUtil;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.util.*;
 
@@ -707,6 +708,7 @@ public abstract class FromTable extends ResultSetNode implements Optimizable{
         return false;
     }
 
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP",justification = "Intentional")
     @Override
     public int[] hashKeyColumns(){
         if(SanityManager.DEBUG){
@@ -717,6 +719,7 @@ public abstract class FromTable extends ResultSetNode implements Optimizable{
         return hashKeyColumns;
     }
 
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP2",justification = "Intentional")
     @Override
     public void setHashKeyColumns(int[] columnNumbers){
         hashKeyColumns=columnNumbers;
