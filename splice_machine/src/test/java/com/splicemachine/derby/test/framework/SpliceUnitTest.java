324fe95228559f6e90cf7ce17792eae860a8e969
Martin Rupp <martinrupp@users.noreply.github.com>
2020-04-29 03:33:52 +0200

    DB-9109 Fix sporadics in jenkins due to overwrite of git data in tests (#3461)
    * DB-9109 Fix sporadics in jenkins due to overwrite of git data in tests
    
    * DB-9109 fix problems with parallel runs of tests all writing to xxx/target/external
    
    every test that needs a temporary output dir will now on-demand create a temp directory,
    that will be deleted automatically after the test has run, using the @After annotation of JUnit.
    
    * DB-9109 fix / missing in directory leading to temp dir leak
    
    delete temporary files created in two places
    
    * DB-9109 create temp files in /target/external so build is self-contained

--- splice_machine/src/test/java/com/splicemachine/derby/test/framework/SpliceUnitTest.java
+++ splice_machine/src/test/java/com/splicemachine/derby/test/framework/SpliceUnitTest.java
@@ -16,6 +16,8 @@ package com.splicemachine.derby.test.framework;
 
 import com.splicemachine.homeless.TestUtils;
 import com.splicemachine.utils.Pair;
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.runner.Description;
 import org.spark_project.guava.base.Joiner;
@@ -39,6 +41,12 @@ public class SpliceUnitTest {
     private static Pattern overallCostP = Pattern.compile("totalCost=[0-9]+\\.?[0-9]*");
     private static Pattern outputRowsP = Pattern.compile("outputRows=[0-9]+\\.?[0-9]*");
     private static Pattern scannedRowsP = Pattern.compile("scannedRows=[0-9]+\\.?[0-9]*");
+
+    /// set the following boolean to true to prevent deletion of temporary files (e.g. for debugging)
+    public boolean debug_no_delete = false;
+    /// the temporary directory (created on demand)
+    /// will reside under e.g. GITROOT/platform_it/target/external when running cdh6.3.0
+    private Path tempDir = null;
 
 	public String getSchemaName() {
 		Class<?> enclosingClass = getClass().getEnclosingClass();
@@ -924,5 +932,68 @@ public class SpliceUnitTest {
             SQLException se = (SQLException) e;
             assertTrue("Incorrect error state: " + se.getSQLState() + ", expected: " + errorState,  errorState.startsWith( se.getSQLState()));
         }
+    }
+
+    /// this will be called after each test, deleting possible temp directories.
+    @After
+    public void deleteTempDirectory() throws Exception
+    {
+        if( tempDir == null )
+            return;
+        else if( debug_no_delete )
+            System.out.println( "Not deleting test temporary directory " + tempDir );
+        else {
+            FileUtils.deleteDirectory(tempDir.toFile());
+        }
+    }
+
+    /// this will return the temp directory, that is created on demand once for each test
+    public String getExternalResourceDirectory() throws Exception
+    {
+        if( tempDir == null) {
+            File basepath = new File(getHBaseDirectory(), "/target/external/");
+            basepath.mkdirs();
+            tempDir = Files.createTempDirectory( basepath.toPath(), "SpliceUnitTest_temp_");
+        }
+        // add '/' to avoid leaking directories if someone misses the '/'
+        return tempDir.toString() + "/";
+    }
+
+    /// creates a temporary file in the temporary directory, so will be deleted after test
+    public File createTempOutputFile( String prefix, String suffix ) throws Exception
+    {
+        return File.createTempFile(prefix, suffix, new File(getExternalResourceDirectory()) );
+    }
+
+    /// copy subfolder of resource directory (i.e. GITROOT/splice_machine/src/test/test-data/)
+    /// to temporary directory @sa getTempOutputDirectory
+    /// @return destination directory as string
+    public String getTempCopyOfResourceDirectory( String subfolder ) throws Exception
+    {
+        File src = new File(SpliceUnitTest.getResourceDirectory() + subfolder);
+        File dest = new File( getExternalResourceDirectory() + subfolder );
+        FileUtils.copyDirectory( src, dest );
+        return dest.toString();
+    }
+
+    /// @return number of files in directory tree
+    // recursively, going down directories, but not counting directories as files
+    public static int getNumberOfFiles(String dirPath)
+    {
+        int count = 0;
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+        if (files == null)
+            return 0;
+
+        for (File file : files)
+        {
+            count++;
+            if (file.isDirectory()) {
+                count += getNumberOfFiles(file.getAbsolutePath());
+            }
+        }
+        return count;
     }
 }
