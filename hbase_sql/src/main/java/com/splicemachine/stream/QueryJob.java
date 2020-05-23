5cb0ef06e7b62e4de819b169856e85da7d3f1e94
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2020-05-23 20:19:43 +0200

    DB-8190 Make ParallelPartitions configurable

--- hbase_sql/src/main/java/com/splicemachine/stream/QueryJob.java
+++ hbase_sql/src/main/java/com/splicemachine/stream/QueryJob.java
@@ -95,7 +95,7 @@ public class QueryJob implements Callable<Void>{
 
             JavaRDD rdd =  sparkDataSet.rdd;
             StreamableRDD streamableRDD = new StreamableRDD<>(rdd, context, uuid, clientHost, clientPort,
-                    queryRequest.streamingBatches, queryRequest.streamingBatchSize);
+                    queryRequest.streamingBatches, queryRequest.streamingBatchSize, queryRequest.parallelPartitions);
             streamableRDD.setJobStatus(status);
             streamableRDD.submit();
 
