c4fd2217eebcd8a8d6eb72c1ffe3700ba108c58b
bklo94 <brandonklo94@gmail.com>
2020-06-22 06:44:58 +0000

    Add patch folder

--- assembly/mapr/src/main/resources/scripts/splice/patch.sh
+++ assembly/mapr/src/main/resources/scripts/splice/patch.sh
@@ -1,4 +1,4 @@
-pwd
+#!/bin/sh
 sed -i "/* Add filters to the SparkUI./,/\*\*/{s/./REPLACE_ME /g}"  target/tmp/org/apache/spark/scheduler/cluster/YarnSchedulerBackend.scala
 awk '/REPLACE_ME/&&c++>0 {next} 1' target/tmp/org/apache/spark/scheduler/cluster/YarnSchedulerBackend.scala > target/temp.java
 sed -e "/REPLACE_ME/{r mapr/src/main/resources/patch/yarn-patch.class" -e "d}" target/temp.java >  target/YarnSchedulerBackend.scala
