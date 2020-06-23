e5a93828171d74174e14fbaabca51e620326dd05
Arnaud Lacurie <alacurie@splicemachine.com>
2020-06-23 15:07:13 +0200

    DB-9640 Fix spotbugs

--- hbase_storage/src/main/java/com/splicemachine/si/impl/server/SICompactionStateMutate.java
+++ hbase_storage/src/main/java/com/splicemachine/si/impl/server/SICompactionStateMutate.java
@@ -12,10 +12,7 @@ import org.apache.hadoop.hbase.KeyValue;
 import org.apache.log4j.Logger;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 
 class SICompactionStateMutate {
@@ -39,6 +36,19 @@ class SICompactionStateMutate {
         @Override
         public int compareTo(@NotNull Object other) {
             return KeyValue.COMPARATOR.compare(cell, ((CellAndCommit) other).cell);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(cell, commitTimestamp);
+        }
+
+        @Override
+        public boolean equals(@NotNull Object other) {
+            if (!(other instanceof CellAndCommit)) {
+                return false;
+            }
+            return compareTo(other) == 0;
         }
     }
 
@@ -144,6 +154,8 @@ class SICompactionStateMutate {
             case DELETE_RIGHT_AFTER_FIRST_WRITE_TOKEN:
                 assert deleteRightAfterFirstWriteTimestamp == 0;
                 deleteRightAfterFirstWriteTimestamp = commitTimestamp;
+                break;
+            default:
                 break;
         }
         dataToReturn.add(new CellAndCommit(element, commitTimestamp));
