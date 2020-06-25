cfc82152e233f448238749448ae39b17604a0a1d
Igor Praznik <ipraznik@splicemachine.com>
2020-06-25 12:23:07 +0200

    DB-9575 old unit tests fixed, new added

--- hbase_sql/src/test/java/com/splicemachine/mrio/api/core/SMInputFormatIT.java
+++ hbase_sql/src/test/java/com/splicemachine/mrio/api/core/SMInputFormatIT.java
@@ -16,9 +16,14 @@ package com.splicemachine.mrio.api.core;
 
 import java.io.IOException;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.List;
 import com.splicemachine.db.iapi.sql.execute.ExecRow;
 import com.splicemachine.db.iapi.types.RowLocation;
+import com.splicemachine.test.HBaseTestUtils;
+import com.splicemachine.test.SerialTest;
+import com.splicemachine.test.SlowTest;
+import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.junit.Assert;
@@ -26,6 +31,8 @@ import org.junit.ClassRule;
 import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
@@ -37,18 +44,19 @@ import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
 import com.splicemachine.derby.test.framework.SpliceTableWatcher;
 import com.splicemachine.mrio.MRConstants;
 
-@Ignore
-public class SMInputFormatIT extends BaseMRIOTest {	
-	private static final String CLASS_NAME = SMInputFormatIT.class.getSimpleName().toUpperCase();
+import static org.junit.Assert.assertTrue;
+
+@Category({SlowTest.class, SerialTest.class})
+public class SMInputFormatIT extends BaseMRIOTest {
+    private static final String CLASS_NAME = SMInputFormatIT.class.getSimpleName().toUpperCase();
     private static SpliceWatcher classWatcher = new SpliceWatcher(CLASS_NAME);
     private static final SpliceSchemaWatcher schemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);
     private static final SpliceTableWatcher tableWatcherA = new SpliceTableWatcher(
-    	"A", schemaWatcher.schemaName, "(col1 varchar(100) primary key, col2 varchar(100))");
+            "A", schemaWatcher.schemaName, "(col1 varchar(100) primary key, col2 varchar(100))");
     private static final SpliceTableWatcher tableWatcherB = new SpliceTableWatcher(
-        	"B", schemaWatcher.schemaName, "(col1 bigint primary key, col2 varchar(100))");
+            "B", schemaWatcher.schemaName, "(col1 bigint primary key, col2 varchar(100))");
     private static final SpliceSparkWatcher sparkWatcher = new SpliceSparkWatcher(CLASS_NAME);
-       
-    
+
     @ClassRule
     public static TestRule chain = RuleChain.outerRule(classWatcher)
             .around(schemaWatcher)
@@ -62,74 +70,118 @@ public class SMInputFormatIT extends BaseMRIOTest {
                         PreparedStatement ps1;
                         PreparedStatement ps2;
                         ps1 = classWatcher.prepareStatement(
-							"insert into " + tableWatcherA + " (col1, col2) values (?,?)");
+                                "insert into " + tableWatcherA + " (col1, col2) values (?,?)");
                         ps1.setString(1, "John");
                         ps1.setString(2, "Leach");
                         ps1.executeUpdate();
                         ps1.setString(1, "Jenny");
                         ps1.setString(2, "Swift");
                         ps1.executeUpdate();
-    
+
                         ps2 = classWatcher.prepareStatement(
-							"insert into " + tableWatcherB + " (col1, col2) values (?,?)");
+                                "insert into " + tableWatcherB + " (col1, col2) values (?,?)");
                         for (int i =0; i<10000;i++) {
-                        	ps2.setInt(1, i);
-                        	ps2.setString(2, i+"sdfasdgffdgdfgdfgdfgdfgdfgdfgd");
-                        	ps2.executeUpdate();     
-                        	if (i==500) {
-                        		flushTable(tableWatcherB.toString());
-                        		splitTable(tableWatcherB.toString());                        		
-                        	}
-                        	if (i==750) {
-                        		flushTable(tableWatcherB.toString());
-                        	}
+                            ps2.setInt(1, i);
+                            ps2.setString(2, i+"sdfasdgffdgdfgdfgdfgdfgdfgdfgd");
 
-                        	if (i==1000) {
-                        		flushTable(tableWatcherB.toString());
-                        	}
+                            ps2.addBatch();
 
+                            if (i==500) {
+                                ps2.executeBatch();
+                                flushTable(tableWatcherB.toString());
+                            }
+
+                            if (i==1000) {
+                                ps2.executeBatch();
+                                flushTable(tableWatcherB.toString());
+                            }
+
+                            if (i==2000) {
+                                ps2.executeBatch();
+                                flushTable(tableWatcherB.toString());
+                                splitTable(tableWatcherB.toString());
+                            }
                         }
+                        ps2.executeBatch();
                     } catch (Exception e) {
                         throw new RuntimeException(e);
+                    } catch (Throwable t) {
+                        throw new RuntimeException(t);
                     } finally {
                         classWatcher.closeAll();
                     }
                 }
             });
-   
+
     @Rule
     public SpliceWatcher methodWatcher = new SpliceWatcher(CLASS_NAME);
-	
-    
-    
+
     @Test
-    public void testSparkIntegrationWithInputFormat() throws IOException {
-    	config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherA.toString());
-    	Job job = Job.getInstance(config, "Test Scan");
+    public void testSparkIntegrationWithInputFormat() throws Exception {
+        config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherA.toString());
+        config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherA.toString());
+        config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherA.tableName, tableWatcherA.getSchema())));
+        Job job = Job.getInstance(config, "Test Scan");
         JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
         List<Tuple2<RowLocation, ExecRow>> data = table.collect();
         int i = 0;
         for (Tuple2<RowLocation, ExecRow> tuple: data) {
-        	i++;
-        	Assert.assertNotNull(tuple._1());
-        	Assert.assertNotNull(tuple._2());
+            i++;
+            Assert.assertNotNull(tuple._1());
+            Assert.assertNotNull(tuple._2());
         }
-        Assert.assertEquals("Incorrect Results Returned", 2,i);   	
+        Assert.assertEquals("Incorrect Results Returned", 2,i);
     }
-    
+
     @Test
-    public void testCountOverMultipleRegionsInSpark() throws IOException {
-    	config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherB.toString());
-    	Job job = Job.getInstance(config, "Test Scan");
+    public void testCountOverMultipleRegionsInSpark() throws Exception {
+        config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherB.toString());
+        config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherB.toString());
+        config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherB.tableName, tableWatcherB.getSchema())));
+        Job job = Job.getInstance(config, "Test Scan");
         JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
         List<Tuple2<RowLocation, ExecRow>> data = table.collect();
         int i = 0;
         for (Tuple2<RowLocation, ExecRow> tuple: data) {
-        	i++;
-        	Assert.assertNotNull(tuple._1());
-        	Assert.assertNotNull(tuple._2());
+            i++;
+            Assert.assertNotNull(tuple._1());
+            Assert.assertNotNull(tuple._2());
         }
         Assert.assertEquals("Incorrect Results Returned", 10000,i);
+
     }
-    
+
+    @Test
+    public void testQueryOverMultipleRegionsInSpark() throws Exception {
+        try (PreparedStatement ps = methodWatcher.prepareStatement("select count(*) from " + tableWatcherB.toString() + " where col1 in (1,2,4) --splice-properties useSpark=true")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                Assert.assertTrue("No rows returned from count query!", rs.next());
+                Assert.assertEquals("Incorrect query count!", 3, rs.getLong(1));
+            }
+        }
+
+        try (PreparedStatement ps = methodWatcher.prepareStatement("select count(*) from " + tableWatcherB.toString() + " where col2 in ('1sdfasdgffdgdfgdfgdfgdfgdfgdfgd') --splice-properties useSpark=true")) {
+            try (ResultSet rs = ps.executeQuery()) {
+                Assert.assertTrue("No rows returned from count query!", rs.next());
+                Assert.assertEquals("Incorrect query count!", 1, rs.getLong(1));
+            }
+        }
+    }
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Test
+    public void testSplitNeedsRefreshMaxRetries() throws Exception {
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("MAX_RETRIES exceeded during getSplits");
+
+        config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherB.toString());
+        config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherB.toString());
+        config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherB.tableName, tableWatcherB.getSchema())));
+        Job job = Job.getInstance(config, "Test Scan");
+        JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormatFail.class, RowLocation.class, ExecRow.class);
+        List<Tuple2<RowLocation, ExecRow>> data = table.collect();
+    }
+
 }
