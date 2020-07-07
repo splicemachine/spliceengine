b160e55c99f87b954548c768b09bfa2a6016d264
Joe Panko <jpanko1@earthlink.net>
2020-07-06 21:21:03 -0500

    DB-9774 NSDS v1 IT added bulk import test.

--- splice_spark/src/test/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContextIT.scala
+++ splice_spark/src/test/spark2.1/com/splicemachine/spark/splicemachine/SplicemachineContextIT.scala
@@ -16,7 +16,8 @@
  */
 package com.splicemachine.spark.splicemachine
 
-import java.io.{ByteArrayInputStream, ByteArrayOutputStream, ObjectInputStream, ObjectOutputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File, ObjectInputStream, ObjectOutputStream}
+
 import org.apache.spark.sql.SQLContext
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
@@ -69,7 +70,46 @@ class SplicemachineContextIT extends FunSuite with TestContext with Matchers {
     insertInternalRows(rowCount)
     val sqlContext = new SQLContext(sc)
     val rdd = splicemachineContext.rdd(internalTN,Seq("C2_CHAR","C7_BIGINT"))
-    org.junit.Assert.assertEquals("Schema Changed!","List([0    ,0], [1    ,1], [2    ,2], [3    ,3], [4    ,4], [5    ,5], [6    ,6], [7    ,7], [null,8], [null,9])",rdd.collect().toList.toString)
+    org.junit.Assert.assertEquals("RDD Changed!","List([0    ,0], [1    ,1], [2    ,2], [3    ,3], [4    ,4], [5    ,5], [6    ,6], [7    ,7], [null,8], [null,9])",rdd.collect().toList.toString)
   }
 
+  test("Test Create Table") {
+    val tableName = schema +"."+ getClass.getSimpleName + "_TestCreateTable"
+    
+    if( splicemachineContext.tableExists(tableName) ) {
+      splicemachineContext.dropTable(tableName)
+    }
+    org.junit.Assert.assertFalse( splicemachineContext.tableExists(tableName) )
+
+    import org.apache.spark.sql.types._
+
+    val schema_createTable = StructType(
+      StructField("NUMBER", IntegerType, false) :: 
+        StructField("MAKE", StringType, true) ::
+        StructField("MODEL", StringType, true) :: Nil)
+
+    splicemachineContext.createTable(tableName, schema_createTable, Seq("NUMBER"))
+
+    org.junit.Assert.assertTrue( splicemachineContext.tableExists(tableName) )
+
+    splicemachineContext.dropTable(tableName)
+  }
+
+  test("Test Bulk Import") {
+    if( splicemachineContext.tableExists(internalTN) ) {
+      splicemachineContext.dropTable(internalTN)
+    }
+    insertInternalRows(0)
+
+    val bulkImportDirectory = new File( System.getProperty("java.io.tmpdir")+"/splice_spark-SplicemachineContextIT/bulkImport" )
+    bulkImportDirectory.mkdirs()
+
+    splicemachineContext.bulkImportHFile(internalTNDF, internalTN,
+      collection.mutable.Map("bulkImportDirectory" -> bulkImportDirectory.getAbsolutePath)
+    )
+
+    val rs = getConnection.createStatement.executeQuery("select count(*) from "+internalTN)
+    rs.next
+    org.junit.Assert.assertEquals("Bulk Import Failed!", 1, rs.getInt(1))
+  }
 }
