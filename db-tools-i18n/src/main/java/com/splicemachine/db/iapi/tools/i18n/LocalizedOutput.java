bee802718a2528227168fcb9ece5c58d5397430b
Youssef Hatem <yhatem@splicemachine.com>
2020-07-10 14:58:23 +0200

    Address SpotBug issue.

--- db-tools-i18n/src/main/java/com/splicemachine/db/iapi/tools/i18n/LocalizedOutput.java
+++ db-tools-i18n/src/main/java/com/splicemachine/db/iapi/tools/i18n/LocalizedOutput.java
@@ -34,11 +34,12 @@ import java.io.PrintWriter;
 import java.io.OutputStreamWriter;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 public class LocalizedOutput extends PrintWriter {
 	private OutputStream out;
 	public LocalizedOutput(OutputStream o){
-		super(new OutputStreamWriter(o), true);
+		super(new OutputStreamWriter(o, StandardCharsets.UTF_8), true);
 		out = o;
 	}
 	LocalizedOutput(OutputStream o, String enc) throws UnsupportedEncodingException {
