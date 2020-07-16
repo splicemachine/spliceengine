3724a26136bb4908fda82164a2c8730b3cf9f3e5
Youssef Hatem <yhatem@splicemachine.com>
2020-07-16 09:14:09 +0200

    Address comments and show timezone offset.

--- db-tools-ij/src/main/java/com/splicemachine/db/impl/tools/ij/utilMain.java
+++ db-tools-ij/src/main/java/com/splicemachine/db/impl/tools/ij/utilMain.java
@@ -659,7 +659,7 @@ public class utilMain implements java.security.PrivilegedAction {
 	 */
 	 static void doPrompt(boolean newStatement, LocalizedOutput out, String tag)
 	 {
-	 	final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+	 	final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZZZZ");
 
 	 	String clock = showPromptClock ? " " + sdf.format(new Timestamp(System.currentTimeMillis())) : "";
 
@@ -863,7 +863,7 @@ public class utilMain implements java.security.PrivilegedAction {
 		}
 	}
 
-	public void setPromptClock(boolean showPromptClock) {
-		this.showPromptClock = showPromptClock;
+	public static void setPromptClock(boolean showPromptClock) {
+		utilMain.showPromptClock = showPromptClock;
 	}
 }
