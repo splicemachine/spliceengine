2a79cf30b31b49ab0587babf74b8ae2284d5c4bc
Igor Praznik <ipraznik@splicemachine.com>
2020-07-17 12:14:40 +0200

    DB-9846 adjusted for API change

--- splice_machine/src/main/java/com/splicemachine/derby/stream/control/ControlDataSet.java
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/control/ControlDataSet.java
@@ -591,7 +591,7 @@ public class ControlDataSet<V> implements DataSet<V> {
                     ValueRow vr = (ValueRow) iterator.next();
                     context.recordWrite();
 
-                    rw.write(null, encoder.toRow(vr));
+                    rw.write(null, ParquetWriterService.getFactory().encodeToRow(tableSchema, vr, encoder));
                 }
             } finally {
                 rw.close(null);
