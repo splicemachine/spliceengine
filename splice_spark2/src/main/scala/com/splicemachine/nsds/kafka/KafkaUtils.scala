e0977a26b345440115cc0c9d97236d05412ac59b
Joe Panko <jpanko1@earthlink.net>
2020-07-23 18:20:06 -0500

    DB-9606 NSDS v2 KafkaUtils clean up.

--- splice_spark2/src/main/scala/com/splicemachine/nsds/kafka/KafkaUtils.scala
+++ splice_spark2/src/main/scala/com/splicemachine/nsds/kafka/KafkaUtils.scala
@@ -26,7 +26,8 @@ import org.apache.kafka.common.serialization.IntegerDeserializer
 import scala.collection.JavaConverters._
 
 object KafkaUtils {
-  def messageCount(bootstrapServers: String, topicName: String): Long = {
+
+  private def getConsumer(bootstrapServers: String): KafkaConsumer[Integer, Externalizable] = {
     val props = new Properties
     val groupId = "spark-consumer-nsdsk-ku"
     props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
@@ -34,29 +35,27 @@ object KafkaUtils {
     props.put(ConsumerConfig.CLIENT_ID_CONFIG, groupId +"-"+ UUID.randomUUID)
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[IntegerDeserializer].getName)
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[ExternalizableDeserializer].getName)
+    new KafkaConsumer[Integer, Externalizable](props)
+  }
 
-    @transient lazy val consumer = new KafkaConsumer[Integer, Externalizable](props)
+  def messageCount(bootstrapServers: String, topicName: String): Long = {
+    @transient lazy val consumer = getConsumer(bootstrapServers)
+    
     val partitionInfo = consumer.partitionsFor(topicName).asScala
     val partitions = partitionInfo.map(pi => new TopicPartition(topicName, pi.partition()))
     consumer.assign(partitions.asJava)
     consumer.seekToEnd(Collections.emptySet())
     val endPartitions: Map[TopicPartition, Long] = partitions.map(p => p -> consumer.position(p))(collection.breakOut)
+    
     consumer.seekToBeginning(Collections.emptySet())
     val count = partitions.map(p => endPartitions(p) - consumer.position(p)).sum
+    
     consumer.close
     count
   }
 
   def messageCount(bootstrapServers: String, topicName: String, partition: Int): Long = {
-    val props = new Properties
-    val groupId = "spark-consumer-nsdsk-ku"
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
-    props.put(ConsumerConfig.CLIENT_ID_CONFIG, groupId +"-"+ UUID.randomUUID)
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[IntegerDeserializer].getName)
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[ExternalizableDeserializer].getName)
-
-    @transient lazy val consumer = new KafkaConsumer[Integer, Externalizable](props)
+    @transient lazy val consumer = getConsumer(bootstrapServers)
 
     val topicPartition = new TopicPartition(topicName, partition)
     val partitions = Seq(topicPartition)
