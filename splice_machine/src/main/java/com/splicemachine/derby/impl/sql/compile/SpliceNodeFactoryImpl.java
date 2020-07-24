e0c5049fddfe24a28b1c0cb72a02017d1b260c86
hatyo <52752081+hatyo@users.noreply.github.com>
2020-07-24 05:20:43 +0200

    DB-9620 Add ROWID SQL functions (2.8) (#3876)

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceNodeFactoryImpl.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/compile/SpliceNodeFactoryImpl.java
@@ -671,6 +671,12 @@ public class SpliceNodeFactoryImpl extends NodeFactory implements ModuleControl,
             case C_NodeTypes.STATEMENT_LIST_NODE:
                 return C_NodeNames.STATEMENT_LIST_NODE_NAME;
 
+            case C_NodeTypes.TO_INSTANT_NODE:
+                return C_NodeNames.TO_INSTANT_NODE_NAME;
+
+            case C_NodeTypes.TO_HBASE_ESCAPED_NODE:
+                return C_NodeNames.TO_HBASE_ESCAPED_NODE_NAME;
+
             // WARNING: WHEN ADDING NODE TYPES HERE, YOU MUST ALSO ADD
             // THEM TO tools/jar/DBMSnodes.properties
 
