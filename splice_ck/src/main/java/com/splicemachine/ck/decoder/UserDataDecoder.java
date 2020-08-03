e11ee1457d27d0098879817b633453482947e54e
Youssef Hatem <yhatem@splicemachine.com>
2020-08-03 13:22:32 +0200

    DB-9883 use decoder bitset to check if values are set.
    - rget now distinguishes between non-set values and NULL-set
      values.
    - this is relevant when printing partial row updates where some
      values are not set, previously they were printed as NULL, now
      '[]' is printed instead.

--- splice_ck/src/main/java/com/splicemachine/ck/decoder/UserDataDecoder.java
+++ splice_ck/src/main/java/com/splicemachine/ck/decoder/UserDataDecoder.java
@@ -8,16 +8,24 @@ import com.splicemachine.utils.IntArrays;
 import com.splicemachine.utils.Pair;
 import org.apache.hadoop.hbase.Cell;
 
+import java.util.BitSet;
+
 public abstract class UserDataDecoder {
 
-    public ExecRow decode(final Cell c) throws StandardException {
+    public Pair<BitSet, ExecRow> decode(final Cell c) throws StandardException {
         Pair<ExecRow, DescriptorSerializer[]> p = getExecRowAndDescriptors();
         ExecRow er = p.getFirst();
         DescriptorSerializer[] serializerDescriptors = p.getSecond();
         EntryDataDecoder entryDataDecoder = new EntryDataDecoder(IntArrays.count(er.nColumns()), null, serializerDescriptors);
         entryDataDecoder.set(c.getValueArray(), c.getValueOffset(), c.getValueLength());
         entryDataDecoder.decode(er);
-        return er;
+        BitSet bitSet = new BitSet(er.nColumns());
+        for(int i = 0; i < er.nColumns(); ++i) {
+            if(entryDataDecoder.getFieldDecoder().isSet(i)) {
+                bitSet.set(i);
+            }
+        }
+        return new Pair<>(bitSet, er);
     }
 
     public abstract Pair<ExecRow, DescriptorSerializer[]> getExecRowAndDescriptors() throws StandardException;
