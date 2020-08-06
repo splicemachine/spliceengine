b23ddbdb5a5c22982513f0c5aa8a5a38a42dfb77
Oleg Mazurov <omazurov@splicemachine.com>
2020-08-06 12:49:07 -0700

    DB-9939 Memory leak caused by unclosed scanners (#3918)

--- hbase_sql/src/main/java/com/splicemachine/hbase/MemstoreAwareObserver.java
+++ hbase_sql/src/main/java/com/splicemachine/hbase/MemstoreAwareObserver.java
@@ -199,6 +199,8 @@ public class MemstoreAwareObserver implements RegionCoprocessor, RegionObserver,
                     InternalScan iscan = new InternalScan(scan);
                     iscan.checkOnlyMemStore();
                     HRegion region = (HRegion) c.getEnvironment().getRegion();
+                    // We substitute the pre-created scanner, must close it to avoid a memory leak
+                    s.close();
                     return new MemStoreFlushAwareScanner(region, store, ((HStore)store).getScanInfo(), iscan, targetCols, getReadpoint(region), memstoreAware, memstoreAware.get());
                 } else { // Partition Miss
                     while (true) {
