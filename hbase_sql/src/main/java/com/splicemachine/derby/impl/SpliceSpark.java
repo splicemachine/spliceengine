b0352fedd450d928c06a619c784296cdbcc980ee
Mark Sirek <msirek@splicemachine.com>
2020-08-09 20:03:21 -0700

    DB-10025 A first write marker should only flag an insert or upsert, not a delete.

--- hbase_sql/src/main/java/com/splicemachine/derby/impl/SpliceSpark.java
+++ hbase_sql/src/main/java/com/splicemachine/derby/impl/SpliceSpark.java
@@ -17,7 +17,6 @@ package com.splicemachine.derby.impl;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.NoSuchElementException;
 
 import com.splicemachine.client.SpliceClient;
 import com.splicemachine.db.catalog.types.RoutineAliasInfo;
@@ -85,8 +84,10 @@ public class SpliceSpark {
             throw new RuntimeException("Trying to get a SparkSession from outside the OlapServer");
         }
         SparkSession result = sessions.get();
-        if (result == null)
+        if (result == null) {
             result = getSessionUnsafe().newSession();
+            sessions.set(result);
+        }
         return result;
     }
 
@@ -94,16 +95,12 @@ public class SpliceSpark {
      * get a local Spark Context, it should never be used when implementing Splice operations or functions
      */
     public static synchronized SparkSession getSessionUnsafe() {
-        SparkSession sessionToUse = sessions.get();
-        boolean isOlapWorker = Thread.currentThread().getName().startsWith("olap-worker-");
-        boolean needsReinitialization = !isOlapWorker &&
-                (sessionToUse == null || sessionToUse.sparkContext().isStopped()) &&
-                (session != null && !session.sparkContext().isStopped());
         if (!initialized) {
-            sessionToUse = session = initializeSparkSession();
+            session = initializeSparkSession();
             ctx =  new JavaSparkContext(session.sparkContext());
             initialized = true;
-        } else if (!needsReinitialization && session.sparkContext().isStopped()) {
+        } else if (session.sparkContext().isStopped() && !SpliceClient.isClient()) {
+            // Try to reinitialize the SparkContext, unless this is SpliceClient, in that case let the user handle the SparkContext
             LOG.warn("SparkContext is stopped, reinitializing...");
             try {
                 if (UserGroupInformation.isSecurityEnabled() && UserGroupInformation.isLoginKeytabBased()) {
@@ -112,30 +109,11 @@ public class SpliceSpark {
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
-            if (isOlapWorker)
-                System.exit(0);
-            sessionToUse = session = initializeSparkSession();
+            System.exit(0);
+            session = initializeSparkSession();
             ctx =  new JavaSparkContext(session.sparkContext());
         }
-        else {
-            if (sessionToUse == null || needsReinitialization) {
-                String allowMultipleContextsString = null;
-                try {
-                    allowMultipleContextsString = session.conf().get("spark.driver.allowMultipleContexts");
-                }
-                catch (NoSuchElementException e) {
-                }
-                boolean allowMultipleContexts =
-                        allowMultipleContextsString != null &&
-                        allowMultipleContextsString.equals("true");
-                if (session != null && !allowMultipleContexts)
-                    sessionToUse = session.newSession();
-                else
-                    sessionToUse = initializeSparkSession();
-            }
-        }
-        sessions.set(sessionToUse);
-        return sessionToUse;
+        return session;
     }
 
     public static synchronized JavaSparkContext getContext() {
