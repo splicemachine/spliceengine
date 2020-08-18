1e9e9d0b406aa5e8ee3134950858398f980d0087
ipraznik-splice <63786110+ipraznik-splice@users.noreply.github.com>
2020-08-18 18:52:50 +0200

    DB-9845 Spark 3.0 Migrate org.apache.spark.Accumulable to AccumulatorV2 (3.0) (#3833)
    * DB-9845 Spark 3.0 Migrate org.apache.spark.Accumulable to AccumulatorV2
    
    * DB-9845 reset in BadRecordsAccumulator copy is improved
    
    Co-authored-by: Martin Rupp <mrupp@splicemachine.com>

--- hbase_sql/src/main/java/com/splicemachine/stream/accumulator/BadRecordsAccumulator.java
+++ hbase_sql/src/main/java/com/splicemachine/stream/accumulator/BadRecordsAccumulator.java
@@ -14,34 +14,59 @@
 
 package com.splicemachine.stream.accumulator;
 
-import org.apache.spark.AccumulableParam;
 import com.splicemachine.derby.stream.control.BadRecordsRecorder;
+import org.apache.spark.util.AccumulatorV2;
 
 /**
  *
  * Accumulator for Bad Records from bulk import.
  * <p/>
  * Uses a {@link BadRecordsRecorder} to record each bad record to a temp file. Temp files
- * will be merged {@link #addInPlace(BadRecordsRecorder, BadRecordsRecorder)}.
+ * will be merged {@link #merge(AccumulatorV2<String, BadRecordsRecorder>)}.
  *
- * @see org.apache.spark.AccumulableParam
+ * @see org.apache.spark.util.AccumulatorV2;
  *
  */
-public class BadRecordsAccumulator implements AccumulableParam<BadRecordsRecorder, String> {
+public class BadRecordsAccumulator extends AccumulatorV2<String, BadRecordsRecorder> {
 
-    @Override
-    public BadRecordsRecorder addAccumulator(BadRecordsRecorder r, String s) {
-        r.recordBadRecord(s);
-        return r;
+    private final BadRecordsRecorder badRecordsRecorder;
+
+    public BadRecordsAccumulator(BadRecordsRecorder badRecordsRecorder) {
+        this.badRecordsRecorder = badRecordsRecorder;
     }
 
     @Override
-    public BadRecordsRecorder addInPlace(BadRecordsRecorder r1, BadRecordsRecorder r2) {
-        return r1.merge(r2); // r1 or r2 may have no files...
+    public boolean isZero() {
+        return badRecordsRecorder.getNumberOfBadRecords() == 0;
     }
 
     @Override
-    public BadRecordsRecorder zero(BadRecordsRecorder r) {
-        return r;
+    public AccumulatorV2<String, BadRecordsRecorder> copy() {
+        BadRecordsRecorder copyBadRecordsRecorder = new BadRecordsRecorder(
+                badRecordsRecorder.getStatusDirectory(),
+                badRecordsRecorder.getInputFilePath(),
+                badRecordsRecorder.getBadRecordTolerance());
+
+        return new BadRecordsAccumulator(copyBadRecordsRecorder);
+    }
+
+    @Override
+    public void reset() {
+        badRecordsRecorder.reset();
+    }
+
+    @Override
+    public void add(String badRecord) {
+        badRecordsRecorder.recordBadRecord(badRecord);
+    }
+
+    @Override
+    public void merge(AccumulatorV2<String, BadRecordsRecorder> other) {
+        badRecordsRecorder.merge(other.value());
+    }
+
+    @Override
+    public BadRecordsRecorder value() {
+        return badRecordsRecorder;
     }
 }
