0a8ebc795acff4712f40638e59492775e1f990d6
Zhen Li <zli@splicemachine.com>
2020-08-31 17:22:25 +0200

    DB-10067 Prevent from using indexes on expressions for now

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/FromBaseTable.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/FromBaseTable.java
@@ -268,6 +268,7 @@ public class FromBaseTable extends FromTable {
                     && (predList == null || !predList.canSupportIndexExcludedDefaults(tableNumber,currentConglomerateDescriptor, tableDescriptor))) {
                 return false;
             }
+            return currentConglomerateDescriptor.getIndexDescriptor().getCompiledExpressions().length <= 0;
         }
         return true;
     }
