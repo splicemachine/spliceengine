204271b1db9534c29db87d2f7cf76c26f996009f
Youssef Hatem <yhatem@splicemachine.com>
2020-09-01 16:07:29 +0200

    DB-10084 Add default flusher to known places.

--- hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultFlusher.java
+++ hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultFlusher.java
@@ -1,9 +1,21 @@
+/*
+ * Copyright (c) 2012 - 2020 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.splicemachine.compactions;
 
 import com.splicemachine.access.api.SConfiguration;
 import com.splicemachine.hbase.SpliceCompactionUtils;
-import com.splicemachine.hbase.TransactionsWatcher;
-import com.splicemachine.si.impl.Transaction;
 import com.splicemachine.si.impl.driver.SIDriver;
 import com.splicemachine.si.impl.server.FlushLifeCycleTrackerWithConfig;
 import com.splicemachine.si.impl.server.PurgeConfigBuilder;
@@ -17,8 +29,6 @@ import org.apache.hadoop.hbase.regionserver.MemStoreSnapshot;
 import org.apache.hadoop.hbase.regionserver.throttle.ThroughputController;
 
 import java.io.IOException;
-import java.sql.Driver;
-import java.sql.Timestamp;
 import java.util.List;
 
 public class SpliceDefaultFlusher extends DefaultStoreFlusher {
