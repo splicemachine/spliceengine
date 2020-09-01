be7fbb57ce684b6e435bcc7ddc2cfd0bf3f16d5a
Youssef Hatem <yhatem@splicemachine.com>
2020-09-01 15:39:06 +0200

    DB-10084 flush/compaction are aware of minRetentionPeriod.

--- hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultCompactor.java
+++ hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultCompactor.java
@@ -126,7 +126,7 @@ public class SpliceDefaultCompactor extends DefaultCompactor {
         assert request instanceof SpliceCompactionRequest;
         SpliceCompactionRequest spliceRequest = (SpliceCompactionRequest)request;
         // Used if we cannot compact in Spark
-        spliceRequest.setPurgeConfig(buildPurgeConfig(request, TransactionsWatcher.getLowWatermarkTransaction()));
+        spliceRequest.setPurgeConfig(buildPurgeConfig(request, SpliceCompactionUtils.getTxnLowWatermark(store)));
 
         if(!allowSpark || store.getRegionInfo().getTable().isSystemTable()) {
             isSpark = false;
@@ -207,7 +207,7 @@ public class SpliceDefaultCompactor extends DefaultCompactor {
         return paths;
     }
 
-    private SparkCompactionFunction getCompactionFunction(boolean isMajor, InetSocketAddress[] favoredNodes) {
+    private SparkCompactionFunction getCompactionFunction(boolean isMajor, InetSocketAddress[] favoredNodes) throws IOException {
         return new SparkCompactionFunction(
                 smallestReadPoint,
                 store.getTableName().getNamespace(),
@@ -215,7 +215,7 @@ public class SpliceDefaultCompactor extends DefaultCompactor {
                 store.getRegionInfo(),
                 store.getColumnFamilyDescriptor().getName(),
                 isMajor,
-                TransactionsWatcher.getLowWatermarkTransaction(),
+                SpliceCompactionUtils.getTxnLowWatermark(store),
                 favoredNodes);
     }
 
