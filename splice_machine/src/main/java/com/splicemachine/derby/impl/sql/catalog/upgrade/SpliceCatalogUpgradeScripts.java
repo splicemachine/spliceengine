65b52fb9e793ee6dc56c9d8fa003097f6e9c7441
Youssef Hatem <yhatem@splicemachine.com>
2020-09-17 18:23:53 +0200

    DB-10110 upgrade script for setting JavaClassName column.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/upgrade/SpliceCatalogUpgradeScripts.java
@@ -84,6 +84,7 @@ public class SpliceCatalogUpgradeScripts{
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1959), new UpgradeScriptForTriggerMultipleStatements(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1964), new UpgradeScriptForAliasToTableView(sdd,tc));
         scripts.put(new Splice_DD_Version(sdd,2,8,0, 1965), new UpgradeScriptToInvalidateStoredStatement(sdd,tc));
+        scripts.put(new Splice_DD_Version(sdd,2,8,0, 1975), new UpgradeScriptToSetJavaClassNameColumnInSYSALIASES(sdd, tc));
     }
 
     public void run() throws StandardException{
