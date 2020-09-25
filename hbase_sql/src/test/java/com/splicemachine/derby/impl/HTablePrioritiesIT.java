b7c26db28fd4f4c9e469222f7f03a15aa41f55e0
Martin Rupp <mrupp@splicemachine.com>
2020-09-25 11:04:08 +0200

    DB-10230 extract HTablePrioritiesIT

---
+++ hbase_sql/src/test/java/com/splicemachine/derby/impl/HTablePrioritiesIT.java
@@ -0,0 +1,90 @@
+package com.splicemachine.derby.impl;
+
+import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
+import com.splicemachine.derby.test.framework.SpliceTableWatcher;
+import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.test.SerialTest;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+
+@Category({SerialTest.class})
+public class HTablePrioritiesIT {
+    public static final String CLASS_NAME = HTablePrioritiesIT.class.getSimpleName().toUpperCase();
+    protected static String TABLE = "T";
+    protected static String TABLEA = "A";
+    protected static String TABLED = "D";
+    protected static String TABLEE = "E";
+
+    private static final SpliceWatcher spliceClassWatcher = new SpliceWatcher();
+    @ClassRule
+    public static TestRule classRule = spliceClassWatcher;
+
+    @Rule
+    public SpliceWatcher methodRule = new SpliceWatcher();
+
+    @Rule
+    public SpliceWatcher methodWatcher = new SpliceWatcher();
+
+    protected static SpliceSchemaWatcher spliceSchemaWatcher = new SpliceSchemaWatcher(CLASS_NAME);
+    protected static SpliceTableWatcher spliceTableWatcher = new SpliceTableWatcher(TABLE, spliceSchemaWatcher
+            .schemaName, "(name varchar(40), title varchar(40), age int)");
+    protected static SpliceTableWatcher spliceTableAWatcher = new SpliceTableWatcher(TABLEA, spliceSchemaWatcher
+            .schemaName, "(name varchar(40), title varchar(40), age int)");
+    protected static SpliceTableWatcher spliceTableDWatcher = new SpliceTableWatcher(TABLED, spliceSchemaWatcher
+            .schemaName, "(name varchar(40), title varchar(40), age int)");
+    protected static SpliceTableWatcher spliceTableEWatcher = new SpliceTableWatcher(TABLEE, spliceSchemaWatcher
+            .schemaName, "(name varchar(40), title varchar(40), age int)");
+    @ClassRule
+    public static TestRule chain = RuleChain.outerRule(spliceClassWatcher)
+            .around(spliceSchemaWatcher)
+            .around(spliceTableWatcher)
+            .around(spliceTableAWatcher)
+            .around(spliceTableDWatcher)
+            .around(spliceTableEWatcher);
+
+    @Test
+    public void testTablesPriority() throws Exception {
+        int prio0 = 0, prio100 = 0, prio110 = 0;
+        try(Admin admin= ConnectionFactory.createConnection(new Configuration()).getAdmin()){
+            HTableDescriptor[] tableDescriptors = admin.listTables();
+            for( HTableDescriptor td : tableDescriptors) {
+                String s = td.getValue("tableDisplayName");
+                if( s == null ) {
+
+                    String arr[] = {"splice:DROPPED_CONGLOMERATES", "splice:SPLICE_CONGLOMERATE",
+                            "splice:SPLICE_MASTER_SNAPSHOTS", "splice:SPLICE_REPLICATION_PROGRESS",
+                            "splice:SPLICE_SEQUENCES", "splice:SPLICE_TXN", "splice:TENTATIVE_DDL"};
+                    Assert.assertTrue(td.toString(), ArrayUtils.contains(arr, td.getTableName().getNameAsString()));
+                    Assert.assertEquals(td.toString(), 110, td.getPriority());
+                    prio110 ++;
+                }
+                else
+                {
+                    if( s.startsWith("SYS") || s.startsWith("splice:") || s.equals("MON_GET_CONNECTION") ) {
+                        Assert.assertEquals(td.toString(), 100, td.getPriority());
+                        prio100 ++;
+                    }
+                    else {
+                        Assert.assertEquals(td.toString(), 0, td.getPriority());
+                        prio0 ++;
+                    }
+                }
+            }
+        }
+        // assert there's actually tables we are checking
+        Assert.assertTrue( prio0 >=  4 );
+        Assert.assertTrue( prio100 > 5 );
+        Assert.assertTrue( prio110 > 5 );
+    }
+
+}
--- hbase_sql/src/test/java/com/splicemachine/derby/utils/VacuumIT.java
+++ hbase_sql/src/test/java/com/splicemachine/derby/utils/VacuumIT.java
@@ -131,34 +131,6 @@ public class VacuumIT extends SpliceUnitTest{
     }
 
     @Test
-    public void testSysTablesPriority() throws Exception {
-        try(Admin admin=ConnectionFactory.createConnection(new Configuration()).getAdmin()){
-            HTableDescriptor[] tableDescriptors = admin.listTables();
-            for( HTableDescriptor td : tableDescriptors) {
-                String s = td.getValue("tableDisplayName");
-                if( s == null ) {
-
-                    String arr[] = {"splice:DROPPED_CONGLOMERATES", "splice:SPLICE_CONGLOMERATE",
-                                    "splice:SPLICE_MASTER_SNAPSHOTS", "splice:SPLICE_REPLICATION_PROGRESS",
-                                    "splice:SPLICE_SEQUENCES", "splice:SPLICE_TXN", "splice:TENTATIVE_DDL"};
-                    Assert.assertTrue(td.toString(), ArrayUtils.contains(arr, td.getTableName().getNameAsString()));
-                    // check this
-                    Assert.assertEquals(td.toString(), 110, td.getPriority());
-                }
-                else
-                {
-                    if( s.startsWith("SYS") || s.startsWith("splice:") || s.equals("MON_GET_CONNECTION") ) {
-                        Assert.assertEquals(td.toString(), 100, td.getPriority());
-                    }
-                    else {
-                        Assert.assertEquals(td.toString(), 0, td.getPriority());
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
     public void testVacuumDoesNotDeleteConcurrentCreatedTable() throws Exception {
         Connection connection = spliceClassWatcher.getOrCreateConnection();
         connection.createStatement().execute(String.format("drop table %s.b if exists", CLASS_NAME));
