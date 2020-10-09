6878a73c6f2bf77794eeff14e6e5c912f427157c
Zhen Li <zli@splicemachine.com>
2020-10-09 16:08:51 +0200

    DB-10378 Fix duplicattion check for expression-based indexes (#4233)

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/actions/CreateIndexConstantOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/actions/CreateIndexConstantOperation.java
@@ -25,6 +25,8 @@ import com.splicemachine.db.iapi.services.io.FormatableBitSet;
 import com.splicemachine.db.iapi.services.property.PropertyUtil;
 import com.splicemachine.db.iapi.services.sanity.SanityManager;
 import com.splicemachine.db.iapi.sql.Activation;
+import com.splicemachine.db.iapi.sql.compile.CompilerContext;
+import com.splicemachine.db.iapi.sql.compile.Parser;
 import com.splicemachine.db.iapi.sql.conn.LanguageConnectionContext;
 import com.splicemachine.db.iapi.sql.depend.DependencyManager;
 import com.splicemachine.db.iapi.sql.dictionary.*;
@@ -39,6 +41,7 @@ import com.splicemachine.db.iapi.types.DataTypeDescriptor;
 import com.splicemachine.db.iapi.types.DataValueDescriptor;
 import com.splicemachine.db.iapi.types.HBaseRowLocation;
 import com.splicemachine.db.iapi.util.ByteArray;
+import com.splicemachine.db.impl.sql.compile.ValueNode;
 import com.splicemachine.db.impl.sql.execute.IndexColumnOrder;
 import com.splicemachine.db.impl.sql.execute.RowUtil;
 import com.splicemachine.db.impl.sql.execute.ValueRow;
@@ -473,6 +476,28 @@ public class CreateIndexConstantOperation extends IndexConstantOperation impleme
                         irg.excludeDefaults() != excludeDefaults) {
                     continue;
                 }
+
+                if (irg.isOnExpression() && exprBytecode.length > 0 && irg.getExprTexts().length == exprTexts.length) {
+                    CompilerContext newCC = lcc.pushCompilerContext();
+                    Parser p = newCC.getParser();
+
+                    boolean duplicateExprIndex = true;
+                    for (int i = 0; i < exprTexts.length; i++) {
+                        ValueNode currentExprAst = (ValueNode) p.parseSearchCondition(exprTexts[i]);
+                        ValueNode existingExprAst = (ValueNode) p.parseSearchCondition(irg.getExprTexts()[i]);
+                        if (!currentExprAst.equals(existingExprAst)) {
+                            duplicateExprIndex = false;
+                            break;
+                        }
+                    }
+                    lcc.popCompilerContext(newCC);
+
+                    if (duplicateExprIndex) {
+                        activation.addWarning(StandardException.newWarning(SQLState.LANG_INDEX_DUPLICATE, cd.getConglomerateName()));
+                        return;
+                    }
+                }
+
                 int[] bcps = irg.baseColumnPositions();
                 boolean[] ia = irg.isAscending();
                 int j = 0;
@@ -515,7 +540,7 @@ public class CreateIndexConstantOperation extends IndexConstantOperation impleme
                     }
                 }
 
-                if (j == baseColumnPositions.length) {  // share
+                if (j == baseColumnPositions.length && !irg.isOnExpression() && exprBytecode.length <= 0) {  // share
                     /*
                      * Don't allow users to create a duplicate index. Allow if being done internally
                      * for a constraint
