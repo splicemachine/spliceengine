6f7298d538b52ff9ce34c351d433c3d70beb90c9
Martin Rupp <mrupp@splicemachine.com>
2020-10-16 01:21:32 +0200

    DB-10506 fix SET_PURGE_DELETED_ROWS using unset minRetentionPeriod

--- splice_machine/src/main/java/com/splicemachine/derby/utils/SpliceAdmin.java
+++ splice_machine/src/main/java/com/splicemachine/derby/utils/SpliceAdmin.java
@@ -1455,7 +1455,7 @@ public class SpliceAdmin extends BaseAdminProcedures{
 
         tc.prepareDataDictionaryChange(DDLUtils.notifyMetadataChange(ddlChange));
         boolean b = "TRUE".compareToIgnoreCase(enable) == 0;
-        if(td.getMinRetentionPeriod() > 0) {
+        if(td.getMinRetentionPeriod() != null && td.getMinRetentionPeriod() > 0) {
             SpliceLogUtils.warn(LOG, "setting purge deleted rows on table %s which min retention period " +
                     "set to non-negative value, this could lead to incorrect time travel query results", td.getName());
         }
