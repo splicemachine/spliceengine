87597157af221ac7613a1472913352239fecaf10
Joe Panko <jpanko1@earthlink.net>
2020-10-15 03:40:41 -0500

    DB-9924 NSDS v2 Added kryo serialization.

--- hbase_sql/src/main/java/com/splicemachine/derby/stream/spark/KafkaStreamer.java
+++ hbase_sql/src/main/java/com/splicemachine/derby/stream/spark/KafkaStreamer.java
@@ -15,36 +15,16 @@
 
 package com.splicemachine.derby.stream.spark;
 
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.splicemachine.db.impl.sql.execute.ValueRow;
-import com.splicemachine.derby.stream.ActivationHolder;
-import com.splicemachine.derby.stream.iapi.OperationContext;
-import com.splicemachine.derby.utils.marshall.dvd.KryoDescriptorSerializer;
+import com.splicemachine.derby.impl.kryo.KryoSerialization;
 import com.splicemachine.si.impl.driver.SIDriver;
-import com.splicemachine.stream.StreamProtocol;
-import com.splicemachine.stream.handlers.OpenHandler;
-import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioSocketChannel;
-import org.apache.hadoop.hive.ql.exec.spark.KryoSerializer;
-import org.apache.hive.com.esotericsoftware.kryo.serializers.DefaultSerializers;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.log4j.Logger;
 import org.apache.spark.TaskContext;
-import org.apache.spark.TaskKilledException;
 import org.apache.spark.api.java.function.Function2;
 
 import java.io.Externalizable;
@@ -52,18 +32,10 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.io.Serializable;
-import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.Properties;
 import java.util.UUID;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Future;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * Created by dgomezferro on 5/25/16.
@@ -107,19 +79,25 @@ public class KafkaStreamer<T> implements Function2<Integer, Iterator<T>, Iterato
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.CLIENT_ID_CONFIG, "spark-producer-dss-ks-"+UUID.randomUUID() );
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ExternalizableSerializer.class.getName());
-        KafkaProducer<Integer, Externalizable> producer = new KafkaProducer<>(props);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
+        KafkaProducer<Integer, byte[]> producer = new KafkaProducer<>(props);
+
+        KryoSerialization kryo = new KryoSerialization();
+        kryo.init();
+
         int count = 0 ;
         while (locatedRowIterator.hasNext()) {
             T lr = locatedRowIterator.next();
 
-            ProducerRecord<Integer, Externalizable> record = new ProducerRecord(topicName, count++, lr);
+            ProducerRecord<Integer, byte[]> record = new ProducerRecord(topicName, count++, kryo.serialize(lr));
             producer.send(record);
             LOG.trace("KS.c sent "+partition.intValue()+" "+count+" "+lr);
         }
         LOG.trace("KS.c count "+partition.intValue()+" "+count);
 
         producer.close();
+        kryo.close();
+        
         // TODO Clean up
         return Arrays.asList("OK").iterator();
     }
