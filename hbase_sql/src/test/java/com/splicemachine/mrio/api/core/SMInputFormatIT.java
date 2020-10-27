7c4d79d5e08c2cc07267363f23c9283f71c02293
Martin Rupp <mrupp@splicemachine.com>
2020-10-27 13:50:15 +0100

    DB-10534 more 2.8 fixes

--- hbase_sql/src/test/java/com/splicemachine/mrio/api/core/SMInputFormatIT.java
+++ hbase_sql/src/test/java/com/splicemachine/mrio/api/core/SMInputFormatIT.java
@@ -130,17 +130,16 @@ public class SMInputFormatIT extends BaseMRIOTest {
         config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherA.toString());
         config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherA.toString());
         config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherA.tableName, tableWatcherA.getSchema())));
-        try(Job job = Job.getInstance(config, "Test Scan")) {
-            JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
-            List<Tuple2<RowLocation, ExecRow>> data = table.collect();
-            int i = 0;
-            for (Tuple2<RowLocation, ExecRow> tuple : data) {
-                i++;
-                Assert.assertNotNull(tuple._1());
-                Assert.assertNotNull(tuple._2());
-            }
-            Assert.assertEquals("Incorrect Results Returned", 2, i);
+        Job job = Job.getInstance(config, "Test Scan");
+        JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
+        List<Tuple2<RowLocation, ExecRow>> data = table.collect();
+        int i = 0;
+        for (Tuple2<RowLocation, ExecRow> tuple: data) {
+            i++;
+            Assert.assertNotNull(tuple._1());
+            Assert.assertNotNull(tuple._2());
         }
+        Assert.assertEquals("Incorrect Results Returned", 2,i);
     }
 
     @Test
@@ -148,17 +147,16 @@ public class SMInputFormatIT extends BaseMRIOTest {
         config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherB.toString());
         config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherB.toString());
         config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherB.tableName, tableWatcherB.getSchema())));
-        try(Job job = Job.getInstance(config, "Test Scan")) {
-            JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
-            List<Tuple2<RowLocation, ExecRow>> data = table.collect();
-            int i = 0;
-            for (Tuple2<RowLocation, ExecRow> tuple : data) {
-                i++;
-                Assert.assertNotNull(tuple._1());
-                Assert.assertNotNull(tuple._2());
-            }
-            Assert.assertEquals("Incorrect Results Returned", 10000, i);
+        Job job = Job.getInstance(config, "Test Scan");
+        JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormat.class, RowLocation.class, ExecRow.class);
+        List<Tuple2<RowLocation, ExecRow>> data = table.collect();
+        int i = 0;
+        for (Tuple2<RowLocation, ExecRow> tuple : data) {
+            i++;
+            Assert.assertNotNull(tuple._1());
+            Assert.assertNotNull(tuple._2());
         }
+        Assert.assertEquals("Incorrect Results Returned", 10000, i);
     }
 
     private void executeQueryAndAssert(String query, int expected) throws SQLException {
@@ -203,10 +201,9 @@ public class SMInputFormatIT extends BaseMRIOTest {
         config.set(MRConstants.SPLICE_TABLE_NAME, tableWatcherB.toString());
         config.set(MRConstants.SPLICE_INPUT_TABLE_NAME, tableWatcherB.toString());
         config.set(MRConstants.SPLICE_INPUT_CONGLOMERATE, String.valueOf(classWatcher.getConglomId(tableWatcherB.tableName, tableWatcherB.getSchema())));
-        try(Job job = Job.getInstance(config, "Test Scan")) {
-            JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormatFail.class, RowLocation.class, ExecRow.class);
-            table.collect();
-        }
+        Job job = Job.getInstance(config, "Test Scan");
+        JavaPairRDD<RowLocation, ExecRow> table = sparkWatcher.jsc.newAPIHadoopRDD(job.getConfiguration(), SMInputFormatFail.class, RowLocation.class, ExecRow.class);
+        table.collect();
     }
 
 }
