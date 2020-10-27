4ba87d4830b93cea06cbe0d78b891dcfa23c76c7
Martin Rupp <mrupp@splicemachine.com>
2020-10-27 13:43:48 +0100

    DB-9843 add unit tests

--- hbase_sql/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/ExternalTableUnitTests.java
+++ hbase_sql/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/ExternalTableUnitTests.java
@@ -1,6 +1,7 @@
 package com.splicemachine.derby.impl.sql.execute.operations;
 
 import com.splicemachine.derby.stream.spark.SparkExternalTableUtil;
+import com.splicemachine.derby.stream.utils.ExternalTableUtils;
 import org.apache.hadoop.fs.Path;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructType;
@@ -15,11 +16,6 @@ import static java.util.stream.Collectors.toList;
 
 public class ExternalTableUnitTests {
 
-    // todo: add error test for
-    // "hdfs://host:123/partition_test/web_sales5/ws_sold_date_sk=2450817/c=3.14/part-01434.c000.snappy.parquet",
-    // "hdfs://host:123/partition_test/web_sales5/ws_sold_date_sk=2450817/c=3.14/part-01434.c000.snappy.parquet",
-    // -> Conflicting partition column names detected
-    // todo: add test for more types (date, decimal, string etc.)
     @Test
     public void testParsePartitionsFromFiles() {
         String root = "hdfs://host:123/partition_test/web_sales5/";
@@ -39,14 +35,15 @@ public class ExternalTableUnitTests {
         List<Path> files = Arrays.stream(spaths).map(Path::new).collect(toList());
 
         com.splicemachine.spark.splicemachine.PartitionSpec ps = SparkExternalTableUtil.parsePartitionsFromFiles(
-                files, true, basePaths, null,
-                null );
-        Assert.assertEquals(ps.partitions().size(), 4);
-        Assert.assertEquals(ps.partitionColumns().fields().length, 2);
-        Assert.assertEquals(ps.partitionColumns().fields()[0].toString(), "StructField(c,DoubleType,true)");
-        Assert.assertEquals(ps.partitionColumns().fields()[1].toString(), "StructField(ws_sold_date_sk,IntegerType,true)");
-        Assert.assertEquals(ps.partitions().toList().last().toString(),
-                "PartitionPath([3.14,2450818]," + root + "c=3.14/ws_sold_date_sk=2450818)");
+                files, true, basePaths, null, null );
+        Assert.assertEquals("StructType(StructField(c,DoubleType,true), StructField(ws_sold_date_sk,IntegerType,true))",
+                ps.partitionColumns().toString());
+        Assert.assertEquals("List(" +
+                "PartitionPath([3.14,null]," + root + "c=3.14/ws_sold_date_sk=__HIVE_DEFAULT_PARTITION__), " +
+                "PartitionPath([3.14,2450817]," + root + "c=3.14/ws_sold_date_sk=2450817), " +
+                "PartitionPath([3.14,2450816]," + root + "c=3.14/ws_sold_date_sk=2450816), " +
+                "PartitionPath([3.14,2450818]," + root + "c=3.14/ws_sold_date_sk=2450818))",
+                ps.partitions().toString());
     }
 
     @Test
@@ -65,14 +62,11 @@ public class ExternalTableUnitTests {
         s = s.add("ws_sold_date_sk", DataTypes.DoubleType);
 
         com.splicemachine.spark.splicemachine.PartitionSpec ps = SparkExternalTableUtil.parsePartitionsFromFiles(
-                files, true, basePaths, s,
-                null );
-        Assert.assertEquals(1, ps.partitions().size(),1);
-        Assert.assertEquals(2, ps.partitionColumns().fields().length);
-        Assert.assertEquals("StructField(c,StringType,true)", ps.partitionColumns().fields()[0].toString());
-        Assert.assertEquals("StructField(ws_sold_date_sk,DoubleType,true)", ps.partitionColumns().fields()[1].toString());
-        Assert.assertEquals("PartitionPath([3.14,2450818.0]," + root + "c=3.14/ws_sold_date_sk=2450818)",
-                ps.partitions().toList().last().toString());
+                files, true, basePaths, s, null );
+        Assert.assertEquals("StructType(StructField(c,StringType,true), StructField(ws_sold_date_sk,DoubleType,true))",
+                ps.partitionColumns().toString());
+        Assert.assertEquals("List(PartitionPath([3.14,2450818.0]," + root + "c=3.14/ws_sold_date_sk=2450818))",
+                ps.partitions().toString());
     }
 
     @Test
@@ -118,11 +112,64 @@ public class ExternalTableUnitTests {
         com.splicemachine.spark.splicemachine.PartitionSpec ps = SparkExternalTableUtil.parsePartitionsFromFiles(
                 files, true, basePaths, s, null);
 
-        Assert.assertEquals(1, ps.partitions().size());
-        Assert.assertEquals(2, ps.partitionColumns().fields().length);
-        Assert.assertEquals("StructField(c,DoubleType,true)", ps.partitionColumns().fields()[0].toString());
-        Assert.assertEquals("StructField(ws_sold_date_sk,DoubleType,true)", ps.partitionColumns().fields()[1].toString());
-        Assert.assertEquals("PartitionPath([3.14,null]," + root + "c=3.14/ws_sold_date_sk=__HIVE_DEFAULT_PARTITION__)",
-                ps.partitions().toList().last().toString());
+        Assert.assertEquals("StructType(StructField(c,DoubleType,true), StructField(ws_sold_date_sk,DoubleType,true))",
+                ps.partitionColumns().toString());
+        Assert.assertEquals("List(PartitionPath([3.14,null]," + root + "c=3.14/ws_sold_date_sk=__HIVE_DEFAULT_PARTITION__))",
+                ps.partitions().toString());
+    }
+
+    StructType structTypeSample1()
+    {
+        StructType s = new StructType();
+        s = s.add("col0", DataTypes.DoubleType);
+        s = s.add("col1", DataTypes.IntegerType);
+        s = s.add("col2", DataTypes.BooleanType);
+        s = s.add("col3", DataTypes.LongType);
+        return s;
+    }
+
+    void testSort(int[] partition, int[] expected)
+    {
+        StructType s = structTypeSample1();
+        StructType original = structTypeSample1();
+
+        SparkExternalTableUtil.preSortColumns(s.fields(), partition);
+        assert s.length() == original.length();
+        for(int i = 0; i < s.length(); i++) {
+            Assert.assertEquals( s.fields()[i].toString(), original.fields()[expected[i]].toString());
+        }
+
+        // use sortColumns to reverse the sorting done by preSortColumns
+        SparkExternalTableUtil.sortColumns(s.fields(), partition);
+        Assert.assertEquals(original.toString(), s.toString());
+    }
+
+    @Test
+    public void testSortColumns()
+    {
+        testSort( new int[]{0, 2}, new int[]{1, 3, 0, 2} );
+        testSort( new int[]{0},    new int[]{1, 2, 3, 0} );
+        testSort( new int[]{3},    new int[]{0, 1, 2, 3} );
+        testSort( new int[]{},     new int[]{0, 1, 2, 3} );
+    }
+
+    @Test
+    public void testGetSuggestedSchema()
+    {
+        Assert.assertEquals( "CREATE EXTERNAL TABLE T (col0 DOUBLE, col1 INT, col2 BOOLEAN, col3 BIGINT); " +
+                        "(note: could not check path, so no PARTITIONED BY information available)",
+                SparkExternalTableUtil.getSuggestedSchema(structTypeSample1(), null).toString() );
+
+        StructType s = new StructType();
+        s = s.add("col0", DataTypes.DoubleType);
+        s = s.add("col3", DataTypes.StringType);
+
+        StructType part = new StructType();
+        part = part.add("col1", DataTypes.IntegerType);
+        part = part.add("col2", DataTypes.FloatType);
+        Assert.assertEquals( "CREATE EXTERNAL TABLE T (col0 DOUBLE, col3 CHAR/VARCHAR(x), col1 INT, col2 REAL) " +
+                        "PARTITIONED BY(col1, col2);",
+                ExternalTableUtils.getSuggestedSchema(s, part).toString() );
+
     }
 }
