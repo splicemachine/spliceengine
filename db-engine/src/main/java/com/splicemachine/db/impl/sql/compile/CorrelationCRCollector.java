15ec0be4b62e8d0bf4660cce175c01355c6b46a9
Youssef Hatem <yhatem@splicemachine.com>
2020-10-27 14:23:13 +0100

    DB-10521 cleanup and refactoring.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CorrelationCRCollector.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CorrelationCRCollector.java
@@ -1,18 +1,15 @@
 /*
+ *  Copyright (c) 2012 - 2020 Splice Machine, Inc.
  *
- *  * Copyright (c) 2012 - 2020 Splice Machine, Inc.
- *  *
- *  * This file is part of Splice Machine.
- *  * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
- *  * GNU Affero General Public License as published by the Free Software Foundation, either
- *  * version 3, or (at your option) any later version.
- *  * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  * See the GNU Affero General Public License for more details.
- *  * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
- *  * If not, see <http://www.gnu.org/licenses/>.
- *
- *
+ *  This file is part of Splice Machine.
+ *  Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ *  GNU Affero General Public License as published by the Free Software Foundation, either
+ *  version 3, or (at your option) any later version.
+ *  Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Affero General Public License for more details.
+ *  You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ *  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.splicemachine.db.impl.sql.compile;
@@ -35,23 +32,9 @@ public class CorrelationCRCollector implements Visitor {
 
     List<ValueNode> correlationCRs = new ArrayList<>();
 
-    boolean shouldAdd(Visitable node) {
-        if (node instanceof ColumnReference) {
-            return ((ColumnReference) node).getCorrelated();
-        } else if (node instanceof VirtualColumnNode) {
-            return ((VirtualColumnNode) node).getCorrelated();
-        } else if (node instanceof MethodCallNode) {
-            /* trigger action references are correlated */
-            return ((MethodCallNode) node).getMethodName().equals("getTriggerExecutionContext") ||
-                    ((MethodCallNode) node).getMethodName().equals("TriggerOldTransitionRows") ||
-                    ((MethodCallNode) node).getMethodName().equals("TriggerNewTransitionRows");
-        }
-        return false;
-    }
-
     @Override
     public Visitable visit(Visitable node, QueryTreeNode parent) throws StandardException {
-        if(shouldAdd(node)) {
+        if(CorrelationUtil.isCorrelated(node)) {
             assert node instanceof ValueNode;
             correlationCRs.add((ValueNode)node);
         }
@@ -69,7 +52,7 @@ public class CorrelationCRCollector implements Visitor {
     }
 
     @Override
-    public boolean skipChildren(Visitable node) throws StandardException {
+    public boolean skipChildren(Visitable node) {
         return false;
     }
 }
