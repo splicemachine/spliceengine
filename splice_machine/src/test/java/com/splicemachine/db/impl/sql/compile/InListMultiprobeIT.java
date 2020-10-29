305d8c946f6015d2a83daa5ff993c476993895ba
Martin Rupp <mrupp@splicemachine.com>
2020-10-29 14:31:34 +0100

    DB-10614 re-enable InListMultiprobeIT

--- splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/InListMultiprobeIT.java
+++ splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/InListMultiprobeIT.java
@@ -40,9 +40,8 @@ import static org.junit.Assert.assertEquals;
 /**
  * Test the IN list predicates with the multiprobe index scan
  */
-@Ignore
 @RunWith(Parameterized.class)
-@Category({SerialTest.class, LongerThanTwoMinutes.class})
+@Category({SerialTest.class})
 public class InListMultiprobeIT  extends SpliceUnitTest {
     
     private Boolean useSpark;
