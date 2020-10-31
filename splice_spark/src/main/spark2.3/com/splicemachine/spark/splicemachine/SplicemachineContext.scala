2ea657acf79c241b35ce18b91750e088691087e3
Joe Panko <jpanko1@earthlink.net>
2020-10-31 04:31:28 -0500

    DB-9519 NSDS v1 Refactored schemaString and modifyRdd.

--- splice_spark/src/main/spark2.3/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
+++ splice_spark/src/main/spark2.3/com/splicemachine/spark/splicemachine/SplicemachineContext.scala
@@ -148,64 +148,6 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
 
   /**
     *
-    * Generate the schema string for create table.
-    *
-    * @param schema
-    * @param url
-    * @return
-    */
-  def schemaString(schema: StructType, url: String): String = {
-    val sb = new StringBuilder()
-    val dialect = JdbcDialects.get(url)
-    schema.fields foreach { field =>
-      val name = dialect.quoteIdentifier(field.name)
-      val typ: String = getJdbcType(field.dataType, dialect).databaseTypeDefinition
-      val nullable = if (field.nullable) "" else "NOT NULL"
-      sb.append(s", $name $typ $nullable")
-    }
-    if (sb.length < 2) "" else sb.substring(2)
-  }
-
-  /**
-    *
-    * Retrieve the JDBC type based on the Spark DataType and JDBC Dialect.
-    *
-    * @param dt
-    * @param dialect
-    * @return
-    */
-  private[this]def getJdbcType(dt: DataType, dialect: JdbcDialect): JdbcType = {
-    dialect.getJDBCType(dt).orElse(getCommonJDBCType(dt)).getOrElse(
-      throw new IllegalArgumentException(s"Can't get JDBC type for ${dt.simpleString}"))
-  }
-
-  /**
-    * Retrieve standard jdbc types.
-    *
-    * @param dt The datatype (e.g. [[org.apache.spark.sql.types.StringType]])
-    * @return The default JdbcType for this DataType
-    */
-  private[this] def getCommonJDBCType(dt: DataType) = {
-    dt match {
-      case IntegerType => Option(JdbcType("INTEGER", java.sql.Types.INTEGER))
-      case LongType => Option(JdbcType("BIGINT", java.sql.Types.BIGINT))
-      case DoubleType => Option(JdbcType("DOUBLE PRECISION", java.sql.Types.DOUBLE))
-      case FloatType => Option(JdbcType("REAL", java.sql.Types.FLOAT))
-      case ShortType => Option(JdbcType("INTEGER", java.sql.Types.SMALLINT))
-      case ByteType => Option(JdbcType("BYTE", java.sql.Types.TINYINT))
-      case BooleanType => Option(JdbcType("BIT(1)", java.sql.Types.BIT))
-      case StringType => Option(JdbcType("TEXT", java.sql.Types.CLOB))
-      case BinaryType => Option(JdbcType("BLOB", java.sql.Types.BLOB))
-      case TimestampType => Option(JdbcType("TIMESTAMP", java.sql.Types.TIMESTAMP))
-      case DateType => Option(JdbcType("DATE", java.sql.Types.DATE))
-      case t: DecimalType => Option(
-        JdbcType(s"DECIMAL(${t.precision},${t.scale})", java.sql.Types.DECIMAL))
-      case _ => None
-    }
-  }
-
-  /**
-    *
     * Determine whether a table exists (uses JDBC).
     *
     * @param schemaTableName
@@ -651,7 +593,7 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
     * @param schemaTableName table to delete from
     */
   def delete(rdd: JavaRDD[Row], schema: StructType, schemaTableName: String): Unit = {
-    val tableSchemaStr = schemaString(schemaTableName, schema)
+    val tableSchemaStr = schemaString( columnInfo(schemaTableName) , schema)
     val modRdd = modifyRdd(rdd, tableSchemaStr)
     SpliceRDDVTI.datasetThreadLocal.set(modRdd)
     val jdbcOptions = new JDBCOptions(Map(
@@ -689,7 +631,7 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
     * @param schemaTableName
     */
   def update(rdd: JavaRDD[Row], schema: StructType, schemaTableName: String): Unit = {
-    val tableSchemaStr = schemaString(schemaTableName, schema)
+    val tableSchemaStr = schemaString( columnInfo(schemaTableName) , schema)
     val modRdd = modifyRdd(rdd, tableSchemaStr)
     SpliceRDDVTI.datasetThreadLocal.set(modRdd)
     val jdbcOptions = new JDBCOptions(Map(
@@ -712,53 +654,12 @@ class SplicemachineContext(options: Map[String, String]) extends Serializable {
     executeUpd(combinedText)
   }
 
-  private[this] def modifyRdd(rdd: RDD[Row], schemaStr: String): RDD[Row] =
-    if( schemaStr.contains("TIME,") || schemaStr.endsWith("TIME") ) {
-      var i = 0
-      var tmIdx = collection.mutable.Seq.empty[Int]
-      schemaStr.split(",").foreach(s => {
-        if( s.endsWith("TIME") ) { tmIdx = tmIdx :+ i }
-        i += 1
-      })
-      rdd.map(r => {
-        val cols = r.toSeq
-        tmIdx.foreach(i =>
-          if( cols(i).isInstanceOf[java.sql.Timestamp] ) {
-            cols.updated(i, java.sql.Time.valueOf( r.getTimestamp(i).toLocalDateTime.toLocalTime ))
-          }
-        )
-        Row.fromSeq(cols)
-      })
-    } else {
-      rdd
-    }
-
-  /** Schema string built from JDBC metadata. */
-  def schemaString(schemaTableName: String, schema: StructType = new StructType()): String = {
-    val info = SpliceJDBCUtil.retrieveColumnInfo(
-      new JDBCOptions( Map(
+  def columnInfo(schemaTableName: String): Array[Seq[String]] =
+    SpliceJDBCUtil.retrieveColumnInfo(
+      new JDBCOptions(Map(
         JDBCOptions.JDBC_URL -> url,
-        JDBCOptions.JDBC_TABLE_NAME -> schemaTableName
-      ))
-    ).map(i => {
-      val colName = i(0)
-      val sqlType = i(1)
-      val size = sqlType match {
-        case "VARCHAR" => s"(${i(2)})"
-        case "DECIMAL" | "NUMERIC" => s"(${i(2)},${i(3)})"
-        case _ => ""
-      }
-      s"$colName $sqlType$size"
-    })
-
-    if( schema.isEmpty ) {
-      info.mkString(", ")
-    } else {
-      schema.map( field => {
-        info.find( col => col.toUpperCase.startsWith( s"${field.name.toUpperCase} " ) ).getOrElse("")
-      }).mkString(", ")
-    }
-  }
+        JDBCOptions.JDBC_TABLE_NAME -> schemaTableName))
+    )
 
   /**
    * Bulk Import HFile from a dataframe into a schemaTableName(schema.table)
