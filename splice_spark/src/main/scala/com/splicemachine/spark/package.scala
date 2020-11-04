d39f6d6397097fbc36a50c5730772db4b1fd704d
jpanko1 <jpanko1@users.noreply.github.com>
2020-11-04 16:24:22 -0600

    DB-9519 NSDS Time Support (2.8) (#4337)
    * DB-9519 NSDS v1 Time column support.
    
    * DB-9519 NSDS v1 Refactored schemaString and modifyRdd.
    
    * DB-9519 Fix Spotbugs.
    
    * DB-9519 SQLDouble.read() support for float.
    
    * DB-9519 Added decimal digits to column info.
    
    * DB-9519 Fix Spotbugs (NaN comparison).

--- splice_spark/src/main/scala/com/splicemachine/spark/package.scala
+++ splice_spark/src/main/scala/com/splicemachine/spark/package.scala
@@ -16,7 +16,10 @@
  */
 package com.splicemachine.spark
 
-import org.apache.spark.sql.{DataFrame, DataFrameReader, DataFrameWriter}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
+import org.apache.spark.sql.types.{BinaryType, BooleanType, ByteType, DataType, DateType, DecimalType, DoubleType, FloatType, IntegerType, LongType, ShortType, StringType, StructType, TimestampType}
+import org.apache.spark.sql.{DataFrame, DataFrameReader, DataFrameWriter, Row}
 
 package object splicemachine {
 
@@ -35,4 +38,107 @@ package object splicemachine {
   implicit class SplicemachineDataFrameWriter[T](writer: DataFrameWriter[T]) {
     def splicemachine = writer.format("com.splicemachine.spark.splicemachine").save
   }
+
+  def modifyRdd(rdd: RDD[Row], schemaStr: String): RDD[Row] =
+    if( schemaStr.contains("TIME,") || schemaStr.endsWith("TIME") ) {
+      var i = 0
+      var tmIdx = collection.mutable.Seq.empty[Int]
+      schemaStr.split(",").foreach(s => {
+        if( s.endsWith("TIME") ) { tmIdx = tmIdx :+ i }
+        i += 1
+      })
+      rdd.map(r => {
+        val cols = r.toSeq
+        tmIdx.foreach(i =>
+          if( cols(i).isInstanceOf[java.sql.Timestamp] ) {
+            cols.updated(i, java.sql.Time.valueOf( r.getTimestamp(i).toLocalDateTime.toLocalTime ))
+          }
+        )
+        Row.fromSeq(cols)
+      })
+    } else {
+      rdd
+    }
+
+  /** Schema string built from JDBC metadata. */
+  def schemaString(columnInfo: Array[Seq[String]], schema: StructType = new StructType()): String = {
+    val info = columnInfo
+      .map(i => {
+        val colName = i(0)
+        val sqlType = i(1)
+        val size = sqlType match {
+          case "VARCHAR" => s"(${i(2)})"
+          case "DECIMAL" | "NUMERIC" => s"(${i(2)},${i(3)})"
+          case _ => ""
+        }
+        s"$colName $sqlType$size"
+      })
+
+    if( schema.isEmpty ) {
+      info.mkString(", ")
+    } else {
+      schema.map( field => {
+        info.find( col => col.toUpperCase.startsWith( s"${field.name.toUpperCase} " ) ).getOrElse("")
+      }).mkString(", ")
+    }
+  }
+  
+  /**
+   *
+   * Generate the schema string for create table.
+   *
+   * @param schema
+   * @param url
+   * @return
+   */
+  def schemaString(schema: StructType, url: String): String = {
+    val sb = new StringBuilder()
+    val dialect = JdbcDialects.get(url)
+    schema.fields foreach { field =>
+      val name = dialect.quoteIdentifier(field.name)
+      val typ: String = getJdbcType(field.dataType, dialect).databaseTypeDefinition
+      val nullable = if (field.nullable) "" else "NOT NULL"
+      sb.append(s", $name $typ $nullable")
+    }
+    if (sb.length < 2) "" else sb.substring(2)
+  }
+
+  /**
+   *
+   * Retrieve the JDBC type based on the Spark DataType and JDBC Dialect.
+   *
+   * @param dt
+   * @param dialect
+   * @return
+   */
+  private[this]def getJdbcType(dt: DataType, dialect: JdbcDialect): JdbcType = {
+    dialect.getJDBCType(dt).orElse(getCommonJDBCType(dt)).getOrElse(
+      throw new IllegalArgumentException(s"Can't get JDBC type for ${dt.simpleString}"))
+  }
+
+  /**
+   * Retrieve standard jdbc types.
+   *
+   * @param dt The datatype (e.g. [[org.apache.spark.sql.types.StringType]])
+   * @return The default JdbcType for this DataType
+   */
+  private[this] def getCommonJDBCType(dt: DataType) = {
+    dt match {
+      case IntegerType => Option(JdbcType("INTEGER", java.sql.Types.INTEGER))
+      case LongType => Option(JdbcType("BIGINT", java.sql.Types.BIGINT))
+      case DoubleType => Option(JdbcType("DOUBLE PRECISION", java.sql.Types.DOUBLE))
+      case FloatType => Option(JdbcType("REAL", java.sql.Types.FLOAT))
+      case ShortType => Option(JdbcType("INTEGER", java.sql.Types.SMALLINT))
+      case ByteType => Option(JdbcType("BYTE", java.sql.Types.TINYINT))
+      case BooleanType => Option(JdbcType("BIT(1)", java.sql.Types.BIT))
+      case StringType => Option(JdbcType("TEXT", java.sql.Types.CLOB))
+      case BinaryType => Option(JdbcType("BLOB", java.sql.Types.BLOB))
+      case TimestampType => Option(JdbcType("TIMESTAMP", java.sql.Types.TIMESTAMP))
+      case DateType => Option(JdbcType("DATE", java.sql.Types.DATE))
+      case t: DecimalType => Option(
+        JdbcType(s"DECIMAL(${t.precision},${t.scale})", java.sql.Types.DECIMAL))
+      case _ => None
+    }
+  }
+
 }
