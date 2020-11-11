9a5918ea6c6e5a489bec5955c2efcdd49458199a
Joe Panko <jpanko1@earthlink.net>
2020-11-10 22:35:30 -0600

    DB-9723 NSDS v2 Mapping Spark StringType to varchar instead of clob, plus refactoring.

--- splice_spark2/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/SplicemachineDialect2.scala
+++ splice_spark2/src/main/scala/org/apache/spark/sql/execution/datasources/jdbc/SplicemachineDialect2.scala
@@ -1,9 +1,24 @@
+/*
+ * Copyright (c) 2012 - 2020 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.apache.spark.sql.execution.datasources.jdbc
 
 import java.sql.Types
 
 import com.splicemachine.db.iapi.reference.Limits
-import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcDialects, JdbcType}
+import org.apache.spark.sql.jdbc.{JdbcDialect, JdbcType}
 import org.apache.spark.sql.types._
 
 /**
@@ -28,7 +43,7 @@ class SplicemachineDialect2 extends JdbcDialect {
   }
 
   override def getJDBCType(dt: DataType): Option[JdbcType] = dt match {
-    case StringType => Option(JdbcType("CLOB", java.sql.Types.CLOB))
+    case StringType => Option(JdbcType("VARCHAR(5000)", java.sql.Types.VARCHAR))
     case ByteType => Option(JdbcType("TINYINT", java.sql.Types.TINYINT))
     case ShortType => Option(JdbcType("SMALLINT", java.sql.Types.SMALLINT))
     case BooleanType => Option(JdbcType("BOOLEAN", java.sql.Types.BOOLEAN))
