04a37bb9bf29415de2f15a0a2632346f0d46c630
jpanko1 <jpanko1@users.noreply.github.com>
2020-11-11 23:25:58 -0600

    DB-9581 DB-10588 NSDS Support Case Sensitive Column Names (#4509)
    * DB-9581 DB-10588 NSDS v1 Support toggling case sensitivity of column names.
    
    * DB-9581 DB-10588 NSDS v1 IT for case sensitive column names.
    
    * DB-9581 DB-10588 NSDS v1 IT Added df, delete, and upsert.
    
    * DB-9581 DB-10588 NSDS v2 Support toggling case sensitivity of column names.
    
    * DB-9581 DB-10588 NSDS v2 IT for case sensitive column names.
    
    * DB-9581 DB-10588 NSDS v2 IT Added df, delete, and upsert.
    
    * DB-9581 DB-10588 NSDS v2 Support case insensitive insert.
    
    * DB-9581 DB-10588 NSDS v1 Update column comparison for case insensitive scenario.
    
    * DB-9581 DB-10588 NSDS v1 Update exception messages.
    
    * DB-9581 DB-10588 NSDS v1 IT for lower case column names in dataframe.
    
    * DB-9581 DB-10588 NSDS v2 IT for lower case column names in dataframe.
    
    Co-authored-by: Mark Sirek <msirek@splicemachine.com>

---
+++ splice_spark2/src/test/scala/com/splicemachine/spark2/splicemachine/column_case/OppositeCase_TestContext.scala
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2012 - 2020 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.splicemachine.spark2.splicemachine.column_case
+
+import com.splicemachine.spark2.splicemachine._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{Row, SparkSession}
+import org.scalatest.Suite
+
+/** Table column names are in all caps, but dataframe fields are all lower case. */
+trait OppositeCase_TestContext extends TestContext { self: Suite =>
+  override def table(): String = "oppositeCaseColumnTable"
+
+  override val allTypesCreateStringWithPrimaryKey = "(" +
+    "\"A\" int, " +
+    "\"B\" int, " +
+    "primary key (\"A\")" +
+    ")"
+
+  override val allTypesInsertString = "(" +
+    "\"A\", " +
+    "\"B\" " +
+    ") "
+  
+  override def allTypesSchema(withPrimaryKey: Boolean): StructType =
+    StructType(
+      StructField("a", IntegerType, false) ::
+      StructField("b", IntegerType, true) ::
+      Nil)
+
+  override def beforeAll() {
+    spark = SparkSession.builder.config(conf).getOrCreate
+    splicemachineContext = new SplicemachineContext(defaultJDBCURL)
+    internalTNDF = dataframe(
+      rdd( Seq( Row.fromSeq( testRow ) ) ),
+      allTypesSchema(true)
+    )
+  }
+
+  override def afterAll() {
+    dropInternalTable
+    dropSchema(schema)
+    if (spark != null) spark.stop()
+  }
+}
