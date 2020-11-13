95cdc47002dca9a67dac51bc144d88e6ff4791dd
Mark Sirek <msirek@splicemachine.com>
2020-11-13 14:56:01 -0800

    Revert "DB-10690 SICompactionStateMutate shouldn't have public state"
    This reverts commit adb172ffc877c0c9e132f4993afbd24e15e3937d.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CastNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CastNode.java
@@ -1220,7 +1220,6 @@ public class CastNode extends ValueNode
 
     public void setForSbcsData(boolean forSbcsData) {
         this.forSbcsData = forSbcsData;
-    }
 
     @Override
     public boolean isKnownConstant(boolean considerParameters) { return castOperand.isKnownConstant(considerParameters); }
