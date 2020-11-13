eab4e73d20ff677abde236e1930369589897db85
Martin Rupp <martinrupp@users.noreply.github.com>
2020-11-13 03:42:55 +0100

    DB-10665 fix TypeDescriptorImpl serialization problem (3.0) (#4503)
    * DB-10665 fix TypeDescriptorImpl serialization problem
    
    * DB-10665 address reviews

--- db-engine/src/main/java/com/splicemachine/db/iapi/types/DataTypeDescriptor.java
+++ db-engine/src/main/java/com/splicemachine/db/iapi/types/DataTypeDescriptor.java
@@ -45,6 +45,7 @@ import com.splicemachine.db.iapi.services.loader.ClassFactory;
 import com.splicemachine.db.iapi.services.loader.ClassInspector;
 import com.splicemachine.db.iapi.services.sanity.SanityManager;
 import com.splicemachine.db.iapi.sql.conn.ConnectionUtil;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.spark.sql.types.StructField;
 
 import java.io.IOException;
@@ -67,7 +68,12 @@ import static com.splicemachine.db.iapi.types.TypeId.DECFLOAT_PRECISION;
  * <p/>
  * A DataTypeDescriptor is immutable.
  */
+@SuppressFBWarnings({"EI_EXPOSE_REP2", "EI_EXPOSE_REP"})
 public class DataTypeDescriptor implements Formatable{
+    // DO NOT CHANGE OR REMOVE THIS WITHOUT PROVIDING AN UPDATE SCRIPT
+    // it is needed for ObjectStreamClass.getDeclaredSUID. see DB-10665
+    public static final long serialVersionUID = 804804029538241393l;
+
     /********************************************************
      **
      **    This class implements Formatable. That means that it
