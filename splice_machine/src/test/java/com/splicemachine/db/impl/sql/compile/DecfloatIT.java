7ae5df642591a98004e6172eca4449651a0db584
Arnaud Lacurie <alacurie@splicemachine.com>
2020-11-23 18:20:06 +0100

    DB-10677 First batch of tests for DECFLOAT

--- splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/DecfloatIT.java
+++ splice_machine/src/test/java/com/splicemachine/db/impl/sql/compile/DecfloatIT.java
@@ -17,6 +17,7 @@ package com.splicemachine.db.impl.sql.compile;
 import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
 import com.splicemachine.derby.test.framework.SpliceUnitTest;
 import com.splicemachine.derby.test.framework.SpliceWatcher;
+import com.splicemachine.derby.test.framework.TestConnection;
 import com.splicemachine.test.SerialTest;
 import com.splicemachine.test_tools.TableCreator;
 import org.junit.*;
@@ -30,10 +31,7 @@ import splice.com.google.common.collect.Lists;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.Types;
+import java.sql.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -46,6 +44,7 @@ import static com.splicemachine.test_tools.Rows.rows;
 public class DecfloatIT extends SpliceUnitTest {
 
     private Boolean useSpark;
+    private TestConnection conn;
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
@@ -64,15 +63,25 @@ public class DecfloatIT extends SpliceUnitTest {
     @Rule
     public SpliceWatcher methodWatcher = new SpliceWatcher(CLASS_NAME);
 
+    @Before
+    public void setUp() throws Exception{
+        SpliceWatcher.ConnectionBuilder connBuilder = methodWatcher.connectionBuilder();
+        if (useSpark != null){
+            connBuilder.useOLAP(useSpark);
+        }
+        conn = connBuilder.build();
+        conn.setAutoCommit(false);
+    }
+
     public DecfloatIT(Boolean useSpark) {
         this.useSpark = useSpark;
     }
 
     @Test
     public void castToDecfloat() throws Exception {
-        String sqlText = format("select cast('1.012345678901234567890123456789' as decfloat) from sysibm.sysdummy1 --spliceproperties useSpark=%s\n", useSpark);
+        String sqlText = "select cast('1.012345678901234567890123456789' as decfloat)";
 
-        try (ResultSet rs = methodWatcher.executeQuery(sqlText)) {
+        try (ResultSet rs = conn.query(sqlText)) {
             rs.next();
             Assert.assertEquals(new BigDecimal("1.012345678901234567890123456789"), rs.getBigDecimal(1));
         }
@@ -86,10 +95,10 @@ public class DecfloatIT extends SpliceUnitTest {
                 "cast('%s' as decfloat) / cast('%s' as decfloat), " +
                 "cast('%s' as decfloat) + cast('%s' as decfloat), " +
                 "cast('%s' as decfloat) - cast('%s' as decfloat), " +
-                "cast('%s' as decfloat) * cast('%s' as decfloat) " +
-                "from sysibm.sysdummy1 --spliceproperties useSpark=%s\n", lhs, rhs, lhs, rhs, lhs, rhs, lhs, rhs, useSpark);
+                "cast('%s' as decfloat) * cast('%s' as decfloat) ",
+                lhs, rhs, lhs, rhs, lhs, rhs, lhs, rhs);
 
-        try(ResultSet rs = methodWatcher.executeQuery(sqlText)) {
+        try(ResultSet rs = conn.query(sqlText)) {
             rs.next();
             BigDecimal lhsBd = new BigDecimal(lhs, MathContext.DECIMAL128);
             BigDecimal rhsBd = new BigDecimal(rhs, MathContext.DECIMAL128);
@@ -102,9 +111,8 @@ public class DecfloatIT extends SpliceUnitTest {
 
     @Test
     public void typeFunction() throws Exception {
-        String sqlText = format("select cast('1.00000000001' as decfloat), decfloat(1.00000000001) " +
-                "from sysibm.sysdummy1 --spliceproperties useSpark=%s\n", useSpark);
-        try (ResultSet rs = methodWatcher.executeQuery(sqlText)) {
+        String sqlText = "select cast('1.00000000001' as decfloat), decfloat(1.00000000001)";
+        try (ResultSet rs = conn.query(sqlText)) {
             rs.next();
             Assert.assertEquals(rs.getBigDecimal(1), rs.getBigDecimal(2));
         }
@@ -112,10 +120,10 @@ public class DecfloatIT extends SpliceUnitTest {
 
     @Test
     public void insertIntoTable() throws Exception {
-        methodWatcher.executeUpdate("drop table insertIntoTable if exists");
-        methodWatcher.executeUpdate("create table insertIntoTable (a int, b decfloat, c decfloat, d decfloat not null with default, e decfloat not null with default 5.4)");
-        methodWatcher.executeUpdate("insert into insertIntoTable (a, b) values (1, 1.2)");
-        try (ResultSet rs = methodWatcher.executeQuery("select * from insertIntoTable")) {
+        conn.execute("drop table insertIntoTable if exists");
+        conn.execute("create table insertIntoTable (a int, b decfloat, c decfloat, d decfloat not null with default, e decfloat not null with default 5.4)");
+        conn.execute("insert into insertIntoTable (a, b) values (1, 1.2)");
+        try (ResultSet rs = conn.query("select * from insertIntoTable")) {
             rs.next();
             Assert.assertEquals(1, rs.getInt(1));
             Assert.assertEquals(new BigDecimal("1.2"), rs.getBigDecimal(2));
@@ -127,18 +135,92 @@ public class DecfloatIT extends SpliceUnitTest {
 
     @Test
     public void testJoin() throws Exception {
-        methodWatcher.executeUpdate("drop table testJoin_A if exists");
-        methodWatcher.executeUpdate("drop table testJoin_B if exists");
-        methodWatcher.executeUpdate("create table testJoin_A(a1 decfloat, a2 int)");
-        methodWatcher.executeUpdate("create table testJoin_B(b1 decfloat, b2 int)");
-        methodWatcher.executeUpdate("insert into testJoin_A values ('3',3), ('3.0000000000000000000000000000001', 4)");
-        methodWatcher.executeUpdate("insert into testJoin_B values ('3',3), ('3.0000000000000000000000000000001', 4)");
-        String sql = format("select * from testJoin_A, testJoin_B --splice-properties useSpark=%s, joinStrategy=broadcast\n" +
-                "where a1 = b1", useSpark);
+        conn.execute("drop table testJoin_A if exists");
+        conn.execute("drop table testJoin_B if exists");
+        conn.execute("create table testJoin_A(a1 decfloat, a2 int)");
+        conn.execute("create table testJoin_B(b1 decfloat, b2 int)");
+        conn.execute("insert into testJoin_A values ('3',3), ('3.0000000000000000000000000000001', 4)");
+        conn.execute("insert into testJoin_B values ('3',3), ('3.0000000000000000000000000000001', 4)");
+        String sql = "select * from testJoin_A, testJoin_B --splice-properties joinStrategy=broadcast\n" +
+                "where a1 = b1";
         String expected = "A1                 |A2 |               B1                 |B2 |\n" +
                 "------------------------------------------------------------------------------\n" +
                 "                3                 | 3 |                3                 | 3 |\n" +
                 "3.0000000000000000000000000000001 | 4 |3.0000000000000000000000000000001 | 4 |";
-        testQuery(sql, expected, methodWatcher);
+        testQuery(sql, expected, conn);
+    }
+
+    @Test
+    public void testRoundDefaultHalfEven() throws Exception {
+        // exactly 34 digits
+        checkDecimalExpression("decfloat('0.1234567890123456789012345678901234')", "0.1234567890123456789012345678901234", conn);
+        // 35 digits round down
+        checkDecimalExpression("decfloat('0.12345678901234567890123456789012341')", "0.1234567890123456789012345678901234", conn);
+        // 35 digits round up
+        checkDecimalExpression("decfloat('0.12345678901234567890123456789012348')", "0.1234567890123456789012345678901235", conn);
+        // 35 digits half round down
+        checkDecimalExpression("decfloat('0.12345678901234567890123456789012345')", "0.1234567890123456789012345678901234", conn);
+        // 35 digits half round up
+        checkDecimalExpression("decfloat('0.12345678901234567890123456789012335')", "0.1234567890123456789012345678901234", conn);
+    }
+
+    @Test
+    public void testCompare() throws Exception {
+        checkBooleanExpression("decfloat('3.0') = decfloat('3.0')", true, conn);
+        checkBooleanExpression("decfloat('3.00') = decfloat('3.0')", true, conn);
+        checkBooleanExpression("decfloat('3.0') = decfloat('3.00')", true, conn);
+        checkBooleanExpression("decfloat('3.01') = decfloat('3.0')", false, conn);
+        checkBooleanExpression("decfloat('3.01') > decfloat('3.0')", true, conn);
+        checkBooleanExpression("decfloat('-0') = decfloat('0')", true, conn);
+    }
+
+    @Test
+    public void testBorders() throws Exception {
+        checkDecimalExpression("decfloat('-9.999999999999999999999999999999999E6144')", "-9.999999999999999999999999999999999E+6144", conn);
+        checkDecimalExpression("decfloat('-1E-6143')", "-1E-6143", conn);
+        checkDecimalExpression("decfloat('1E-6143')", "1E-6143", conn);
+
+        // FIXME DB-10864
+        //checkDecimalExpression("decfloat('9.999999999999999999999999999999999E6144')", "9.999999999999999999999999999999999E+6144", conn);
+    }
+
+    @Test
+    public void testRoundFunction() throws Exception {
+        checkDecimalExpression("round(decfloat('0.123459'), 5)", "0.12346", conn);
+        checkDecimalExpression("round(decfloat('0.123459'), 5)", "0.12346", conn);
+        checkDecimalExpression("round(decfloat('0.123451'), 5)", "0.12345", conn);
+        checkDecimalExpression("round(decfloat('0.123455'), 5)", "0.12346", conn);
+        checkDecimalExpression("round(decfloat('0.123445'), 5)", "0.12345", conn);
+    }
+
+    @Test
+    public void testCastFromOtherTypes() throws Exception {
+        checkDecimalExpression("cast( cast(1 as int) as decfloat)", "1", conn);
+        checkDecimalExpression("cast( cast(1 as bigint) as decfloat)", "1", conn);
+
+        // FIXME DB-10866 (different toString representation between spark and control)
+        checkDecimalExpression("cast( cast(1 as double) as decfloat)", useSpark? "1": "1.0", conn);
+        checkDecimalExpression("cast( char('1.0') as decfloat)", useSpark ?"1": "1.0", conn);
+        checkDecimalExpression("cast( cast(1.0 as float) as decfloat)", useSpark?"1" : "1.0", conn);
+        checkDecimalExpression("cast( cast (1.0 as real) as decfloat)", useSpark?"1":"1.0", conn);
+
+        checkDecimalExpression("cast( smallint(1) as decfloat)", "1", conn);
+        checkDecimalExpression("cast( tinyint(1) as decfloat)", "1", conn);
+
+        try {
+            checkDecimalExpression("cast( true as decfloat)", "1", conn);
+            Assert.fail("bool to decfloat cast should fail");
+        } catch (SQLException e) {
+            Assert.assertEquals("42846", e.getSQLState());
+        }
+    }
+
+    @Test
+    @Ignore("DB-10868")
+    public void testOverflow() throws Exception {
+        checkDecimalExpression("decfloat('-9.999999999999999999999999999999999E6144') * 10", "", conn);
+        checkDecimalExpression("decfloat('-1E-6143') / 10", "", conn);
+        checkDecimalExpression("decfloat('1E-6143') / 10", "", conn);
+        checkDecimalExpression("decfloat('9.999999999999999999999999999999999E6144') * 10", "", conn);
     }
 }
