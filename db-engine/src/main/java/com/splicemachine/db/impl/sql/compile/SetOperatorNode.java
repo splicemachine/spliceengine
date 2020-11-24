623b9ee8e3ec4da053948800999900fb94f139ae
Mark Sirek <msirek@splicemachine.com>
2020-11-23 23:32:05 -0800

    DB-10774 Wrap SetOp subquery in a derived table so it can be flattened.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SetOperatorNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SetOperatorNode.java
@@ -1164,4 +1164,13 @@ abstract class SetOperatorNode extends TableOperatorNode
 
         return this;
     }
+
+    public void setLevel(int level){
+        super.setLevel(level);
+        if (leftResultSet instanceof FromTable)
+            ((FromTable)leftResultSet).setLevel(level);
+        if (rightResultSet instanceof FromTable)
+            ((FromTable)rightResultSet).setLevel(level);
+    }
+
 }
