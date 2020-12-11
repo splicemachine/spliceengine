d36de51abdb423755d94f7b79a06531e68bb112a
Arnaud Lacurie <alacurie@splicemachine.com>
2020-12-11 16:14:28 +0100

    DB-11012 Truncate when casting timestamp to [var]char  (#4840)
    * DB-11012 Truncate when casting timestamp to [var]char
    
    * DB-11012 throw when char(constant timestamp, <format>)

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CastNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CastNode.java
@@ -374,14 +374,10 @@ public class CastNode extends ValueNode
 
                 case Types.DATE:
                 case Types.TIME:
-                case Types.TIMESTAMP:
                     if (destJDBCTypeId == Types.CHAR)
                     {
                         DataValueDescriptor dvd = ((ConstantNode) castOperand).getValue();
                         String castValue;
-                        if (dvd instanceof SQLTimestamp) {
-                            ((SQLTimestamp) dvd).setTimestampFormat(getCompilerContext().getTimestampFormat());
-                        }
                         if (dvd instanceof DateTimeDataValue && dateToStringFormat >= 0) {
                             ((DateTimeDataValue) dvd).setStringFormat(dateToStringFormat);
                             castValue = dvd.getString();
@@ -396,7 +392,25 @@ public class CastNode extends ValueNode
                                 getContextManager());
                     }
                     break;
-
+                case Types.TIMESTAMP:
+                    if (destJDBCTypeId == Types.CHAR)
+                    {
+                        if(dateToStringFormat >= 0) { // similar to DB2, we don't support custom formats for Timestamp (DB-10461)
+                            throw StandardException.newException(SQLState.LANG_FORMAT_EXCEPTION, "timestamp");
+                        }
+                        SQLTimestamp dvd = (SQLTimestamp) ((ConstantNode) castOperand).getValue();
+                        dvd.setTimestampFormat(getCompilerContext().getTimestampFormat());
+                        String castValue = ((UserTypeConstantNode) castOperand).getObjectValue().toString();
+                        SQLChar sqlChar = new SQLChar(castValue);
+                        int width = getTypeServices().getMaximumWidth();
+                        sqlChar.setWidth(width, -1, false);
+                        retNode = (ValueNode) getNodeFactory().getNode(
+                                C_NodeTypes.CHAR_CONSTANT_NODE,
+                                sqlChar.toString(),
+                                ReuseFactory.getInteger(width),
+                                getContextManager());
+                    }
+                    break;
                 case Types.DECIMAL:
                     // ignore decimal -> decimal casts for now
                     if (destJDBCTypeId == Types.DECIMAL ||
@@ -1104,7 +1118,7 @@ public class CastNode extends ValueNode
 
             mb.push(isNumber ? getTypeServices().getPrecision() : getTypeServices().getMaximumWidth());
             mb.push(getTypeServices().getScale());
-            mb.push(!sourceCTI.variableLength() ||
+            mb.push(!(sourceCTI.variableLength() || sourceCTI.getJDBCTypeId() == Types.TIMESTAMP) ||
                     isNumber ||
                     assignmentSemantics);
             mb.callMethod(VMOpcode.INVOKEINTERFACE, ClassName.VariableSizeDataValue,
