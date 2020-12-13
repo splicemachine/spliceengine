04211ca205d6b620de5dac3ca6c8e9641ebea16f
Youssef Hatem <yhatem@splicemachine.com>
2020-12-13 22:36:32 +0100

    DB-10882 refactoring.

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/CurrentOfResultSetOperation.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/execute/operations/CurrentOfResultSetOperation.java
@@ -20,7 +20,6 @@ import com.splicemachine.db.iapi.sql.Activation;
 import com.splicemachine.db.iapi.sql.conn.LanguageConnectionContext;
 import com.splicemachine.db.iapi.sql.execute.*;
 import com.splicemachine.db.iapi.sql.execute.CursorActivation;
-import com.splicemachine.db.iapi.types.RowLocation;
 import com.splicemachine.db.impl.sql.execute.ValueRow;
 import com.splicemachine.derby.iapi.sql.execute.SpliceOperation;
 import com.splicemachine.derby.stream.control.MaterializedControlDataSet;
@@ -30,7 +29,6 @@ import com.splicemachine.derby.stream.iapi.DataSetProcessor;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
 
 /**
  * Takes a cursor name and returns the current row
@@ -42,344 +40,226 @@ import java.util.List;
  */
 public class CurrentOfResultSetOperation extends SpliceBaseOperation {
 
-    private boolean     next;
-	private RowLocation rowLocation;
+    private CursorResultSet cursor;
+    private CursorResultSet target;
+    private ExecRow         sparseRow;
 
-	private CursorResultSet cursor;
-	private CursorResultSet target;
-	private ExecRow         sparseRow;
-
-	protected static final String NAME = CurrentOfResultSetOperation.class.getSimpleName().replaceAll("Operation", "");
+    protected static final String NAME = CurrentOfResultSetOperation.class.getSimpleName().replaceAll("Operation", "");
 
     // set in constructor and not altered during
     // life of object.
-	private String cursorName;
+    private String cursorName;
 
-	public CurrentOfResultSetOperation() {}
+    public CurrentOfResultSetOperation() {}
 
     //
     // class interface
     //
     public CurrentOfResultSetOperation(String cursorName, Activation activation,
-									   int resultSetNumber) throws StandardException {
-		super(activation, resultSetNumber, 0.0d, 0.0d);
-		if (SanityManager.DEBUG)
-			SanityManager.ASSERT( cursorName!=null, "current of scan must get cursor name");
+                                       int resultSetNumber) throws StandardException {
+        super(activation, resultSetNumber, 0.0d, 0.0d);
+        if (SanityManager.DEBUG)
+            SanityManager.ASSERT( cursorName!=null, "current of scan must get cursor name");
         this.cursorName = cursorName;
     }
 
-	//
-	// ResultSet interface (leftover from NoPutResultSet)
-	//
-	/**
-     * open a scan on the table. scan parameters are evaluated
-     * at each open, so there is probably some way of altering
-     * their values...
-	 *
-	 * @exception StandardException thrown on failure to open
+    /**
+     * Return a sparse heap row, based on a compact index row.
+     *
+     * @param row  compact referenced index row
+     * @param indexCols base column positions of index keys, signed with asc/desc info
+     *
+     * @return   a sparse heap row with referenced columns
      */
-	public void	openCore() throws StandardException {
-//		if (SanityManager.DEBUG)
-//	    	SanityManager.ASSERT( ! isOpen, "CurrentOfResultSet already open");
+    private ExecRow getSparseRow(ExecRow row, int[] indexCols) throws StandardException
+    {
+        int colPos;
+        if (sparseRow == null)
+        {
+            int numCols = 1;
+            for (int i = 0; i < indexCols.length; i++)
+            {
+                colPos = (indexCols[i] > 0) ? indexCols[i] : -indexCols[i];
+                if (colPos > numCols)
+                    numCols = colPos;
+            }
+            sparseRow = new ValueRow(numCols);
+        }
+        for (int i = 1; i <= indexCols.length; i++)
+        {
+            colPos = (indexCols[i-1] > 0) ? indexCols[i-1] : -indexCols[i-1];
+            sparseRow.setColumn(colPos, row.getColumn(i));
+        }
 
-		// get the cursor
-		getCursor();
+        return sparseRow;
+    }
 
-		next = false;
-	    isOpen = true;
-	}
-
-	/**
-     * If open and not returned yet, returns the row.
-	 *
-	 * @exception StandardException thrown on failure.
+    /**
+     * Return the total amount of time spent in this ResultSet
+     *
+     * @param type CURRENT_RESULTSET_ONLY - time spent only in this ResultSet
+     *    ENTIRE_RESULTSET_TREE  - time spent in this ResultSet and below.
+     *
+     * @return long  The total amount of time spent (in milliseconds).
      */
-	public ExecRow	getNextRowCore() throws StandardException {
+    public long getTimeSpent(int type)
+    {
+        /* RESOLVE - RunTimeStats not implemented yet */
+        return 0;
+    }
 
-		if ( isOpen ) {
-	        if ( ! next ) {
-	            next = true;
-				if (SanityManager.DEBUG)
-					SanityManager.ASSERT(! cursor.isClosed(), "cursor closed");
+    //
+    // class implementation
+    //
+    /**
+        Because the positioned operation only gets one location
+        per execution, and the cursor could be completely different
+        for each execution (closed and reopened, perhaps), we
+        determine where caching the cursor could be applied.
+        <p>
+        When cached, we check if the cursor was closed'd,
+        and if so, throw it out and
+        see if there's one in the cache with our name.
 
-				ExecRow cursorRow = cursor.getCurrentRow();
+     */
+    private void getCursor() throws StandardException {
 
-				// requalify the current row
-				if (cursorRow == null) {
-					throw StandardException.newException(SQLState.NO_CURRENT_ROW);
-				}
-				// we know it will be requested, may as well get it now.
-				rowLocation = cursor.getRowLocation();
+        // need to look again if cursor was closed
+        if (cursor != null) {
+            if (cursor.isClosed())
+            {
+                cursor = null;
+                target = null;
+            }
+        }
 
-				// get the row from the base table, which is the real result
-				// row for the CurrentOfResultSet
-				currentRow = target.getCurrentRow();
+        if (cursor == null) {
 
-				// if the source result set is a ScrollInsensitiveResultSet, and
-				// the current row has been deleted (while the cursor was 
-				// opened), the cursor result set (scroll insensitive) will 
-				// return the cached row, while the target result set will 
-				// return null (row has been deleted under owr feet).
-				if (rowLocation == null  || 
-						(cursorRow != null && currentRow == null)) {
-					activation.addWarning(StandardException.
-							newWarning(SQLState.CURSOR_OPERATION_CONFLICT));
-					return null;
-				}
+            LanguageConnectionContext lcc = getActivation().getLanguageConnectionContext();
 
-				/* beetle 3865: updateable cursor using index.  If underlying is a covering
-				 * index, target is a TableScanRS (instead of a IndexRow2BaseRowRS) for the
-				 * index scan.  But the problem is it returns a compact row in index key order.
-				 * However the ProjectRestrictRS above us that sets up the old and new column
-				 * values expects us to return a sparse row in heap order.  We have to do the
-				 * wiring here, since we don't have IndexRow2BaseRowRS to do this work.  This
-				 * problem was not exposed before, because we never used index scan for updateable
-				 * cursors.
-				 */
-				/*
-				if (target instanceof TableScanResultSet)
-				{
-					TableScanResultSet scan = (TableScanResultSet) target;
-					if (scan.indexCols != null && currentRow != null)
-						currentRow = getSparseRow(currentRow, scan.indexCols);
-				}
-				 */
+            CursorActivation cursorActivation = lcc.lookupCursorActivation(cursorName);
 
-				// REMIND: verify the row is still there
-				// at present we get an ugly exception from the store,
-				// Hopefully someday we can just do this:
-				//
-				// if (!rowLocation.rowExists())
-				//     throw StandardException.newException(SQLState.LANG_NO_CURRENT_ROW, cursorName);
-			}
-			else {
-				currentRow = null;
-				rowLocation = null;
-			}
-	    }
-		else {
-			currentRow = null;
-			rowLocation = null;
-		}
-		setCurrentRow(currentRow);
-	    return currentRow;
-	}
+            if (cursorActivation != null)
+            {
 
-	/**
-	 * Return a sparse heap row, based on a compact index row.
-	 *
-	 * @param row		compact referenced index row
-	 * @param indexCols	base column positions of index keys, signed with asc/desc info
-	 *
-	 * @return			a sparse heap row with referenced columns
-	 */
-	private ExecRow getSparseRow(ExecRow row, int[] indexCols) throws StandardException
-	{
-		int colPos;
-		if (sparseRow == null)
-		{
-			int numCols = 1;
-			for (int i = 0; i < indexCols.length; i++)
-			{
-				colPos = (indexCols[i] > 0) ? indexCols[i] : -indexCols[i];
-				if (colPos > numCols)
-					numCols = colPos;
-			}
-			sparseRow = new ValueRow(numCols);
-		}
-		for (int i = 1; i <= indexCols.length; i++)
-		{
-			colPos = (indexCols[i-1] > 0) ? indexCols[i-1] : -indexCols[i-1];
-			sparseRow.setColumn(colPos, row.getColumn(i));
-		}
+                cursor = cursorActivation.getCursorResultSet();
+                target = cursorActivation.getTargetResultSet();
+                /* beetle 3865: updateable cursor using index. 2 way communication between
+                 * update activation and cursor activation. Cursor passes index scan to
+                 * update and update passes heap conglom controller to cursor.
+                 */
+                activation.setForUpdateIndexScan(cursorActivation.getForUpdateIndexScan());
+                if (cursorActivation.getHeapConglomerateController() != null)
+                    cursorActivation.getHeapConglomerateController().close();
+                cursorActivation.setHeapConglomerateController(activation.getHeapConglomerateController());
+            }
+        }
 
-		return sparseRow;
-	}
+        if (cursor == null || cursor.isClosed()) {
+            throw StandardException.newException(SQLState.LANG_CURSOR_NOT_FOUND, cursorName);
+        }
+    }
 
+    /**
+     * @see NoPutResultSet#updateRow
+     */
+    public void updateRow(ExecRow row, RowChanger rowChanger)
+            throws StandardException {
+        ((NoPutResultSet)cursor).updateRow(row, rowChanger);
+    }
 
+    /**
+     * @see NoPutResultSet#markRowAsDeleted
+     */
+    public void markRowAsDeleted() throws StandardException {
+        ((NoPutResultSet)cursor).markRowAsDeleted();
+    }
 
-	/**
-	 * If the result set has been opened,
-	 * close the open scan.
-	 *
-	 * @exception StandardException thrown on error
-	 */
-	public void	close() throws StandardException
-	{
-	    if ( isOpen ) {
-			// we don't want to keep around a pointer to the
-			// row ... so it can be thrown away.
-			// REVISIT: does this need to be in a finally
-			// block, to ensure that it is executed?
-	    	clearCurrentRow();
-	        next = false;
+    @Override
+    public DataSet<ExecRow> getDataSet(DataSetProcessor dsp) throws StandardException {
+        if (!isOpen)
+            throw new IllegalStateException(String.format("Operation %s is not open", NAME));
 
-			super.close();
-	    }
-		else
-			if (SanityManager.DEBUG)
-				SanityManager.DEBUG("CloseRepeatInfo","Close of CurrentOfResultSet repeated");
-	}
+        getCursor();
 
-	boolean finished = false;
+        if (SanityManager.DEBUG)
+            SanityManager.ASSERT(! cursor.isClosed(), "cursor closed");
 
-	@Override
-	public void open() throws StandardException {
-		super.open();
-		finished = false;
-	}
+        ExecRow cursorRow = cursor.getCurrentRow();
 
-	@Override
-	public void	finish() throws StandardException
-	{
-		if (!finished) {
-			if (!isClosed())
-				close();
+        // requalify the current row
+        if (cursorRow == null) {
+            throw StandardException.newException(SQLState.NO_CURRENT_ROW);
+        }
+        // we know it will be requested, may as well get it now.
+        setCurrentRowLocation(cursor.getRowLocation());
 
-			finished = true;
+        // get the row from the base table, which is the real result
+        // row for the CurrentOfResultSet
+        setCurrentRow(target.getCurrentRow());
 
-			if (isTopResultSet && activation.isSingleExecution())
-				activation.close();
-		}
-	}
+        // if the source result set is a ScrollInsensitiveResultSet, and
+        // the current row has been deleted (while the cursor was
+        // opened), the cursor result set (scroll insensitive) will
+        // return the cached row, while the target result set will
+        // return null (row has been deleted under owr feet).
+        if (getCurrentRowLocation() == null || getCurrentRow() == null) {
+            activation.addWarning(StandardException.newWarning(SQLState.CURSOR_OPERATION_CONFLICT));
+            return null;
+        }
 
-	/**
-	 * Return the total amount of time spent in this ResultSet
-	 *
-	 * @param type	CURRENT_RESULTSET_ONLY - time spent only in this ResultSet
-	 *				ENTIRE_RESULTSET_TREE  - time spent in this ResultSet and below.
-	 *
-	 * @return long		The total amount of time spent (in milliseconds).
-	 */
-	public long getTimeSpent(int type)
-	{
-		/* RESOLVE - RunTimeStats not implemented yet */
-		return 0;
-	}
+        /* beetle 3865: updateable cursor using index.  If underlying is a covering
+         * index, target is a TableScanRS (instead of a IndexRow2BaseRowRS) for the
+         * index scan.  But the problem is it returns a compact row in index key order.
+         * However the ProjectRestrictRS above us that sets up the old and new column
+         * values expects us to return a sparse row in heap order.  We have to do the
+         * wiring here, since we don't have IndexRow2BaseRowRS to do this work.  This
+         * problem was not exposed before, because we never used index scan for updateable
+         * cursors.
+         */
+                /*
+                if (target instanceof TableScanResultSet)
+                {
+                    TableScanResultSet scan = (TableScanResultSet) target;
+                    if (scan.indexCols != null && currentRow != null)
+                        currentRow = getSparseRow(currentRow, scan.indexCols);
+                }
+                 */
 
-	/**
-	 * This result set has its row location from
-	 * the last fetch done. If it is closed,
-	 * a null is returned.
-	 *
-	 * @see CursorResultSet
-	 *
-	 * @return the row location of the current row.
-	 * @exception StandardException thrown on failure to get row location
-	 */
-	public RowLocation getRowLocation()  {
-		return rowLocation;
-	}
+        // REMIND: verify the row is still there
+        // at present we get an ugly exception from the store,
+        // Hopefully someday we can just do this:
+        //
+        // if (!rowLocation.rowExists())
+        //     throw StandardException.newException(SQLState.LANG_NO_CURRENT_ROW, cursorName);
 
-	/**
-	 * @see CursorResultSet
-	 *
-	 * @return the last row returned by getNextRow.
-	 */
-	public ExecRow getCurrentRow() {
-		return currentRow;
-	}
+        List<ExecRow> rows = new ArrayList<ExecRow>();
+        rows.add(cursorRow);
+        return new MaterializedControlDataSet<ExecRow>( rows );
+    }
 
-	//
-	// class implementation
-	//
-	/**
-		Because the positioned operation only gets one location
-		per execution, and the cursor could be completely different
-		for each execution (closed and reopened, perhaps), we 
-		determine where caching the cursor could be applied.
-		<p>
-		When cached, we check if the cursor was closed'd, 
-		and if so, throw it out and 
-		see if there's one in the cache with our name. 
+    @Override
+    public String getName() {
+        return NAME;
+    }
 
-	 */
-	private void getCursor() throws StandardException {
+    @Override
+    public int[] getRootAccessedCols(long tableNumber) throws StandardException {
+        return null;
+    }
 
-		// need to look again if cursor was closed
-		if (cursor != null) {
-			if (cursor.isClosed())
-			{
-				cursor = null;
-				target = null;
-			}
-		}
+    @Override
+    public boolean isReferencingTable(long tableNumber) {
+        return false;
+    }
 
-		if (cursor == null) {
+    @Override
+    public String prettyPrint(int indentLevel) {
+        return "CurrentOfResultSet";
+    }
 
-			LanguageConnectionContext lcc = getActivation().getLanguageConnectionContext();
-
-			CursorActivation cursorActivation = lcc.lookupCursorActivation(cursorName);
-
-			if (cursorActivation != null)
-			{
-				
-				cursor = cursorActivation.getCursorResultSet();
-				target = cursorActivation.getTargetResultSet();
-				/* beetle 3865: updateable cursor using index. 2 way communication between
-				 * update activation and cursor activation. Cursor passes index scan to
-				 * update and update passes heap conglom controller to cursor.
-				 */
-				activation.setForUpdateIndexScan(cursorActivation.getForUpdateIndexScan());
-				if (cursorActivation.getHeapConglomerateController() != null)
-					cursorActivation.getHeapConglomerateController().close();
-				cursorActivation.setHeapConglomerateController(activation.getHeapConglomerateController());				
-			}
-		}
-
-		if (cursor == null || cursor.isClosed()) {
-			throw StandardException.newException(SQLState.LANG_CURSOR_NOT_FOUND, cursorName);	
-		}
-	}
-
-	/**
-	 * @see NoPutResultSet#updateRow
-	 */
-	public void updateRow (ExecRow row, RowChanger rowChanger)
-			throws StandardException {
-		((NoPutResultSet)cursor).updateRow(row, rowChanger);
-	}
-	
-	/**
-	 * @see NoPutResultSet#markRowAsDeleted
-	 */
-	public void markRowAsDeleted() throws StandardException {
-		((NoPutResultSet)cursor).markRowAsDeleted();
-	}
-
-	@Override
-	public DataSet<ExecRow> getDataSet(DataSetProcessor dsp) throws StandardException {
-		if(currentRow == null) {
-			openCore();
-			getNextRowCore();
-		}
-		List<ExecRow> rows = new ArrayList<ExecRow>();
-		rows.add(getCurrentRow());
-		return new MaterializedControlDataSet<ExecRow>( rows );
-	}
-
-	@Override
-	public String getName() {
-		return NAME;
-	}
-
-	@Override
-	public int[] getRootAccessedCols(long tableNumber) throws StandardException {
-		return new int[0];
-	}
-
-	@Override
-	public boolean isReferencingTable(long tableNumber) {
-		return false;
-	}
-
-	@Override
-	public String prettyPrint(int indentLevel) {
-		return "CurrentOfResultSet";
-	}
-
-	@Override
-	public List<SpliceOperation> getSubOperations() {
-		return Collections.emptyList();
-	}
+    @Override
+    public List<SpliceOperation> getSubOperations() {
+        return Collections.emptyList();
+    }
 }
