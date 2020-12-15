ea99e299eeb65a021a091f5e57243b703dd39d1f
Mark Sirek <msirek@splicemachine.com>
2020-12-15 07:57:21 -0800

    DB-11019 Address review comments.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/QualifierPhase.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/QualifierPhase.java
@@ -38,9 +38,8 @@ package com.splicemachine.db.impl.sql.compile;
  * BASE - > Qualifier is going against a key value in a way where splice can perform partitioning.
  * FILTER_BASE -> Qualifier for base table but does not restrict I/O from base scan.  This is evaluated prior to a possible lookup.
  * FILTER_PROJECTION -> Qualifer applied after possible lookup in the projection node on top of the base conglomerate scan.
- * JOIN -> Join predicate used to adjust the join selectivity.
  *
  */
 public enum QualifierPhase {
-    BASE,FILTER_BASE,FILTER_PROJECTION,JOIN
+    BASE,FILTER_BASE,FILTER_PROJECTION
 }
