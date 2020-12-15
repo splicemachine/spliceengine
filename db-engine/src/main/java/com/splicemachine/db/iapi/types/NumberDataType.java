f560ea7e308b7992e2b882dca2099dec8c86dbae
Arnaud Lacurie <alacurie@splicemachine.com>
2020-12-15 19:37:00 +0100

    Revert "DB-10944 DB2 compatible casting format from floating point to string (#4814)"
    This reverts commit 1fa357ac81bfaeeae3298a706b4d2e1b7322c0db.

--- db-engine/src/main/java/com/splicemachine/db/iapi/types/NumberDataType.java
+++ db-engine/src/main/java/com/splicemachine/db/iapi/types/NumberDataType.java
@@ -38,8 +38,6 @@ import com.splicemachine.db.iapi.services.sanity.SanityManager;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.math.BigDecimal;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 
 /**
  * NumberDataType is the superclass for all exact and approximate 
@@ -60,9 +58,6 @@ public abstract class NumberDataType extends DataType
     static final BigDecimal ONE = BigDecimal.valueOf(1L);
     static final BigDecimal MAXLONG_PLUS_ONE = BigDecimal.valueOf(Long.MAX_VALUE).add(ONE);
     static final BigDecimal MINLONG_MINUS_ONE = BigDecimal.valueOf(Long.MIN_VALUE).subtract(ONE);
-
-    // DB2 compatible string format for double, float, and real
-    protected static final NumberFormat floatingStrFormat = new DecimalFormat("0.0#############E0");
 
     protected final NumberDataValue getNullDVD(NumberDataValue parm1, NumberDataValue parm2) {
         NumberDataValue highPrec, result;
@@ -642,8 +637,6 @@ public abstract class NumberDataType extends DataType
         return result;
     }
 
-    protected static String toDB2String(double value) {
-        return value == 0 ? "0E0" : floatingStrFormat.format(value);
-    }
+
 }
 
