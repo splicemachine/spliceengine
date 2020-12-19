e1de8d9ad821f7d916481c09e0e7b770de98c88b
Arnaud Lacurie <alacurie@splicemachine.com>
2020-12-19 02:55:20 +0100

    DB-11073 Fix spotbugs

--- splice_machine/src/main/java/com/splicemachine/derby/stream/ActivationHolder.java
+++ splice_machine/src/main/java/com/splicemachine/derby/stream/ActivationHolder.java
@@ -259,7 +259,7 @@ public class ActivationHolder implements Externalizable {
                     so.init(context);
                 }
             }
-        } catch (Exception e) {
+        } catch (SQLException | StandardException | IOException e) {
             throw new RuntimeException(e);
         }
     }
