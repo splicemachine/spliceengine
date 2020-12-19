dd9aec03bebb228387c8aabd0bff24e188348e63
Joe Panko <jpanko1@earthlink.net>
2020-12-18 22:51:49 -0600

    DB-11055 NSDS v2 IT In TriggerIT added tests for mergeInto and update.

--- splice_spark2/src/test/scala/com/splicemachine/spark2/splicemachine/triggers/TestContext.scala
+++ splice_spark2/src/test/scala/com/splicemachine/spark2/splicemachine/triggers/TestContext.scala
@@ -20,7 +20,7 @@ import com.splicemachine.spark2.splicemachine._
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.execution.datasources.jdbc.{JDBCOptions, JdbcUtils}
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.SparkConf
 import org.scalatest.{BeforeAndAfterAll, Suite}
 
@@ -35,6 +35,16 @@ trait TestContext extends BeforeAndAfterAll { self: Suite =>
   val appID = new Date().toString + math.floor(math.random * 10E4).toLong.toString
   val defaultJDBCURL = "jdbc:splice://localhost:1527/splicedb;user=splice;password=admin"
 
+  val t1 = s"$schema.foo"
+  val t2 = s"$schema.bar"
+
+  val dataSchema = StructType(
+    StructField("I", IntegerType, false) ::
+    StructField("C", StringType, true) :: Nil
+  )
+  
+  var df, dfUpd: DataFrame = _
+
   override def beforeAll() {
     spark = SparkSession.builder.config(
       new SparkConf().
@@ -44,15 +54,37 @@ trait TestContext extends BeforeAndAfterAll { self: Suite =>
         set("spark.app.id", appID)
     ).getOrCreate
     splicemachineContext = new SplicemachineContext(defaultJDBCURL)
+
+    splicemachineContext.createTable(t1, dataSchema, Seq("I") )
+    splicemachineContext.createTable(t2, dataSchema, Seq("I") )
+
+    df = dataframe(
+      rdd( Seq(
+        Row.fromSeq( Seq(1,"one") ),
+        Row.fromSeq( Seq(2,"two") ),
+        Row.fromSeq( Seq(3,"three") )
+      ) ),
+      dataSchema
+    )
+
+    dfUpd = dataframe(
+      rdd( Seq(
+        Row.fromSeq( Seq(1,"won") )
+      ) ),
+      dataSchema
+    )
   }
 
   override def afterAll() {
+    splicemachineContext.dropTable(t1)
+    splicemachineContext.dropTable(t2)
+    
     if (spark != null) spark.stop()
   }
 
   def rdd(rows: Seq[Row]): RDD[Row] = spark.sparkContext.parallelize(rows)
 
-  def dataframe(rdd: RDD[Row], schema: StructType): Dataset[Row] = spark.createDataFrame( rdd , schema )
+  def dataframe(rdd: RDD[Row], schema: StructType): DataFrame = spark.createDataFrame( rdd , schema )
 
   def getConnection(): Connection = JdbcUtils.createConnectionFactory(
     new JDBCOptions(Map(
