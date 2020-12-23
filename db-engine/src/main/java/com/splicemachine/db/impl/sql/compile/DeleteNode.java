b369bd88c6a6f78f8af7f78aa0e715c830310d84
Youssef Hatem <yhatem@splicemachine.com>
2020-12-23 18:35:06 +0100

    DB-10882 fix normal update / delete plans with subqueries.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/DeleteNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/DeleteNode.java
@@ -329,12 +329,29 @@ public class DeleteNode extends DMLModStatementNode
                     rowIdColumn.setName(COLUMNNAME);
                 }
 
-                rowLocationColumn =
-                        (ResultColumn) getNodeFactory().getNode(
-                                C_NodeTypes.RESULT_COLUMN,
-                                COLUMNNAME,
-                                rowIdColumn,
-                                getContextManager());
+                if(!cursorDelete) {
+                    ColumnReference columnReference = (ColumnReference) getNodeFactory().getNode(
+                            C_NodeTypes.COLUMN_REFERENCE,
+                            rowIdColumn.getName(),
+                            null,
+                            getContextManager());
+                    columnReference.setSource(rowIdColumn);
+                    columnReference.setNestingLevel(targetTable.getLevel());
+                    columnReference.setSourceLevel(targetTable.getLevel());
+                    rowLocationColumn =
+                            (ResultColumn) getNodeFactory().getNode(
+                                    C_NodeTypes.RESULT_COLUMN,
+                                    COLUMNNAME,
+                                    columnReference,
+                                    getContextManager());
+                } else {
+                    rowLocationColumn =
+                            (ResultColumn) getNodeFactory().getNode(
+                                    C_NodeTypes.RESULT_COLUMN,
+                                    COLUMNNAME,
+                                    rowIdColumn,
+                                    getContextManager());
+                }
 
                 rowLocationColumn.bindResultColumnToExpression();
 
