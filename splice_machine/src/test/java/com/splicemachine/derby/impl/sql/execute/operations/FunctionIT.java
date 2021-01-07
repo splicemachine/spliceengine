d9fcbdb97ac8861f83fea6e4bd2956ce9d4fad97
Arnaud Lacurie <alacurie@splicemachine.com>
2021-01-07 01:23:23 +0100

    DB-11089 Fix NPE in SQLChar.setWidth if warning occurs without resultset (#4939)

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/FunctionIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/FunctionIT.java
@@ -1038,6 +1038,16 @@ public class FunctionIT extends SpliceUnitTest {
     }
 
     @Test
+    public void testDb11089() throws Exception {
+        try (TestConnection conn = methodWatcher.getOrCreateConnection()) {
+            try (ResultSet rs = conn.query("SELECT * FROM sysibm.sysdummy1 WHERE '?2,BL' NOT IN ( CAST('1969-12-16 17:40:41' AS VARCHAR(16)))")) {
+                Assert.assertTrue(rs.next());
+                Assert.assertEquals("Y", rs.getString(1));
+            }
+        }
+    }
+
+    @Test
     public void testCurrentSchema() throws Exception {
         // DB-11073
         try (TestConnection conn = methodWatcher.connectionBuilder().schema(SCHEMA).build()) {
