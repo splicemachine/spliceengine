9d34236f5d56d374b3451670c17541cbf34caf48
Martin Rupp <mrupp@splicemachine.com>
2021-01-22 00:23:54 +0100

    DB-11156 restrict execution of SYSCS_GET_REGION_SERVER_CONFIG_INFO to admin only

--- splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
+++ splice_machine/src/main/java/com/splicemachine/derby/impl/sql/catalog/SpliceSystemProcedures.java
@@ -246,7 +246,7 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .integer("mode")            // 0 = fetch all, 1 = fetch only props where value not same on all servers
                 .numOutputParams(0)
                 .numResultSets(1)
-                .sqlControl(RoutineAliasInfo.NO_SQL)
+                .modifiesSql() // restrict execution
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
                 .build();
         procedures.add(getRegionServerConfig);
@@ -872,7 +872,7 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .numResultSets(0)
                 .varchar("loggerName", 128)
                 .varchar("loggerLevel", 128)
-                .sqlControl(RoutineAliasInfo.NO_SQL)
+                .modifiesSql() // restriction execution
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
                 .build();
         procedures.add(setLoggerLevel);
@@ -882,7 +882,7 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .numResultSets(0)
                 .varchar("loggerName", 128)
                 .varchar("loggerLevel", 128)
-                .sqlControl(RoutineAliasInfo.NO_SQL)
+                .modifiesSql() // restriction execution
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
                 .build();
         procedures.add(setLoggerLevelLocal);
@@ -1067,7 +1067,8 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .numOutputParams(0)
                 .numResultSets(1)
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
-                .sqlControl(RoutineAliasInfo.READS_SQL_DATA).returnType(null).isDeterministic(false)
+                .modifiesSql()
+                .returnType(null).isDeterministic(false)
                 .catalog("KEY")
                 .build());
 
@@ -1078,7 +1079,8 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .numOutputParams(0)
                 .numResultSets(0)
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
-                .sqlControl(RoutineAliasInfo.MODIFIES_SQL_DATA).returnType(null).isDeterministic(false)
+                .modifiesSql() // restrict execution
+                .returnType(null).isDeterministic(false)
                 .catalog("KEY")
                 .varchar("VALUE", Limits.DB2_VARCHAR_MAXWIDTH)
                 .build());
@@ -1173,7 +1175,8 @@ public class SpliceSystemProcedures extends DefaultSystemProcedureGenerator {
                 .numOutputParams(0)
                 .numResultSets(0)
                 .ownerClass(SpliceAdmin.class.getCanonicalName())
-                .sqlControl(RoutineAliasInfo.NO_SQL).returnType(null).isDeterministic(false)
+                .modifiesSql() // restriction execution
+                .returnType(null).isDeterministic(false)
                 .build());
 
         procedures.add(Procedure.newBuilder().name("SYSCS_BACKUP_TABLE")
