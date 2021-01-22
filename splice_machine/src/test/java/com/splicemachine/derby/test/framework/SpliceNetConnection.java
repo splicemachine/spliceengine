5269ab3738ca618ba1f082df3808a681872b0dbd
Youssef Hatem <yhatem@splicemachine.com>
2021-01-22 19:40:08 +0100

    DB-11162 add tests.

--- splice_machine/src/test/java/com/splicemachine/derby/test/framework/SpliceNetConnection.java
+++ splice_machine/src/test/java/com/splicemachine/derby/test/framework/SpliceNetConnection.java
@@ -135,6 +135,7 @@ public class SpliceNetConnection {
         private String ssl;
         private String useOLAP;
         private String useNativeSpark;
+        private String currentFunctionPath;
 
         @Override
         public ConnectionBuilder clone() throws CloneNotSupportedException {
@@ -185,6 +186,10 @@ public class SpliceNetConnection {
             this.useNativeSpark = Boolean.toString(useNativeSpark);
             return this;
         }
+        public ConnectionBuilder setCurrentFunctionPath(String currentFunctionPath) {
+            this.currentFunctionPath = currentFunctionPath;
+            return this;
+        }
 
         public Connection build() throws SQLException {
             Properties info = new Properties();
@@ -202,6 +207,8 @@ public class SpliceNetConnection {
                 info.put("useOLAP", useOLAP != null ? useOLAP : jdbcUseOLAP);
             if (useNativeSpark != null)
                 info.put("useNativeSpark", useNativeSpark);
+            if (currentFunctionPath != null)
+                info.put("CurrentFunctionPath", currentFunctionPath);
             StringBuilder url = new StringBuilder();
             if (host != null || port != null) {
                 url.append(URL_PREFIX);
