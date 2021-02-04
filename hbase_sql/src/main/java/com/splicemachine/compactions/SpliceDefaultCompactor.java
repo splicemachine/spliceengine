784a4dd0a6115e8fe949b71136075f0d49ef0336
Joe Panko <jpanko1@earthlink.net>
2021-02-04 17:08:54 -0600

    DB-10436 Removed unneeded refresh.

--- hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultCompactor.java
+++ hbase_sql/src/main/java/com/splicemachine/compactions/SpliceDefaultCompactor.java
@@ -436,7 +436,6 @@ public class SpliceDefaultCompactor extends DefaultCompactor {
         List<Cell> cells = new ArrayList<>();
         boolean hasMore;
         ScannerContext scannerContext = ScannerContext.newBuilder().setBatchLimit(compactionKVMax).build();
-        SIDriver.driver().getIgnoreTxnSupplier().refresh();
         do {
             hasMore = scanner.next(cells, scannerContext);
             if (LOG.isDebugEnabled()) {
