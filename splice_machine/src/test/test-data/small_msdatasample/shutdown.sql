5502626375684b3af66163bfa227de4f7f6badd6
Joe Panko <jpanko1@earthlink.net>
2021-02-09 23:51:25 -0600

    DB-10436-test Remove extra test code.

--- splice_machine/src/test/test-data/small_msdatasample/shutdown.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE category;
-DROP TABLE category_sub;
-DROP TABLE item;
-DROP TABLE customer;
-DROP TABLE order_header;
-DROP TABLE order_line;
--- splice_spark2/src/test/spark3.0/com/splicemachine/spark2/splicemachine/ThisVersionSpecificItems.scala
+++
@@ -1,5 +0,0 @@
-package com.splicemachine.spark2.splicemachine
-
-object ThisVersionSpecificItems {
-  val schema = SparkVersionSpecificItems.schemaWithoutMetadata
-}
