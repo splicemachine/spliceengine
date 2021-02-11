cb95e63b51f0e529cda2ac2968e19bf80e61cbf8
Zhen Li <zli@splicemachine.com>
2021-02-11 06:14:28 +0100

    DB-11238 Promote NLJ+IndexLookup+IndexScan on OLTP and optimizer bug fixes (#5114)
    * DB-11238 Reduce index lookup cost on OLTP
    
    * DB-11238 Fix join cost estimation when lhs of left join is not joining
    with its rhs
    
    * DB-11238 Fix cost functions of nested loop join
    
    * DB-11238 Adapt tests
    
    * DB-11238 Fix accumulating currentCost in OptimizerImpl
    
    * DB-11238 Adapt ITs (part 2)
    
    * DB-11238 Avoid regression on OLAP and adapt tests
    
    * DB-11238 Improve cost accumulation logic
    
    * DB-11238 Fix binary reference selectivity for self join
    
    * DB-11238 Partially revert changes to test in OrderByEliminationIT

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/actions/index/IndexIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/actions/index/IndexIT.java
@@ -2185,12 +2185,12 @@ public class IndexIT extends SpliceUnitTest{
 
         /* check plan */
         expectedOps = new String[] {
-                "IndexLookup",                 // base row retrieving for ADDRESS_1
-                "IndexScan[PA_IDX_1",
                 "IndexLookup",                 // base row retrieving for PERSON_ADDRESS_1
                 "MultiProbeIndexScan[A_IDX_1", // in-list for ADDRESS_1
+                "IndexLookup",                 // base row retrieving for ADDRESS_1
+                "IndexScan[PA_IDX_1",
         };
-        rowContainsQuery(new int[]{7,8,10,11}, "explain " + format(query, ""), methodWatcher, expectedOps);
+        rowContainsQuery(new int[]{6,7,9,10}, "explain " + format(query, ""), methodWatcher, expectedOps);
 
         /* check result */
         expected = "PID | ADDR_ID | ADDR_ID |STD_STATE_PROVENCE |\n" +
