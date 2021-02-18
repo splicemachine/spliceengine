3b7e7af449d87a51fdb84b27720cc770b07d60ab
Jun Yuan <jyuan@splicemachine.com>
2021-02-18 10:05:49 -0800

    DB-11344: execute SQL from Olap server correctly

--- hbase_sql/src/main/java/com/splicemachine/derby/lifecycle/CostChoosingDataSetProcessorFactory.java
+++ hbase_sql/src/main/java/com/splicemachine/derby/lifecycle/CostChoosingDataSetProcessorFactory.java
@@ -71,9 +71,10 @@ public class CostChoosingDataSetProcessorFactory implements DataSetProcessorFact
                 SpliceLogUtils.trace(LOG, "chooseProcessor(): localProcessor for op %s", op==null?"null":op.getName());
             return new ControlDataSetProcessor(driver.getTxnSupplier(), driver.getTransactor(), driver.getOperationFactory());
         }
-        // If we've already committed to running on spark, due to running a substatement
-        // of a statement chosen to run on spark, or for some other reason, stick with the decision.
-        if (op.isOlapServer())
+        // Due to the way transactions are committed with SparkDataSetProcessors, if the main statement starts in spark,
+        // trigger substatements must also use a SparkDataSetProcessor.
+        // TODO-msirek: Lift this restriction in the future.
+        if (op != null && op.isOlapServer() && activation.isSubStatement())
             return new SparkDataSetProcessor();
 
         if (((BaseActivation)activation).datasetProcessorType().isOlap()) {
