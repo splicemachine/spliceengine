e8d1e7d4c3a42cd78663c41f918b42371aa7fc7c
Joe Panko <jpanko1@earthlink.net>
2021-04-15 15:53:13 -0500

    DB-11853 Added param for passing Kafka properties.

--- platform_it/src/test/java/com/splicemachine/kafka/TestKafkaCluster.java
+++ platform_it/src/test/java/com/splicemachine/kafka/TestKafkaCluster.java
@@ -22,35 +22,46 @@ import kafka.server.KafkaServerStartable;
 public class TestKafkaCluster {
     private KafkaServerStartable kafkaServer;
 
+    /**
+     * 
+     * @param connectString Zookeeper host:port
+     * @param offsetsTopicReplicationFactor The value to set for property offsets.topic.replication.factor
+     * @param externalListenerHost The host or IP to be used for the advertised listener on port 19092
+     * @param kafkaProps Any Kafka properties in the form "prop1=val1;prop2=val2;prop3=val3"
+     */
     public TestKafkaCluster(
             String connectString, 
             String offsetsTopicReplicationFactor,
-            String externalListenerHost
+            String externalListenerHost,
+            String kafkaProps
     ) {
-        KafkaConfig config = getKafkaConfig(connectString, offsetsTopicReplicationFactor, externalListenerHost);
+        KafkaConfig config = getKafkaConfig(connectString, offsetsTopicReplicationFactor, externalListenerHost, kafkaProps);
         kafkaServer = new KafkaServerStartable(config);
         kafkaServer.startup();
     }
 
     public TestKafkaCluster(String connectString) {
-        this(connectString, "1", "localhost");
+        this(connectString, "1", "localhost", "");
     }
 
     public static void main(String [] args) throws Exception {
         if (args.length==1)
             new TestKafkaCluster(args[0]);
         else if (args.length==2)
-            new TestKafkaCluster(args[0], args[1], "localhost");
+            new TestKafkaCluster(args[0], args[1], "localhost", "");
         else if (args.length==3)
-            new TestKafkaCluster(args[0], args[1], args[2]);
+            new TestKafkaCluster(args[0], args[1], args[2], "");
+        else if (args.length==4)
+            new TestKafkaCluster(args[0], args[1], args[2], args[3]);
         else
-            throw new RuntimeException("No zookeper local");
+            throw new RuntimeException("No zookeeper local");
     }
 
     private static KafkaConfig getKafkaConfig(
             final String zkConnectString, 
             final String offsetsTopicReplicationFactor,
-            final String externalListenerHost
+            final String externalListenerHost,
+            final String kafkaProps
     ) {
         Properties props = new Properties();
         assert props.containsKey("zookeeper.connect");
@@ -62,6 +73,14 @@ public class TestKafkaCluster {
         props.put("listener.security.protocol.map","PLAINTEXT:PLAINTEXT,EXTERNAL:PLAINTEXT");
         props.put("offsets.topic.replication.factor", offsetsTopicReplicationFactor);  // helps splice standalone work on Kafka 2.2
         props.put("log.dir", System.getProperty("java.io.tmpdir", "target/tmp") + "/kafka-logs");
+        if( ! kafkaProps.isEmpty() ) {
+            String[] keyValues = kafkaProps.split(";");
+            for(String keyValue: keyValues) {
+                System.out.println("Setting Kafka property " + keyValue);
+                String[] kv = keyValue.split("=");
+                props.put(kv[0], kv[1]);
+            }
+        }
         return new KafkaConfig(props);
     }
 
