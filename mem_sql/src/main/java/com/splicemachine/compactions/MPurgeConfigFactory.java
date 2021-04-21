0ef25410cdf8b685bb998839e3b03950558912e7
Arnaud Lacurie <alacurie@splicemachine.com>
2021-04-21 20:09:38 +0200

    DB-9876 fix mem platform

---
+++ mem_sql/src/main/java/com/splicemachine/compactions/MPurgeConfigFactory.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2012 - 2020 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.splicemachine.compactions;
+
+import com.splicemachine.si.impl.server.PurgeConfig;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class MPurgeConfigFactory implements PurgeConfigFactory {
+    @Override
+    public PurgeConfig compactionConfig(String tableName, boolean isMajor) throws IOException {
+        return null;
+    }
+
+    @Override
+    public PurgeConfig flushConfig(String tableName, boolean tableNeedsSI, boolean regionIsClosing) {
+        return null;
+    }
+
+    @Override
+    public PurgeConfig memStoreCompactionConfig(String tableName, boolean tableNeedsSI, boolean regionIsClosing) {
+        return null;
+    }
+}
