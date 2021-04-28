eb9149c4b1fd8a30b4185b90ecce69d52feca02a
Daniel Gomez Ferro <dgomezferro@splicemachine.com>
2021-04-28 19:22:38 +0200

    DB-11957 Query hinting mechanism
    With hints for DB-11940, DB-11941 and DB-11942
    
    DB-11957 Remove DB-11941 hint
    
    DB-11957 Move QueryHinter to ee

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CompilerContextImpl.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/CompilerContextImpl.java
@@ -168,6 +168,12 @@ public class CompilerContextImpl extends ContextImpl
     {    return lcf.getNodeFactory(); }
 
 
+    @Override
+    public QueryHinter getQueryHinter() {
+        return queryHinter;
+    }
+
+
     public int getNextColumnNumber()
     {
         return nextColumnNumber++;
@@ -859,6 +865,7 @@ public class CompilerContextImpl extends ContextImpl
         this.lcc = lcc;
         lcf = lcc.getLanguageConnectionFactory();
         this.parser = lcf.newParser(this);
+        this.queryHinter = QueryHintingService.loadQueryHinter();
         this.typeCompilerFactory = typeCompilerFactory;
 
         // the prefix for classes in this connection
@@ -1198,6 +1205,7 @@ public class CompilerContextImpl extends ContextImpl
     */
 
     private final Parser                              parser;
+    private final QueryHinter                         queryHinter;
     private final LanguageConnectionContext           lcc;
     private final LanguageConnectionFactory           lcf;
     private       TypeCompilerFactory                 typeCompilerFactory;
