278f2a36b5cf85a528d56b1c6f69197bff1a0421
Youssef Hatem <yhatem@splicemachine.com>
2021-05-07 11:55:48 +0200

    DB-11791 polishing.

--- splice_machine/src/test/java/com/splicemachine/benchmark/ScanBenchmark.java
+++ splice_machine/src/test/java/com/splicemachine/benchmark/ScanBenchmark.java
@@ -33,6 +33,17 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertTrue;
 
+/**
+ * This test suite measure the performance of scans on both OLTP and OLAP with a varying number
+ * of physical splits.
+ *
+ * The test runs as following:
+ *    1. Create a table with ten columns of type int, first column is primary key.
+ *    2. Define N physical split points (regions) of the table.
+ *    3. Insert data into the table.
+ *    4. Perform a full table scan in either OLAP or OLTP.
+ *    5. Reports estimated scan cost and actual cost.
+ */
 @Category(Benchmark.class)
 @RunWith(Parameterized.class)
 public class ScanBenchmark extends Benchmark {
@@ -110,78 +121,6 @@ public class ScanBenchmark extends Benchmark {
         testStatement.close();
         testConnection.close();
         reportStats();
-    }
-
-    static String toHBaseEscaped(String s) {
-        if(s == null) {
-            return null;
-        }
-        if(s.length() % 2 != 0) {
-            throw new IllegalArgumentException("argument length must be an even number");
-        }
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length(); i += 2) {
-            sb.append("\\x").append(s, i, i+2);
-        }
-        return sb.toString();
-    }
-
-    private int getNumSplits(Connection c) throws SQLException {
-        int cnt = 0;
-        String getRegionsSQL = String.format("CALL SYSCS_UTIL.GET_REGIONS('%s', '%s', null,null, null, '|',null,null,null,null)",
-                                             SCHEMA, BASE_TABLE);
-        try (ResultSet rs = c.createStatement().executeQuery(getRegionsSQL)) {
-
-            while (rs.next()) {
-                LOG.info(String.format("Region %s, %s", rs.getString(2), rs.getString(3)));
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    private String rowIdOf(Connection conn, int pkColValue) {
-        // remove once we figure out how to calculate rowid correctly.
-        try(Statement s = conn.createStatement()) {
-            s.execute(String.format("CREATE TABLE %s.TEMP_TABLE(col1 int primary key)", SCHEMA));
-            s.execute(String.format("INSERT INTO %s.TEMP_TABLE VALUES %d", SCHEMA, pkColValue));
-            try(ResultSet rs = s.executeQuery(String.format("SELECT ROWID FROM %s.TEMP_TABLE where col1 = %d", SCHEMA, pkColValue))) {
-                rs.next();
-                return rs.getString(1);
-            }
-        } catch(Throwable t) {
-            LOG.error("Error running statement to get RowId", t);
-            System.exit(-1);
-            return "?";
-        } finally {
-            try {
-                conn.createStatement().execute(String.format("DROP TABLE %s.TEMP_TABLE", SCHEMA));
-            } catch (Exception e) {
-                // ignore.
-            }
-        }
-    }
-
-    private void splitAt(Connection conn, String splits) {
-        String splitAtSql = null;
-        splitAtSql = String.format("call SYSCS_UTIL.syscs_split_table_or_index_at_points('%s', '%s', null,'%s')",
-                                   SCHEMA, BASE_TABLE, splits);
-        try (Statement statement = conn.createStatement()) {
-            statement.execute(splitAtSql);
-        } catch (Throwable t) {
-            LOG.error("Error running statement to split", t);
-            System.exit(-1);
-        }
-    }
-
-    private void splitRegions(Connection conn, int numSplits) {
-        assert numSplits != 0;
-        int splitSize = tableSize / numSplits;
-        String splits = "";
-        for(int i = 1; i < numSplits; i++) {
-            splits += toHBaseEscaped(rowIdOf(conn, splitSize * i)) + ",";
-        }
-        splitAt(conn, splits.substring(0, splits.length() - 1));
     }
 
     static final String STAT_ERROR = "ERROR";
@@ -271,22 +210,6 @@ public class ScanBenchmark extends Benchmark {
         return cost;
     }
 
-    private void splitTable(int numSplits) {
-        if (numSplits > 0) {
-            try (Connection conn = makeConnection()) {
-                getNumSplits(conn);
-                LOG.info(String.format("After splitting to %d region(s)", numSplits));
-                splitRegions(conn, numSplits);
-                int actualSplitCount = getNumSplits(conn);
-                if (numSplits != actualSplitCount) {
-                    LOG.error(String.format("number of actual splits %d is not equal to what was requested (%d)", actualSplitCount, numSplits));
-                }
-            } catch (Throwable t) {
-                LOG.error("Connection broken", t);
-            }
-        }
-    }
-
     private void benchmark(int numSplits, int numExec, boolean warmUp, boolean onOlap) {
         String sqlText;
         String dataLabel = "Base table scan with OLAP = " + onOlap;
@@ -340,6 +263,171 @@ public class ScanBenchmark extends Benchmark {
     @Parameterized.Parameters
     public static Collection testParams() {
         return Arrays.asList(new Object[][]{
+                {10, -1, 1000,    false},
+                {10, -1, 5000,    false},
+                {10, -1, 10000,   false},
+                {10, -1, 15000,   false},
+                {10, -1, 20000,   false},
+                {10, -1, 25000,   false},
+                {10, -1, 30000,   false},
+                {10, -1, 35000,   false},
+                {10, -1, 40000,   false},
+                {10, -1, 45000,   false},
+                {10, -1, 50000,   false},
+                {10, -1, 100000,  false},
+                {10, -1, 125000,  false},
+                {10, -1, 150000,  false},
+                {10, -1, 175000,  false},
+                {10, -1, 200000,  false},
+                {10, -1, 250000,  false},
+                {10, -1, 300000,  false},
+                {10, -1, 350000,  false},
+                {10, -1, 400000,  false},
+                {10, -1, 450000,  false},
+                {10, -1, 500000,  false},
+                {10, -1, 550000,  false},
+                {10, -1, 600000,  false},
+                {10, -1, 650000,  false},
+                {10, -1, 700000,  false},
+                {10, -1, 750000,  false},
+                {10, -1, 800000,  false},
+                {10, -1, 850000,  false},
+                {10, -1, 900000,  false},
+                {10, -1, 950000,  false},
+                {10, -1, 1000000, false},
+
+                {10, 2, 1000,    false},
+                {10, 2, 5000,    false},
+                {10, 2, 10000,   false},
+                {10, 2, 15000,   false},
+                {10, 2, 20000,   false},
+                {10, 2, 25000,   false},
+                {10, 2, 30000,   false},
+                {10, 2, 35000,   false},
+                {10, 2, 40000,   false},
+                {10, 2, 45000,   false},
+                {10, 2, 50000,   false},
+                {10, 2, 100000,  false},
+                {10, 2, 125000,  false},
+                {10, 2, 150000,  false},
+                {10, 2, 175000,  false},
+                {10, 2, 200000,  false},
+                {10, 2, 250000,  false},
+                {10, 2, 300000,  false},
+                {10, 2, 350000,  false},
+                {10, 2, 400000,  false},
+                {10, 2, 450000,  false},
+                {10, 2, 500000,  false},
+                {10, 2, 550000,  false},
+                {10, 2, 600000,  false},
+                {10, 2, 650000,  false},
+                {10, 2, 700000,  false},
+                {10, 2, 750000,  false},
+                {10, 2, 800000,  false},
+                {10, 2, 850000,  false},
+                {10, 2, 900000,  false},
+                {10, 2, 950000,  false},
+                {10, 2, 1000000, false},
+
+                {10, 4, 1000,    false},
+                {10, 4, 5000,    false},
+                {10, 4, 10000,   false},
+                {10, 4, 15000,   false},
+                {10, 4, 20000,   false},
+                {10, 4, 25000,   false},
+                {10, 4, 30000,   false},
+                {10, 4, 35000,   false},
+                {10, 4, 40000,   false},
+                {10, 4, 45000,   false},
+                {10, 4, 50000,   false},
+                {10, 4, 100000,  false},
+                {10, 4, 125000,  false},
+                {10, 4, 150000,  false},
+                {10, 4, 175000,  false},
+                {10, 4, 200000,  false},
+                {10, 4, 250000,  false},
+                {10, 4, 300000,  false},
+                {10, 4, 350000,  false},
+                {10, 4, 400000,  false},
+                {10, 4, 450000,  false},
+                {10, 4, 500000,  false},
+                {10, 4, 550000,  false},
+                {10, 4, 600000,  false},
+                {10, 4, 650000,  false},
+                {10, 4, 700000,  false},
+                {10, 4, 750000,  false},
+                {10, 4, 800000,  false},
+                {10, 4, 850000,  false},
+                {10, 4, 900000,  false},
+                {10, 4, 950000,  false},
+                {10, 4, 1000000, false},
+
+                {10, 8, 1000,    false},
+                {10, 8, 5000,    false},
+                {10, 8, 10000,   false},
+                {10, 8, 15000,   false},
+                {10, 8, 20000,   false},
+                {10, 8, 25000,   false},
+                {10, 8, 30000,   false},
+                {10, 8, 35000,   false},
+                {10, 8, 40000,   false},
+                {10, 8, 45000,   false},
+                {10, 8, 50000,   false},
+                {10, 8, 100000,  false},
+                {10, 8, 125000,  false},
+                {10, 8, 150000,  false},
+                {10, 8, 175000,  false},
+                {10, 8, 200000,  false},
+                {10, 8, 250000,  false},
+                {10, 8, 300000,  false},
+                {10, 8, 350000,  false},
+                {10, 8, 400000,  false},
+                {10, 8, 450000,  false},
+                {10, 8, 500000,  false},
+                {10, 8, 550000,  false},
+                {10, 8, 600000,  false},
+                {10, 8, 650000,  false},
+                {10, 8, 700000,  false},
+                {10, 8, 750000,  false},
+                {10, 8, 800000,  false},
+                {10, 8, 850000,  false},
+                {10, 8, 900000,  false},
+                {10, 8, 950000,  false},
+                {10, 8, 1000000, false},
+
+                {10, 16, 1000,    false},
+                {10, 16, 5000,    false},
+                {10, 16, 10000,   false},
+                {10, 16, 15000,   false},
+                {10, 16, 20000,   false},
+                {10, 16, 25000,   false},
+                {10, 16, 30000,   false},
+                {10, 16, 35000,   false},
+                {10, 16, 40000,   false},
+                {10, 16, 45000,   false},
+                {10, 16, 50000,   false},
+                {10, 16, 100000,  false},
+                {10, 16, 125000,  false},
+                {10, 16, 150000,  false},
+                {10, 16, 175000,  false},
+                {10, 16, 200000,  false},
+                {10, 16, 250000,  false},
+                {10, 16, 300000,  false},
+                {10, 16, 350000,  false},
+                {10, 16, 400000,  false},
+                {10, 16, 450000,  false},
+                {10, 16, 500000,  false},
+                {10, 16, 550000,  false},
+                {10, 16, 600000,  false},
+                {10, 16, 650000,  false},
+                {10, 16, 700000,  false},
+                {10, 16, 750000,  false},
+                {10, 16, 800000,  false},
+                {10, 16, 850000,  false},
+                {10, 16, 900000,  false},
+                {10, 16, 950000,  false},
+                {10, 16, 1000000, false},
+
                 {10, -1, 1000,    true},
                 {10, -1, 5000,    true},
                 {10, -1, 10000,   true},
@@ -517,5 +605,95 @@ public class ScanBenchmark extends Benchmark {
         LOG.info("baseTableScanWarmup");
 
         runBenchmark(1, () -> benchmark(splitCount, NUM_EXECS, true, onOlap));
+    }
+
+    //////// splitting logic ///////////////////////////////////////////////////////////////////
+
+    private void splitTable(int numSplits) {
+        if (numSplits > 0) {
+            try (Connection conn = makeConnection()) {
+                getNumSplits(conn);
+                LOG.info(String.format("After splitting to %d region(s)", numSplits));
+                splitRegions(conn, numSplits);
+                int actualSplitCount = getNumSplits(conn);
+                if (numSplits != actualSplitCount) {
+                    LOG.error(String.format("number of actual splits %d is not equal to what was requested (%d)", actualSplitCount, numSplits));
+                }
+            } catch (Throwable t) {
+                LOG.error("Connection broken", t);
+            }
+        }
+    }
+
+    static String toHBaseEscaped(String s) {
+        if(s == null) {
+            return null;
+        }
+        if(s.length() % 2 != 0) {
+            throw new IllegalArgumentException("argument length must be an even number");
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i += 2) {
+            sb.append("\\x").append(s, i, i+2);
+        }
+        return sb.toString();
+    }
+
+    private int getNumSplits(Connection c) throws SQLException {
+        int cnt = 0;
+        String getRegionsSQL = String.format("CALL SYSCS_UTIL.GET_REGIONS('%s', '%s', null,null, null, '|',null,null,null,null)",
+                                             SCHEMA, BASE_TABLE);
+        try (ResultSet rs = c.createStatement().executeQuery(getRegionsSQL)) {
+
+            while (rs.next()) {
+                LOG.info(String.format("Region %s, %s", rs.getString(2), rs.getString(3)));
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    private String rowIdOf(Connection conn, int pkColValue) {
+        // remove once we figure out how to calculate rowid correctly.
+        try(Statement s = conn.createStatement()) {
+            s.execute(String.format("CREATE TABLE %s.TEMP_TABLE(col1 int primary key)", SCHEMA));
+            s.execute(String.format("INSERT INTO %s.TEMP_TABLE VALUES %d", SCHEMA, pkColValue));
+            try(ResultSet rs = s.executeQuery(String.format("SELECT ROWID FROM %s.TEMP_TABLE where col1 = %d", SCHEMA, pkColValue))) {
+                rs.next();
+                return rs.getString(1);
+            }
+        } catch(Throwable t) {
+            LOG.error("Error running statement to get RowId", t);
+            System.exit(-1);
+            return "?";
+        } finally {
+            try {
+                conn.createStatement().execute(String.format("DROP TABLE %s.TEMP_TABLE", SCHEMA));
+            } catch (Exception e) {
+                // ignore.
+            }
+        }
+    }
+
+    private void splitAt(Connection conn, String splits) {
+        String splitAtSql = null;
+        splitAtSql = String.format("call SYSCS_UTIL.syscs_split_table_or_index_at_points('%s', '%s', null,'%s')",
+                                   SCHEMA, BASE_TABLE, splits);
+        try (Statement statement = conn.createStatement()) {
+            statement.execute(splitAtSql);
+        } catch (Throwable t) {
+            LOG.error("Error running statement to split", t);
+            System.exit(-1);
+        }
+    }
+
+    private void splitRegions(Connection conn, int numSplits) {
+        assert numSplits != 0;
+        int splitSize = tableSize / numSplits;
+        String splits = "";
+        for(int i = 1; i < numSplits; i++) {
+            splits += toHBaseEscaped(rowIdOf(conn, splitSize * i)) + ",";
+        }
+        splitAt(conn, splits.substring(0, splits.length() - 1));
     }
 }
