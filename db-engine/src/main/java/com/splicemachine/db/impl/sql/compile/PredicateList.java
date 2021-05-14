b4cf96a1baf12f3e89059a4221d251850ef0a92b
Zhen Li <zli@splicemachine.com>
2021-05-14 16:20:45 +0200

    DB-12064 Fix NPE in code generation for index prefix iteration mode

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/PredicateList.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/PredicateList.java
@@ -3777,7 +3777,10 @@ public class PredicateList extends QueryTreeNodeVector<Predicate> implements Opt
          * by (column #, selectivity) once the store does just in time
          * instantiation.
          */
+        PredicateList cloneMe = null;
         if(numberOfQualifiers>0){
+            cloneMe = (PredicateList)getNodeFactory().getNode(C_NodeTypes.PREDICATE_LIST, getContextManager());
+            this.copyPredicatesToOtherList(cloneMe);
             orderQualifiers();
         }
 
@@ -3972,6 +3975,11 @@ public class PredicateList extends QueryTreeNodeVector<Predicate> implements Opt
                 qualNum++;
             }
 
+        }
+
+        if (cloneMe != null) {
+            this.removeAllPredicates();
+            cloneMe.copyPredicatesToOtherList(this);
         }
 
         //assert qualNum==numberOfQualifiers: qualNum+" Qualifiers found, "+ numberOfQualifiers+" expected.";
