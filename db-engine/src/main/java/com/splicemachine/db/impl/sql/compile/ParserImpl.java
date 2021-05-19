c7051f64a48ece0528fe7d3d6c8612b9496a8431
Martin Rupp <mrupp@splicemachine.com>
2021-05-20 00:08:22 +0200

    DB-12097 fix indentation ParserException, ParserImpl

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/ParserImpl.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/ParserImpl.java
@@ -37,198 +37,183 @@ import com.splicemachine.db.iapi.sql.compile.Visitable;
 
 import com.splicemachine.db.iapi.reference.SQLState;
 import com.splicemachine.db.iapi.error.StandardException;
+
 import java.io.StringReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ParserImpl implements Parser
-{
-	/*
-	** We will use the following constant to pass in to
-	** our CharStream.  It is the size of the internal
-	** buffers that are used to buffer tokens.  It
-	** should be set to what is typically around the
-	** largest token that is likely to be hit.  Note
-	** that if the size is exceeded, the buffer will
-	** automatically be expanded by 2048, so it is ok
-	** to choose something that is smaller than the
-	** max token supported.
-	**
-	** Since, JavaCC generates parser and tokenmanagers classes
-	** tightly connected, to use another parser or tokenmanager
-	** inherit this class, override the following methods
-	** to use specific instances:<ul>
-	** <li>getTokenManager()</li>
-	** <li>getParser()</li>
-	** <li>parseGoalProduction(...)</li>
-	** </ul>
-	**
-	*/
-	static final int LARGE_TOKEN_SIZE = 128;
+public class ParserImpl implements Parser {
+    /*
+     ** We will use the following constant to pass in to
+     ** our CharStream.  It is the size of the internal
+     ** buffers that are used to buffer tokens.  It
+     ** should be set to what is typically around the
+     ** largest token that is likely to be hit.  Note
+     ** that if the size is exceeded, the buffer will
+     ** automatically be expanded by 2048, so it is ok
+     ** to choose something that is smaller than the
+     ** max token supported.
+     **
+     ** Since, JavaCC generates parser and tokenmanagers classes
+     ** tightly connected, to use another parser or tokenmanager
+     ** inherit this class, override the following methods
+     ** to use specific instances:<ul>
+     ** <li>getTokenManager()</li>
+     ** <li>getParser()</li>
+     ** <li>parseGoalProduction(...)</li>
+     ** </ul>
+     **
+     */
+    static final int LARGE_TOKEN_SIZE = 128;
 
-        /* Don't ever access these objects directly, call getParser(), and getTokenManager() */
-        private SQLParser cachedParser; 
-	protected Object cachedTokenManager;
+    /* Don't ever access these objects directly, call getParser(), and getTokenManager() */
+    private SQLParser cachedParser;
+    protected Object cachedTokenManager;
 
-	protected CharStream charStream;
-        protected String SQLtext;
+    protected CharStream charStream;
+    protected String SQLtext;
 
-        protected final CompilerContext cc;
+    protected final CompilerContext cc;
 
-	/**
-	 * Constructor for Parser
-	 */
+    /**
+     * Constructor for Parser
+     */
+    public ParserImpl(CompilerContext cc) {
+        this.cc = cc;
+    }
 
-	public ParserImpl(CompilerContext cc)
-	{
-		this.cc = cc;
-	}
+    public Visitable parseStatement(String statementSQLText)
+            throws StandardException {
+        return parseStatement(statementSQLText, (Object[]) null);
+    }
 
-	public Visitable parseStatement(String statementSQLText)
-		throws StandardException
-	{
-		return parseStatement(statementSQLText, (Object[])null);
-	}
+    /**
+     * Returns a initialized (clean) TokenManager, paired w. the Parser in getParser,
+     * Appropriate for this ParserImpl object.
+     */
+    protected Object getTokenManager() {
+        /* returned a cached tokenmanager if already exists, otherwise create */
+        SQLParserTokenManager tm = (SQLParserTokenManager) cachedTokenManager;
+        if (tm == null) {
+            tm = new SQLParserTokenManager(charStream);
+            cachedTokenManager = tm;
+        } else {
+            tm.ReInit(charStream);
+        }
+        return tm;
+    }
 
-        /**
-	 * Returns a initialized (clean) TokenManager, paired w. the Parser in getParser,
-	 * Appropriate for this ParserImpl object.
-	 */
-        protected Object getTokenManager()
-        {
-	    /* returned a cached tokenmanager if already exists, otherwise create */
-	    SQLParserTokenManager tm = (SQLParserTokenManager) cachedTokenManager;
-	    if (tm == null) {
-		tm = new SQLParserTokenManager(charStream);
-		cachedTokenManager = tm;
-	    } else {
-		tm.ReInit(charStream);
-	    }
-	    return tm;
-	}
+    /**
+     * new parser, appropriate for the ParserImpl object.
+     */
+    private SQLParser getParser() {
+        SQLParserTokenManager tm = (SQLParserTokenManager) getTokenManager();
+        /* returned a cached Parser if already exists, otherwise create */
+        SQLParser p = (SQLParser) cachedParser;
+        if (p == null) {
+            p = new SQLParser(tm);
+            p.setCompilerContext(cc);
+            cachedParser = p;
+        } else {
+            p.ReInit(tm);
+        }
+        return p;
+    }
 
-     /**
-	 * new parser, appropriate for the ParserImpl object.
-	 */
-     private SQLParser getParser()
-        {
-	    SQLParserTokenManager tm = (SQLParserTokenManager) getTokenManager();
-	    /* returned a cached Parser if already exists, otherwise create */
-	    SQLParser p = (SQLParser) cachedParser;
-	    if (p == null) {
-		p = new SQLParser(tm);
-		p.setCompilerContext(cc);
-		cachedParser = p;
-	    } else {
-		p.ReInit(tm);
-	    }
-	    return p;
-	}
+    /**
+     * @param statementSQLText original statement to underly problem
+     * @return e.g. CREATE TABEL (i INTEGER);
+     * ^^^^^--------------
+     */
+    String highlightProblematicSql(String statementSQLText,
+                                   int problematicLine, int beginColumn, int endColumn) {
+        StringBuilder sb = new StringBuilder();
+        String[] str = statementSQLText.split("\n"); // or \\\n?
+        int from = Math.max(problematicLine - 3, 0);
+        int end = Math.min(problematicLine + 3, str.length);
+        for (int i = from; i < end; i++) {
+            sb.append(i + ":\t");
+            sb.append(str[i] + "\n");
+            if (i + 1 == problematicLine) {
+                int j = 0;
+                sb.append("   \t");
+                for (; j < beginColumn - 1; j++)
+                    sb.append(" ");
+                for (; j < endColumn; j++)
+                    sb.append("^");
+                sb.append("---------\n");
+            }
+        }
+        return sb.toString();
+    }
 
-	/**
-	 * @param statementSQLText original statement to underly problem
-	 * @return e.g. CREATE TABEL (i INTEGER);
-	 *                     ^^^^^--------------
-	 */
-	String highlightProblematicSql(String statementSQLText,
-								   int problematicLine, int beginColumn, int endColumn) {
-		StringBuilder sb = new StringBuilder();
-		String[] str = statementSQLText.split("\n"); // or \\\n?
-		int from = Math.max(problematicLine-3, 0);
-		int end  = Math.min(problematicLine+3, str.length);
-		for(int i=from; i<end; i++) {
-			sb.append(i + ":\t");
-			sb.append(str[i] + "\n");
-			if(i+1 == problematicLine) {
-				int j=0;
-				sb.append("   \t");
-				for(; j< beginColumn-1; j++)
-					sb.append(" ");
-				for(; j<endColumn; j++)
-					sb.append("^");
-				sb.append("---------\n");
-			}
-		}
-		return sb.toString();
-	}
+    /**
+     * Parse a statement and return a query tree.  Implements the Parser
+     * interface
+     *
+     * @param statementSQLText Statement to parse
+     * @param paramDefaults    parameter defaults. Passed around as an array
+     *                         of objects, but is really an array of StorableDataValues
+     * @throws StandardException Thrown on error
+     * @return A QueryTree representing the parsed statement
+     */
+    public Visitable parseStatement(String statementSQLText, Object[] paramDefaults)
+            throws StandardException {
 
-	/**
-	 * Parse a statement and return a query tree.  Implements the Parser
-	 * interface
-	 *
-	 * @param statementSQLText	Statement to parse
-	 * @param paramDefaults	parameter defaults. Passed around as an array
-	 *                      of objects, but is really an array of StorableDataValues
-	 * @return	A QueryTree representing the parsed statement
-	 *
-	 * @exception StandardException	Thrown on error
-	 */
-	public Visitable parseStatement(String statementSQLText, Object[] paramDefaults)
-		throws StandardException
-	{
+        java.io.Reader sqlText = new java.io.StringReader(statementSQLText);
 
-		java.io.Reader sqlText = new java.io.StringReader(statementSQLText);
+        /* Get a char stream if we don't have one already */
+        if (charStream == null) {
+            charStream = new UCode_CharStream(sqlText, 1, 1, LARGE_TOKEN_SIZE);
+        } else {
+            charStream.ReInit(sqlText, 1, 1, LARGE_TOKEN_SIZE);
+        }
 
-		/* Get a char stream if we don't have one already */
-		if (charStream == null)
-		{
-			charStream = new UCode_CharStream(sqlText, 1, 1, LARGE_TOKEN_SIZE);
-		}
-		else
-		{
-			charStream.ReInit(sqlText, 1, 1, LARGE_TOKEN_SIZE);
-		}
+        /* remember the string that we're parsing */
+        SQLtext = statementSQLText;
 
-		/* remember the string that we're parsing */
-		SQLtext = statementSQLText;
+        /* Parse the statement, and return the QueryTree */
+        try {
+            return getParser().Statement(statementSQLText, paramDefaults);
+        } catch (ParseException e) {
+            String highlight = highlightProblematicSql(statementSQLText,
+                    e.currentToken.next.beginLine, e.currentToken.next.beginColumn, e.currentToken.next.endColumn);
+            throw StandardException.newException(SQLState.LANG_SYNTAX_ERROR, e.getMessage() + "\n" + highlight);
+        } catch (TokenMgrError e) {
+            // Derby - 2103.
+            // When the exception occurs cachedParser may live with
+            // some flags set inappropriately that may cause Exception
+            // in the subsequent compilation. This seems to be a javacc bug.
+            // Issue Javacc-152 has been raised.
+            // As a workaround, the cachedParser object is cleared to ensure
+            // that the exception does not have any side effect.
+            // TODO : Remove the following line if javacc-152 is fixed.
+            cachedParser = null;
+            // e.getMessage() ==
+            // Lexical error at line L, column C.
+            String highlight = "";
 
-		/* Parse the statement, and return the QueryTree */
-		try
-		{
-		    return getParser().Statement(statementSQLText, paramDefaults);
-		}
-		catch (ParseException e)
-		{
-			String highlight = highlightProblematicSql(statementSQLText,
-					e.currentToken.next.beginLine, e.currentToken.next.beginColumn, e.currentToken.next.endColumn);
-			throw StandardException.newException(SQLState.LANG_SYNTAX_ERROR, e.getMessage() + "\n" + highlight);
-		}
-		catch (TokenMgrError e)
-		{
-			// Derby - 2103.
-			// When the exception occurs cachedParser may live with
-			// some flags set inappropriately that may cause Exception
-			// in the subsequent compilation. This seems to be a javacc bug.
-			// Issue Javacc-152 has been raised.
-			// As a workaround, the cachedParser object is cleared to ensure
-			// that the exception does not have any side effect.
-			// TODO : Remove the following line if javacc-152 is fixed.
-			cachedParser = null;
-			// e.getMessage() ==
-			// Lexical error at line L, column C.
-			String highlight = "";
+            Pattern outputRowsP = Pattern.compile("Lexical error at line (\\d+), column (\\d+)");
+            Matcher m = outputRowsP.matcher(e.getMessage());
+            if (m.find()) {
+                int line = Integer.parseInt(m.group(1));
+                int col = Integer.parseInt(m.group(2));
+                highlight = "\n\n" + highlightProblematicSql(statementSQLText, line, col - 1, col);
+            }
 
-			Pattern outputRowsP = Pattern.compile("Lexical error at line (\\d+), column (\\d+)");
-			Matcher m = outputRowsP.matcher(e.getMessage());
-			if(m.find()) {
-				int line = Integer.parseInt(m.group(1));
-				int col  = Integer.parseInt(m.group(2));
-				highlight = "\n\n" + highlightProblematicSql(statementSQLText, line, col-1, col);
-			}
-
-			throw StandardException.newException(SQLState.LANG_LEXICAL_ERROR, e.getMessage() + highlight);
-		}
-	}
+            throw StandardException.newException(SQLState.LANG_LEXICAL_ERROR, e.getMessage() + highlight);
+        }
+    }
 
     /**
      * Parse a full SQL statement or a fragment that represents a
      * {@code <search condition>}.
      *
-     * @param sql the SQL statement or fragment to parse
+     * @param sql           the SQL statement or fragment to parse
      * @param paramDefaults parameter defaults to pass on to the parser
-     *   in the case where {@code sql} is a full SQL statement
-     * @param isStatement {@code true} if {@code sql} is a full SQL statement,
-     *   {@code false} if it is a fragment
+     *                      in the case where {@code sql} is a full SQL statement
+     * @param isStatement   {@code true} if {@code sql} is a full SQL statement,
+     *                      {@code false} if it is a fragment
      * @return parse tree for the SQL
      * @throws StandardException if an error happens during parsing
      */
@@ -238,57 +223,50 @@ public class ParserImpl implements Parser
     {
         StringReader sqlText = new StringReader(sql);
 
-	/* Get a char stream if we don't have one already */
-	if (charStream == null)
-	{
-		charStream = new UCode_CharStream(sqlText, 1, 1, LARGE_TOKEN_SIZE);
-	}
-	else
-	{
-		charStream.ReInit(sqlText, 1, 1, LARGE_TOKEN_SIZE);
-	}
+        /* Get a char stream if we don't have one already */
+        if (charStream == null) {
+            charStream = new UCode_CharStream(sqlText, 1, 1, LARGE_TOKEN_SIZE);
+        } else {
+            charStream.ReInit(sqlText, 1, 1, LARGE_TOKEN_SIZE);
+        }
 
-	/* remember the string that we're parsing */
+        /* remember the string that we're parsing */
         SQLtext = sql;
 
-	/* Parse the statement, and return the QueryTree */
-	try
-	{
+        /* Parse the statement, and return the QueryTree */
+        try {
             SQLParser p = getParser();
             return isStatement
                     ? p.Statement(sql, paramDefaults)
                     : p.SearchCondition(sql);
-		}
-		catch (ParseException e)
-		{
-		    throw StandardException.newException(SQLState.LANG_SYNTAX_ERROR, e.getMessage());
-		}
-		catch (TokenMgrError e)
-		{
-			// Derby - 2103.
-			// When the exception occurs cachedParser may live with
-			// some flags set inappropriately that may cause Exception
-			// in the subsequent compilation. This seems to be a javacc bug.
-			// Issue Javacc-152 has been raised.
-			// As a workaround, the cachedParser object is cleared to ensure
-			// that the exception does not have any side effect.
-			// TODO : Remove the following line if javacc-152 is fixed.
-			cachedParser = null;
-		    throw StandardException.newException(SQLState.LANG_LEXICAL_ERROR, e.getMessage());
-		}
-	}
+        } catch (ParseException e) {
+            throw StandardException.newException(SQLState.LANG_SYNTAX_ERROR, e.getMessage());
+        } catch (TokenMgrError e) {
+            // Derby - 2103.
+            // When the exception occurs cachedParser may live with
+            // some flags set inappropriately that may cause Exception
+            // in the subsequent compilation. This seems to be a javacc bug.
+            // Issue Javacc-152 has been raised.
+            // As a workaround, the cachedParser object is cleared to ensure
+            // that the exception does not have any side effect.
+            // TODO : Remove the following line if javacc-152 is fixed.
+            cachedParser = null;
+            throw StandardException.newException(SQLState.LANG_LEXICAL_ERROR, e.getMessage());
+        }
+    }
 
-	@Override
-	public Visitable parseSearchCondition(String sqlFragment)
-	    throws StandardException {
-		return parseStatementOrSearchCondition(sqlFragment, null, false);
-	}
-	/**
-	 * Returns the current SQL text string that is being parsed.
-	 *
-	 * @return	Current SQL text string.
-	 *
-	 */
-	public	String		getSQLtext()
-	{	return	SQLtext; }
+    @Override
+    public Visitable parseSearchCondition(String sqlFragment)
+            throws StandardException {
+        return parseStatementOrSearchCondition(sqlFragment, null, false);
+    }
+
+    /**
+     * Returns the current SQL text string that is being parsed.
+     *
+     * @return Current SQL text string.
+     */
+    public String getSQLtext() {
+        return SQLtext;
+    }
 }
