7c0ef7c57bd4d7689c45234fc7f8ef9e21f6e4ca
Zhen Li <zli@splicemachine.com>
2021-05-27 18:51:04 +0200

    DB-10980 Wait for real exception when an OLAP job fails (3.0) (#5495)
    * DB-10980 Wait for real exception when an OLAP job fails
    
    * DB-10980 Fix complilation error
    
    * DB-10980 Fix SpotBugs

--- hbase_sql/src/main/java/com/splicemachine/stream/RemoteQueryClientImpl.java
+++ hbase_sql/src/main/java/com/splicemachine/stream/RemoteQueryClientImpl.java
@@ -14,6 +14,7 @@
 
 package com.splicemachine.stream;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import splice.com.google.common.net.HostAndPort;
 import com.splicemachine.EngineDriver;
 import com.splicemachine.access.HConfiguration;
@@ -41,7 +42,6 @@ import java.util.*;
 import java.util.concurrent.*;
 
 
-
 /**
  * Created by dgomezferro on 5/20/16.
  */
@@ -57,6 +57,7 @@ public class RemoteQueryClientImpl implements RemoteQueryClient {
     private StreamListener streamListener;
     private long offset = 0;
     private long limit = -1;
+    private CountDownLatch olapFutureCallbackInvoked = new CountDownLatch(1);
 
     public RemoteQueryClientImpl(SpliceBaseOperation root, String hostname) {
         this.root = root;
@@ -147,6 +148,7 @@ public class RemoteQueryClientImpl implements RemoteQueryClient {
                         LOG.error("Unexpected exception, shouldn't happen", e);
                         streamListener.failed(e);
                     }
+                    olapFutureCallbackInvoked.countDown();
                 }
             }, MoreExecutors.sameThreadExecutor());
         } catch (IOException e) {
@@ -244,5 +246,11 @@ public class RemoteQueryClientImpl implements RemoteQueryClient {
         streamListener.stopAllStreams();
         if (olapFuture != null)
             olapFuture.cancel(false);
+    }
+
+    @SuppressFBWarnings(value = "RV_RETURN_VALUE_IGNORED", justification = "intended")
+    public Exception getException() throws InterruptedException {
+        olapFutureCallbackInvoked.await(5, TimeUnit.SECONDS);
+        return (Exception) streamListener.getFailure();
     }
 }
