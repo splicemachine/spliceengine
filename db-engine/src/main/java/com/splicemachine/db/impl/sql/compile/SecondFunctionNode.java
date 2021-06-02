86c76140556706caaa4b8f76ad1e61e2fba022c1
Youssef Hatem <yhatem@splicemachine.com>
2021-06-02 14:26:06 +0200

    DB-12089 cleanup.

--- db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SecondFunctionNode.java
+++ db-engine/src/main/java/com/splicemachine/db/impl/sql/compile/SecondFunctionNode.java
@@ -16,19 +16,17 @@ package com.splicemachine.db.impl.sql.compile;
 import com.splicemachine.db.iapi.error.StandardException;
 import com.splicemachine.db.iapi.reference.SQLState;
 import com.splicemachine.db.iapi.services.classfile.VMOpcode;
-import com.splicemachine.db.iapi.services.compiler.LocalField;
 import com.splicemachine.db.iapi.services.compiler.MethodBuilder;
 import com.splicemachine.db.iapi.services.context.ContextManager;
 import com.splicemachine.db.iapi.sql.compile.C_NodeTypes;
-import com.splicemachine.db.iapi.types.*;
+import com.splicemachine.db.iapi.types.DataTypeDescriptor;
+import com.splicemachine.db.iapi.types.DataTypeUtilities;
+import com.splicemachine.db.iapi.types.TypeId;
 import com.splicemachine.db.iapi.util.JBitSet;
 
-import java.lang.reflect.Modifier;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
-
-import static com.splicemachine.db.iapi.types.DateTimeDataValue.SECOND_FIELD;
 
 /**
  * This node represents the SECOND function, which returns the "second" part of a time / timestamp
