375e529fe77917f5f77d758b6bc5da459bec3909
Martin Rupp <mrupp@splicemachine.com>
2021-06-17 12:28:27 +0200

    preparation for running derby's MergeStatementTest

--- splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MergeNodeIT.java
+++ splice_machine/src/test/java/com/splicemachine/derby/impl/sql/execute/operations/MergeNodeIT.java
@@ -1,22 +1,28 @@
+/*
+ * Copyright (c) 2012 - 2021 Splice Machine, Inc.
+ *
+ * This file is part of Splice Machine.
+ * Splice Machine is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU Affero General Public License as published by the Free Software Foundation, either
+ * version 3, or (at your option) any later version.
+ * Splice Machine is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with Splice Machine.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.splicemachine.derby.impl.sql.execute.operations;
 
 import com.splicemachine.db.shared.common.reference.SQLState;
 import com.splicemachine.derby.test.framework.SpliceSchemaWatcher;
-import com.splicemachine.derby.test.framework.SpliceTableWatcher;
 import com.splicemachine.derby.test.framework.SpliceUnitTest;
 import com.splicemachine.derby.test.framework.SpliceWatcher;
-import com.splicemachine.test_tools.TableCreator;
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.ErrorMsg;
 import org.junit.*;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import static com.splicemachine.test_tools.Rows.row;
-import static com.splicemachine.test_tools.Rows.rows;
 import static org.junit.Assert.*;
 
 public class MergeNodeIT
